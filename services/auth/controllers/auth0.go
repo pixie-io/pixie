d8495b8a72d50b8700bf89d36ae5397b9065c1f8
Zain Asgar <zasgar@pixielabs.ai>
2018-10-09 14:51:49 -0700

    PL-26 Added Auth0 connector implementation
    Summary: Implementation of the connector. Endpoint is coming soon.
    
    Test Plan: Added unit tests.
    
    Reviewers: michelle, kgandhi
    
    Reviewed By: michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D63
    
    GitOrigin-RevId: dac771de2974ffa5797fe3f3518de68002e8c1fd

---
+++ services/auth/controllers/auth0.go
@@ -0,0 +1,233 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	flag.String("auth0_host", "https://pixie-labs.auth0.com", "The auth0 hostname")
+	flag.String("auth0_client_id", "", "Auth0 client ID")
+	flag.String("auth0_client_secret", "", "Auth0 client secret")
+}
+
+// UserMetadata is a part of the Auth0 response.
+type UserMetadata struct {
+	PLUserID string `json:"pl_user_id,omitempty"`
+}
+
+// UserInfo tracks the returned auth0 info.
+type UserInfo struct {
+	Email       string        `json:",omitempty"`
+	Name        string        `json:",omitempty"`
+	Picture     string        `json:",omitempty"`
+	Sub         string        `json:"sub,omitempty"`
+	AppMetadata *UserMetadata `json:"app_metadata,omitempty"`
+}
+
+// Auth0Connector defines an interface we use to access information from Auth0.
+type Auth0Connector interface {
+	Init() error
+	GetUserIDFromToken(token string) (string, error)
+	GetUserInfo(userID string) (*UserInfo, error)
+	SetPLUserID(userID, plUserID string) error
+}
+
+// Auth0Config is the config data required for Auth0.
+type Auth0Config struct {
+	Auth0Host               string
+	Auth0MgmtAPI            string
+	Auth0OAuthTokenEndpoint string
+	Auth0UserInfoEndpoint   string
+	Auth0ClientID           string
+	Auth0ClientSecret       string
+}
+
+// NewAuth0Config generates and Auth0Config based on env vars and flags.
+func NewAuth0Config() Auth0Config {
+	auth0Host := viper.GetString("auth0_host")
+	cfg := Auth0Config{
+		Auth0Host:               auth0Host,
+		Auth0MgmtAPI:            auth0Host + "/api/v2",
+		Auth0OAuthTokenEndpoint: auth0Host + "/oauth/token",
+		Auth0UserInfoEndpoint:   auth0Host + "/userinfo",
+		Auth0ClientID:           viper.GetString("auth0_client_id"),
+		Auth0ClientSecret:       viper.GetString("auth0_client_secret"),
+	}
+	return cfg
+}
+
+type auth0ConnectorImpl struct {
+	cfg             Auth0Config
+	managementToken string
+}
+
+// NewAuth0Connector provides an implementation of an Auth0Connector.
+func NewAuth0Connector(cfg Auth0Config) Auth0Connector {
+	return &auth0ConnectorImpl{cfg: cfg}
+}
+
+func (a *auth0ConnectorImpl) Init() error {
+	if a.cfg.Auth0ClientID == "" {
+		return errors.New("auth0 Client ID missing")
+	}
+
+	if a.cfg.Auth0ClientSecret == "" {
+		return errors.New("auth0 Client secret missing")
+	}
+
+	return nil
+}
+
+func (a *auth0ConnectorImpl) GetUserIDFromToken(token string) (string, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", a.cfg.Auth0UserInfoEndpoint, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization",
+		fmt.Sprintf("Bearer %s", token))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("bad response from auth0")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return "", err
+	}
+
+	var userInfo struct {
+		Sub string `json:"sub,omitempty"`
+	}
+	if err = json.Unmarshal(body, &userInfo); err != nil {
+		return "", err
+	}
+	return userInfo.Sub, nil
+}
+
+func (a *auth0ConnectorImpl) getManagementToken() (string, error) {
+	payload := map[string]interface{}{
+		"grant_type":    "client_credentials",
+		"client_id":     a.cfg.Auth0ClientID,
+		"client_secret": a.cfg.Auth0ClientSecret,
+		"audience":      a.cfg.Auth0MgmtAPI + "/",
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	log.Debug("Requesting AUTH0 management token.")
+	resp, err := http.Post(a.cfg.Auth0OAuthTokenEndpoint, "application/json", bytes.NewReader(jsonPayload))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	type TokenInfo struct {
+		AccessToken string `json:"access_token"`
+	}
+	var tokenInfo TokenInfo
+	if err = json.Unmarshal(body, &tokenInfo); err != nil {
+		return "", err
+	}
+	return tokenInfo.AccessToken, nil
+}
+
+func (a *auth0ConnectorImpl) GetUserInfo(userID string) (*UserInfo, error) {
+	getPath := fmt.Sprintf("%s/users/%s", a.cfg.Auth0MgmtAPI, userID)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", getPath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	managementToken, err := a.getManagementToken()
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", managementToken))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad response when getting user app metadata: %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	userInfo := &UserInfo{}
+	if err = json.Unmarshal(body, userInfo); err != nil {
+		return nil, err
+	}
+	return userInfo, nil
+}
+
+func (a *auth0ConnectorImpl) SetPLUserID(userID, plUserID string) error {
+	userInfo := &UserInfo{
+		AppMetadata: &UserMetadata{
+			PLUserID: plUserID,
+		},
+	}
+
+	jsonStr, err := json.Marshal(userInfo)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	patchPath := fmt.Sprintf("%s/users/%s", a.cfg.Auth0MgmtAPI, userID)
+	log.Printf(patchPath)
+	req, err := http.NewRequest("PATCH", patchPath, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		return err
+	}
+
+	managementToken, err := a.getManagementToken()
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", managementToken))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad response from auth0: %d", resp.StatusCode)
+	}
+	return nil
+}
