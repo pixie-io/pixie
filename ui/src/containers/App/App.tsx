77c283e120b1a433fbfe3505537ccdcde2970969
Zain Asgar <zasgar@pixielabs.ai>
2018-10-18 23:17:27 -0700

    Added Apollo client
    Summary: TSIA
    
    Test Plan: load UI
    
    Reviewers: #engineering, michelle
    
    Reviewed By: #engineering, michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D81
    
    GitOrigin-RevId: 9a03693e83de5c30ff110a9bd8f48b0e8064f68f

--- ui/src/containers/App/App.tsx
+++ ui/src/containers/App/App.tsx
@@ -2,20 +2,35 @@ import {Auth} from 'containers/Auth';
 import {Home} from 'containers/Home';
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as fetch from 'unfetch';
+
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import {ApolloClient} from 'apollo-client';
+import {createHttpLink} from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
 
 export interface AppProps {
   name: string;
 }
 
+export const link = createHttpLink({
+  uri: '/gql',
+  fetch,
+});
+const gqlCache = new InMemoryCache();
+const gqlClient = new ApolloClient({cache: gqlCache, link});
+
 export class App extends React.Component<AppProps, any> {
   render() {
     return (
       <Router>
-        <div>
-          Pixie Labs UI!
-          <Route exact path='/' component={Home} />
-          <Route path='/login' component={Auth} />
-        </div>
+        <ApolloProvider client={gqlClient}>
+          <div>
+            Pixie Labs UI!
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Auth} />
+          </div>
+        </ApolloProvider>
       </Router>
     );
   }
