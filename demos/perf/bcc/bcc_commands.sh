8ed2fc2db2a7e9cf4d447f6b0935c81bd65886f2
Kaushal Gandhi <kgandhi@pixielabs.ai>
2018-11-20 11:01:49 -0800

    PL-80: Collect application performance data on GKE with bcc
    Summary:
    Using a compiled docker image of bcc (Ubuntu xenial) and adding a
    shell script to collect application performace data. The command for collecting
    data can be modified in the daemonset yaml using the options for the shell
    script.
    
    TODO: Subsequent diffs will tie data collection to the load generation for the sample
    applications that we have.
    
    Test Plan: Deployed on GKE cluster and collected data for sample application
    
    Reviewers: #engineering, michelle
    
    Reviewed By: #engineering, michelle
    
    Subscribers: michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D126
    
    GitOrigin-RevId: d2aa62b86e5bd59c1bb6466cbea279f4e70bfe5e

---
+++ demos/perf/bcc/bcc_commands.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+interval_secs=2
+capture_secs=10
+outdir="bcc_data"
+usage()
+{
+  echo "Usage: $0 [-i interval in secs to store data] [-t time for capture in seconds] \
+  [-o output directory under /usr/share/bcc/tools]"
+  exit 2
+}
+
+# Input options
+# interval_secs is the interval at which data should be printed to the output file
+# capture_secs is the duration for which data should be collected
+# outdir is the directory under /usr/share/tools/bcc where output data will be stored
+
+while getopts 'hi:t:o:' c
+do
+    case $c in
+        h) usage ;;
+        i) interval_secs=$OPTARG
+            echo "Script will collect data at intervals of $interval_secs secs."
+           ;;
+        t) capture_secs=$OPTARG
+            echo "Script will collect data for $capture_secs secs. before exiting"
+           ;;
+        o) outdir=$OPTARG
+            echo "Results will be stored in /usr/share/bcc/tools/$outdir"
+           ;;
+        :) usage ;;
+    esac
+done
+
+mount -t debugfs none /sys/kernel/debug
+mkdir -p $outdir
+
+timeout -s SIGINT $capture_secs ./cachestat -T $interval_secs > ./data/cachestat.txt &
+timeout -s SIGINT $capture_secs ./execsnoop > ./$outdir/execsnoop.txt &
+./opensnoop -d $capture_secs > ./$outdir/opensnoop.txt &
+timeout -s SIGINT $capture_secs ./ext4slower > ./$outdir/ext4slower.txt &
+timeout -s SIGINT $capture_secs ./biolatency > ./$outdir/biolatency.txt &
+timeout -s SIGINT $capture_secs ./biosnoop > ./$outdir/biosnoop.txt &
+timeout -s SIGINT $capture_secs ./tcpconnect > ./$outdir/tcpconnect.txt &
+timeout -s SIGINT $capture_secs ./tcpaccept > ./$outdir/tcpaccept.txt &
+timeout -s SIGINT $capture_secs ./tcpretrans > ./$outdir/tcpretrans.txt &
+timeout -s SIGINT $capture_secs ./runqlat > ./$outdir/runqlat.txt &
+timeout -s SIGINT $capture_secs ./profile > ./$outdir/profile.txt &
+timeout -s SIGINT $capture_secs ./funccount 'vfs_*' > ./$outdir/funccount_vfs.txt &
+timeout -s SIGINT $capture_secs ./funccount 'c:malloc_*' > ./$outdir/funccount_malloc.txt &
