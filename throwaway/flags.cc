aaae86ad3a1b76b36bfb0569f112b2dfe4428c2a
Zain Asgar <zasgar@pixielabs.ai>
2018-12-04 01:44:14 -0800

    PL-137 - Added TBB
    Summary: TSIA
    
    Test Plan: Added to demo file
    
    Reviewers: #engineering, michelle
    
    Reviewed By: #engineering, michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D150
    
    GitOrigin-RevId: 8599d79b8b4885541a38296a67c26741779b6219

--- throwaway/flags.cc
+++ throwaway/flags.cc
@@ -1,5 +1,6 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
+#include <tbb/task_group.h>
 
 #include <iostream>
 #include "absl/debugging/failure_signal_handler.h"
@@ -7,6 +8,19 @@
 #include "absl/strings/str_format.h"
 
 DEFINE_string(message, "", "The message to print");
+
+int Fib(int n) {
+  if (n < 2) {
+    return n;
+  } else {
+    int x, y;
+    tbb::task_group g;
+    g.run([&] { x = Fib(n - 1); });  // spawn a task
+    g.run([&] { y = Fib(n - 2); });  // spawn another task
+    g.wait();                        // wait for both tasks to complete
+    return x + y;
+  }
+}
 
 int main(int argc, char **argv) {
   absl::InitializeSymbolizer(argv[0]);
@@ -16,5 +30,6 @@ int main(int argc, char **argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   LOG(INFO) << absl::StrFormat("Message: %s", FLAGS_message);
+  std::cout << Fib(10) << std::endl;
   return 0;
 }
