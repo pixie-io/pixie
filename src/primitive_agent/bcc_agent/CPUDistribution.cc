9f06be7dea1011fd682b3910f93993b2a7f10952
Zain Asgar <zasgar@pixielabs.ai>
2018-12-09 21:16:14 -0800

    Fix bug with bcc program size
    Summary: If there is a null in the file then we will pick out the wrong string.
    
    Test Plan: Existing
    
    Reviewers: #engineering, philkuz, michelle
    
    Reviewed By: #engineering, philkuz, michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D158
    
    GitOrigin-RevId: 94a81c7b3291f6897da4b587ffad02be569428fa

--- src/primitive_agent/bcc_agent/CPUDistribution.cc
+++ src/primitive_agent/bcc_agent/CPUDistribution.cc
@@ -18,11 +18,12 @@
 #include <string>
 
 extern char _binary_src_primitive_agent_bcc_agent_CPUDistribution_bpf_c_start;
-
+extern int _binary_src_primitive_agent_bcc_agent_CPUDistribution_bpf_c_size;
 int main(int argc, char** argv) {
   ebpf::BPF bpf;
   char* bpf_prog_ptr = &_binary_src_primitive_agent_bcc_agent_CPUDistribution_bpf_c_start;
-  std::string bpf_program = std::string(bpf_prog_ptr);
+  int bpf_prog_len = _binary_src_primitive_agent_bcc_agent_CPUDistribution_bpf_c_size;
+  std::string bpf_program = std::string(bpf_prog_ptr, bpf_prog_len);
 
   auto init_res = bpf.init(bpf_program);
   if (init_res.code() != 0) {
