9453446e3d8461fe01700cfd60b98515c9aaedea
Zain Asgar <zasgar@pixielabs.ai>
2019-01-07 20:34:22 -0800

    VSCode setup with debugger working with Macos and Linux
    Summary:
    This adds support for vscode. The C++ extension and LLDB extension need to be installed.
    
    run
    `python scripts/generate_vscode_tasks.py` to do the setup. After this you can just run the task from within vscode.
    
    To use the lldb debugger you need a patched version:
    `https://github.com/pixie-labs/vscode-lldb`
    
    Otherwise, you can just use the builtin cpp debugger but it's not as good.
    
    You need to add `build --spawn_strategy=standalone`  to `.bazelrc` to get this to work. It's non-ideal but we need it until: https://github.com/bazelbuild/bazel/issues/6327 is fixed.
    
    Test Plan: Tested on vscode 1.30.1/
    
    Reviewers: #engineering, kgandhi
    
    Reviewed By: #engineering, kgandhi
    
    Differential Revision: https://phab.corp.pixielabs.ai/D171
    
    GitOrigin-RevId: a8fee13b649cfd8d9a3df7d18572b02140e3dbac

---
+++ scripts/generate_vscode_tasks.py
@@ -0,0 +1,169 @@
+# This file manages the generation of tasks.json (builds) and launch.json (debug commands).
+# Simply run: python scripts/generate_vscode_tasks.py from the bazel workspace root.
+import argparse
+import json
+import os
+import subprocess
+import sys
+
+
+def getOutputBase():
+    return subprocess.check_output(
+        ['bazel', 'info', 'output_base']).rstrip()
+
+
+# Returns source file map. Some of these are only relevant for Mac or Linux, respectively,
+# but they don't cause any negative side-effects so we leave them all in here.
+def getSourceFileMap(output_base):
+    return {
+        '/proc/self/cwd/': '${workspaceFolder}/',
+        '{0}/execroot/pl/src/'.format(output_base): '${workspaceFolder}/src/',
+        '{0}/execroot/pl/src/'.format(output_base.replace('/private/', '/')):
+            '${workspaceFolder}/src/',
+    }
+
+
+# Generates a path based on the Bazel test target.
+def getPathFromTestTarget(target):
+    return target.replace('//', '').replace(':', '/')
+
+
+# Generate task segment based on compilation and exec modes.
+def generateTaskSegment(target, exec_mode, comp_mode):
+    if exec_mode not in ['build', 'test']:
+        raise ValueError('exec_mode must be either build or test')
+    if comp_mode not in ['dbg', 'opt']:
+        raise ValueError('comp_mode must be either dbg or opt')
+
+    return {
+        'label': '(bazel:{0}:{1}) {2}'.format(comp_mode, exec_mode, target),
+        'type': 'shell',
+        'command': 'bazel',
+        'args': [
+            '{0}'.format(exec_mode),
+            '--compilation_mode={0}'.format(comp_mode),
+            target,
+        ],
+        'group': '{0}'.format(exec_mode),
+    }
+
+
+def generateTaskSegments(target):
+    tasks = []
+    for exec_mode in ['build', 'test']:
+        for comp_mode in ['dbg', 'opt']:
+            tasks += [generateTaskSegment(target, exec_mode, comp_mode)]
+
+    return tasks
+
+
+def generateLaunchSegments(lldb_mode, target, output_base):
+    if lldb_mode:
+        return [{
+            "args": [],
+            "cwd": "${workspaceFolder}",
+            'name': '(lldb) Launch {0}'.format(target),
+            'preLaunchTask': '(bazel:dbg:build) {0}'.format(target),
+            'program': '{0}/bazel-bin/{1}'.format('${workspaceFolder}',
+                                                  getPathFromTestTarget(target)),
+            "request": "launch",
+            "sourceMap": getSourceFileMap(output_base),
+            "type": "lldb"
+        }]
+    else:
+        return [{
+            'name': '(lldb) Launch {0}'.format(target),
+            'type': 'cppdbg',
+            'request': 'launch',
+            'program': '{0}/bazel-bin/{1}'.format('${workspaceFolder}',
+                                                  getPathFromTestTarget(target)),
+            'args': [],
+            'stopAtEntry': False,
+            'cwd': '${workspaceFolder}',
+            'environment': [],
+            'externalConsole': False,
+            'MIMode': 'lldb',
+            'preLaunchTask': '(bazel:dbg:build) {0}'.format(target),
+            'sourceFileMap': getSourceFileMap(output_base),
+            # This is required to get the debugger to work properly on macos.
+            'miDebuggerPath': 'lldb-mi',
+        }]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate files for vscode.')
+    parser.add_argument('--lldb', action='store_true',
+                        help='Generate for LLDB (defaults to vscode mode)', default=False)
+    parsed = parser.parse_args(sys.argv[1:])
+    if parsed.lldb:
+        print('In LLDB mode')
+
+    targets = subprocess.check_output(
+        ['bazel', 'query', 'kind(\'cc_test rule\', //...)'])
+    output_base = getOutputBase()
+    task_list = []
+    launch_list = []
+    for target in targets.split('\n'):
+        if target != '':
+            task_list += generateTaskSegments(target)
+            launch_list += generateLaunchSegments(
+                parsed.lldb, target, output_base)
+
+    # Task to regnerate the file.
+    task_list += [{
+        'label': 'generate tasks/launch json files (lldb)',
+        'type': 'shell',
+        'command': 'python',
+        'args': [
+            '${workspaceFolder}/scripts/generate_vscode_tasks.py',
+            '--lldb'
+        ],
+        'group': 'build'
+    }]
+
+    task_list += [{
+        'label': 'generate tasks/launch json files',
+        'type': 'shell',
+        'command': 'python',
+        'args': [
+            '${workspaceFolder}/scripts/generate_vscode_tasks.py',
+            '--lldb'
+        ],
+        'group': 'build'
+    }]
+
+    # Task to generate all bazel targets.
+    task_list += [{
+        'label': '(bazel:{0}:build) all'.format(comp_mode),
+        'type': 'shell',
+        'command': 'bazel',
+        'args': [
+            'build',
+            '--compilation_mode={0}'.format(comp_mode),
+            '//...',
+            '--',
+            '-//src/ui/...',
+        ],
+        'group': 'build',
+    } for comp_mode in ['opt', 'dbg']]
+
+    tasks = {
+        'version': '2.0.0',
+        'tasks': task_list,
+    }
+    launch = {
+        'version': '0.2.0',
+        'configurations': launch_list,
+    }
+    workspace_root = subprocess.check_output(
+        ['bazel', 'info', 'workspace']).rstrip()
+
+    with open(os.path.join(workspace_root, '.vscode/launch.json'), 'w') as launch_file:
+        launch_file.write(json.dumps(launch, indent=4, sort_keys=True))
+
+    with open(os.path.join(workspace_root, '.vscode/tasks.json'), 'w') as task_file:
+        task_file.write(json.dumps(tasks, indent=4, sort_keys=True))
+
+
+if __name__ == '__main__':
+    main()
