5fa33550f2783cfecf9e485ddd413620a630cbaf
Zain Asgar <zasgar@pixielabs.ai>
2019-01-08 00:40:13 -0800

    Remove some of the throwaway test code
    Summary: We have lots of actual code examples now. Still maintaining the C++ grpc example until we have that in our actual code base.
    
    Test Plan: `bazel test //...`
    
    Reviewers: #engineering, michelle, kgandhi
    
    Reviewed By: #engineering, kgandhi
    
    Differential Revision: https://phab.corp.pixielabs.ai/D176
    
    GitOrigin-RevId: fc8e237f0fba15d518ce5ec5fda49dee5196c6cf

--- throwaway/adder-test.cc
+++
@@ -1,4 +0,0 @@
-#include "throwaway/adder.h"
-#include "gtest/gtest.h"
-
-TEST(AdderTest, TestAddNumbersShouldReturnCorrectResults) { EXPECT_EQ(pl::AddNumbers(1, 2), 3); }
--- throwaway/adder.cc
+++
@@ -1,7 +0,0 @@
-#include "throwaway/adder.h"
-
-namespace pl {
-
-int AddNumbers(int x, int y) { return x + y; }
-
-}  // namespace pl
