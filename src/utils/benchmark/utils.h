676c3825b666bf566918aa05694e2bb339da419b
Zain Asgar <zasgar@pixielabs.ai>
2019-01-28 18:32:30 -0800

    PL-272 - Add some utility functions to make Benchmarks easier.
    Summary: Added functions to make string/integer values.
    
    Test Plan: Will be used in benchmarks.
    
    Reviewers: #engineering, michelle
    
    Reviewed By: #engineering, michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D225
    
    GitOrigin-RevId: 9d70c2c6122004eaf6956682eac0af5131e8a92c

---
+++ src/utils/benchmark/utils.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <random>
+#include <string>
+#include <vector>
+
+namespace pl {
+namespace bmutils {
+
+template <typename T>
+std::vector<T> CreateLargeData(int size, int64_t min_val = 0, int64_t max_val = 100) {
+  std::vector<T> data(size);
+
+  std::random_device rnd_device;
+  std::mt19937 mersenne_engine{rnd_device()};  // Generates random integers
+  std::uniform_int_distribution<int64_t> dist{min_val, max_val};
+
+  auto gen = [&dist, &mersenne_engine]() { return dist(mersenne_engine); };
+
+  std::generate(begin(data), end(data), gen);
+  return data;
+}
+
+std::string RandomString(size_t length) {
+  auto randchar = []() -> char {
+    const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    const size_t max_index = (sizeof(charset) - 1);
+    return charset[rand() % max_index];
+  };
+  std::string str(length, 0);
+  std::generate_n(str.begin(), length, randchar);
+  return str;
+}
+
+}  // namespace bmutils
+}  // namespace pl
