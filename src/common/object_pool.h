7704017c39d09e17535a13bdefd06f9a36357d82
Zain Asgar <zasgar@pixielabs.ai>
2019-01-31 14:17:42 -0800

    PL-298 - Add an ObjectPool class
    Summary: TSIA
    
    Test Plan: Added unit tests.
    
    Reviewers: #engineering, michelle
    
    Reviewed By: #engineering, michelle
    
    Subscribers: oazizi
    
    Differential Revision: https://phab.corp.pixielabs.ai/D241
    
    GitOrigin-RevId: 45e6d4da0796ff3178b922f940551455ace20610

---
+++ src/common/object_pool.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <glog/logging.h>
+#include <memory>
+#include <vector>
+
+#include "absl/base/internal/spinlock.h"
+#include "src/utils/macros.h"
+
+namespace pl {
+/**
+ * This class hangs onto pointer and will deallocate them upon destruction.
+ *
+ * Concepts and some code similar to Impala.
+ */
+class ObjectPool final {
+ public:
+  ObjectPool() = default;
+  ~ObjectPool() { Clear(); }
+  /**
+   * Take ownership of passed in pointer.
+   *
+   * @tparam T The entity type to track.
+   * @param entity A pointer to the entity.
+   * @return The pointer to the entity.
+   */
+  template <typename T>
+  T *Add(T *entity) {
+    absl::base_internal::SpinLockHolder lock(&lock_);
+    obj_list_.emplace_back(Entity{entity, [](void *obj) { delete reinterpret_cast<T *>(obj); }});
+    return entity;
+  }
+
+  void Clear() {
+    absl::base_internal::SpinLockHolder lock(&lock_);
+    for (auto &obj : obj_list_) obj.delete_fn(obj.obj);
+    obj_list_.clear();
+  }
+
+ private:
+  PL_DISALLOW_COPY_AND_ASSIGN(ObjectPool);
+
+  // A generic deletion function pointer. Deletes its first argument.
+  typedef void (*DeleteFn)(void *);
+
+  // For each object, a pointer to the object and a function that deletes it.
+  struct Entity {
+    void *obj;
+    DeleteFn delete_fn;
+  };
+
+  absl::base_internal::SpinLock lock_;
+  std::vector<Entity> obj_list_;
+};
+
+}  // namespace pl
