95c7907645507ab580ea59cda49cdcaea3c3ceb0
Zain Asgar <zasgar@pixielabs.ai>
2019-02-04 00:21:50 -0800

    PL-304 - Added scoped timer
    Summary: Added both scoped timer and elapsed time timer.
    
    Test Plan: Added a unit test (but this does not actually simulate the time).
    
    Reviewers: #engineering, michelle, kgandhi
    
    Reviewed By: #engineering, kgandhi
    
    Subscribers: kgandhi
    
    Differential Revision: https://phab.corp.pixielabs.ai/D254
    
    GitOrigin-RevId: 86c5007260f7e4e69dffba64b1a65d5a163eeca2

---
+++ src/common/elapsed_timer.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <glog/logging.h>
+#include <chrono>
+#include <iostream>
+
+#include "src/common/base.h"
+
+namespace pl {
+
+/**
+ * Timing class.
+ *
+ * Allows for measurement with high resolution timer with clocks that can be started and stopped.
+ */
+class ElapsedTimer : public NotCopyable {
+ public:
+  /**
+   * Start the timer.
+   */
+  void Start() {
+    DCHECK(!timer_running_) << "Timer already running";
+    timer_running_ = true;
+    start_time_ = std::chrono::high_resolution_clock::now();
+  }
+
+  /**
+   * Stop the timer.
+   */
+  void Stop() {
+    DCHECK(timer_running_) << "Stop called when timer is not running";
+    timer_running_ = false;
+    elapsed_time_us_ += TimeDiff();
+  }
+
+  /**
+   * Reset the timer.
+   */
+  void Reset() {
+    timer_running_ = false;
+    elapsed_time_us_ = 0;
+  }
+
+  /**
+   * @return the elapsed time in us.
+   */
+  double ElapsedTime_us() const { return elapsed_time_us_ + (timer_running_ ? TimeDiff() : 0); }
+
+ private:
+  double TimeDiff() const {
+    auto current = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(current - start_time_).count();
+  }
+  bool timer_running_ = false;
+  std::chrono::high_resolution_clock::time_point start_time_;
+  double elapsed_time_us_ = 0;
+};
+
+}  // namespace pl
---
+++ src/common/scoped_timer.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <glog/logging.h>
+#include <string>
+
+#include "absl/strings/str_format.h"
+#include "src/common/base.h"
+#include "src/common/elapsed_timer.h"
+
+namespace pl {
+
+/**
+ * Times a particular function scope and prints the time to the log.
+ * @tparam TTimer Can be any class that implements Start and ElapsedTime_us().
+ */
+template <class TTimer = ElapsedTimer>
+class ScopedTimer : public NotCopyable {
+ public:
+  /**
+   * Creates a scoped timer with the given name.
+   * @param name
+   */
+  explicit ScopedTimer(const std::string& name) : name_(name) { timer_.Start(); }
+
+  /**
+   * Writes to the log the elapsed time.
+   */
+  ~ScopedTimer() {
+    double elapsed = timer_.ElapsedTime_us();
+    LOG(INFO) << absl::StrFormat("Timer(%s) : %s", name_, PrettyString(elapsed));
+  }
+
+ private:
+  TTimer timer_;
+  std::string name_;
+
+  std::string PrettyString(double t) {
+    // us,ms,s,
+    if (t < 1000) {
+      return absl::StrFormat("%.2f\u03BCs", t);
+    } else if (t < 100'000) {
+      return absl::StrFormat("%.2fms", t / 1000.0);
+    }
+    return absl::StrFormat("%.2fs", t / 1000.0 / 1000.0);
+  }
+};
+
+}  // namespace pl
