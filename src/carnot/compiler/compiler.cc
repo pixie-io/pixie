3b3a63a1f750cdb152afe2dd83f731bf92d246f7
Michelle Nguyen <michelle@pixielabs.ai>
2019-02-13 14:47:18 -0800

    PL-328 Compiler class
    Summary: Add the compiler class, which will bring together all of @philkuz's work.
    
    Test Plan: tests will come when more of the compiler is implemented
    
    Reviewers: #engineering, philkuz, zasgar
    
    Reviewed By: #engineering, zasgar
    
    Subscribers: philkuz
    
    Differential Revision: https://phab.corp.pixielabs.ai/D284
    
    GitOrigin-RevId: 496bacddcf048b9c1dd25aaa073b021500e4f794

---
+++ src/carnot/compiler/compiler.cc
@@ -0,0 +1,46 @@
+#include <pypa/ast/tree_walker.hh>
+#include <pypa/parser/parser.hh>
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "src/carnot/compiler/ast_visitor.h"
+#include "src/carnot/compiler/compiler.h"
+#include "src/carnot/compiler/compiler_state.h"
+#include "src/carnot/compiler/string_reader.h"
+#include "src/carnot/proto/plan.pb.h"
+
+namespace pl {
+namespace carnot {
+namespace compiler {
+StatusOr<carnotpb::Plan> Compiler::Compile(const std::string& query) {
+  auto ir = QueryToIR(query);
+  return IRToLogicalPlan(ir.ConsumeValueOrDie());
+}
+
+StatusOr<std::shared_ptr<IR>> Compiler::QueryToIR(const std::string& query) {
+  std::shared_ptr<IR> ir = std::make_shared<IR>();
+  ASTWalker ast_walker(ir);
+
+  pypa::AstModulePtr ast;
+  pypa::SymbolTablePtr symbols;
+  pypa::ParserOptions options;
+  pypa::Lexer lexer(std::make_unique<StringReader>(query));
+
+  Status result;
+  if (pypa::parse(lexer, ast, symbols, options)) {
+    result = ast_walker.ProcessModuleNode(ast);
+  } else {
+    result = error::InvalidArgument("Parsing was unsuccessful, likely because of broken argument.");
+  }
+  PL_RETURN_IF_ERROR(result);
+  return ir;
+}
+
+StatusOr<carnotpb::Plan> Compiler::IRToLogicalPlan(std::shared_ptr<IR>) { return carnotpb::Plan(); }
+
+}  // namespace compiler
+}  // namespace carnot
+}  // namespace pl
---
+++ src/carnot/compiler/compiler.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "src/carnot/compiler/compiler_state.h"
+#include "src/carnot/compiler/ir_nodes.h"
+#include "src/carnot/proto/plan.pb.h"
+
+namespace pl {
+namespace carnot {
+namespace compiler {
+
+/**
+ * The compiler takes a query in the form of a string and compiles it into a logical plan.
+ */
+class Compiler {
+ public:
+  /**
+   * Constructor for the compiler.
+   * @param compiler_state which manages state needed for compilation.
+   */
+  explicit Compiler(std::shared_ptr<CompilerState> compiler_state)
+      : compiler_state_(compiler_state) {}
+
+  /**
+   * Compile the query into a logical plan.
+   * @param query the query to compile.
+   * @return the logical plan in the form of a plan protobuf message.
+   */
+  StatusOr<carnotpb::Plan> Compile(const std::string& query);
+
+ private:
+  StatusOr<std::shared_ptr<IR>> QueryToIR(const std::string& query);
+  StatusOr<carnotpb::Plan> IRToLogicalPlan(std::shared_ptr<IR> ir);
+
+  std::shared_ptr<CompilerState> compiler_state_;
+};
+
+}  // namespace compiler
+}  // namespace carnot
+}  // namespace pl
