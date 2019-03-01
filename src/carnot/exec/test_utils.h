b19ef8b14f550607f1fdec1d6d7e97c6e972dc78
Zain Asgar <zasgar@pixielabs.ai>
2019-03-01 12:57:06 -0800

    Change some expects to PL_CHECK_OK
    Summary: This is throwaway code, but we currently use it in the agent and it's preventing builds since we don't have EXPECTS.
    
    Test Plan: N/A
    
    Reviewers: philkuz, michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D385
    
    GitOrigin-RevId: b142f748e27d26b3a6e2da64c4333c10727de092

--- src/carnot/exec/test_utils.h
+++ src/carnot/exec/test_utils.h
@@ -22,17 +22,17 @@ class CarnotTestUtils {
     auto col1 = std::make_shared<exec::Column>(udf::UDFDataType::FLOAT64, "col1");
     std::vector<udf::Float64Value> col1_in1 = {0.5, 1.2, 5.3};
     std::vector<udf::Float64Value> col1_in2 = {0.1, 5.1};
-    EXPECT_OK(col1->AddBatch(udf::ToArrow(col1_in1, arrow::default_memory_pool())));
-    EXPECT_OK(col1->AddBatch(udf::ToArrow(col1_in2, arrow::default_memory_pool())));
+    PL_CHECK_OK(col1->AddBatch(udf::ToArrow(col1_in1, arrow::default_memory_pool())));
+    PL_CHECK_OK(col1->AddBatch(udf::ToArrow(col1_in2, arrow::default_memory_pool())));
 
     auto col2 = std::make_shared<exec::Column>(udf::UDFDataType::INT64, "col2");
     std::vector<udf::Int64Value> col2_in1 = {1, 2, 3};
     std::vector<udf::Int64Value> col2_in2 = {5, 6};
-    EXPECT_OK(col2->AddBatch(udf::ToArrow(col2_in1, arrow::default_memory_pool())));
-    EXPECT_OK(col2->AddBatch(udf::ToArrow(col2_in2, arrow::default_memory_pool())));
+    PL_CHECK_OK(col2->AddBatch(udf::ToArrow(col2_in1, arrow::default_memory_pool())));
+    PL_CHECK_OK(col2->AddBatch(udf::ToArrow(col2_in2, arrow::default_memory_pool())));
 
-    EXPECT_OK(table->AddColumn(col1));
-    EXPECT_OK(table->AddColumn(col2));
+    PL_CHECK_OK(table->AddColumn(col1));
+    PL_CHECK_OK(table->AddColumn(col2));
 
     return table;
   }
