ddbeee5c97f6c559a0079d63c7b5082e7564d893
Zain Asgar <zasgar@pixielabs.ai>
2019-03-10 16:36:18 -0700

    Add ability to run clang-tidy
    Summary:
    Use `./scripts/run_clang_tidy.sh` to run clang_tidy.
    We can now run clang-tidy but can't put it in our build yet, because of failures.
    
    Test Plan: `./scripts/run_clang_tidy.sh`
    
    Reviewers: michelle, oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D433
    
    GitOrigin-RevId: e9623d79c764c7ae36ddfdfc68e0a09d45d27b69

---
+++ scripts/run_clang_tidy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# We can have the Clang tidy script in a few different places. Check them in priority
+# order.
+clang_tidy_scripts=(
+    "/opt/clang-7.0/share/clang/run-clang-tidy.py"
+    "/usr/local/opt/llvm/share/clang/run-clang-tidy.py")
+
+clang_tidy_script=""
+for script_option in "${clang_tidy_scripts[@]}"
+do
+    echo $script_option
+    if [ -f "${script_option}" ]; then
+        clang_tidy_script=${script_option}
+        break
+    fi
+done
+
+if [ -z "${clang_tidy_script}" ]; then
+    echo "Failed to find a valid clang tidy script runner (check LLVM/Clang install"
+    exit 1
+fi
+
+echo "Selected: ${clang_tidy_script}"
+
+set -e
+SRCDIR=$(bazel info workspace)
+echo "Generating compilation database..."
+pushd $SRCDIR
+# Bazel build need to be run to setup virtual includes, generating files which are consumed
+# by clang-tidy.
+"${SRCDIR}/scripts/gen_compilation_database.py" --include_headers --run_bazel_build
+
+# Actually invoke clang-tidy.
+"${clang_tidy_script}" -j $(nproc)
+popd
