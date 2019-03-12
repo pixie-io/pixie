c03ac62b89422401d05c2c5cfe9209e964e5ada8
Zain Asgar <zasgar@pixielabs.ai>
2019-03-11 22:15:58 -0700

    Add macro to ignore warnings
    Summary: This is needed because some of our external headers have unused variables.
    
    Test Plan: N/A
    
    Reviewers: oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D465
    
    GitOrigin-RevId: a933fc98839b11ba38b5dff4c366832d6ea0a3d6

--- src/common/macros.h
+++ src/common/macros.h
@@ -14,3 +14,31 @@
 // Internal helper for concatenating macro values.
 #define PL_CONCAT_NAME_INNER(x, y) x##y
 #define PL_CONCAT_NAME(x, y) PL_CONCAT_NAME_INNER(x, y)
+
+// Disable clang format since it insists on inlining these macros.
+// clang-format off
+#if defined(__clang__)
+
+#define PL_SUPPRESS_WARNINGS_START()                          \
+  _Pragma("clang diagnostic push")                            \
+  _Pragma("clang diagnostic ignored \"-Weverything\"")
+
+#define PL_SUPPRESS_WARNINGS_END()                            \
+  _Pragma("clang diagnostic pop")
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+
+// GCC does not support disabling all warnings, so we just suppress the most common one.
+// We can add to this list if needed.
+#define PL_SUPPRESS_WARNINGS_START()                         \
+  _Pragma("GCC diagnostic push")                             \
+  _Pragma("GCC diagnostic ignored \"-Wunused-variable\"")
+
+#define PL_SUPPRESS_WARNINGS_END()                           \
+  _Pragma("GCC diagnostic pop")
+
+#else
+#error "Unsupported compiler"
+#endif
+
+// clang-format on
