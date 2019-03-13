45144098bfacfcc098af4ebeafdda459c8de9f3d
Zain Asgar <zasgar@pixielabs.ai>
2019-03-13 15:02:28 -0700

    Enable new sketches/json udfs/udas
    Summary: TSIA
    
    Test Plan: N/A
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D486
    
    GitOrigin-RevId: b6f8828542ae4106153a72a31a9a79ce5984ad3f

--- src/carnot/builtins/builtins.cc
+++ src/carnot/builtins/builtins.cc
@@ -1,13 +1,23 @@
 #include "src/carnot/builtins/builtins.h"
+#include "src/carnot/builtins/json_ops.h"
 #include "src/carnot/builtins/math_ops.h"
+#include "src/carnot/builtins/math_sketches.h"
+
 #include "src/carnot/udf/registry.h"
 
 namespace pl {
 namespace carnot {
 namespace builtins {
 
-void RegisterBuiltinsOrDie(udf::ScalarUDFRegistry* registry) { RegisterMathOpsOrDie(registry); }
-void RegisterBuiltinsOrDie(udf::UDARegistry* registry) { RegisterMathOpsOrDie(registry); }
+void RegisterBuiltinsOrDie(udf::ScalarUDFRegistry* registry) {
+  RegisterMathOpsOrDie(registry);
+  RegisterJSONOpsOrDie(registry);
+}
+
+void RegisterBuiltinsOrDie(udf::UDARegistry* registry) {
+  RegisterMathOpsOrDie(registry);
+  RegisterMathSketchesOrDie(registry);
+}
 
 }  // namespace builtins
 }  // namespace carnot
