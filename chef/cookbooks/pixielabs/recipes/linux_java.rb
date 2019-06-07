2a0fd93bd873d436c7b13c422caf6779277627f1
Zain Asgar <zasgar@pixielabs.ai>
2019-06-06 23:03:15 -0700

    Switch to OpenJDK
    Summary: Java license changes broke our Oracle JDK PPA.
    
    Test Plan: Jenkins
    
    Reviewers: oazizi, #engineering, chengruizhe
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D925
    
    GitOrigin-RevId: 3e18ae352f94d9da8d109b850c21d7f539b5d551

--- chef/cookbooks/pixielabs/recipes/linux_java.rb
+++ chef/cookbooks/pixielabs/recipes/linux_java.rb
@@ -1,18 +1,4 @@
 # Install Java.
-apt_repository 'oracle-java' do
-  uri         'ppa:webupd8team/java'
-end
-
-apt_update 'update packages' do
-  action :update
-end
-
-execute 'java-selection' do
-  command 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true |'\
-          ' /usr/bin/debconf-set-selections'
-  action :run
-end
-
-apt_package 'oracle-java8-installer' do
+apt_package 'openjdk-11-jdk' do
   action :upgrade
 end
