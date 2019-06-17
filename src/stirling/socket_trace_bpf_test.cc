1a106dfd780d771f2b5a20472bda2ddd16adf2b3
Yaxiong Zhao <yzhao@pixielabs.ai>
2019-06-17 10:17:31 -0700

    Move gRPC test into a separate test file, and rename socket_trace_bpf_test.cc to http_trace_bpf_test.cc.
    Summary: Improves readability, workaround the go testing issue.
    
    Test Plan: Unit test in Jenkins.
    
    Reviewers: oazizi, chengruizhe, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D993
    
    GitOrigin-RevId: a43758192863ca3d0ad3a648d4219777a3c2a338

--- src/stirling/socket_trace_bpf_test.cc
+++ src/stirling/socket_trace_bpf_test.cc
@@ -8,8 +8,6 @@
 #include <string_view>
 #include <thread>
 
-#include "src/common/subprocess/subprocess.h"
-#include "src/common/testing/testing.h"
 #include "src/shared/types/column_wrapper.h"
 #include "src/shared/types/types.h"
 #include "src/stirling/bcc_bpf/socket_trace.h"
@@ -19,14 +17,9 @@
 namespace pl {
 namespace stirling {
 
-namespace fs = std::experimental::filesystem;
-
 using ::pl::stirling::testing::TCPSocket;
 using ::pl::types::ColumnWrapper;
 using ::pl::types::ColumnWrapperRecordBatch;
-using ::pl::types::DataType;
-using ::testing::_;
-using ::testing::ElementsAre;
 using ::testing::Pair;
 using ::testing::SizeIs;
 using ::testing::UnorderedElementsAre;
@@ -329,38 +322,6 @@ TEST_F(HTTPTraceBPFTest, TestConnectionCloseAndGenerationNumberAreInSync) {
     }
   }
   EXPECT_THAT(seq_msgs, UnorderedElementsAre(Pair(0, kHTTPRespMsg1), Pair(0, kHTTPRespMsg2)));
-}
-
-TEST(GRPCTraceBPFTest, TestGolangGrpcService) {
-  constexpr char kBaseDir[] = "src/stirling/testing";
-  std::string s_path =
-      TestEnvironment::PathToTestDataFile(absl::StrCat(kBaseDir, "/go_greeter_server"));
-  std::string c_path =
-      TestEnvironment::PathToTestDataFile(absl::StrCat(kBaseDir, "/go_greeter_client"));
-  SubProcess s({s_path});
-  EXPECT_OK(s.Start());
-
-  // TODO(yzhao): We have to install probes after starting server. Otherwise we will run into
-  // failures when detaching them. This might be relevant to probes are inherited by child process
-  // when fork() and execvp().
-  std::unique_ptr<SourceConnector> connector =
-      SocketTraceConnector::Create("socket_trace_connector");
-  ASSERT_OK(connector->Init());
-
-  SubProcess c({c_path, "-name=PixieLabs", "-once"});
-  EXPECT_OK(c.Start());
-  EXPECT_EQ(0, c.Wait()) << "Client should exit normally.";
-  s.Kill();
-  EXPECT_EQ(9, s.Wait()) << "Server should have been killed.";
-
-  auto* socket_trace_connector = dynamic_cast<SocketTraceConnector*>(connector.get());
-  ASSERT_NE(nullptr, socket_trace_connector);
-
-  const int kTableNum = 2;
-  socket_trace_connector->ReadPerfBuffer(kTableNum);
-
-  EXPECT_THAT(socket_trace_connector->TestOnlyHTTP2Streams(), SizeIs(2));
-  EXPECT_OK(connector->Stop());
 }
 
 }  // namespace stirling
