798b3e7cd05dc9694edb56f9bbb0d8a795a1d581
Yaxiong Zhao <yzhao@pixielabs.ai>
2019-07-19 12:09:08 -0700

    Add pb_parsing_benchmark.cc.
    Summary:
    Benchmark dynamic message vs. compiled message.
    
    The result shows compiled message parsing is ~5.2X of dynamic.
    Under optimized build:
    
    $ bazel build :pb_parsing_benchmark -c opt
    $ bazel-bin/src/benchmarks/pb_parsing_benchmark
    
    -----------------------------------------------------------------------------------------------
    Benchmark                                     Time             CPU   Iterations UserCounters...
    -----------------------------------------------------------------------------------------------
    BM_dynamic_message_parsing/1000          803126 ns       803125 ns          874 items_per_second=1.24514M/s
    BM_dynamic_message_parsing/10000        8100706 ns      8100681 ns           86 items_per_second=1.23446M/s
    BM_dynamic_message_parsing/100000      81259224 ns     81259358 ns            9 items_per_second=1.23063M/s
    BM_dynamic_message_parsing/1000000    811107874 ns    811109418 ns            1 items_per_second=1.23288M/s
    BM_dynamic_message_parsing/10000000  8282869339 ns   8282894938 ns            1 items_per_second=1.20731M/s
    BM_compiled_message_parsing/1000         145436 ns       145437 ns         4813 items_per_second=6.87584M/s
    BM_compiled_message_parsing/10000       1449272 ns      1449281 ns          479 items_per_second=6.89998M/s
    BM_compiled_message_parsing/100000     14459774 ns     14459857 ns           48 items_per_second=6.9157M/s
    BM_compiled_message_parsing/1000000   144806814 ns    144806490 ns            5 items_per_second=6.90577M/s
    BM_compiled_message_parsing/10000000 1449469328 ns   1449476420 ns            1 items_per_second=6.89904M/s
    
    Test Plan: Jenkins
    
    Reviewers: oazizi, zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Subscribers: chengruizhe
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1258
    
    GitOrigin-RevId: 0ce80d5453aa47732a6cbd7cdba14bd91e5eec5f

---
+++ src/benchmarks/pb_parsing_benchmark.cc
@@ -0,0 +1,71 @@
+#include <benchmark/benchmark.h>
+
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/message.h>
+
+#include <string>
+
+#include "src/benchmarks/proto/benchmark.pb.h"
+#include "src/common/grpcutils/service_descriptor_database.h"
+
+const size_t kRangeMultiplier = 10;
+const size_t kRangeBegin = 1'000;
+const size_t kRangeEnd = 10'000'000;
+
+using ::google::protobuf::FileDescriptorSet;
+using ::google::protobuf::Message;
+using ::pl::benchmarks::ChargeRequest;
+using ::pl::grpc::MethodInputOutput;
+using ::pl::grpc::ServiceDescriptorDatabase;
+
+static ChargeRequest SampleChargeRequest() {
+  ChargeRequest r;
+  r.mutable_amount()->set_currency_code("USD");
+  r.mutable_amount()->set_units(1000);
+  r.mutable_amount()->set_nanos(1'000'000);
+  r.mutable_credit_card()->set_credit_card_number("1234 1234 1234 1234");
+  r.mutable_credit_card()->set_credit_card_cvv(123);
+  r.mutable_credit_card()->set_credit_card_expiration_year(2022);
+  r.mutable_credit_card()->set_credit_card_expiration_month(12);
+  return r;
+}
+
+// NOLINTNEXTLINE : runtime/references.
+static void BM_dynamic_message_parsing(benchmark::State& state) {
+  const ChargeRequest sample_charge_req = SampleChargeRequest();
+  const std::string serialized_charge_req = sample_charge_req.SerializeAsString();
+
+  FileDescriptorSet fd_set;
+  ChargeRequest::descriptor()->file()->CopyTo(fd_set.add_file());
+  ServiceDescriptorDatabase db(fd_set);
+  MethodInputOutput in_out = db.GetMethodInputOutput("pl.benchmarks.PaymentService.Charge");
+
+  for (auto _ : state) {
+    for (size_t i = 0; i < static_cast<size_t>(state.range(0)); ++i) {
+      std::unique_ptr<Message> t(in_out.input->New());
+      t->ParseFromString(serialized_charge_req);
+    }
+  }
+  state.SetItemsProcessed(state.iterations() * state.range(0));
+}
+
+// NOLINTNEXTLINE : runtime/references.
+static void BM_compiled_message_parsing(benchmark::State& state) {
+  const ChargeRequest sample_charge_req = SampleChargeRequest();
+  const std::string serialized_charge_req = sample_charge_req.SerializeAsString();
+
+  for (auto _ : state) {
+    for (size_t i = 0; i < static_cast<size_t>(state.range(0)); ++i) {
+      ChargeRequest t;
+      t.ParseFromString(serialized_charge_req);
+    }
+  }
+  state.SetItemsProcessed(state.iterations() * state.range(0));
+}
+
+BENCHMARK(BM_dynamic_message_parsing)
+    ->RangeMultiplier(kRangeMultiplier)
+    ->Range(kRangeBegin, kRangeEnd);
+BENCHMARK(BM_compiled_message_parsing)
+    ->RangeMultiplier(kRangeMultiplier)
+    ->Range(kRangeBegin, kRangeEnd);
