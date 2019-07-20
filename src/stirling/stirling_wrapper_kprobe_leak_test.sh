6e9aa48a14d5b0cc58f7882818685dc86c5edb08
Omid Azizi <oazizi@pixielabs.ai>
2019-07-19 21:48:50 -0700

    Enable a test to check for kprobe leaks
    Summary: Set requires_ebpf tag, so doesn't run by default in user workspaces
    
    Test Plan: This is the test :)
    
    Reviewers: zasgar, yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1255
    
    GitOrigin-RevId: f64bfcea5c6f243757432c6716d9af0e91e711b6

---
+++ src/stirling/stirling_wrapper_kprobe_leak_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# Assuming this script was run through bazel, the executable should be here.
+BIN_DIR=.
+
+# If the script was run in a stand-alone way,
+# then build and set the correct directory of the binary.
+if [ -z "$BUILD_WORKSPACE_DIRECTORY" ]; then
+    bazel build //src/stirling:stirling_wrapper
+    BIN_DIR=$(bazel info bazel-bin)
+fi
+
+# Main test run.
+./scripts/kprobe_leak_test.sh $BIN_DIR/stirling_wrapper
