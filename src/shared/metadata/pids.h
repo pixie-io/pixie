8cd3c68604e312000e1a6ce8404cd85f5427a815
Zain Asgar <zasgar@pixielabs.ai>
2019-07-22 17:09:25 -0700

    PL-596 - Add PID information to the metadata
    Summary: This adds PIDs and related events to the metadat.
    
    Test Plan: Added unit tests.
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1312
    
    GitOrigin-RevId: e9adc0bf5e3f994111f489be2b0d1d1285c289a1

--- src/shared/metadata/pids.h
+++ src/shared/metadata/pids.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <string>
+#include <utility>
 
 #include "src/common/base/base.h"
 #include "src/shared/metadata/base_types.h"
@@ -12,46 +13,54 @@ namespace md {
 /**
  * Store information about PIDs.
  */
-struct PIDInfo {
-  PIDInfo() = default;
+class PIDInfo {
+ public:
+  PIDInfo() = delete;
+  PIDInfo(UPID upid, std::string_view cmdline, CID container_id)
+      : upid_(upid), cmdline_(cmdline), container_id_(std::move(container_id)), stop_time_ns_(0) {}
 
-  UPID upid;
-  int64_t pid;
+  UPID upid() const { return upid_; }
+
+  int64_t start_time_ns() const { return upid_.start_ts(); }
+
+  int64_t stop_time_ns() const { return stop_time_ns_; }
+
+  void set_stop_time_ns(int64_t ts) { stop_time_ns_ = ts; }
+
+  const std::string& cmdline() const { return cmdline_; }
+
+  // FIXME: change container_id to cid;
+  CID cid() const { return container_id_; }
+
+  std::unique_ptr<PIDInfo> Clone() {
+    auto pid_info = std::make_unique<PIDInfo>(*this);
+    return pid_info;
+  }
+
+  bool operator==(const PIDInfo& other) const {
+    return (other.upid_ == upid_) && (other.cmdline_ == cmdline_) &&
+           (other.container_id_ == container_id_) && (other.stop_time_ns_ == stop_time_ns_);
+  }
+
+  bool operator!=(const PIDInfo& other) const { return !(other == *this); }
+
+ private:
+  UPID upid_;
 
   /**
    * The command line used to start this PID.
    */
-  std::string cmdline;
+  std::string cmdline_;
 
   /**
    * The container running this PID.
    */
-  CID container_id;
+  CID container_id_;
 
   /**
-   * Start time of this K8s object.
+   * The time that this PID stopped running. If 0 we can assume it's still running.
    */
-  int64_t start_time_ns = 0;
-
-  /**
-   * The time that this container info was last updated.
-   * If not updated we assume that the Container has died. We use this to know if
-   * a given PID has been deleted.
-   */
-  int64_t last_update_time_ns = 0;
-
-  void CloneFrom(const PIDInfo& other) { *this = other; }
-
-  std::unique_ptr<PIDInfo> Clone() {
-    auto pid_info = std::make_unique<PIDInfo>();
-    pid_info->CloneFrom(*this);
-    return pid_info;
-  }
-
- private:
-  // Private copy constructor to make this class not implicitly copyable.
-  PIDInfo(const PIDInfo&) = default;
-  PIDInfo& operator=(const PIDInfo&) = default;
+  int64_t stop_time_ns_ = 0;
 };
 
 /*
@@ -62,7 +71,7 @@ struct PIDInfo {
 /**
  * The PID status event type.
  */
-enum class PIDStatusEventType : uint8_t { kUnknown = 0, kStarted, kStopped };
+enum class PIDStatusEventType : uint8_t { kUnknown = 0, kStarted, kTerminated };
 
 /**
  * Base class for PID status event.
@@ -78,21 +87,24 @@ struct PIDStatusEvent {
  * It contains a copy of the newly created PID Info.
  */
 struct PIDStartedEvent : public PIDStatusEvent {
-  explicit PIDStartedEvent(const PIDInfo& other) : PIDStatusEvent(PIDStatusEventType::kStarted) {
-    pid_info.CloneFrom(other);
-  }
+  explicit PIDStartedEvent(const PIDInfo& other)
+      : PIDStatusEvent(PIDStatusEventType::kStarted), pid_info(other) {}
 
-  PIDInfo pid_info;
+  const PIDInfo pid_info;
 };
 
 /**
- * PIDStopEvent has information about deleted PIDs.
- * It only contains the unique PID that was deleted.
+ * PIDTerminatedEvent has information about deleted PIDs.
+ * It only contains the unique PID that was deleted and the termination time stamp.
  */
-struct PIDStoppedEvent : public PIDStatusEvent {
-  explicit PIDStoppedEvent(UPID stopped_pid)
-      : PIDStatusEvent(PIDStatusEventType::kStopped), upid(stopped_pid) {}
-  UPID upid;
+struct PIDTerminatedEvent : public PIDStatusEvent {
+  explicit PIDTerminatedEvent(UPID stopped_pid, int64_t _stop_time_ns)
+      : PIDStatusEvent(PIDStatusEventType::kTerminated),
+        upid(stopped_pid),
+        stop_time_ns(_stop_time_ns) {}
+
+  const UPID upid;
+  const int64_t stop_time_ns;
 };
 
 }  // namespace md
---
+++ src/shared/metadata/pids_test.cc
@@ -0,0 +1,64 @@
+#include <gmock/gmock.h>
+#include <google/protobuf/text_format.h>
+#include <gtest/gtest.h>
+
+#include "src/shared/metadata/pids.h"
+
+namespace pl {
+namespace md {
+
+class PIDInfoTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    info_ = std::make_unique<PIDInfo>(UPID(1, 2, 3), "test.py --abc=def", "container_1");
+  }
+  std::unique_ptr<PIDInfo> info_;
+};
+
+TEST_F(PIDInfoTest, BasicAccessors) {
+  EXPECT_EQ(UPID(1, 2, 3), info_->upid());
+  EXPECT_EQ(0, info_->stop_time_ns());
+  EXPECT_EQ(3, info_->start_time_ns());
+  EXPECT_EQ("test.py --abc=def", info_->cmdline());
+  EXPECT_EQ("container_1", info_->cid());
+}
+
+TEST_F(PIDInfoTest, OperatorEq) {
+  EXPECT_EQ(PIDInfo(UPID(1, 2, 3), "test.py --abc=def", "container_1"), *info_);
+
+  EXPECT_NE(PIDInfo(UPID(1, 2, 4), "test.py --abc=def", "container_1"), *info_);
+
+  EXPECT_NE(PIDInfo(UPID(1, 2, 3), "est.py --abc=def", "container_1"), *info_);
+
+  EXPECT_NE(PIDInfo(UPID(1, 2, 3), "test.py --abc=def", "container_2"), *info_);
+}
+
+TEST_F(PIDInfoTest, OperatorEqStopTime) {
+  PIDInfo other(UPID(1, 2, 3), "test.py --abc=def", "container_1");
+
+  other.set_stop_time_ns(1000);
+  EXPECT_NE(other, *info_);
+
+  info_->set_stop_time_ns(1000);
+  EXPECT_EQ(other, *info_);
+}
+
+TEST_F(PIDInfoTest, Clone) { EXPECT_EQ(*info_, *info_->Clone()); }
+
+TEST(PIDStartedEvent, CheckInfo) {
+  PIDInfo pid_info(UPID(1, 2, 3), "test.py --abc=def", "container_1");
+  PIDStartedEvent ev(pid_info);
+
+  EXPECT_EQ(PIDStatusEventType::kStarted, ev.type);
+  EXPECT_EQ(pid_info, ev.pid_info);
+}
+
+TEST(PIDTerminatedEvent, CheckUPIDAndStopTime) {
+  PIDTerminatedEvent ev(UPID(1, 2, 3), 100);
+
+  EXPECT_EQ(UPID(1, 2, 3), ev.upid);
+  EXPECT_EQ(100, ev.stop_time_ns);
+  EXPECT_EQ(PIDStatusEventType::kTerminated, ev.type);
+}
+}  // namespace md
+}  // namespace pl
