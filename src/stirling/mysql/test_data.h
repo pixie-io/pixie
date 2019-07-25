5058942069fb80f7018edcda9f90a283b43d4713
Ryan Cheng <rcheng@pixielabs.ai>
2019-07-25 10:36:18 -0700

    [From D1284] Test data for MySQL Stitcher and parser tests
    Summary: mysql/test_data.h stores a normal Statment Prepare and Statement Execute Event from sockshop.
    
    Test Plan: Passes existing tests
    
    Reviewers: oazizi, yzhao, zasgar, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1369
    
    GitOrigin-RevId: fc0adde5dbfbb4745854b7a938a74627b99b2565

---
+++ src/stirling/mysql/test_data.h
@@ -0,0 +1,99 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+#include "src/common/base/base.h"
+#include "src/stirling/mysql/mysql.h"
+
+namespace pl {
+namespace stirling {
+namespace mysql {
+namespace testutils {
+
+/**
+ * A StmtPrepare and StmtExecute pair extracted from SockShop to test parsing and stitching
+ * of MySQL packets and events. They are associated such that StmtExecute has the parameters
+ * that can fit into the StmtPrepare's request.
+ */
+
+/**
+ * Statement Prepare Event with 2 col definitions and 2 params.
+ */
+const StringRequest kStmtPrepareRequest(
+    "SELECT sock.sock_id AS id, GROUP_CONCAT(tag.name) AS tag_name FROM sock JOIN sock_tag ON "
+    "sock.sock_id=sock_tag.sock_id JOIN tag ON sock_tag.tag_id=tag.tag_id WHERE tag.name=? GROUP "
+    "BY id ORDER BY ?");
+
+const StmtPrepareRespHeader kStmtPrepareRespHeader{2 /*stmt_id*/, 2 /*num_cols*/, 2 /*num_param*/,
+                                                   0 /*num_warning*/};
+
+// The following columns definitions and resultset rows are from real packet capture, but the
+// contents don't really matter to the functionality of the test.
+const std::vector<ColDefinition> kStmtPrepareParamDefs{
+    ColDefinition{std::string(ConstStrView(
+        "\x03\x64\x65\x66\x00\x00\x00\x01\x3f\x00\x0c\x3f\x00\x00\x00\x00\x00\xfd\x80\x00"
+        "\x00\x00\x00"))},
+    ColDefinition{std::string(ConstStrView(
+        "\x03\x64\x65\x66\x00\x00\x00\x01\x3f\x00\x0c\x3f\x00\x00\x00\x00\x00\xfd\x80\x00"
+        "\x00\x00\x00"))}};
+
+const std::vector<ColDefinition> kStmtPrepareColDefs{
+    ColDefinition{std::string(ConstStrView(
+        "\x03\x64\x65\x66\x07\x73\x6f\x63\x6b\x73\x64\x62\x04\x73\x6f\x63\x6b\x04\x73\x6f"
+        "\x63\x6b\x02"
+        "\x69\x64\x07\x73\x6f\x63\x6b\x5f\x69\x64\x0c\x21\x00\x78\x00\x00\x00\xfd\x03\x50"
+        "\x00\x00\x00"))},
+    ColDefinition{std::string(ConstStrView(
+        "\x03\x64\x65\x66\x07\x73\x6f\x63\x6b\x73\x64\x62\x04\x73\x6f\x63\x6b\x04\x73\x6f\x63"
+        "\x6b"
+        "\x04"
+        "\x6e\x61\x6d\x65\x04\x6e\x61\x6d\x65\x0c\x21\x00\x3c\x00\x00\x00\xfd\x00\x00\x00\x00"
+        "\x00"))}};
+
+const StmtPrepareOKResponse kStmtPrepareResponse(kStmtPrepareRespHeader, kStmtPrepareColDefs,
+                                                 kStmtPrepareParamDefs);
+
+ReqRespEvent InitStmtPrepare() {
+  std::unique_ptr<Request> req_ptr(new StringRequest(kStmtPrepareRequest));
+  std::unique_ptr<Response> resp_ptr(new StmtPrepareOKResponse(kStmtPrepareResponse));
+  return ReqRespEvent(MySQLEventType::kComStmtPrepare, std::move(req_ptr), std::move(resp_ptr));
+}
+
+/**
+ * Statement Execute Event with 2 params, 2 col definitions, and 2 resultset rows.
+ */
+const std::vector<ParamPacket> kStmtExecuteParams = {
+    {StmtExecuteParamType::kString, "\x62\x72\x6f\x77\x6e"},
+    {StmtExecuteParamType::kString, "\x69\x64"}};
+
+StmtExecuteRequest kStmtExecuteRequest(2 /*stmt_id*/, kStmtExecuteParams /*params*/);
+
+const std::vector<ColDefinition> kStmtExecuteColDefs = {
+    ColDefinition{
+        std::string(ConstStrView("\x03\x64\x65\x66\x07\x73\x6f\x63\x6b\x73\x64\x62"
+                                 "\x04\x73\x6f\x63\x6b\x04\x73\x6f\x63\x6b\x02\x69\x64\x07\x73\x6f"
+                                 "\x63\x6b\x5f\x69\x64\x0c\x21\x00\x78\x00\x00\x00\xfd\x01\x10\x00"
+                                 "\x00\x00"))},
+    ColDefinition{std::string(
+        ConstStrView("\x03\x64\x65\x66\x07\x73\x6f\x63\x6b\x73\x64\x62\n"
+                     "\x04\x73\x6f\x63\x6b\x04\x73\x6f\x63\x6b\x04\x6e\x61\x6d\x65\x04\n"
+                     "\x6e\x61\x6d\x65\x0c\x21\x00\x3c\x00\x00\x00\xfd\x00\x00\x00\x00\n"
+                     "\x00"))}};
+
+const std::vector<ResultsetRow> kStmtExecuteResultsetRows = {
+    ResultsetRow{std::string(ConstStrView("\x00\x00\x24\x33\x33\x39id1"))},
+    ResultsetRow{std::string(ConstStrView("\x00\x00\x24\x38\x33\x37id2"))}};
+
+const Resultset kStmtExecuteResultset(2, kStmtExecuteColDefs, kStmtExecuteResultsetRows);
+
+ReqRespEvent InitStmtExecute() {
+  std::unique_ptr<Request> req_ptr(new StmtExecuteRequest(kStmtExecuteRequest));
+  std::unique_ptr<Response> resp_ptr(new Resultset(kStmtExecuteResultset));
+  return ReqRespEvent(MySQLEventType::kComStmtExecute, std::move(req_ptr), std::move(resp_ptr));
+}
+
+}  // namespace testutils
+}  // namespace mysql
+}  // namespace stirling
+}  // namespace pl
