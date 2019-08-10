1b06b923b15a1097c3e7d3657eba550dca7057af
Zain Asgar <zasgar@pixielabs.ai>
2019-08-09 22:48:09 -0700

    Add Enumerate function to allow iteration with indices
    Summary: Making c++ a bit more like python :)
    
    Test Plan: Added a unit test.
    
    Reviewers: oazizi, philkuz, nserrino, yzhao, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1527
    
    GitOrigin-RevId: 9e52708c69757488fd467bb94745a95a8a4b9e94

--- src/common/base/utils.h
+++ src/common/base/utils.h
@@ -4,6 +4,8 @@
 
 #include <cstring>
 #include <string>
+#include <tuple>
+#include <utility>
 
 #include "absl/strings/str_format.h"
 
@@ -22,6 +24,28 @@ inline MemAlignedType CopyFromBPF(const void* data) {
   MemAlignedType result;
   memcpy(&result, data, sizeof(MemAlignedType));
   return result;
+}
+
+// Implementation borrowed from: http://reedbeta.com/blog/python-like-enumerate-in-cpp17/
+template <typename T, typename TIter = decltype(std::begin(std::declval<T>())),
+          typename = decltype(std::end(std::declval<T>()))>
+constexpr auto Enumerate(T&& iterable) {
+  struct iterator {
+    size_t i;
+    TIter iter;
+    bool operator!=(const iterator& other) const { return iter != other.iter; }
+    void operator++() {
+      ++i;
+      ++iter;
+    }
+    auto operator*() const { return std::tie(i, *iter); }
+  };
+  struct iterable_wrapper {
+    T iterable;
+    auto begin() { return iterator{0, std::begin(iterable)}; }
+    auto end() { return iterator{0, std::end(iterable)}; }
+  };
+  return iterable_wrapper{std::forward<T>(iterable)};
 }
 
 }  // namespace pl
--- src/common/base/utils_test.cc
+++ src/common/base/utils_test.cc
@@ -1,6 +1,8 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "src/common/base/utils.h"
 
 namespace pl {
@@ -10,6 +12,14 @@ using ::testing::StrEq;
 TEST(ToHexStringTest, ResultsAreAsExpected) {
   EXPECT_THAT(ToHexString("test\b"), StrEq(R"(test\x08)"));
   EXPECT_THAT(ToHexString("test\xab"), StrEq(R"(test\xAB)"));
+}
+
+TEST(Enumerate, LoopsThroughVectorWithIndex) {
+  std::vector<int> vals = {0, 2, 4, 6, 8};
+
+  for (const auto& [idx, val] : Enumerate(vals)) {
+    EXPECT_EQ(idx * 2, val);
+  }
 }
 
 }  // namespace pl
