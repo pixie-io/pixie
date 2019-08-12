0980b8fcd3f6978d3dc7f9187385cb8653f8cbec
Zain Asgar <zasgar@pixielabs.ai>
2019-08-11 21:20:49 -0700

    remove some dead code
    Summary: This removes the old smoke tests, which will be superceded by agent level checks.
    
    Test Plan: N/A
    
    Reviewers: michelle, oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1531
    
    GitOrigin-RevId: 5a10ec8e713f246a547d2c69a8df6f838bf62cd0

--- src/platform_dependent/smoke_test/scripts/give_permissions.sh
+++
@@ -1,3 +0,0 @@
-export EMAIL=$1
-gsutil iam ch user:$EMAIL:objectViewer gs://artifacts.pl-shared.appspot.com
-gsutil acl ch -u $EMAIL:W gs://pl-infra-shared
--- src/platform_dependent/smoke_test/scripts/revoke_permissions.sh
+++
@@ -1,4 +0,0 @@
-export EMAIL=$1
-
-gsutil iam ch -d user:$EMAIL:objectViewer gs://artifacts.pl-shared.appspot.com
-gsutil acl ch -d $EMAIL gs://pl-infra-shared
