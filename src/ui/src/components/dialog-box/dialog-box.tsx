2b13891f0a410ff614fef739fe2dfdb22921f497
Michelle Nguyen <michelle@pixielabs.ai>
2019-08-12 17:35:33 -0700

    PL-734 UI: Add DialogBox component to be used in signup/login/deploy page
    Summary:
    This just implements the simple dialog box, where the header contains the Pixie logo, and the contents can contain anything.
    This will be used for the Claim your Site box, Log into your Company box, and the deploy pixie agent box.
    Here's a screenshot of it in storybook:
    {F17037}
    You can also view it directly in storybook, but it's not super interesting.
    The box will be centered on the page it's used in, but the component itself does not handle any of that logic
    
    Test Plan: unit test
    
    Reviewers: zasgar, yike, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1541
    
    GitOrigin-RevId: 5fd075547a4417722343cb8079ff693fde9fe284

---
+++ src/ui/src/components/dialog-box/dialog-box.tsx
@@ -0,0 +1,26 @@
+import * as _ from 'lodash';
+import * as React from 'react';
+import './dialog-box.scss';
+
+// @ts-ignore : TS does not like image files.
+import * as infoImage from 'images/pixieLogo.svg';
+
+export interface DialogBoxProps {
+    width: number;
+    children: any;
+}
+
+export class DialogBox extends React.Component<DialogBoxProps, {}> {
+  render() {
+    return (
+      <div className='dialog-box' style={{width: this.props.width}}>
+        <div className='dialog-box--header'>
+          <img src={infoImage} />
+        </div>
+        <div className='dialog-box--content'>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/ui/stories/dialog-box.tsx
@@ -0,0 +1,19 @@
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+
+import * as React from 'react';
+import { DialogBox } from '../src/components/dialog-box/dialog-box';
+
+storiesOf('DialogBox', module)
+  .add('Basic', () => (
+    <DialogBox
+      width={480}
+    >
+      <div>
+        This is some content. It can be a string or more JSX.
+      </div>
+    </DialogBox>
+  ), {
+      info: { inline: true },
+      notes: 'This is a dialog box.',
+    });
