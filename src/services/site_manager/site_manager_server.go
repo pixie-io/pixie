b20b03d065203732343038fa4c5134b79c4c19e0
Zain Asgar <zasgar@pixielabs.ai>
2019-08-14 22:02:54 -0700

    PL-829 - Add datastore and migrations to site-manager
    Summary: Need to move migrations code to utils after we figure out the story there. Right now it migrates on start up.
    
    Test Plan: Manually tested
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1587
    
    GitOrigin-RevId: 7a7a415c338ec668f3ca9829a5cd0ef77543ce0d

--- src/services/site_manager/site_manager_server.go
+++ src/services/site_manager/site_manager_server.go
@@ -3,10 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+	bindata "github.com/golang-migrate/migrate/source/go_bindata"
 	log "github.com/sirupsen/logrus"
 	"pixielabs.ai/pixielabs/src/services/common"
 	"pixielabs.ai/pixielabs/src/services/common/healthz"
+	"pixielabs.ai/pixielabs/src/services/common/pg"
 	controllers "pixielabs.ai/pixielabs/src/services/site_manager/controller"
+	"pixielabs.ai/pixielabs/src/services/site_manager/datastore"
+	"pixielabs.ai/pixielabs/src/services/site_manager/schema"
 	"pixielabs.ai/pixielabs/src/services/site_manager/sitemanagerenv"
 	"pixielabs.ai/pixielabs/src/services/site_manager/sitemanagerpb"
 )
@@ -27,7 +33,31 @@ func main() {
 		log.WithError(err).Fatal("Failed to initialize env")
 	}
 
-	server, err := controllers.NewServer(env, nil)
+	db := pg.MustConnectDefaultPostgresDB()
+
+	// TODO(zasgar): Pull out this migration code into a util. Just leaving it here for now for testing.
+	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
+
+	sc := bindata.Resource(schema.AssetNames(), func(name string) (bytes []byte, e error) {
+		return schema.Asset(name)
+	})
+
+	d, err := bindata.WithInstance(sc)
+
+	mg, err := migrate.NewWithInstance(
+		"go-bindata",
+		d, "postgres", driver)
+
+	if err = mg.Up(); err != nil {
+		log.WithError(err).Info("migrations failed: %s", err)
+	}
+
+	datastore, err := datastore.NewDatastore(db)
+	if err != nil {
+		log.WithError(err).Fatalf("Failed to initialize datastore")
+	}
+
+	server, err := controllers.NewServer(env, datastore)
 	if err != nil {
 		log.WithError(err).Fatal("Failed to initialize GRPC server funcs")
 	}
