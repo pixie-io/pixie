d19e5889ff728145fe7c4a1d1bbc7a3745a38634
Zain Asgar <zasgar@pixielabs.ai>
2019-08-21 23:20:14 -0700

    Add vizier cloud connector
    Summary:
    The cloud connector on Vizier phones back the cloud. Right now it has not RPC server since it does not actually
    accept any incoming RPCs. We keep the webserver to respond to health checks.
    
    Test Plan: N/A
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1660
    
    GitOrigin-RevId: 05c8ed6c5e99b771b97dd50cf1842675aae7fa12

---
+++ src/vizier/services/cloud_connector/cloud_connector_server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"pixielabs.ai/pixielabs/src/shared/services"
+	"pixielabs.ai/pixielabs/src/shared/services/env"
+	"pixielabs.ai/pixielabs/src/shared/services/healthz"
+	"pixielabs.ai/pixielabs/src/shared/services/httpmiddleware"
+)
+
+func main() {
+	log.WithField("service", "cloud-connector").Info("Starting service")
+
+	services.SetupService("cloud-connector", 50300)
+	services.SetupSSLClientFlags()
+	services.PostFlagSetupAndParse()
+	services.CheckServiceFlags()
+	services.CheckSSLClientFlags()
+	services.SetupServiceLogging()
+
+	mux := http.NewServeMux()
+	healthz.RegisterDefaultChecks(mux)
+
+	// Connect to metadata service.
+	dialOpts, err := services.GetGRPCClientDialOpts()
+	if err != nil {
+		log.WithError(err).Fatal("Could not get dial opts.")
+	}
+	dialOpts = append(dialOpts, grpc.WithBlock())
+	e := env.New()
+	s := services.NewPLServer(e,
+		httpmiddleware.WithBearerAuthMiddleware(e, mux))
+	s.Start()
+	s.StopOnInterrupt()
+}
