41e3e656766e2294e5a7dc9eddf3bba2ec813c81
Zain Asgar <zasgar@pixielabs.ai>
2019-08-21 23:03:30 -0700

    Add vzmgr service
    Summary:
    This is the first pass at Vizier Manager service. This service is backed by postgres to maintain the
    current state of the vizier clusters. This service is simple for now just providing some stub methods.
    
    Test Plan: N/a
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1659
    
    GitOrigin-RevId: abaea6a5efd0dad34664595b148d4a1cba9be7f0

---
+++ src/cloud/vzmgr/vzmgr_server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"pixielabs.ai/pixielabs/src/cloud/vzmgr/vzmgrpb"
+
+	"pixielabs.ai/pixielabs/src/shared/services/env"
+
+	log "github.com/sirupsen/logrus"
+	"pixielabs.ai/pixielabs/src/shared/services"
+	"pixielabs.ai/pixielabs/src/shared/services/healthz"
+)
+
+func main() {
+	log.WithField("service", "vzmgr-service").Info("Starting service")
+
+	services.SetupService("vzmgr-service", 51500)
+	services.PostFlagSetupAndParse()
+	services.CheckServiceFlags()
+	services.SetupServiceLogging()
+
+	mux := http.NewServeMux()
+	healthz.RegisterDefaultChecks(mux)
+
+	s := services.NewPLServer(env.New(), mux)
+	vzmgrpb.RegisterVZMgrServiceServer(s.GRPCServer(), nil)
+
+	s.Start()
+	s.StopOnInterrupt()
+}
---
+++ src/cloud/vzmgr/vzmgrpb/mock.go
@@ -0,0 +1,3 @@
+package vzmgrpb
+
+//go:generate mockgen -source=service.pb.go -destination=mock/vzmgr_mock.gen.go
