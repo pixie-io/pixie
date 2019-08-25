4fef51b13ec590764e4af44d2b5273f0d2d9c617
Omid Azizi <oazizi@pixielabs.ai>
2019-08-25 01:16:35 -0700

    [Cleanup] Print warning message only once
    Summary: Also, logically cleaner to return . as PWD, instead of having a special if-case in PathToTestDataFile().
    
    Test Plan: No functional change, so rely on existing tests.
    
    Reviewers: zasgar, yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1675
    
    GitOrigin-RevId: fdec6cd5bce106c6b32a4902192291cd04c8562d

--- src/common/testing/test_environment.cc
+++ src/common/testing/test_environment.cc
@@ -10,21 +10,18 @@ constexpr char kPathSep[] = "/";
 std::string TestEnvironment::TestRunDir() {
   const char* test_src_dir = std::getenv("TEST_SRCDIR");
   const char* test_workspace = std::getenv("TEST_WORKSPACE");
-  constexpr char kErrMsg[] =
-      "Test environment variables not defined. Please make sure you are using bazel test, "
-      "or running from the ToT of the repo.";
+
   if (test_src_dir == nullptr || test_workspace == nullptr) {
-    LOG(WARNING) << kErrMsg;
-    return std::string();
+    LOG_FIRST_N(WARNING, 1) << "Test environment variables not defined. Make sure you are running "
+                               "from repo ToT, or use bazel test instead";
+
+    // Return PWD. User has been warned that test will only run properly when run from ToT.
+    return std::string(".");
   }
   return std::string(test_src_dir) + kPathSep + std::string(test_workspace);
 }
 
 std::string TestEnvironment::PathToTestDataFile(const std::string_view& fname) {
-  const std::string test_run_dir = TestRunDir();
-  if (test_run_dir.empty()) {
-    return std::string(fname);
-  }
   return TestRunDir() + kPathSep + std::string(fname);
 }
 
