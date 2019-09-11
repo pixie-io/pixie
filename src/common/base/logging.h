b04a71cbb39bf83c627ceea393382ef9976693a7
Omid Azizi <oazizi@pixielabs.ai>
2019-09-11 12:54:30 -0700

    More ECHECK flavors
    Summary:
    Provide the ECHECK_xx flavors (e.g. ECHECK_EQ).
    
    This was a little more complicated than the basic ECHECK(),
    and had to copy and modify some code from glog/logging.h.
    But tried to keep that to a minimum.
    
    It's not an exact match to what is in glog, so there may be future work.
    
    Test Plan: Corresponding tests added.
    
    Reviewers: zasgar, yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1794
    
    GitOrigin-RevId: b305e40fd1052d6509a0dd6453764e818a3f2d10

--- src/common/base/logging.h
+++ src/common/base/logging.h
@@ -15,9 +15,32 @@
 // DCHECK             FATAL           nothing
 // CHECK              FATAL           FATAL
 // ECHECK             FATAL           ERROR
+//
+// It is essentially a short-hand for LOG(DFATAL).
+
+// NOTE: Macros copied from glog/logging.h, and simplified.
+//       They may not be as optimal as those in glog.
+// TODO(oazizi): Consider optimizations (compare to glog).
 
 #define ECHECK(condition) \
   LOG_IF(DFATAL, GOOGLE_PREDICT_BRANCH_NOT_TAKEN(!(condition))) << "Check failed: " #condition " "
+
+// In optimized mode, CheckOpString provides to hint to compiler that
+// the if statement is unlikely.
+#define ECHECK_OP_LOG(name, op, val1, val2)                                                        \
+  if (google::CheckOpString _result =                                                              \
+          google::Check##name##Impl(google::GetReferenceableValue(val1),                           \
+                                    google::GetReferenceableValue(val2), #val1 " " #op " " #val2)) \
+  LOG(DFATAL) << *_result.str_
+
+#define ECHECK_OP(name, op, val1, val2) ECHECK_OP_LOG(name, op, val1, val2)
+
+#define ECHECK_EQ(val1, val2) ECHECK_OP(_EQ, ==, val1, val2)
+#define ECHECK_NE(val1, val2) ECHECK_OP(_NE, !=, val1, val2)
+#define ECHECK_LE(val1, val2) ECHECK_OP(_LE, <=, val1, val2)
+#define ECHECK_LT(val1, val2) ECHECK_OP(_LT, <, val1, val2)
+#define ECHECK_GE(val1, val2) ECHECK_OP(_GE, >=, val1, val2)
+#define ECHECK_GT(val1, val2) ECHECK_OP(_GT, >, val1, val2)
 
 namespace pl {
 
--- src/common/base/logging_test.cc
+++ src/common/base/logging_test.cc
@@ -4,15 +4,62 @@
 
 namespace pl {
 
-TEST(ECheckTest, check_true) { ECHECK(true); }
+TEST(ECheckTest, check_true) {
+  ECHECK(true);
+
+  ECHECK_EQ(1, 1);
+
+  ECHECK_NE(1, 2);
+  ECHECK_NE(2, 1);
+
+  ECHECK_LE(1, 1);
+  ECHECK_LE(1, 2);
+
+  ECHECK_LT(1, 2);
+
+  ECHECK_GE(1, 1);
+  ECHECK_GE(2, 1);
+
+  ECHECK_GT(2, 1);
+}
 
 TEST(ECheckTest, check_false) {
-// Behavior changes based on build type.
-// Relying on build system (Jenkins) to stress both cases.
+  // Behavior changes based on build type.
+  // Rely on build system (Jenkins) to stress both cases.
 #if DCHECK_IS_ON()
   EXPECT_DEATH(ECHECK(false), "");
+
+  EXPECT_DEATH(ECHECK_EQ(1, 2), "");
+  EXPECT_DEATH(ECHECK_EQ(2, 1), "");
+
+  EXPECT_DEATH(ECHECK_NE(1, 1), "");
+
+  EXPECT_DEATH(ECHECK_LE(2, 1), "");
+
+  EXPECT_DEATH(ECHECK_LT(1, 1), "");
+  EXPECT_DEATH(ECHECK_LT(2, 1), "");
+
+  EXPECT_DEATH(ECHECK_GE(1, 2), "");
+
+  EXPECT_DEATH(ECHECK_GT(1, 1), "");
+  EXPECT_DEATH(ECHECK_GT(1, 2), "");
 #else
   ECHECK(false);
+
+  ECHECK_EQ(1, 2);
+  ECHECK_EQ(2, 1);
+
+  ECHECK_NE(1, 1);
+
+  ECHECK_LE(2, 1);
+
+  ECHECK_LT(1, 1);
+  ECHECK_LT(2, 1);
+
+  ECHECK_GE(1, 2);
+
+  ECHECK_GT(1, 1);
+  ECHECK_GT(1, 2);
 #endif
 }
 
