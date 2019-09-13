37f5ce291f9a7a95c9257b60b8ba4bae19cff068
Zain Asgar <zasgar@pixielabs.ai>
2019-09-13 14:27:38 -0700

    Use correct buildifier binary
    Summary: We were use a wrapper script that just linted all our build files. This makes sure that .arclint excludes work properly.
    
    Test Plan: `arc lint --everything`
    
    Reviewers: nserrino, philkuz, #engineering
    
    Reviewed By: philkuz, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1834
    
    GitOrigin-RevId: 6248927466a8b73c1019b2ff747c7e2500961554

--- linters/buildifier.sh
+++ linters/buildifier.sh
@@ -6,7 +6,7 @@ workspace=$(bazel info workspace)
 buildifier_args="--path=${workspace} ${workspace}/$1"
 
 # Run lint so that arc can get results.
-bazel run --direct_run //:buildifier -- --mode=check ${buildifier_args}
+bazel run --direct_run @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=check ${buildifier_args}
 
 # Auto-fix the build files.
-bazel run --direct_run //:buildifier -- --mode=fix ${buildifier_args}
+bazel run --direct_run @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=fix ${buildifier_args}
