532af7889fee5f75ec3b4792d7b0d19f89feba6f
Zain Asgar <zasgar@pixielabs.ai>
2019-09-16 14:51:18 -0700

    Buildifier only needs to run once to fix the file
    Summary: This way it won't error out if there is a lint error.
    
    Test Plan: N/A
    
    Reviewers: michelle, nserrino, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1853
    
    GitOrigin-RevId: 938a5ac296b194b53c1775d3fb865097e20acc54

--- linters/buildifier.sh
+++ linters/buildifier.sh
@@ -5,8 +5,5 @@ set -e
 workspace=$(bazel info workspace)
 buildifier_args="--path=${workspace} ${workspace}/$1"
 
-# Run lint so that arc can get results.
-bazel run --direct_run @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=check ${buildifier_args}
-
 # Auto-fix the build files.
 bazel run --direct_run @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=fix ${buildifier_args}
