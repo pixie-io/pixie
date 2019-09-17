379671acb613f4bc5ad94e9629da3f36e04bd913
Phillip Kuznetsov <philkuz@pixielabs.ai>
2019-09-16 20:23:53 -0700

    [Small diff] Adding UPIDFromString to the upid utils
    Summary: TSIA
    
    Test Plan: add new test.
    
    Reviewers: michelle, zasgar, nserrino, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1869
    
    GitOrigin-RevId: 2ba791927b4ea08fe2702c3a57fcd4e1f210a267

--- src/shared/k8s/upid.go
+++ src/shared/k8s/upid.go
@@ -2,7 +2,10 @@ package k8s
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"pixielabs.ai/pixielabs/src/shared/types"
 )
 
@@ -24,4 +27,33 @@ func StartTSFromUPID(u *types.UInt128) uint64 {
 // StringFromUPID gets a string from the UPID.
 func StringFromUPID(u *types.UInt128) string {
 	return fmt.Sprintf("%d:%d:%d", ASIDFromUPID(u), PIDFromUPID(u), StartTSFromUPID(u))
+}
+
+// UPIDFromString converts a upid in a string into a UINT128. Returns an error if string is malformed.
+func UPIDFromString(upidString string) (types.UInt128, error) {
+	upid := types.UInt128{}
+	splitUpid := strings.Split(upidString, ":")
+	if len(splitUpid) != 3 {
+		return upid, fmt.Errorf("UPID string malformed: '%s'", upidString)
+	}
+	asid, err := strconv.Atoi(splitUpid[0])
+	if err != nil {
+		log.WithError(err).Error("Could not unmarshal upid string.")
+		return upid, err
+	}
+	pid, err := strconv.Atoi(splitUpid[1])
+	if err != nil {
+		log.WithError(err).Error("Could not unmarshal upid string.")
+		return upid, err
+	}
+	ts, err := strconv.Atoi(splitUpid[2])
+	if err != nil {
+		log.WithError(err).Error("Could not unmarshal upid string.")
+		return upid, err
+	}
+	upid.High = uint64(asid)<<32 + uint64(pid)
+	upid.Low = uint64(ts)
+
+	return upid, nil
+
 }
--- src/shared/k8s/upid_test.go
+++ src/shared/k8s/upid_test.go
@@ -20,3 +20,21 @@ func TestUPID(t *testing.T) {
 	assert.Equal(t, uint64(89101), k8s.StartTSFromUPID(upid))
 	assert.Equal(t, "123:567:89101", k8s.StringFromUPID(upid))
 }
+
+func TestUPIDFromString(t *testing.T) {
+	upid := &types.UInt128{
+		Low:  uint64(89101),
+		High: uint64(528280977975),
+	}
+
+	upidString := k8s.StringFromUPID(upid)
+
+	assert.Equal(t, "123:567:89101", upidString)
+
+	upidObj, err := k8s.UPIDFromString(upidString)
+	if !assert.Nil(t, err) {
+		t.FailNow()
+	}
+	assert.Equal(t, *upid, upidObj)
+
+}
