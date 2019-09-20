18640bf1bb308aaa6e82adf493025d07bd44e323
Phillip Kuznetsov <philkuz@pixielabs.ai>
2019-09-19 18:58:01 -0700

    Adding udf exporter test and pretty print for udfs.
    Summary: TSIA
    
    Test Plan: new udf exporter test.
    
    Reviewers: michelle, zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1911
    
    GitOrigin-RevId: ba14649e9b9fa1324845a2b59e772222c74c6ddf

--- src/carnot/udf_exporter/udf_exporter.h
+++ src/carnot/udf_exporter/udf_exporter.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <memory>
+#include <string>
+#include <vector>
 
 #include "src/carnot/compiler/registry_info.h"
 #include "src/carnot/funcs/builtins/builtins.h"
@@ -9,6 +11,13 @@ namespace pl {
 namespace carnot {
 namespace udfexporter {
 /**
+ * @brief GetUDFProto returns the proto for all of the udfs
+ *
+ * @return udfspb::UDFInfo: returns a proto that describes all of the functions in the repo.
+ */
+udfspb::UDFInfo GetUDFProto();
+
+/**
  * @brief ExportUDFInfo() setups a RegistryInfo using all of the definitions that are
  * defined in the builtins. This greatly simplifies the need to carry around huge protobuf
  * strings in tests and other places, replacing it with a simple one line function call.
@@ -17,6 +26,13 @@ namespace udfexporter {
  * info.
  */
 StatusOr<std::unique_ptr<compiler::RegistryInfo>> ExportUDFInfo();
+
+/**
+ * @brief Print out the udf registry in a nice format.
+ *
+ * @return std::string: the udf registry in a nice format.
+ */
+std::string UDFProtoFormatString();
 
 }  // namespace udfexporter
 }  // namespace carnot
---
+++ src/carnot/udf_exporter/udf_exporter_test.cc
@@ -0,0 +1,49 @@
+#include <gmock/gmock.h>
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/util/message_differencer.h>
+#include <gtest/gtest.h>
+
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+#include <pypa/parser/parser.hh>
+
+#include "src/carnot/udf_exporter/udf_exporter.h"
+#include "src/shared/types/proto/types.pb.h"
+
+namespace pl {
+namespace carnot {
+namespace udfexporter {
+using ::testing::_;
+
+class UDFExporterTest : public ::testing::Test {};
+
+TEST_F(UDFExporterTest, udf_proto) {
+  // Sanity checks in lieu of full tests.
+  auto registry_info = ExportUDFInfo().ConsumeValueOrDie();
+
+  // Contains a math op.
+  auto udf_status = registry_info->GetUDF("pl.add", {types::FLOAT64, types::FLOAT64});
+  EXPECT_OK(udf_status);
+  EXPECT_TRUE(udf_status.ConsumeValueOrDie() == types::FLOAT64);
+
+  // Contains a string op.
+  udf_status = registry_info->GetUDF("pl.pluck", {types::STRING, types::STRING});
+  EXPECT_OK(udf_status);
+  EXPECT_TRUE(udf_status.ConsumeValueOrDie() == types::STRING);
+
+  // Contains metadata ops.
+  udf_status = registry_info->GetUDF("pl.upid_to_pod_id", {types::UINT128});
+  EXPECT_OK(udf_status);
+  EXPECT_TRUE(udf_status.ConsumeValueOrDie() == types::STRING);
+
+  // Contains aggregate ops.
+  auto uda_status = registry_info->GetUDA("pl.count", {types::BOOLEAN});
+  EXPECT_OK(uda_status);
+  EXPECT_TRUE(uda_status.ConsumeValueOrDie() == types::INT64);
+}
+
+}  // namespace udfexporter
+}  // namespace carnot
+}  // namespace pl
