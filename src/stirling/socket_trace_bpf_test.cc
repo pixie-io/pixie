94e97c4639327abd61e8e70a381e29fed89da4f4
Omid Azizi <oazizi@pixielabs.ai>
2019-09-23 17:43:57 -0700

    Moving ClientServerSystem
    Summary:
    Refactoring to testing for two reasons:
    1) It's cleaner, as it's really a test stimulus rather than an integral part of the test.
    2) I may need it for other use cases outside of socket_trace_bpf_test
    
    Test Plan: Existing tests
    
    Reviewers: yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D1973
    
    GitOrigin-RevId: 3db37739da8e99504b4593efd51517f4a9376572

--- src/stirling/socket_trace_bpf_test.cc
+++ src/stirling/socket_trace_bpf_test.cc
@@ -14,6 +14,7 @@
 #include "src/stirling/mysql/test_data.h"
 #include "src/stirling/mysql/test_utils.h"
 #include "src/stirling/socket_trace_connector.h"
+#include "src/stirling/testing/client_server_system.h"
 #include "src/stirling/testing/tcp_socket.h"
 
 namespace pl {
@@ -24,8 +25,6 @@ using ::pl::types::ColumnWrapper;
 using ::pl::types::ColumnWrapperRecordBatch;
 using ::testing::StrEq;
 using ::testing::UnorderedElementsAre;
-
-using SendRecvScript = std::vector<std::vector<std::string_view>>;
 
 class SocketTraceBPFTest : public ::testing::Test {
  protected:
@@ -43,225 +42,6 @@ class SocketTraceBPFTest : public ::testing::Test {
 
   void TearDown() override { ASSERT_OK(source_->Stop()); }
 
-  class ClientServerSystem {
-   public:
-    ClientServerSystem() { server_.Bind(); }
-
-    /**
-     * Create and run a client-server system with the provided send-recv script.
-     *
-     * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
-     * &TCPSocket::Read).
-     * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::Send,
-     * &TCPSocket::Write).
-     * @param script: script that client server use to send messages to each other.
-     */
-    template <bool (TCPSocket::*TRecvFn)(std::string*) const = &TCPSocket::Recv,
-              ssize_t (TCPSocket::*TSendFn)(std::string_view) const = &TCPSocket::Send>
-    void RunClientServer(const SendRecvScript& script) {
-      SpawnClient<TRecvFn, TSendFn>(script);
-      SpawnServer<TRecvFn, TSendFn>(script);
-      JoinThreads();
-    }
-
-    /**
-     * Create and run a client-server system with server writing data to client.
-     *
-     * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
-     * &TCPSocket::Read).
-     * @tparam TSendFn: choose send implementation from TCPSocket (only &TCPSocket::ReadV is valid).
-     * @param write_data: data that server will send to client.
-     */
-    template <ssize_t (TCPSocket::*TRecvFn)(std::string* data) const = &TCPSocket::ReadV,
-              ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&)
-                  const = &TCPSocket::SendMsg>
-    void RunClientServer(const std::vector<std::vector<std::string_view>>& write_data) {
-      SpawnMsgClient<TRecvFn>();
-      SpawnMsgServer<TSendFn>(write_data);
-      JoinThreads();
-    }
-
-    /**
-     * Create and run a client-server system with server writing data to client.
-     *
-     * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
-     * &TCPSocket::Read).
-     * @tparam TSendFn: choose send implementation from TCPSocket (only &TCPSocket::ReadMsg is
-     * valid).
-     * @param write_data: data that server will send to client.
-     */
-    template <ssize_t (TCPSocket::*TRecvFn)(std::vector<std::string>* data)
-                  const = &TCPSocket::RecvMsg,
-              ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&)
-                  const = &TCPSocket::SendMsg>
-    void RunClientServer(const std::vector<std::vector<std::string_view>>& write_data) {
-      SpawnMsgClient<TRecvFn>();
-      SpawnMsgServer<TSendFn>(write_data);
-      JoinThreads();
-    }
-
-   private:
-    /**
-     * Wrapper around TCPSocket Send/Write functionality.
-     * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::Send,
-     * &TCPSocket::Write).
-     */
-    template <ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
-    static void SendData(const TCPSocket& socket, const std::vector<std::string_view>& data) {
-      for (auto d : data) {
-        ASSERT_EQ(d.length(), (socket.*TSendFn)(d));
-      }
-    }
-
-    /**
-     * Wrapper around TCPSocket Recv/Read functionality.
-     * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
-     * &TCPSocket::Read).
-     */
-    template <bool (TCPSocket::*TRecvFn)(std::string*) const>
-    static void RecvData(const TCPSocket& socket, size_t expected_size) {
-      std::string msg;
-      size_t s = 0;
-      while (s < expected_size) {
-        (socket.*TRecvFn)(&msg);
-        s += msg.size();
-      }
-    }
-
-    /**
-     * Wrapper around TCPSocket SendMsg/WriteV functionality.
-     * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::SendMsg,
-     * &TCPSocket::WriteV).
-     */
-    template <ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&) const>
-    static void SendData(const TCPSocket& socket,
-                         const std::vector<std::vector<std::string_view>>& write_data) {
-      std::string msg;
-      for (const auto& data : write_data) {
-        (socket.*TSendFn)(data);
-      }
-    }
-
-    /**
-     * Wrapper around TCPSocket ReadV functionality.
-     * @tparam TSendFn: choose receive implementation from TCPSocket (only &TCPSocket::ReadV is
-     * valid).
-     */
-    template <ssize_t (TCPSocket::*TRecvFn)(std::string* data) const>
-    static void RecvData(const TCPSocket& socket) {
-      std::string msg;
-      while ((socket.*TRecvFn)(&msg) > 0) {
-      }
-    }
-
-    /**
-     * Wrapper around TCPSocket ReadMsg functionality.
-     * @tparam TSendFn: choose receive implementation from TCPSocket (only &TCPSocket::ReadMsg is
-     * valid).
-     */
-    template <ssize_t (TCPSocket::*TRecvFn)(std::vector<std::string>* data) const>
-    static void RecvData(const TCPSocket& socket) {
-      std::vector<std::string> msgs;
-      while ((socket.*TRecvFn)(&msgs) > 0) {
-      }
-    }
-
-    /**
-     * Run the script in an alternating order, client sends and server receives during even phase,
-     * and vice versa.
-     * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
-     * &TCPSocket::Read).
-     * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::Send,
-     * &TCPSocket::Write).
-     * @param socket: client or server socket.
-     * @param is_client: whether it's the client or server.
-     */
-    template <bool (TCPSocket::*TRecvFn)(std::string*) const,
-              ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
-    void Run(const SendRecvScript& script, const TCPSocket& socket, bool is_client) {
-      size_t phase = 0;
-
-      while (phase != script.size()) {
-        // phase%2 == 0 |  client   | is_sender
-        //      1       |     1     |  1
-        //      1       |     0     |  0
-        //      0       |     0     |  1
-        //      0       |     1     |  0
-        if (!((phase % 2 == 0) ^ is_client)) {
-          SendData<TSendFn>(socket, script[phase]);
-          phase++;
-        } else {
-          size_t expected_size = 0;
-          for (size_t i = 0; i < script[phase].size(); ++i) {
-            expected_size += script[phase][i].size();
-          }
-          RecvData<TRecvFn>(socket, expected_size);
-          phase++;
-        }
-      }
-    }
-
-    template <bool (TCPSocket::*TRecvFn)(std::string*) const,
-              ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
-    void SpawnClient(const SendRecvScript& script) {
-      client_thread_ = std::thread([this, script]() {
-        client_.Connect(server_);
-        Run<TRecvFn, TSendFn>(script, client_, true);
-        client_.Close();
-      });
-    }
-
-    template <bool (TCPSocket::*TRecvFn)(std::string*) const,
-              ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
-    void SpawnServer(const SendRecvScript& script) {
-      server_thread_ = std::thread([this, script]() {
-        server_.Accept();
-        Run<TRecvFn, TSendFn>(script, server_, false);
-        server_.Close();
-      });
-    }
-
-    // TODO(chengruizhe): Currently SendMsg/RecvMsg & WriteV/ReadV doesn't follow the script and
-    // multi-round communication model. Fix this.
-    template <ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&) const>
-    void SpawnMsgServer(const std::vector<std::vector<std::string_view>>& write_data) {
-      server_thread_ = std::thread([this, write_data]() {
-        server_.Accept();
-        SendData<TSendFn>(server_, write_data);
-        server_.Close();
-      });
-    }
-
-    template <ssize_t (TCPSocket::*TRecvFn)(std::string* data) const>
-    void SpawnMsgClient() {
-      client_thread_ = std::thread([this]() {
-        client_.Connect(server_);
-        RecvData<TRecvFn>(client_);
-        client_.Close();
-      });
-    }
-
-    template <ssize_t (TCPSocket::*TRecvFn)(std::vector<std::string>* data) const>
-    void SpawnMsgClient() {
-      client_thread_ = std::thread([this]() {
-        client_.Connect(server_);
-        RecvData<TRecvFn>(client_);
-        client_.Close();
-      });
-    }
-
-    void JoinThreads() {
-      server_thread_.join();
-      client_thread_.join();
-    }
-
-    TCPSocket client_;
-    TCPSocket server_;
-
-    std::thread client_thread_;
-    std::thread server_thread_;
-  };
-
   std::string kStmtPrepareReq;
   std::vector<std::string> StmtPrepareResp;
   std::string kStmtExecuteReq;
@@ -269,8 +49,8 @@ class SocketTraceBPFTest : public ::testing::Test {
   std::string kQueryReq;
   std::vector<std::string> QueryResp;
 
-  SendRecvScript GetPrepareExecuteScript() {
-    SendRecvScript prepare_execute_script;
+  testing::SendRecvScript GetPrepareExecuteScript() {
+    testing::SendRecvScript prepare_execute_script;
 
     // Stmt Prepare
     kStmtPrepareReq = mysql::testutils::GenRawPacket(
@@ -306,8 +86,8 @@ class SocketTraceBPFTest : public ::testing::Test {
     return prepare_execute_script;
   }
 
-  SendRecvScript GetQueryScript() {
-    SendRecvScript query_script;
+  testing::SendRecvScript GetQueryScript() {
+    testing::SendRecvScript query_script;
 
     kQueryReq = mysql::testutils::GenRawPacket(
         0, mysql::testutils::GenStringRequest(mysql::testutils::kQueryRequest,
@@ -373,13 +153,13 @@ Content-Length: 0
 TEST_F(SocketTraceBPFTest, TestFramework) {
   ConfigureCapture(kProtocolHTTP, kRoleRequestor | kRoleResponder);
 
-  SendRecvScript script({
+  testing::SendRecvScript script({
       {kHTTPReqMsg1},
       {kHTTPRespMsg1},
       {kHTTPReqMsg2},
       {kHTTPRespMsg2},
   });
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>(script);
 
   DataTable data_table(kHTTPTable);
@@ -396,7 +176,7 @@ TEST_F(SocketTraceBPFTest, TestFramework) {
 TEST_F(SocketTraceBPFTest, TestWriteRespCapture) {
   ConfigureCapture(kProtocolHTTP, kRoleResponder);
 
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>({{kHTTPRespMsg1, kHTTPRespMsg2}});
 
   {
@@ -447,7 +227,7 @@ TEST_F(SocketTraceBPFTest, TestWriteRespCapture) {
 TEST_F(SocketTraceBPFTest, TestSendRespCapture) {
   ConfigureCapture(TrafficProtocol::kProtocolHTTP, kRoleResponder);
 
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Recv, &TCPSocket::Send>({{kHTTPRespMsg1, kHTTPRespMsg2}});
 
   {
@@ -489,7 +269,7 @@ TEST_F(SocketTraceBPFTest, TestSendRespCapture) {
 TEST_F(SocketTraceBPFTest, TestReadRespCapture) {
   ConfigureCapture(TrafficProtocol::kProtocolHTTP, kRoleRequestor);
 
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>({{kHTTPRespMsg1, kHTTPRespMsg2}});
 
   {
@@ -531,7 +311,7 @@ TEST_F(SocketTraceBPFTest, TestReadRespCapture) {
 TEST_F(SocketTraceBPFTest, TestRecvRespCapture) {
   ConfigureCapture(TrafficProtocol::kProtocolHTTP, kRoleRequestor);
 
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Recv, &TCPSocket::Send>({{kHTTPRespMsg1, kHTTPRespMsg2}});
 
   {
@@ -574,7 +354,7 @@ TEST_F(SocketTraceBPFTest, TestEnd2EndMySQLPrepareExecute) {
   FLAGS_stirling_enable_mysql_tracing = true;
 
   ConfigureCapture(TrafficProtocol::kProtocolMySQL, kRoleRequestor);
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>(GetPrepareExecuteScript());
 
   // Check that HTTP table did not capture any data.
@@ -613,7 +393,7 @@ TEST_F(SocketTraceBPFTest, TestEnd2EndMySQLQuery) {
   FLAGS_stirling_enable_mysql_tracing = true;
 
   ConfigureCapture(TrafficProtocol::kProtocolMySQL, kRoleRequestor);
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>(GetQueryScript());
 
   // Check that HTTP table did not capture any data.
@@ -646,7 +426,7 @@ TEST_F(SocketTraceBPFTest, TestNoProtocolWritesNotCaptured) {
   ConfigureCapture(TrafficProtocol::kProtocolHTTP, kRoleRequestor | kRoleResponder);
   ConfigureCapture(TrafficProtocol::kProtocolMySQL, kRoleRequestor);
 
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>(
       {{kNoProtocolMsg, "", kNoProtocolMsg, ""}});
 
@@ -679,10 +459,10 @@ TEST_F(SocketTraceBPFTest, TestMultipleConnections) {
   ConfigureCapture(TrafficProtocol::kProtocolHTTP, kRoleRequestor);
 
   // Two separate connections.
-  ClientServerSystem system1;
+  testing::ClientServerSystem system1;
   system1.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>({{kHTTPRespMsg1}});
 
-  ClientServerSystem system2;
+  testing::ClientServerSystem system2;
   system2.RunClientServer<&TCPSocket::Read, &TCPSocket::Write>({{kHTTPRespMsg2}});
 
   {
@@ -712,7 +492,7 @@ TEST_F(SocketTraceBPFTest, TestMultipleConnections) {
 TEST_F(SocketTraceBPFTest, TestStartTime) {
   ConfigureCapture(TrafficProtocol::kProtocolHTTP, kRoleRequestor);
 
-  ClientServerSystem system;
+  testing::ClientServerSystem system;
   system.RunClientServer<&TCPSocket::Recv, &TCPSocket::Send>({{kHTTPRespMsg1, kHTTPRespMsg2}});
 
   // Kernel uses monotonic clock as start_time, so we must do the same.
@@ -755,7 +535,7 @@ class SyscallPairBPFTest : public SocketTraceBPFTest,
 
 TEST_P(SyscallPairBPFTest, EventsAreCaptured) {
   ConfigureCapture(kProtocolHTTP, kRoleRequestor | kRoleResponder);
-  ClientServerSystem system({});
+  testing::ClientServerSystem system({});
   const std::vector<std::vector<std::string_view>> data = {
       {"HTTP/1.1 200 OK\r\n", "Content-Type: json\r\n", "Content-Length: 1\r\n\r\na"},
       {"HTTP/1.1 404 Not Found\r\n", "Content-Type: json\r\n", "Content-Length: 2\r\n\r\nbc"}};
---
+++ src/stirling/testing/client_server_system.h
@@ -0,0 +1,235 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "src/stirling/testing/tcp_socket.h"
+
+namespace pl {
+namespace stirling {
+namespace testing {
+
+using SendRecvScript = std::vector<std::vector<std::string_view>>;
+
+class ClientServerSystem {
+ public:
+  ClientServerSystem() { server_.Bind(); }
+
+  /**
+   * Create and run a client-server system with the provided send-recv script.
+   *
+   * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
+   * &TCPSocket::Read).
+   * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::Send,
+   * &TCPSocket::Write).
+   * @param script: script that client server use to send messages to each other.
+   */
+  template <bool (TCPSocket::*TRecvFn)(std::string*) const = &TCPSocket::Recv,
+            ssize_t (TCPSocket::*TSendFn)(std::string_view) const = &TCPSocket::Send>
+  void RunClientServer(const SendRecvScript& script) {
+    SpawnClient<TRecvFn, TSendFn>(script);
+    SpawnServer<TRecvFn, TSendFn>(script);
+    JoinThreads();
+  }
+
+  /**
+   * Create and run a client-server system with server writing data to client.
+   *
+   * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
+   * &TCPSocket::Read).
+   * @tparam TSendFn: choose send implementation from TCPSocket (only &TCPSocket::ReadV is valid).
+   * @param write_data: data that server will send to client.
+   */
+  template <ssize_t (TCPSocket::*TRecvFn)(std::string* data) const = &TCPSocket::ReadV,
+            ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&)
+                const = &TCPSocket::SendMsg>
+  void RunClientServer(const std::vector<std::vector<std::string_view>>& write_data) {
+    SpawnMsgClient<TRecvFn>();
+    SpawnMsgServer<TSendFn>(write_data);
+    JoinThreads();
+  }
+
+  /**
+   * Create and run a client-server system with server writing data to client.
+   *
+   * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
+   * &TCPSocket::Read).
+   * @tparam TSendFn: choose send implementation from TCPSocket (only &TCPSocket::ReadMsg is
+   * valid).
+   * @param write_data: data that server will send to client.
+   */
+  template <ssize_t (TCPSocket::*TRecvFn)(std::vector<std::string>* data)
+                const = &TCPSocket::RecvMsg,
+            ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&)
+                const = &TCPSocket::SendMsg>
+  void RunClientServer(const std::vector<std::vector<std::string_view>>& write_data) {
+    SpawnMsgClient<TRecvFn>();
+    SpawnMsgServer<TSendFn>(write_data);
+    JoinThreads();
+  }
+
+ private:
+  /**
+   * Wrapper around TCPSocket Send/Write functionality.
+   * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::Send,
+   * &TCPSocket::Write).
+   */
+  template <ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
+  static void SendData(const TCPSocket& socket, const std::vector<std::string_view>& data) {
+    for (auto d : data) {
+      ASSERT_EQ(d.length(), (socket.*TSendFn)(d));
+    }
+  }
+
+  /**
+   * Wrapper around TCPSocket Recv/Read functionality.
+   * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
+   * &TCPSocket::Read).
+   */
+  template <bool (TCPSocket::*TRecvFn)(std::string*) const>
+  static void RecvData(const TCPSocket& socket, size_t expected_size) {
+    std::string msg;
+    size_t s = 0;
+    while (s < expected_size) {
+      (socket.*TRecvFn)(&msg);
+      s += msg.size();
+    }
+  }
+
+  /**
+   * Wrapper around TCPSocket SendMsg/WriteV functionality.
+   * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::SendMsg,
+   * &TCPSocket::WriteV).
+   */
+  template <ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&) const>
+  static void SendData(const TCPSocket& socket,
+                       const std::vector<std::vector<std::string_view>>& write_data) {
+    std::string msg;
+    for (const auto& data : write_data) {
+      (socket.*TSendFn)(data);
+    }
+  }
+
+  /**
+   * Wrapper around TCPSocket ReadV functionality.
+   * @tparam TSendFn: choose receive implementation from TCPSocket (only &TCPSocket::ReadV is
+   * valid).
+   */
+  template <ssize_t (TCPSocket::*TRecvFn)(std::string* data) const>
+  static void RecvData(const TCPSocket& socket) {
+    std::string msg;
+    while ((socket.*TRecvFn)(&msg) > 0) {
+    }
+  }
+
+  /**
+   * Wrapper around TCPSocket ReadMsg functionality.
+   * @tparam TSendFn: choose receive implementation from TCPSocket (only &TCPSocket::ReadMsg is
+   * valid).
+   */
+  template <ssize_t (TCPSocket::*TRecvFn)(std::vector<std::string>* data) const>
+  static void RecvData(const TCPSocket& socket) {
+    std::vector<std::string> msgs;
+    while ((socket.*TRecvFn)(&msgs) > 0) {
+    }
+  }
+
+  /**
+   * Run the script in an alternating order, client sends and server receives during even phase,
+   * and vice versa.
+   * @tparam TRecvFn: choose receive implementation from TCPSocket (&TCPSocket::Recv,
+   * &TCPSocket::Read).
+   * @tparam TSendFn: choose send implementation from TCPSocket (&TCPSocket::Send,
+   * &TCPSocket::Write).
+   * @param socket: client or server socket.
+   * @param is_client: whether it's the client or server.
+   */
+  template <bool (TCPSocket::*TRecvFn)(std::string*) const,
+            ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
+  void Run(const SendRecvScript& script, const TCPSocket& socket, bool is_client) {
+    size_t phase = 0;
+
+    while (phase != script.size()) {
+      // phase%2 == 0 |  client   | is_sender
+      //      1       |     1     |  1
+      //      1       |     0     |  0
+      //      0       |     0     |  1
+      //      0       |     1     |  0
+      if (!((phase % 2 == 0) ^ is_client)) {
+        SendData<TSendFn>(socket, script[phase]);
+        phase++;
+      } else {
+        size_t expected_size = 0;
+        for (size_t i = 0; i < script[phase].size(); ++i) {
+          expected_size += script[phase][i].size();
+        }
+        RecvData<TRecvFn>(socket, expected_size);
+        phase++;
+      }
+    }
+  }
+
+  template <bool (TCPSocket::*TRecvFn)(std::string*) const,
+            ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
+  void SpawnClient(const SendRecvScript& script) {
+    client_thread_ = std::thread([this, script]() {
+      client_.Connect(server_);
+      Run<TRecvFn, TSendFn>(script, client_, true);
+      client_.Close();
+    });
+  }
+
+  template <bool (TCPSocket::*TRecvFn)(std::string*) const,
+            ssize_t (TCPSocket::*TSendFn)(std::string_view) const>
+  void SpawnServer(const SendRecvScript& script) {
+    server_thread_ = std::thread([this, script]() {
+      server_.Accept();
+      Run<TRecvFn, TSendFn>(script, server_, false);
+      server_.Close();
+    });
+  }
+
+  // TODO(chengruizhe): Currently SendMsg/RecvMsg & WriteV/ReadV doesn't follow the script and
+  // multi-round communication model. Fix this.
+  template <ssize_t (TCPSocket::*TSendFn)(const std::vector<std::string_view>&) const>
+  void SpawnMsgServer(const std::vector<std::vector<std::string_view>>& write_data) {
+    server_thread_ = std::thread([this, write_data]() {
+      server_.Accept();
+      SendData<TSendFn>(server_, write_data);
+      server_.Close();
+    });
+  }
+
+  template <ssize_t (TCPSocket::*TRecvFn)(std::string* data) const>
+  void SpawnMsgClient() {
+    client_thread_ = std::thread([this]() {
+      client_.Connect(server_);
+      RecvData<TRecvFn>(client_);
+      client_.Close();
+    });
+  }
+
+  template <ssize_t (TCPSocket::*TRecvFn)(std::vector<std::string>* data) const>
+  void SpawnMsgClient() {
+    client_thread_ = std::thread([this]() {
+      client_.Connect(server_);
+      RecvData<TRecvFn>(client_);
+      client_.Close();
+    });
+  }
+
+  void JoinThreads() {
+    server_thread_.join();
+    client_thread_.join();
+  }
+
+  TCPSocket client_;
+  TCPSocket server_;
+
+  std::thread client_thread_;
+  std::thread server_thread_;
+};
+
+}  // namespace testing
+}  // namespace stirling
+}  // namespace pl
