88c810c9ded68b108a5656ee6128a9b37d2d2803
Zain Asgar <zasgar@pixielabs.ai>
2019-09-30 23:09:10 -0700

    Update load balancer to accept GRPC traffic for creds.
    Summary: This adds the correct http/2 path to the LoadBalancer.
    
    Test Plan: Test on staging.
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2124
    
    GitOrigin-RevId: 43e32cce4d70d34310db49775258f3060eb2aeec

--- src/cloud/api/api_server.go
+++ src/cloud/api/api_server.go
@@ -17,8 +17,6 @@ import (
 
 func init() {
 	pflag.String("domain_name", "dev.withpixie.dev", "The domain name of Pixie Cloud")
-	pflag.String("vizier_image_secret_path", "/vizier-image-secret", "[WORKAROUND] The path the the image secrets")
-	pflag.String("vizier_image_secret_file", "vizier_image_secret.json", "[WORKAROUND] The image secret file")
 }
 
 func main() {
