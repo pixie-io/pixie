964456942515bdc368578b1cc52dd4de5ede53e1
Michelle Nguyen <michelle@pixielabs.ai>
2019-10-10 13:31:31 -0700

    PL-746 Add utility for etcd leader election
    Summary:
    We will want to add leader election to the metadata service, since we will want to have replicas of the service in the future.
    this diff just adds a utility which uses etcd's leader election module to do leader election.
    this diff doesnt update the metadata service to user leader election yet.
    
    Test Plan: unit test
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2232
    
    GitOrigin-RevId: 7bdbb23f9a2075a8db345d656e9a0698caccf6c0

---
+++ src/vizier/services/metadata/controllers/etcd/leader_election.go
@@ -0,0 +1,75 @@
+package etcd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/coreos/etcd/clientv3/concurrency"
+	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+var electionKey = "/election"
+var electionPollTime = 1 * time.Second
+
+// LeaderElection is an object which tracks an election in etcd.
+type LeaderElection struct {
+	id       uuid.UUID
+	session  *concurrency.Session
+	election *concurrency.Election
+	quitCh   chan bool
+}
+
+// NewLeaderElection starts a new leader election.
+func NewLeaderElection(session *concurrency.Session) *LeaderElection {
+	id := uuid.NewV4()
+	election := concurrency.NewElection(session, electionKey)
+	quitCh := make(chan bool)
+
+	return &LeaderElection{id, session, election, quitCh}
+}
+
+// RunElection starts a loop where the leaderElection actively attempts to become leader.
+func (l *LeaderElection) RunElection() {
+	for {
+		select {
+		case <-l.quitCh:
+			return
+		default:
+			err := l.Campaign()
+			if err == nil {
+				log.Info(fmt.Sprintf("%s is currently leader", l.id.String()))
+			}
+			time.Sleep(electionPollTime)
+		}
+	}
+}
+
+// Campaign attempts to elect the current instance as leader.
+func (l *LeaderElection) Campaign() error {
+	ctx, cancel := context.WithTimeout(context.Background(), electionPollTime/2)
+	defer cancel()
+	return l.election.Campaign(ctx, l.id.String())
+}
+
+// IsLeader returns whether this leaderElection instance is currently the leader.
+func (l *LeaderElection) IsLeader() (bool, error) {
+	ctx := context.Background()
+	resp, err := l.election.Leader(ctx)
+	if err != nil {
+		return false, err
+	}
+	if len(resp.Kvs) == 0 {
+		return false, errors.New("Get leader failed")
+	}
+	return fmt.Sprintf("%s", resp.Kvs[0].Value) == l.id.String(), nil
+}
+
+// Stop stops any further campaign attempts and resigns as leader.
+func (l *LeaderElection) Stop() error {
+	ctx := context.Background()
+	close(l.quitCh)
+	return l.election.Resign(ctx)
+}
---
+++ src/vizier/services/metadata/controllers/etcd/leader_election_test.go
@@ -0,0 +1,107 @@
+package etcd_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/coreos/etcd/clientv3/concurrency"
+	"github.com/stretchr/testify/assert"
+
+	"pixielabs.ai/pixielabs/src/utils/testingutils"
+	"pixielabs.ai/pixielabs/src/vizier/services/metadata/controllers/etcd"
+)
+
+func TestLeaderElection(t *testing.T) {
+	etcdClient, cleanup := testingutils.SetupEtcd(t)
+	defer cleanup()
+
+	sess, err := concurrency.NewSession(etcdClient, concurrency.WithContext(context.Background()))
+	if err != nil {
+		t.Fatal("Could not create new session for etcd")
+	}
+
+	l := etcd.NewLeaderElection(sess)
+
+	// First campaign attempt should succeed.
+	err = l.Campaign()
+	assert.Nil(t, err)
+
+	isLeader, err := l.IsLeader()
+	assert.Equal(t, true, isLeader)
+
+	// Campaigning when the instance is already the leader should succeed.
+	err = l.Campaign()
+	assert.Nil(t, err)
+
+	isLeader, err = l.IsLeader()
+	assert.Equal(t, true, isLeader)
+
+	sess2, err := concurrency.NewSession(etcdClient, concurrency.WithContext(context.Background()))
+	if err != nil {
+		t.Fatal("Could not create new session for etcd")
+	}
+
+	l2 := etcd.NewLeaderElection(sess2)
+
+	// Campaigning when another instance is already the leader should fail.
+	err = l2.Campaign()
+	assert.NotNil(t, err)
+
+	isLeader, err = l2.IsLeader()
+	assert.Equal(t, false, isLeader)
+}
+
+func TestLeaderElection_Stop(t *testing.T) {
+	etcdClient, cleanup := testingutils.SetupEtcd(t)
+	defer cleanup()
+
+	sess, err := concurrency.NewSession(etcdClient, concurrency.WithContext(context.Background()))
+	if err != nil {
+		t.Fatal("Could not create new session for etcd")
+	}
+
+	l := etcd.NewLeaderElection(sess)
+
+	// First campaign attempt should succeed.
+	err = l.Campaign()
+	assert.Nil(t, err)
+
+	isLeader, err := l.IsLeader()
+	assert.Equal(t, true, isLeader)
+
+	// Campaigning when the instance is already the leader should succeed.
+	err = l.Campaign()
+	assert.Nil(t, err)
+
+	isLeader, err = l.IsLeader()
+	assert.Equal(t, true, isLeader)
+
+	sess2, err := concurrency.NewSession(etcdClient, concurrency.WithContext(context.Background()))
+	if err != nil {
+		t.Fatal("Could not create new session for etcd")
+	}
+
+	l2 := etcd.NewLeaderElection(sess2)
+
+	// Campaigning when another instance is already the leader should fail.
+	err = l2.Campaign()
+	assert.NotNil(t, err)
+
+	isLeader, err = l2.IsLeader()
+	assert.Equal(t, false, isLeader)
+
+	err = l.Stop()
+	assert.Nil(t, err)
+
+	// Stop should resign the first instance as leader.
+	isLeader, err = l.IsLeader()
+	assert.Equal(t, false, isLeader)
+
+	// Campaigning should be successful since there is no current leader.
+	err = l2.Campaign()
+	assert.Nil(t, err)
+
+	isLeader, err = l2.IsLeader()
+	assert.Equal(t, true, isLeader)
+
+}
