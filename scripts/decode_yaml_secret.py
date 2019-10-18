345ee56058a80e2857d62e731ae65debf9d02134
Natalie Serrino <nserrino@pixielabs.ai>
2019-10-18 15:13:26 -0700

    Add script to regenerate tls certs
    Summary: tsia
    
    Test Plan: manual
    
    Reviewers: zasgar, michelle, philkuz, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2286
    
    GitOrigin-RevId: 2af02110d20a21f2b29b26457e825403f102039f

---
+++ scripts/decode_yaml_secret.py
@@ -0,0 +1,37 @@
+import yaml
+import sys
+import base64
+
+# Takes a k8s secret yaml from stdout, and base64 decodes the value at each field,
+# writing the result back to stdout. Used for service_tls_certs.
+
+
+def str_presenter(dumper, data):
+    if len(data.splitlines()) > 1:  # check for multiline string
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+
+def main():
+    lines = ""
+    for line in sys.stdin:
+        lines += line
+
+    data = yaml.load(lines)
+    data['stringData'] = {}
+    for k, v in data['data'].items():
+        data['stringData'][k] = base64.b64decode(v)
+
+    del data['data']
+
+    metadata_keys = data['metadata'].keys()
+    for mk in metadata_keys:
+        if mk != "name":
+            del data['metadata'][mk]
+
+    yaml.add_representer(str, str_presenter)
+    yaml.dump(data, sys.stdout)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/update_tls_certs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -ex
+
+# This script updates service_tls_certs for the provided cluster
+
+# Print out the usage information and exit.
+usage() {
+  echo "Usage $0 [-n k8s namespace] [-d path to the directory of the service_tls_certs.yaml file]" 1>&2;
+  exit 1;
+}
+
+parse_args() {
+  local OPTIND
+  # Process the command line arguments.
+  while getopts "n:d:" opt; do
+    case ${opt} in
+      n)
+        namespace=$OPTARG
+        ;;
+      d)
+        dir=$OPTARG
+        ;;
+      :)
+        echo "Invalid option: $OPTARG requires an argument" 1>&2
+        ;;
+      h)
+        usage
+        ;;
+      *)
+        usage
+        ;;
+    esac
+  done
+  shift $((OPTIND -1))
+}
+
+parse_args "$@"
+
+if [ -z $namespace ]; then
+  echo "Namespace (-n) must be provided."
+  exit 1
+fi
+
+if [ -z $dir ]; then
+  echo "Path to the directory service_tls_certs.yaml file (-d) must be provided."
+  exit 1
+fi
+
+workspace=$(bazel info workspace 2> /dev/null)
+pushd $dir
+bazel run //src/utils/pixie_cli:pixie -- install-certs --namespace=$namespace
+kubectl -n $namespace get secrets service-tls-certs -o yaml | \
+  python ${workspace}/scripts/decode_yaml_secret.py > out.unenc.yaml
+sops --encrypt out.unenc.yaml > service_tls_certs.yaml
+rm out.unenc.yaml
+popd
