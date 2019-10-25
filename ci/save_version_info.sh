bc2a24cd19d841bcf280a92baa0dd6377a523b19
Zain Asgar <zasgar@pixielabs.ai>
2019-10-25 11:39:51 -0700

    Add git commit back in to the shell script
    Summary: Forgot to put this back in when moving out of Jenkinsfile.
    
    Test Plan: Test on jenkins.
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2369
    
    GitOrigin-RevId: 22a11f491554f240113e759eaea11bddfcae9c1c

--- ci/save_version_info.sh
+++ ci/save_version_info.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git rev-parse HEAD > GIT_COMMIT
+
 if [ -n "${TAG_NAME}" ]; then
     # Valid tag is present so we write it to the versions file.
     echo "${TAG_NAME##*/v}" > VERSION
@@ -10,7 +12,6 @@ else
     # We automatically set the last digit to the build number.
     echo "0.0.${BUILD_NUMBER}-${JOB_NAME}-dev" > VERSION
 fi
-
 
 git diff -U0 origin/master > diff_origin_master
 git diff -U0 origin/master -- '***.cc' '***.h' '***.c' > diff_origin_master_cc
