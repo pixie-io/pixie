2bdb74ec383bb219ff95682d0c92e2b007c0eb07
Zain Asgar <zasgar@pixielabs.ai>
2019-10-28 18:02:01 -0700

    Added user promp/dialog component for the CLI
    Summary: Adds a choice prompt and a Y/N prompt for user input in the CLI.
    
    Test Plan: Tested manually.
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2392
    
    GitOrigin-RevId: 9509488e638b2460e5ba0d218468360b046f0995

---
+++ src/utils/pixie_cli/components/prompts.go
@@ -0,0 +1,86 @@
+package components
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+// This file has components that interact with the user via prompts.
+
+// Prompter proves a user input dialog.
+type Prompter struct {
+	message string
+	choices []string
+	dv      string
+}
+
+// NewPrompter creates a Prompter with the given message, choices and defaults.
+func NewPrompter(message string, choices []string, defaultValue string) *Prompter {
+	return &Prompter{
+		message: message,
+		choices: choices,
+		dv:      defaultValue,
+	}
+}
+
+// Prompt prompts the user and return the value. If the config parameter "y" is set we will return the default.
+func (p *Prompter) Prompt() string {
+	if p.skip() {
+		return p.dv
+	}
+	fmt.Print(p.msg())
+	input := ""
+	s := bufio.NewScanner(os.Stdin)
+	ok := s.Scan()
+	if ok {
+		input = strings.TrimRight(s.Text(), "\r\n")
+	}
+	if input == "" {
+		return p.dv
+	}
+	if !p.validInput(input) {
+		fmt.Println(p.errorMsg())
+		return p.Prompt()
+	}
+	return input
+}
+
+func (p *Prompter) validInput(s string) bool {
+	// Compare values ignoring case.
+	ls := strings.ToLower(s)
+	for i := range p.choices {
+		if ls == strings.ToLower(p.choices[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Prompter) msg() string {
+	defaultValue := ""
+	if p.dv != "" {
+		defaultValue = fmt.Sprintf("[%s] ", p.dv)
+	}
+	return fmt.Sprintf("%s (%s) %s: ", p.message, strings.Join(p.choices, "/"), defaultValue)
+}
+
+func (p *Prompter) errorMsg() string {
+	return fmt.Sprintf("Invalid input, must be one of: [%s]", strings.Join(p.choices, ", "))
+}
+
+func (p *Prompter) skip() bool {
+	return viper.GetBool("y")
+}
+
+// YNPrompt is a helper function that prompts the user for a Y/N response.
+func YNPrompt(message string, defaultValue bool) bool {
+	defaultChoice := "n"
+	if defaultValue {
+		defaultChoice = "y"
+	}
+	return strings.ToLower(NewPrompter(message, []string{"y", "n"}, defaultChoice).Prompt()) == "y"
+}
