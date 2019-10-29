c42167ab71c4b2b7c02ccfd4402bd950312f3801
Michelle Nguyen <michelle@pixielabs.ai>
2019-10-29 16:11:17 -0700

    Update Pixie delete to use Vizier YAMLs artifact
    Summary:
    D2390 updates the Pixie deploy command to use the Vizier YAMLs artifact. this diff updates the Pixie delete command to delete based on a label. this diff also deletes the
    go-generate file for the Vizier yamls, since we no longer need it.
    
    Test Plan: tried it manually
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2391
    
    GitOrigin-RevId: 78469acd228e0615cee602b911140c667898d541

--- src/utils/pixie_cli/cmd/delete_pixie.go
+++ src/utils/pixie_cli/cmd/delete_pixie.go
@@ -1,10 +1,7 @@
 package cmd
 
 import (
-	"io/ioutil"
-	"os"
 	"os/exec"
-	"path"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -41,38 +38,14 @@ func deletePixie(ns string, clobberAll bool) {
 		return
 	}
 
-	// Extract yamls into tmp dir, to use for deletion.
-	dir, err := ioutil.TempDir("", "vizier_yamls")
-	if err != nil {
-		log.WithError(err).Fatal("Could not create tmp directory")
-	}
-
-	defer os.RemoveAll(dir)
-
-	// Create a file for each asset, and write the asset's contents to the file.
-	for _, asset := range AssetNames() {
-		contents, err := Asset(asset)
-		if err != nil {
-			log.WithError(err).Fatal("Could not load asset")
-		}
-
-		fname := path.Join(dir, path.Base(asset))
-		f, err := os.Create(fname)
-		defer f.Close()
-		err = ioutil.WriteFile(fname, contents, 0644)
-		if err != nil {
-			log.WithError(err).Fatal("Could not write to file")
-		}
-	}
-
 	log.Info("Deleting Vizier pods/services")
-	err = deleteFile(ns, path.Join(dir, "vizier.yaml"))
+	err := deleteVizier(ns)
 	if err != nil {
 		log.WithError(err).Error("Could not delete Vizier")
 	}
 }
 
-func deleteFile(ns, filePath string) error {
-	kcmd := exec.Command("kubectl", "-n", ns, "delete", "-f", filePath)
+func deleteVizier(ns string) error {
+	kcmd := exec.Command("kubectl", "-n", ns, "delete", "pods,deployments,services", "-l", "component=vizier")
 	return kcmd.Run()
 }
