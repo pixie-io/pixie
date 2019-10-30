8572f1740aded1fd11d91227589fffb0863df1f4
Zain Asgar <zasgar@pixielabs.ai>
2019-10-30 16:49:13 -0700

    Allow manually selecting a version of the CLI (hidden flag)
    Summary: This flag is for internal use and will allow us to manually specify a version of the CLI to use.
    
    Test Plan: tested manually.
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2423
    
    GitOrigin-RevId: dad4dfdee6f2d8a20a65e861e0539515bc648ae6

--- src/utils/pixie_cli/cmd/update.go
+++ src/utils/pixie_cli/cmd/update.go
@@ -13,6 +13,10 @@ import (
 
 func init() {
 	UpdateCmd.AddCommand(CLIUpdateCmd)
+
+	CLIUpdateCmd.Flags().StringP("use_version", "v", "", "Select a specific version to install")
+	_ = CLIUpdateCmd.Flags().MarkHidden("use_version")
+	_ = viper.BindPFlag("use_version", CLIUpdateCmd.Flags().Lookup("use_version"))
 }
 
 // UpdateCmd is the "update" sub-command of the CLI.
@@ -31,34 +35,44 @@ var CLIUpdateCmd = &cobra.Command{
 	Use:   "cli",
 	Short: "Run updates of CLI/Pixie",
 	Run: func(cmd *cobra.Command, args []string) {
-		currentSemver := version.GetVersion().Semver()
+		selectedVersion := viper.GetString("use_version")
 		updater := update.NewCLIUpdater(viper.GetString("cloud_addr"))
-		versions, err := updater.GetAvailableVersions(currentSemver)
-		if err != nil {
-			panic(err)
-		}
-
-		if len(versions) <= 0 {
-			fmt.Println("No updates available")
-			return
-		}
-
-		if ok, err := updater.IsUpdatable(); !ok || err != nil {
+		if len(selectedVersion) == 0 {
+			// Not specified try to get available.
+			currentSemver := version.GetVersion().Semver()
+			versions, err := updater.GetAvailableVersions(currentSemver)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Println("cannot perform update, it's likely the file is not in a writable path.")
-			os.Exit(1)
-			// TODO(zasgar): Provide a means to update this as well.
+			if len(versions) <= 0 {
+				fmt.Println("No updates available")
+				return
+			}
+
+			if ok, err := updater.IsUpdatable(); !ok || err != nil {
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println("cannot perform update, it's likely the file is not in a writable path.")
+				os.Exit(1)
+				// TODO(zasgar): Provide a means to update this as well.
+			}
+			selectedVersion = versions[0]
+			if len(selectedVersion) == 0 {
+				return
+			}
 		}
 
-		selectedVersion := versions[0]
 		fmt.Printf("Updating to version: %s\n", selectedVersion)
-		err = updater.UpdateSelf(selectedVersion)
-		if err != nil {
-			fmt.Println("Failed to apply update")
-			panic(err)
-		}
-		fmt.Println("Update completed successfully")
+		mustInstallVersion(updater, selectedVersion)
 	},
+}
+
+func mustInstallVersion(u *update.CLIUpdater, v string) {
+	err := u.UpdateSelf(v)
+	if err != nil {
+		fmt.Println("Failed to apply update")
+		panic(err)
+	}
+	fmt.Println("Update completed successfully")
 }
