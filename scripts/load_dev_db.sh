4f40eed743c5c1c19fc7290421120713fe46de29
Michelle Nguyen <michelle@pixielabs.ai>
2019-11-04 10:07:48 -0800

    Create script to load dev db with artifacts/ssl certs
    Summary: tsia
    
    Test Plan: ran script manually, checked postgres to verify tables have been loaded
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2463
    
    GitOrigin-RevId: 076174ccfaf24cabbffc50b0275e3cf67afef71d

---
+++ scripts/load_dev_db.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -ex
+
+if [ $# -lt 1 ]; then
+  echo "This script requires exactly one argument: <namespace>"
+  exit
+fi
+namespace=$1
+
+repo_path=$(pwd)
+versions_file="$(pwd)/src/utils/artifacts/artifact_db_updater/VERSIONS.json"
+certs_path=$(pwd)/credentials/certs
+
+# Port-forward the postgres pod.
+postgres_pod=$(kubectl get pod --namespace "$namespace" --selector="name=postgres" \
+    --output jsonpath='{.items[0].metadata.name}')
+kubectl port-forward pods/"$postgres_pod" 5432:5432 -n "$namespace" &
+
+# Update database with Vizier versions.
+bazel run -c opt //src/utils/artifacts/versions_gen:versions_gen -- \
+      --repo_path "${repo_path}" --artifact_name vizier --versions_file "${versions_file}"
+bazel run -c opt //src/utils/artifacts/artifact_db_updater:artifact_db_updater -- \
+    --versions_file "${versions_file}" --postgres_db "pl"
+
+# Update database with CLI versions.
+bazel run -c opt //src/utils/artifacts/versions_gen:versions_gen -- \
+      --repo_path "${repo_path}" --artifact_name cli --versions_file "${versions_file}"
+bazel run -c opt //src/utils/artifacts/artifact_db_updater:artifact_db_updater -- \
+    --versions_file "${versions_file}" --postgres_db "pl"
+
+git checkout master "$versions_file"
+
+# Update database with SSL certs.
+bazel run -c opt //src/cloud/dnsmgr/load_certs:load_certs -- \
+    --certs_path "${certs_path}" --postgres_db "pl"
+
+# Kill kubectl port-forward.
+kill -15 "$!"
+sleep 2
+# Make sure process cleans up properly.
+kill -9 "$!"
