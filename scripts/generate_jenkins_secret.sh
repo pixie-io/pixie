c6b6eb15d8ba47c5de4814d6cd7794ece82de019
Michelle Nguyen <michelle@pixielabs.ai>
2019-11-06 13:28:01 -0800

    Update script for generating jenkins secret
    Summary: we want a clusterrolebinding instead of a rolebinding, so that we're not bound to a namespace and can create namespaces.
    
    Test Plan: tried it in jenkins
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2504
    
    GitOrigin-RevId: f12479ea0eae885d269d31ca9f4af52939f48970

--- scripts/generate_jenkins_secret.sh
+++ scripts/generate_jenkins_secret.sh
@@ -4,13 +4,13 @@
 # Any cluster accessed by Jenkins needs credentials to be registered
 # in the UI under: https://jenkins.pixielabs.ai/credentials.
 
-namespace=pl
+namespace=default
 account_name=jenkins-robot
 
 kubectl -n ${namespace} create serviceaccount ${account_name}
 
 # The next line gives `${account_name}` administator permissions for this namespace.
-kubectl -n ${namespace} create rolebinding "${account_name}-binding" \
+kubectl -n ${namespace} create clusterrolebinding "${account_name}-binding" \
         --clusterrole=cluster-admin --serviceaccount="${namespace}:${account_name}"
 
 # Get the name of the token that was automatically generated for the ServiceAccount `${account_name}`.
@@ -20,3 +20,4 @@ token_name=$(kubectl -n ${namespace} get serviceaccount "${account_name}" \
 # Retrieve the token and decode it using base64.
 kubectl -n ${namespace} get secrets "${token_name}" -o go-template \
         --template '{{index .data "token"}}' | base64 -d
+printf "\n"
