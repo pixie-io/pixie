a2f5e35c53b2d7d79019dd99fcee20e49e6f098b
Omid Azizi <oazizi@pixielabs.ai>
2019-12-02 17:29:13 -0800

    BPF utilities
    Summary:
    Starting to move some of our code from experimental to Stirling.
    
    Simple utils is a good starting point.
    
    Test Plan: Existing tests.
    
    Reviewers: yzhao, #engineering, zasgar
    
    Reviewed By: #engineering, zasgar
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2710
    
    GitOrigin-RevId: bdaadecf4a87a3e8c6ff8f079b67d8e1ec5294c1

--- src/stirling/bcc_bpf/utils.h
+++ src/stirling/bcc_bpf/utils.h
@@ -11,3 +11,24 @@
 #define __inline inline __attribute__((__always_inline__))
 #endif  // SUPPORT_BPF2BPF_CALL
 #endif  // __inline
+
+// This macro is essentially a min() function that caps a number.
+// It performs the min in a way that keeps the the BPF verifier happy.
+// It is essentially a traditional min(), plus a mask that helps old versions of the BPF verifier
+// reason about the maximum value of a number.
+//
+// NOTE: cap must be a power-of-2.
+// This is not checked for the caller, and behavior is undefined when this is not true.
+//
+// Note that we still apply a min() function before masking, otherwise, the mask may create a number
+// lower than the min if the original number is greater than the cap_mask.
+//
+// Example:
+//   cap = 16
+//   cap-1 = 16-1 = 0xf
+//   x = 36 = 0x24
+//   BPF_LEN_CAP(x, cap) = 16
+//
+// However, if we remove the min() before applying the mask, we would get a smaller number.
+//   x & (cap-1) = 4
+#define BPF_LEN_CAP(x, cap) (x < cap ? (x & (cap - 1)) : cap)
