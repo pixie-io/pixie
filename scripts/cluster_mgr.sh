6b0e7c3b1b6aa69df41ff47abdb930aef5710327
Phillip Kuznetsov <philkuz@pixielabs.ai>
2019-12-09 14:56:42 -0800

    Update cluster_mgr to be lint friendly and extra logging
    Summary: TSIA
    
    Test Plan: tried it
    
    Reviewers: nserrino, zasgar, michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2838
    
    GitOrigin-RevId: 28605c95fbe988076c004135d67ff377803030a9

--- scripts/cluster_mgr.sh
+++ scripts/cluster_mgr.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 set -e
-function usage() {
+usage() {
     echo "Usage: $0 -c CLUSTER -[u|d] [-h] [-n NODEPOOLS] [-z ZONE]"
     echo "  -c : the cluster name as found on gke"
     echo "  -u : start the cluster"
@@ -17,64 +18,82 @@ function usage() {
     exit 2
 }
 
-function upscale() {
+print_config() {
+  echo "Config: "
+  echo "  CLUSTER          : ${CLUSTER}"
+  echo "  ZONE             : ${ZONE}"
+  echo "  SIZE             : ${SIZE}"
+  echo "  NODEPOOLS        : ${NODEPOOLS}"
+  echo "  ACTION           : ${ACTION}"
+  echo ""
+}
+
+upscale() {
   CLUSTER=$1
   ZONE=$2
   NODEPOOL=$3
   SIZE=$4
-  gcloud container clusters update $CLUSTER --enable-autoscaling \
-      --min-nodes 1 --max-nodes $SIZE --zone $ZONE --node-pool $NODEPOOL
-  gcloud container clusters resize $CLUSTER --node-pool $NODEPOOL --size=$SIZE --zone $ZONE -q
+  gcloud container clusters update "${CLUSTER}" --enable-autoscaling \
+      --min-nodes 1 --max-nodes "${SIZE}" --zone "${ZONE}" --node-pool "${NODEPOOL}"
+  gcloud container clusters resize "${CLUSTER}" --node-pool "${NODEPOOL}" --num-nodes="${SIZE}" --zone "${ZONE}" -q
 }
 
-function downscale() {
+downscale() {
     CLUSTER=$1
     ZONE=$2
     NODEPOOL=$3
-    gcloud container clusters update $CLUSTER --no-enable-autoscaling \
-        --zone $ZONE --node-pool $NODEPOOL
-    gcloud container clusters resize $CLUSTER --node-pool $NODEPOOL --size=0 --zone $ZONE -q
+    gcloud container clusters update "${CLUSTER}" --no-enable-autoscaling \
+        --zone "${ZONE}" --node-pool "${NODEPOOL}"
+    gcloud container clusters resize "${CLUSTER}" --node-pool "${NODEPOOL}" --size=0 --zone "${ZONE}" -q
 }
 
 unset NODEPOOLS CLUSTER ACTION ZONE HELP
 
 # default options
-NODEPOOLS=default
+NODEPOOLS=default-pool
 ZONE=us-west1-a
 SIZE=1
 
 while getopts 'udhn:c:z:s:' c
 do
-  case $c in
-    n) NODEPOOLS=$OPTARG ;;
-    c) CLUSTER=$OPTARG ;;
+  case "${c}" in
+    n) NODEPOOLS="${OPTARG}" ;;
+    c) CLUSTER="${OPTARG}" ;;
     u) ACTION=START;;
     d) ACTION=STOP ;;
-    z) ZONE=$OPTARG ;;
-    s) SIZE=$OPTARG ;;
+    z) ZONE="${OPTARG}" ;;
+    s) SIZE="${OPTARG}" ;;
     h) HELP=true ;;
+    *) HELP=true ;;
   esac
 done
 
 # display usage when -[u|d] and -c options are missing
-[ -z "$ACTION"  ] && [ -z "$CLUSTER" ] && usage
-[ "$HELP" = true  ] && usage
+[ -z "${ACTION}"  ] && [ -z "${CLUSTER}" ] && usage
+[ "${HELP}" = true  ] && usage
 
 # Convert arguments into arrays from comma delineated strings
-IFS=', ' read -r -a NODEPOOLS <<< "$NODEPOOLS"
-IFS=', ' read -r -a SIZES <<< "$SIZE"
+IFS=', ' read -r -a NODEPOOLS <<< "${NODEPOOLS}"
+IFS=', ' read -r -a SIZES <<< "${SIZE}"
+
 # if sizes is of length 1, just repeat as long as nodepools
 if [ ${#SIZES[@]} -eq 1 ]; then
-  SIZES=($(for n in ${NODEPOOLS[@]}; do echo -n "$SIZE ";done; ))
+  for ((i=0;i<"${#NODEPOOLS[@]}";i++)); do
+    SIZES[${#SIZES[@]}]="${SIZE}"
+  done
 elif [ ! ${#SIZES[@]} -eq ${#NODEPOOLS[@]} ]; then
   echo "ERROR: sizes and nodepools expected to be the same"
   usage
 fi
+
+echo "Running with the following"
+print_config
+
 for index in ${!NODEPOOLS[*]}; do
   NP=${NODEPOOLS[$index]}
   SIZE=${SIZES[$index]}
-  case $ACTION in
-    START) upscale $CLUSTER $ZONE $NP $SIZE ;;
-    STOP) downscale $CLUSTER $ZONE $NP ;;
+  case "${ACTION}" in
+    START) upscale "${CLUSTER}" "${ZONE}" "${NP}" "${SIZE}" ;;
+    STOP) downscale "${CLUSTER}" "${ZONE}" "${NP}" ;;
   esac
 done
