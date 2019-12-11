82c8c6027b7766f0fd1825b352dd0a40be9ac014
Zain Asgar <zasgar@pixielabs.ai>
2019-12-10 23:55:48 -0800

    Upgrade bazel to 1.2.1
    Summary: TSIA
    
    Test Plan: `bazel test //...` and Jenkins
    
    Reviewers: oazizi, michelle, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2868
    
    GitOrigin-RevId: 0cc808e69f307aa2c993fb2922963acfdf2ce877

--- chef/cookbooks/pixielabs/attributes/default.rb
+++ chef/cookbooks/pixielabs/attributes/default.rb
@@ -22,9 +22,9 @@ default['shellcheck']                = {}
 
 if node[:platform] == 'ubuntu'
   default['bazel']['download_path'] =
-    'https://github.com/bazelbuild/bazel/releases/download/1.1.0/bazel-1.1.0-linux-x86_64'
+    'https://github.com/bazelbuild/bazel/releases/download/1.2.1/bazel-1.2.1-linux-x86_64'
   default['bazel']['sha256'] =
-    'f54ab5f31b8d7c6a0ce9dee387af45b1d6577ff9625ef6c535896b59cdf8828a'
+    'f5e21d7448419d1596ad0c5bb71fb336a0af08c832587aec394970ea56701d88'
 
   default['golang']['download_path'] =
     'https://dl.google.com/go/go1.13.linux-amd64.tar.gz'
@@ -67,9 +67,9 @@ if node[:platform] == 'ubuntu'
     '39c501aaca6aae3f3c7fc125b3c3af779ddbe4e67e4ebdc44c2ae5cba76c847f'
 elsif node[:platform] == 'mac_os_x'
   default['bazel']['download_path'] =
-    'https://github.com/bazelbuild/bazel/releases/download/1.1.0/bazel-1.1.0-darwin-x86_64'
+    'https://github.com/bazelbuild/bazel/releases/download/1.2.1/bazel-1.2.1-darwin-x86_64'
   default['bazel']['sha256'] =
-    'eae082635743b753d2df6744bc2f4b7ed6af90c270769ff2f60cd6cebc4b7032'
+    '6729be5a56e6eadf7a9112afd2d87ce348da8fca22077b882d9bb7a6f5d41d1c'
 
   default['golang']['download_path'] =
     'https://dl.google.com/go/go1.13.darwin-amd64.tar.gz'
