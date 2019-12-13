837c1fe95c6217ab24d53f51d27f3262bad9d33d
Omid Azizi <oazizi@pixielabs.ai>
2019-12-12 22:34:23 -0800

    HTTP2 Tracer Tests: Stream generator and a new test
    Summary: Starting to build out tests.
    
    Test Plan: This is the test.
    
    Reviewers: yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2894
    
    GitOrigin-RevId: 49ce50d782bc5b6c32d13a1b5fc9baac1e2ea22d

--- src/stirling/connection_tracker_http2_test.cc
+++ src/stirling/connection_tracker_http2_test.cc
@@ -1,11 +1,8 @@
 #include "src/stirling/connection_tracker.h"
 
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include <sys/socket.h>
 
 #include "src/common/base/types.h"
-#include "src/stirling/mysql/test_utils.h"
 #include "src/stirling/testing/events_fixture.h"
 
 namespace pl {
@@ -13,38 +10,67 @@ namespace stirling {
 
 using ConnectionTrackerHTTP2Test = testing::EventsFixture;
 
+class StreamFrameGenerator {
+ public:
+  StreamFrameGenerator(ReqRespRole role, upid_t upid, uint32_t fd, uint32_t stream_id) {
+    base_frame_.attr.conn_id.upid = upid;
+    base_frame_.attr.conn_id.fd = fd;
+    base_frame_.attr.stream_id = stream_id;
+    base_frame_.attr.traffic_class.protocol = kProtocolHTTP2;
+    base_frame_.attr.traffic_class.role = role;
+  }
+
+  template <DataFrameEventType TType>
+  HTTP2DataEvent GenFrame(std::string_view body) {
+    HTTP2DataEvent frame = base_frame_;
+    frame.attr.ftype = TType;
+    frame.attr.timestamp_ns = ++ts_;
+    frame.attr.data_len = body.length();
+    frame.payload = body;
+    return frame;
+  }
+
+ private:
+  HTTP2DataEvent base_frame_;
+  uint64_t ts_ = 0;
+};
+
 TEST_F(ConnectionTrackerHTTP2Test, Basic) {
   ConnectionTracker tracker;
 
-  {
-    HTTP2DataEvent data_frame;
-    data_frame.attr.ftype = DataFrameEventType::kDataFrameEventWrite;
-    data_frame.attr.timestamp_ns = 11223344;
-    data_frame.attr.conn_id.upid.pid = 123;
-    data_frame.attr.conn_id.upid.start_time_ticks = 11000000;
-    data_frame.attr.conn_id.fd = 5;
-    data_frame.attr.traffic_class.protocol = kProtocolHTTP2;
-    data_frame.attr.traffic_class.role = kRoleRequestor;
-    data_frame.attr.stream_id = 7;
-    data_frame.payload = "Request";
+  auto frame_generator = StreamFrameGenerator(kRoleRequestor, upid_t{{123}, 11000000}, 5, 7);
+  HTTP2DataEvent data_frame;
 
-    tracker.AddHTTP2Data(data_frame);
-  }
+  data_frame = frame_generator.GenFrame<kDataFrameEventWrite>("Request");
+  tracker.AddHTTP2Data(data_frame);
 
-  {
-    HTTP2DataEvent data_frame;
-    data_frame.attr.ftype = DataFrameEventType::kDataFrameEventRead;
-    data_frame.attr.timestamp_ns = 11223355;
-    data_frame.attr.conn_id.upid.pid = 123;
-    data_frame.attr.conn_id.upid.start_time_ticks = 11000000;
-    data_frame.attr.conn_id.fd = 5;
-    data_frame.attr.traffic_class.protocol = kProtocolHTTP2;
-    data_frame.attr.traffic_class.role = kRoleRequestor;
-    data_frame.attr.stream_id = 7;
-    data_frame.payload = "Response";
+  data_frame = frame_generator.GenFrame<kDataFrameEventRead>("Response");
+  tracker.AddHTTP2Data(data_frame);
 
-    tracker.AddHTTP2Data(data_frame);
-  }
+  std::vector<http2::NewRecord> records = tracker.ProcessMessages<http2::NewRecord>();
+
+  EXPECT_EQ(records.size(), 1);
+  EXPECT_EQ(records[0].req.data, "Request");
+  EXPECT_EQ(records[0].resp.data, "Response");
+}
+
+TEST_F(ConnectionTrackerHTTP2Test, MultipleDataFrames) {
+  ConnectionTracker tracker;
+
+  auto frame_generator = StreamFrameGenerator(kRoleRequestor, upid_t{{123}, 11000000}, 5, 7);
+  HTTP2DataEvent data_frame;
+
+  data_frame = frame_generator.GenFrame<kDataFrameEventWrite>("Req");
+  tracker.AddHTTP2Data(data_frame);
+
+  data_frame = frame_generator.GenFrame<kDataFrameEventWrite>("uest");
+  tracker.AddHTTP2Data(data_frame);
+
+  data_frame = frame_generator.GenFrame<kDataFrameEventRead>("Resp");
+  tracker.AddHTTP2Data(data_frame);
+
+  data_frame = frame_generator.GenFrame<kDataFrameEventRead>("onse");
+  tracker.AddHTTP2Data(data_frame);
 
   std::vector<http2::NewRecord> records = tracker.ProcessMessages<http2::NewRecord>();
 
