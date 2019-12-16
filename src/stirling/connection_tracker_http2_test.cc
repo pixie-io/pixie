e113acfc0e5eac0218b9a0a492bce57fd659f054
Omid Azizi <oazizi@pixielabs.ai>
2019-12-16 14:25:41 -0800

    Refactor HTTP2 stream generator
    Summary:
    The plan is to use the HTTP2 stream generator in other test files, so refactor it.
    
    lint
    
    Test Plan: This is part of test code.
    
    Reviewers: yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2923
    
    GitOrigin-RevId: 9b3e9b75db80e730cd191a13fe50ee2fe9afc027

--- src/stirling/connection_tracker_http2_test.cc
+++ src/stirling/connection_tracker_http2_test.cc
@@ -5,6 +5,7 @@
 
 #include "src/common/base/types.h"
 #include "src/stirling/testing/events_fixture.h"
+#include "src/stirling/testing/http2_stream_generator.h"
 
 namespace pl {
 namespace stirling {
@@ -14,58 +15,17 @@ using ::testing::UnorderedElementsAre;
 
 using ConnectionTrackerHTTP2Test = testing::EventsFixture;
 
-class StreamEventGenerator {
- public:
-  StreamEventGenerator(upid_t upid, uint32_t fd, uint32_t stream_id)
-      : upid_(upid), fd_(fd), stream_id_(stream_id) {}
-
-  template <DataFrameEventType TType>
-  HTTP2DataEvent GenDataFrame(std::string_view body) {
-    HTTP2DataEvent frame;
-    frame.attr.conn_id.upid = upid_;
-    frame.attr.conn_id.fd = fd_;
-    frame.attr.conn_id.generation = 0;
-    frame.attr.stream_id = stream_id_;
-    frame.attr.ftype = TType;
-    frame.attr.timestamp_ns = ++ts_;
-    frame.attr.data_len = body.length();
-    frame.payload = body;
-    return frame;
-  }
-
-  template <HeaderEventType TType>
-  HTTP2HeaderEvent GenHeader(std::string_view name, std::string_view value) {
-    HTTP2HeaderEvent hdr;
-    hdr.attr.conn_id.upid = upid_;
-    hdr.attr.conn_id.fd = fd_;
-    hdr.attr.conn_id.generation = 0;
-    hdr.attr.stream_id = stream_id_;
-    hdr.attr.htype = TType;
-    hdr.attr.timestamp_ns = ++ts_;
-    hdr.name = name;
-    hdr.value = value;
-    return hdr;
-  }
-
- private:
-  upid_t upid_;
-  uint32_t fd_;
-  uint32_t stream_id_;
-
-  uint64_t ts_ = 0;
-};
-
 TEST_F(ConnectionTrackerHTTP2Test, BasicData) {
   ConnectionTracker tracker;
 
-  auto frame_generator = StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
-  HTTP2DataEvent data_frame;
+  auto frame_generator = testing::StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
+  std::unique_ptr<HTTP2DataEvent> data_frame;
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventWrite>("Request");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventRead>("Response");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   std::vector<http2::NewRecord> records = tracker.ProcessMessages<http2::NewRecord>();
 
@@ -77,14 +37,14 @@ TEST_F(ConnectionTrackerHTTP2Test, BasicData) {
 TEST_F(ConnectionTrackerHTTP2Test, BasicHeader) {
   ConnectionTracker tracker;
 
-  auto frame_generator = StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
-  HTTP2HeaderEvent header_event;
+  auto frame_generator = testing::StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
+  std::unique_ptr<HTTP2HeaderEvent> header_event;
 
   header_event = frame_generator.GenHeader<kHeaderEventWrite>(":method", "post");
-  tracker.AddHTTP2Header(header_event);
+  tracker.AddHTTP2Header(*header_event);
 
   header_event = frame_generator.GenHeader<kHeaderEventRead>(":status", "200");
-  tracker.AddHTTP2Header(header_event);
+  tracker.AddHTTP2Header(*header_event);
 
   std::vector<http2::NewRecord> records = tracker.ProcessMessages<http2::NewRecord>();
 
@@ -96,20 +56,20 @@ TEST_F(ConnectionTrackerHTTP2Test, BasicHeader) {
 TEST_F(ConnectionTrackerHTTP2Test, MultipleDataFrames) {
   ConnectionTracker tracker;
 
-  auto frame_generator = StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
-  HTTP2DataEvent data_frame;
+  auto frame_generator = testing::StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
+  std::unique_ptr<HTTP2DataEvent> data_frame;
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventWrite>("Req");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventWrite>("uest");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventRead>("Resp");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventRead>("onse");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   std::vector<http2::NewRecord> records = tracker.ProcessMessages<http2::NewRecord>();
 
@@ -121,33 +81,33 @@ TEST_F(ConnectionTrackerHTTP2Test, MultipleDataFrames) {
 TEST_F(ConnectionTrackerHTTP2Test, MixedHeadersAndData) {
   ConnectionTracker tracker;
 
-  auto frame_generator = StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
-  HTTP2DataEvent data_frame;
-  HTTP2HeaderEvent header_event;
+  auto frame_generator = testing::StreamEventGenerator(upid_t{{123}, 11000000}, 5, 7);
+  std::unique_ptr<HTTP2DataEvent> data_frame;
+  std::unique_ptr<HTTP2HeaderEvent> header_event;
 
   header_event = frame_generator.GenHeader<kHeaderEventWrite>(":method", "post");
-  tracker.AddHTTP2Header(header_event);
+  tracker.AddHTTP2Header(*header_event);
 
   header_event = frame_generator.GenHeader<kHeaderEventWrite>(":host", "pixie.ai");
-  tracker.AddHTTP2Header(header_event);
+  tracker.AddHTTP2Header(*header_event);
 
   header_event = frame_generator.GenHeader<kHeaderEventWrite>(":path", "/magic");
-  tracker.AddHTTP2Header(header_event);
+  tracker.AddHTTP2Header(*header_event);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventWrite>("Req");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventWrite>("uest");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventRead>("Resp");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   data_frame = frame_generator.GenDataFrame<kDataFrameEventRead>("onse");
-  tracker.AddHTTP2Data(data_frame);
+  tracker.AddHTTP2Data(*data_frame);
 
   header_event = frame_generator.GenHeader<kHeaderEventRead>(":status", "200");
-  tracker.AddHTTP2Header(header_event);
+  tracker.AddHTTP2Header(*header_event);
 
   std::vector<http2::NewRecord> records = tracker.ProcessMessages<http2::NewRecord>();
 
---
+++ src/stirling/testing/http2_stream_generator.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <memory>
+
+namespace pl {
+namespace stirling {
+namespace testing {
+
+class StreamEventGenerator {
+ public:
+  StreamEventGenerator(upid_t upid, uint32_t fd, uint32_t stream_id)
+      : conn_id_({.upid = upid, .fd = fd, .generation = 0}), stream_id_(stream_id) {}
+
+  StreamEventGenerator(conn_id_t conn_id, uint32_t stream_id)
+      : conn_id_(conn_id), stream_id_(stream_id) {}
+
+  template <DataFrameEventType TType>
+  std::unique_ptr<HTTP2DataEvent> GenDataFrame(std::string_view body) {
+    auto frame = std::make_unique<HTTP2DataEvent>();
+    frame->attr.conn_id = conn_id_;
+    frame->attr.stream_id = stream_id_;
+    frame->attr.ftype = TType;
+    frame->attr.timestamp_ns = ++ts_;
+    frame->attr.data_len = body.length();
+    frame->payload = body;
+    return frame;
+  }
+
+  template <HeaderEventType TType>
+  std::unique_ptr<HTTP2HeaderEvent> GenHeader(std::string_view name, std::string_view value) {
+    auto hdr = std::make_unique<HTTP2HeaderEvent>();
+    hdr->attr.conn_id = conn_id_;
+    hdr->attr.stream_id = stream_id_;
+    hdr->attr.htype = TType;
+    hdr->attr.timestamp_ns = ++ts_;
+    hdr->name = name;
+    hdr->value = value;
+    return hdr;
+  }
+
+ private:
+  conn_id_t conn_id_;
+  uint32_t stream_id_;
+
+  uint64_t ts_ = 0;
+};
+
+}  // namespace testing
+}  // namespace stirling
+}  // namespace pl
