630112f58191e4f84f4d90e9f3022f5fcaac0eba
Yaxiong Zhao <yzhao@pixielabs.ai>
2019-12-19 13:02:26 -0800

    Move OBJ_STRVIEW to bcc_wrapper.h
    Summary:
    It's only used for stirling anyway. This reduces noises when reading
    utils.h.
    
    Test Plan: Jenkins
    
    Reviewers: oazizi, zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D2980
    
    GitOrigin-RevId: f3b438ebb78527de9a4282e8938e9663cd1e11de

--- src/common/base/utils.h
+++ src/common/base/utils.h
@@ -175,11 +175,3 @@ constexpr auto MakeArray(T&&... values)
 }
 
 }  // namespace pl
-
-// Provides a string view into a char array included in the binary via objcopy.
-// Useful for include BPF programs that are copied into the binary.
-#define OBJ_STRVIEW(varname, objname)     \
-  extern char objname##_start;            \
-  extern char objname##_end;              \
-  inline const std::string_view varname = \
-      std::string_view(&objname##_start, &objname##_end - &objname##_start);
--- src/stirling/bpf_tools/bcc_wrapper.h
+++ src/stirling/bpf_tools/bcc_wrapper.h
@@ -19,6 +19,14 @@ extern "C" {
 #include "src/common/base/base.h"
 #include "src/stirling/obj_tools/elf_tools.h"
 
+// Provides a string view into a char array included in the binary via objcopy.
+// Useful for include BPF programs that are copied into the binary.
+#define OBJ_STRVIEW(varname, objname)     \
+  extern char objname##_start;            \
+  extern char objname##_end;              \
+  inline const std::string_view varname = \
+      std::string_view(&objname##_start, &objname##_end - &objname##_start);
+
 // Macro to load BPF source code embedded in object files.
 // See 'pl_bpf_cc_resource' bazel rule to see how these are generated.
 #define BCC_SRC_STRVIEW(varname, build_label) OBJ_STRVIEW(varname, _binary_##build_label##_bpf_src);
