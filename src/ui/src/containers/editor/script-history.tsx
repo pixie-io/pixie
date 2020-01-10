633ea4fc759d72c0239b7f96e800f7d7ced0970d
Malthus Yau <malthus@pixielabs.ai>
2020-01-09 16:00:24 -0800

    Improve lagginess caused by history list. Also update the styles of the scrollbar.
    Summary: Memoize the history list better to improve rendering performance. Also set overflow: auto to the scrollable table to prevent showing the scroll tracks when it is not overflowing.
    
    Test Plan: {F44471}
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3117
    
    GitOrigin-RevId: c11bb520c8d5e22746bc0a2fb1b823aa3cb74c5a

--- src/ui/src/containers/editor/script-history.tsx
+++ src/ui/src/containers/editor/script-history.tsx
@@ -9,10 +9,11 @@ interface HistoryEntryProps {
 }
 
 const HistoryEntry = (props: HistoryEntryProps) => {
+  const time = React.useMemo(() => relativeTime(props.time), [props.time]);
   return (
     <div className='pixie-history-entry'>
       <span className='pixie-history-title'>{props.name}</span>
-      <span className='pixie-history-time'>({relativeTime(props.time)})</span>
+      <span className='pixie-history-time'>({time})</span>
     </div>
   );
 };
