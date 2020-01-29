5369c7f036e7851edebb57b673536bf5f788c67c
Yaxiong Zhao <yzhao@pixielabs.ai>
2020-01-28 16:01:52 -0800

    Add --count to go_http_client
    Summary: So that it can run indefinitely for testing
    
    Test Plan: Jenkins
    
    Reviewers: oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3335
    
    GitOrigin-RevId: 5d20de2378d00eef305638c3f1bf69a1d2fa2ac9

--- src/stirling/http/testing/go_http_client/main.go
+++ src/stirling/http/testing/go_http_client/main.go
@@ -19,25 +19,28 @@ type helloReply struct {
 func main() {
 	address := flag.String("address", "localhost:50050", "Server end point.")
 	name := flag.String("name", "world", "The name to greet.")
+	count := flag.Int("count", 1, "The count of requests to make.")
 
 	flag.Parse()
 
-	resp, err := http.Get("http://" + *address + "/sayhello?name=" + url.QueryEscape(*name))
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+	for i := 0; i < *count; i++ {
+		resp, err := http.Get("http://" + *address + "/sayhello?name=" + url.QueryEscape(*name))
+		if err != nil {
+			panic(err)
+		}
 
-	body, readErr := ioutil.ReadAll(resp.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
+		body, readErr := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if readErr != nil {
+			log.Fatal(readErr)
+		}
 
-	reply := helloReply{}
-	jsonErr := json.Unmarshal(body, &reply)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
+		reply := helloReply{}
+		jsonErr := json.Unmarshal(body, &reply)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
 
-	fmt.Println(reply.Greeter)
+		fmt.Println(reply.Greeter)
+	}
 }
