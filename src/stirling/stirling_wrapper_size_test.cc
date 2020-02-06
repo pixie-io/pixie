1e53a8e352996c3c68e759a4c2a515ccf938dd15
Omid Azizi <oazizi@pixielabs.ai>
2020-02-06 13:55:09 -0800

    Stirling: file size limit depending on build type
    Summary: To avoid requires_bpf tag hack.
    
    Test Plan: Jenkins
    
    Reviewers: yzhao, zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3442
    
    GitOrigin-RevId: 51a97e7d7a8fa6db0adfc0a15612e92d92dac67f

--- src/stirling/stirling_wrapper_size_test.cc
+++ src/stirling/stirling_wrapper_size_test.cc
@@ -3,16 +3,25 @@
 
 #include <filesystem>
 
+#include "src/common/base/base.h"
 #include "src/common/testing/test_environment.h"
 
 namespace pl {
 namespace stirling {
 
-TEST(StirlingWrapperSizeTest, ExecutableSizeIsLessThan100MiB) {
+#ifdef __OPTIMIZE__
+constexpr uint64_t kFileSizeLimitMB = 100;
+#else
+constexpr uint64_t kFileSizeLimitMB = 200;
+#endif
+
+TEST(StirlingWrapperSizeTest, ExecutableSizeLimit) {
+  LOG(INFO) << absl::Substitute("Size limit = $0 MB", kFileSizeLimitMB);
   const std::string stirling_wrapper_path =
       TestEnvironment::PathToTestDataFile("src/stirling/stirling_wrapper");
+
   EXPECT_LE(std::filesystem::file_size(std::filesystem::path(stirling_wrapper_path)),
-            100 * 1024 * 1024);
+            kFileSizeLimitMB * 1024 * 1024);
 }
 
 }  // namespace stirling
