7067253f0e5142fee0842e74eda3a390498b5325
Omid Azizi <oazizi@pixielabs.ai>
2020-02-07 10:20:37 -0800

    Cassandra: types extractor
    Summary:
    This is a library of tools to extract native types out of cassandra frames.
    
    Currently standalone, but will be used by the cassandra stitcher, when it tries to interpret the contents of frames.
    
    Test Plan: An extensive set of tests have been added, although I have left a few cases as future work because this diff is already very large.
    
    Reviewers: yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    JIRA Issues: PL-711
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3446
    
    GitOrigin-RevId: baa2210b5c1bf5c682e34d020ed357f1ff2b4b31

---
+++ src/stirling/cassandra/cql_types.cc
@@ -0,0 +1,178 @@
+#include "src/stirling/cassandra/cql_types.h"
+
+#include <algorithm>
+#include <utility>
+
+#include "src/common/base/byte_utils.h"
+
+namespace pl {
+namespace stirling {
+namespace cass {
+
+// These Extract functions parse raw byte sequences into CQL types.
+// Note that the CQL protocol is big-endian, so all extract functions
+// also intrinsically convert from big-endian to host ordering.
+
+template <typename TIntType>
+StatusOr<TIntType> ExtractIntCore(std::string_view* buf) {
+  if (buf->size() < sizeof(TIntType)) {
+    return error::ResourceUnavailable("Insufficient number of bytes");
+  }
+  TIntType val = utils::BEndianBytesToInt<TIntType>(*buf);
+  buf->remove_prefix(sizeof(TIntType));
+  return val;
+}
+
+template <typename TCharType>
+StatusOr<std::basic_string<TCharType>> ExtractBytesCore(std::string_view* buf, int64_t len) {
+  if (static_cast<ssize_t>(buf->size()) < len) {
+    return error::ResourceUnavailable("Insufficient number of bytes");
+  }
+
+  // TODO(oazizi): Optimization when input and output types match,
+  // no need to create tbuf.
+  auto tbuf = CreateStringView<TCharType>(*buf);
+  std::basic_string<TCharType> str(tbuf.substr(0, len));
+  buf->remove_prefix(len);
+  return str;
+}
+
+template <typename TCharType, size_t N>
+Status ExtractBytesCore(std::string_view* buf, TCharType* out) {
+  if (buf->size() < N) {
+    return error::Internal("Insufficient number of bytes");
+  }
+
+  // TODO(oazizi): Optimization when input and output types match,
+  // no need to create tbuf.
+  auto tbuf = CreateStringView<TCharType>(*buf);
+  for (size_t i = 0; i < N; ++i) {
+    out[i] = tbuf[i];
+  }
+  buf->remove_prefix(N);
+  return Status::OK();
+}
+
+// [int] A 4 bytes signed integer
+StatusOr<int32_t> ExtractInt(std::string_view* buf) { return ExtractIntCore<int32_t>(buf); }
+
+// [long] A 8 bytes signed integer
+StatusOr<int64_t> ExtractLong(std::string_view* buf) { return ExtractIntCore<int64_t>(buf); }
+
+// [short] A 2 bytes unsigned integer
+StatusOr<uint16_t> ExtractShort(std::string_view* buf) { return ExtractIntCore<uint16_t>(buf); }
+
+// [byte] A 2 bytes unsigned integer
+StatusOr<uint8_t> ExtractByte(std::string_view* buf) { return ExtractIntCore<uint8_t>(buf); }
+
+// [string] A [short] n, followed by n bytes representing an UTF-8 string.
+StatusOr<std::string> ExtractString(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(uint16_t len, ExtractShort(buf));
+  return ExtractBytesCore<char>(buf, len);
+}
+
+// [long string] An [int] n, followed by n bytes representing an UTF-8 string.
+StatusOr<std::string> ExtractLongString(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(int32_t len, ExtractInt(buf));
+  len = std::max(len, 0);
+  return ExtractBytesCore<char>(buf, len);
+}
+
+// [uuid] A 16 bytes long uuid.
+StatusOr<sole::uuid> ExtractUUID(std::string_view* buf) {
+  sole::uuid uuid;
+  PL_ASSIGN_OR_RETURN(uuid.cd, ExtractLong(buf));
+  PL_ASSIGN_OR_RETURN(uuid.ab, ExtractLong(buf));
+  // TODO(oazizi): Handle endianness.
+  return uuid;
+}
+
+// [string list] A [short] n, followed by n [string].
+StatusOr<StringList> ExtractStringList(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(uint16_t n, ExtractShort(buf));
+
+  StringList string_list;
+  for (int i = 0; i < n; ++i) {
+    PL_ASSIGN_OR_RETURN(std::string s, ExtractString(buf));
+    string_list.push_back(std::move(s));
+  }
+
+  return string_list;
+}
+
+// [bytes] A [int] n, followed by n bytes if n >= 0. If n < 0,
+//         no byte should follow and the value represented is `null`.
+StatusOr<std::basic_string<uint8_t>> ExtractBytes(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(int32_t len, ExtractInt(buf));
+  len = std::max(len, 0);
+  return ExtractBytesCore<uint8_t>(buf, len);
+}
+
+// [short bytes]  A [short] n, followed by n bytes if n >= 0.
+StatusOr<std::basic_string<uint8_t>> ExtractShortBytes(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(uint16_t len, ExtractShort(buf));
+  return ExtractBytesCore<uint8_t>(buf, len);
+}
+
+// [inet] An address (ip and port) to a node. It consists of one
+//        [byte] n, that represents the address size, followed by n
+//        [byte] representing the IP address (in practice n can only be
+//        either 4 (IPv4) or 16 (IPv6)), following by one [int]
+//        representing the port.
+StatusOr<SockAddr> ExtractInet(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(uint8_t n, ExtractByte(buf));
+
+  SockAddr addr;
+
+  switch (n) {
+    case 4: {
+      addr.family = SockAddrFamily::kIPv4;
+      PL_ASSIGN_OR_RETURN(uint32_t a, ExtractIntCore<uint32_t>(buf));
+      addr.addr = in_addr{a};
+    } break;
+    case 16: {
+      addr.family = SockAddrFamily::kIPv6;
+      addr.addr = in6_addr{};
+      PL_RETURN_IF_ERROR(
+          (ExtractBytesCore<uint8_t, 16>(buf, reinterpret_cast<uint8_t*>(&addr.addr))));
+    } break;
+  }
+
+  PL_ASSIGN_OR_RETURN(addr.port, ExtractInt(buf));
+
+  return addr;
+}
+
+// [string map] A [short] n, followed by n pair <k><v> where <k> and <v>
+//              are [string].
+StatusOr<StringMap> ExtractStringMap(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(uint16_t n, ExtractShort(buf));
+
+  StringMap string_map;
+  for (int i = 0; i < n; ++i) {
+    PL_ASSIGN_OR_RETURN(std::string key, ExtractString(buf));
+    PL_ASSIGN_OR_RETURN(std::string val, ExtractString(buf));
+    string_map.insert({std::move(key), std::move(val)});
+  }
+
+  return string_map;
+}
+
+// [string multimap] A [short] n, followed by n pair <k><v> where <k> is a
+//                   [string] and <v> is a [string list].
+StatusOr<StringMultiMap> ExtractStringMultiMap(std::string_view* buf) {
+  PL_ASSIGN_OR_RETURN(uint16_t n, ExtractShort(buf));
+
+  StringMultiMap string_multimap;
+  for (int i = 0; i < n; ++i) {
+    PL_ASSIGN_OR_RETURN(std::string key, ExtractString(buf));
+    PL_ASSIGN_OR_RETURN(StringList val, ExtractStringList(buf));
+    string_multimap.insert({std::move(key), std::move(val)});
+  }
+
+  return string_multimap;
+}
+
+}  // namespace cass
+}  // namespace stirling
+}  // namespace pl
---
+++ src/stirling/cassandra/cql_types_test.cc
@@ -0,0 +1,459 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "src/stirling/cassandra/cql_types.h"
+
+#include "src/common/testing/testing.h"
+
+using ::testing::ElementsAre;
+using ::testing::IsEmpty;
+using ::testing::Pair;
+using ::testing::UnorderedElementsAre;
+
+namespace pl {
+namespace stirling {
+namespace cass {
+
+constexpr std::string_view kEmpty = ConstStringView("");
+constexpr std::string_view kByte = ConstStringView("\x01");
+constexpr std::string_view kShort = ConstStringView("\x01\x23");
+constexpr std::string_view kInt = ConstStringView("\x01\x23\x45\x67");
+constexpr std::string_view kLong = ConstStringView("\x01\x23\x45\x67\x89\xab\xcd\xef");
+constexpr std::string_view kString = ConstStringView(
+    "\x00\x1a"
+    "abcdefghijklmnopqrstuvwxyz");
+constexpr std::string_view kEmptyString = ConstStringView("\x00\x00");
+constexpr std::string_view kLongString = ConstStringView(
+    "\x00\x00\x00\x1a"
+    "abcdefghijklmnopqrstuvwxyz");
+constexpr std::string_view kEmptyLongString = ConstStringView("\x00\x00\x00\x00");
+constexpr std::string_view kNegativeLengthLongString = ConstStringView("\xf0\x00\x00\x00");
+constexpr std::string_view kStringList = ConstStringView(
+    "\x00\x03"
+    "\x00\x1a"
+    "abcdefghijklmnopqrstuvwxyz"
+    "\x00\x06"
+    "abcdef"
+    "\x00\x05"
+    "pixie");
+constexpr std::string_view kBytes = ConstStringView(
+    "\x00\x00\x00\x04"
+    "\x01\x02\x03\x04");
+constexpr std::string_view kEmptyBytes = ConstStringView("\x00\x00\x00\x00");
+constexpr std::string_view kNegativeLengthBytes = ConstStringView("\xf0\x00\x00\x00");
+constexpr std::string_view kShortBytes = ConstStringView(
+    "\x00\x04"
+    "\x01\x02\x03\x04");
+constexpr std::string_view kEmptyShortBytes = ConstStringView("\x00\x00");
+constexpr std::string_view kStringMap = ConstStringView(
+    "\x00\x03"
+    "\x00\x04"
+    "key1"
+    "\x00\x06"
+    "value1"
+    "\x00\x01"
+    "k"
+    "\x00\x01"
+    "v"
+    "\x00\x08"
+    "question"
+    "\x00\x06"
+    "answer");
+constexpr std::string_view kEmptyStringMap = ConstStringView("\x00\x00");
+constexpr std::string_view kStringMultiMap = ConstStringView(
+    "\x00\x02"
+    "\x00\x03"
+    "USA"
+    "\x00\x02"
+    "\x00\x08"
+    "New York"
+    "\x00\x0d"
+    "San Francisco"
+    "\x00\x06"
+    "Canada"
+    "\x00\x03"
+    "\x00\x07"
+    "Toronto"
+    "\x00\x08"
+    "Montreal"
+    "\x00\x09"
+    "Vancouver");
+constexpr std::string_view kEmptyStringMultiMap = ConstStringView("\x00\x00");
+
+//------------------------
+// ExtractInt
+//------------------------
+
+TEST(ExtractInt, Exact) {
+  std::string_view buf(kInt);
+  ASSERT_OK_AND_EQ(ExtractInt(&buf), 0x01234567);
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractInt, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractInt(&buf));
+}
+
+TEST(ExtractInt, Undersized) {
+  std::string_view buf(kInt.data(), kInt.size() - 1);
+  ASSERT_NOT_OK(ExtractInt(&buf));
+}
+
+TEST(ExtractInt, Oversized) {
+  std::string_view buf(kInt.data(), kInt.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractInt(&buf), 0x01234567);
+  ASSERT_FALSE(buf.empty());
+}
+
+//------------------------
+// ExtractShort
+//------------------------
+
+TEST(ExtractShort, Exact) {
+  std::string_view buf(kShort);
+  ASSERT_OK_AND_EQ(ExtractShort(&buf), 0x0123);
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractShort, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractShort(&buf));
+}
+
+TEST(ExtractShort, Undersized) {
+  std::string_view buf(kShort.data(), kShort.size() - 1);
+  ASSERT_NOT_OK(ExtractShort(&buf));
+}
+
+TEST(ExtractShort, Oversized) {
+  std::string_view buf(kShort.data(), kShort.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractShort(&buf), 0x0123);
+  ASSERT_FALSE(buf.empty());
+}
+
+//------------------------
+// ExtractLong
+//------------------------
+
+TEST(ExtractLong, Exact) {
+  std::string_view buf(kLong);
+  ASSERT_OK_AND_EQ(ExtractLong(&buf), 0x0123456789abcdef);
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractLong, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractLong(&buf));
+}
+
+TEST(ExtractLong, Undersized) {
+  std::string_view buf(kLong.data(), kLong.size() - 1);
+  ASSERT_NOT_OK(ExtractLong(&buf));
+}
+
+TEST(ExtractLong, Oversized) {
+  std::string_view buf(kLong.data(), kLong.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractLong(&buf), 0x0123456789abcdef);
+  ASSERT_FALSE(buf.empty());
+}
+
+//------------------------
+// ExtractByte
+//------------------------
+
+TEST(ExtractByte, Exact) {
+  std::string_view buf(kByte);
+  ASSERT_OK_AND_EQ(ExtractByte(&buf), 0x01);
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractByte, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractByte(&buf));
+}
+
+TEST(ExtractByte, Undersized) {
+  std::string_view buf(kByte.data(), kByte.size() - 1);
+  ASSERT_NOT_OK(ExtractByte(&buf));
+}
+
+TEST(ExtractByte, Oversized) {
+  std::string_view buf(kByte.data(), kByte.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractByte(&buf), 0x01);
+  ASSERT_FALSE(buf.empty());
+}
+
+//------------------------
+// ExtractString
+//------------------------
+
+TEST(ExtractString, Exact) {
+  std::string_view buf(kString);
+  ASSERT_OK_AND_EQ(ExtractString(&buf), std::string("abcdefghijklmnopqrstuvwxyz"));
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractString, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractString(&buf));
+}
+
+TEST(ExtractString, Undersized) {
+  std::string_view buf(kString.data(), kString.size() - 1);
+  ASSERT_NOT_OK(ExtractString(&buf));
+}
+
+TEST(ExtractString, Oversized) {
+  std::string_view buf(kString.data(), kString.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractString(&buf), std::string("abcdefghijklmnopqrstuvwxyz"));
+  ASSERT_FALSE(buf.empty());
+}
+
+TEST(ExtractString, EmptyString) {
+  std::string_view buf(kEmptyString);
+  ASSERT_OK_AND_THAT(ExtractString(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+//------------------------
+// ExtractLongString
+//------------------------
+
+TEST(ExtractLongString, Exact) {
+  std::string_view buf(kLongString);
+  ASSERT_OK_AND_EQ(ExtractLongString(&buf), std::string("abcdefghijklmnopqrstuvwxyz"));
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractLongString, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractLongString(&buf));
+}
+
+TEST(ExtractLongString, Undersized) {
+  std::string_view buf(kLongString.data(), kLongString.size() - 1);
+  ASSERT_NOT_OK(ExtractLongString(&buf));
+}
+
+TEST(ExtractLongString, Oversized) {
+  std::string_view buf(kLongString.data(), kLongString.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractLongString(&buf), std::string("abcdefghijklmnopqrstuvwxyz"));
+  ASSERT_FALSE(buf.empty());
+}
+
+TEST(ExtractLongString, EmptyString) {
+  std::string_view buf(kEmptyLongString);
+  ASSERT_OK_AND_THAT(ExtractLongString(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractLongString, NegativeLengthString) {
+  std::string_view buf(kNegativeLengthLongString);
+  ASSERT_OK_AND_THAT(ExtractLongString(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+//------------------------
+// ExtractStringList
+//------------------------
+
+TEST(ExtractStringList, Exact) {
+  std::string_view buf(kStringList);
+  ASSERT_OK_AND_THAT(ExtractStringList(&buf),
+                     ElementsAre(std::string("abcdefghijklmnopqrstuvwxyz"), std::string("abcdef"),
+                                 std::string("pixie")));
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractStringList, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractStringList(&buf));
+}
+
+TEST(ExtractStringList, Undersized) {
+  std::string_view buf(kStringList.data(), kStringList.size() - 1);
+  ASSERT_NOT_OK(ExtractStringList(&buf));
+}
+
+TEST(ExtractStringList, Oversized) {
+  std::string_view buf(kStringList.data(), kStringList.size() + 1);
+  ASSERT_OK_AND_THAT(ExtractStringList(&buf),
+                     ElementsAre(std::string("abcdefghijklmnopqrstuvwxyz"), std::string("abcdef"),
+                                 std::string("pixie")));
+  ASSERT_FALSE(buf.empty());
+}
+
+TEST(ExtractStringList, BadElement) {
+  std::string buf(kStringList);
+  // Change size encoding of first string in the list.
+  buf[3] = 1;
+  std::string_view buf_view(buf);
+  ASSERT_NOT_OK(ExtractStringList(&buf_view));
+}
+
+//------------------------
+// ExtractBytes
+//------------------------
+
+TEST(ExtractBytes, Exact) {
+  std::string_view buf(kBytes);
+  ASSERT_OK_AND_EQ(ExtractBytes(&buf), std::basic_string<uint8_t>({0x01, 0x02, 0x03, 0x04}));
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractBytes, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractBytes(&buf));
+}
+
+TEST(ExtractBytes, Undersized) {
+  std::string_view buf(kBytes.data(), kBytes.size() - 1);
+  ASSERT_NOT_OK(ExtractBytes(&buf));
+}
+
+TEST(ExtractBytes, Oversized) {
+  std::string_view buf(kBytes.data(), kBytes.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractBytes(&buf), std::basic_string<uint8_t>({0x01, 0x02, 0x03, 0x04}));
+  ASSERT_FALSE(buf.empty());
+}
+
+TEST(ExtractBytes, EmptyBytes) {
+  std::string_view buf(kEmptyBytes);
+  ASSERT_OK_AND_THAT(ExtractBytes(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractBytes, NegativeLengthString) {
+  std::string_view buf(kNegativeLengthBytes);
+  ASSERT_OK_AND_THAT(ExtractBytes(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+//------------------------
+// ExtractShortBytes
+//------------------------
+
+TEST(ExtractShortBytes, Exact) {
+  std::string_view buf(kShortBytes);
+  ASSERT_OK_AND_EQ(ExtractShortBytes(&buf), std::basic_string<uint8_t>({0x01, 0x02, 0x03, 0x04}));
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractShortBytes, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractShortBytes(&buf));
+}
+
+TEST(ExtractShortBytes, Undersized) {
+  std::string_view buf(kShortBytes.data(), kShortBytes.size() - 1);
+  ASSERT_NOT_OK(ExtractShortBytes(&buf));
+}
+
+TEST(ExtractShortBytes, Oversized) {
+  std::string_view buf(kShortBytes.data(), kShortBytes.size() + 1);
+  ASSERT_OK_AND_EQ(ExtractShortBytes(&buf), std::basic_string<uint8_t>({0x01, 0x02, 0x03, 0x04}));
+  ASSERT_FALSE(buf.empty());
+}
+
+TEST(ExtractShortBytes, EmptyBytes) {
+  std::string_view buf(kEmptyShortBytes);
+  ASSERT_OK_AND_THAT(ExtractShortBytes(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+//------------------------
+// ExtractStringMap
+//------------------------
+
+TEST(ExtractStringMap, Exact) {
+  std::string_view buf(kStringMap);
+  ASSERT_OK_AND_THAT(
+      ExtractStringMap(&buf),
+      UnorderedElementsAre(Pair("key1", "value1"), Pair("k", "v"), Pair("question", "answer")));
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractStringMap, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractStringMap(&buf));
+}
+
+TEST(ExtractStringMap, Undersized) {
+  std::string_view buf(kStringMap.data(), kStringMap.size() - 1);
+  ASSERT_NOT_OK(ExtractStringMap(&buf));
+}
+
+TEST(ExtractStringMap, Oversized) {
+  std::string_view buf(kStringMap.data(), kStringMap.size() + 1);
+  ASSERT_OK_AND_THAT(
+      ExtractStringMap(&buf),
+      UnorderedElementsAre(Pair("key1", "value1"), Pair("k", "v"), Pair("question", "answer")));
+  ASSERT_FALSE(buf.empty());
+}
+
+TEST(ExtractStringMap, EmptyMap) {
+  std::string_view buf(kEmptyStringMap);
+  ASSERT_OK_AND_THAT(ExtractStringMap(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+//------------------------
+// ExtractStringMultiMap
+//------------------------
+
+TEST(ExtractStringMultiMap, Exact) {
+  std::string_view buf(kStringMultiMap);
+  ASSERT_OK_AND_THAT(
+      ExtractStringMultiMap(&buf),
+      UnorderedElementsAre(Pair("USA", ElementsAre("New York", "San Francisco")),
+                           Pair("Canada", ElementsAre("Toronto", "Montreal", "Vancouver"))));
+  ASSERT_TRUE(buf.empty());
+}
+
+TEST(ExtractStringMultiMap, Empty) {
+  std::string_view buf(kEmpty);
+  ASSERT_NOT_OK(ExtractStringMultiMap(&buf));
+}
+
+TEST(ExtractStringMultiMap, Undersized) {
+  std::string_view buf(kStringMultiMap.data(), kStringMultiMap.size() - 1);
+  ASSERT_NOT_OK(ExtractStringMultiMap(&buf));
+}
+
+TEST(ExtractStringMultiMap, Oversized) {
+  std::string_view buf(kStringMultiMap.data(), kStringMultiMap.size() + 1);
+  ASSERT_OK_AND_THAT(
+      ExtractStringMultiMap(&buf),
+      UnorderedElementsAre(Pair("USA", ElementsAre("New York", "San Francisco")),
+                           Pair("Canada", ElementsAre("Toronto", "Montreal", "Vancouver"))));
+  ASSERT_FALSE(buf.empty());
+}
+
+TEST(ExtractStringMultiMap, EmptyMap) {
+  std::string_view buf(kEmptyStringMultiMap);
+  ASSERT_OK_AND_THAT(ExtractStringMultiMap(&buf), IsEmpty());
+  ASSERT_TRUE(buf.empty());
+}
+
+//------------------------
+// ExtractUUID
+//------------------------
+
+// TODO(oazizi): Add tests.
+
+//------------------------
+// ExtractInet
+//------------------------
+
+// TODO(oazizi): Add tests.
+
+//------------------------
+// ExtractConsistency
+//------------------------
+
+// TODO(oazizi): Add tests.
+
+}  // namespace cass
+}  // namespace stirling
+}  // namespace pl
