6d66e51c063b6b81ce980460cc252eb24718828c
Yaxiong Zhao <yzhao@pixielabs.ai>
2020-02-19 10:51:23 -0800

    Revert D3544
    Summary: This reverts commit 4369802046b633c6a1842e65ff5642726bbd6512.
    
    Test Plan: revert-hammer
    
    Reviewers: #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3547
    
    GitOrigin-RevId: 8cf270f8a2325d8512c37889d3a6d0b6030f8a35

--- src/stirling/bpf_tools/bcc.cc
+++
@@ -1,8 +0,0 @@
-#include "src/common/base/base.h"
-#include "src/stirling/bpf_tools/bcc_wrapper.h"
-
-int main(int argc, char** argv) {
-  pl::InitEnvironmentOrDie(&argc, argv);
-
-  pl::ShutdownEnvironmentOrDie();
-}
