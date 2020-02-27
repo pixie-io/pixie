6823058ec499cd4541392646d46083aa55c20b60
Malthus Yau <malthus@pixielabs.ai>
2020-02-27 12:38:10 -0800

    Add a util function to debounce function calls.
    Summary: This will be used in the coming diffs to prevent rerendering the live view on every keystroke.
    
    Test Plan: unit test
    
    Reviewers: michelle, #engineering, zasgar
    
    Reviewed By: #engineering, zasgar
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3738
    
    GitOrigin-RevId: 8de6c29677a6fca13b317792a7a712c0ba091065

---
+++ src/ui/src/utils/debounce-test.ts
@@ -0,0 +1,63 @@
+import {debounce} from './debounce';
+
+describe('debounce test', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  it('doesn\'t call method before debounce time', () => {
+    const func = jest.fn();
+    const call = debounce(func, 20);
+    call();
+    jest.advanceTimersByTime(10);
+    expect(func.mock.calls).toHaveLength(0);
+    jest.clearAllTimers();
+  });
+
+  it('calls the method after debounce time', () => {
+    const func = jest.fn();
+    const call = debounce(func, 20);
+    call();
+    jest.advanceTimersByTime(20);
+    expect(func).toHaveBeenCalled();
+  });
+
+  it('debounces the calls', () => {
+    const func = jest.fn();
+    const call = debounce(func, 20);
+    for (let i = 0; i < 10; i++) {
+      call();
+    }
+    jest.advanceTimersByTime(20);
+    expect(func).toHaveBeenCalledTimes(1);
+  });
+
+  it('resets the timer on later calls', () => {
+    const func = jest.fn();
+    const call = debounce(func, 20);
+    call();
+    jest.advanceTimersByTime(10);
+    expect(func).not.toHaveBeenCalled();
+    call();
+    jest.advanceTimersByTime(10);
+    expect(func).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(10);
+    expect(func).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls the function with the right arguments', () => {
+    const func = jest.fn();
+    const call = debounce(func, 20);
+    call('abc');
+    jest.advanceTimersByTime(10);
+    expect(func).not.toHaveBeenCalled();
+    call('def');
+    jest.advanceTimersByTime(10);
+    expect(func).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(10);
+    expect(func).toHaveBeenCalledTimes(1);
+    expect(func).toHaveBeenLastCalledWith('def');
+  });
+});
---
+++ src/ui/src/utils/debounce.ts
@@ -0,0 +1,19 @@
+export function debounce(func: (args: Array<{}>) => void, wait: number) {
+  let timeout;
+  let context;
+  let lastArgs;
+  return function call(...args: Array<{}>) {
+    context = this;
+    lastArgs = args;
+    const onTimeout = () => {
+      func.apply(context, lastArgs);
+      timeout = null;
+    };
+    const newTimeout = setTimeout(onTimeout, wait);
+    if (timeout) {
+      // Restart the timer if one already exists.
+      clearTimeout(timeout);
+    }
+    timeout = newTimeout;
+  };
+}
