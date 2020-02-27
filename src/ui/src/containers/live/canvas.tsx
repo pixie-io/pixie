ce91285357f26b1fc82509f6065b1f748835c594
Malthus Yau <malthus@pixielabs.ai>
2020-02-27 14:34:15 -0800

    Add react-grid-layout to the live view
    Summary: Use react-grid-layout to tile the live view.
    
    Test Plan: tested manually
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Subscribers: philkuz
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3661
    
    GitOrigin-RevId: 5c2bfeaa50ce4ce30c1f7b5999c8f9839734ccc7

--- src/ui/src/containers/live/canvas.tsx
+++ src/ui/src/containers/live/canvas.tsx
@@ -1,18 +1,42 @@
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
 import {parseSpecs} from 'components/vega/spec';
 import Vega from 'components/vega/vega';
 import * as React from 'react';
+import * as GridLayout from 'react-grid-layout';
 import {AutoSizer} from 'react-virtualized';
 
 import {ResultsContext, VegaContext} from './context';
+import {buildLayout, VisualizationSpecMap} from './layout';
 
 const Canvas = () => {
   const inputJSON = React.useContext(VegaContext);
   const results = React.useContext(ResultsContext);
 
-  const spec = React.useMemo(() => parseSpecs(inputJSON), [inputJSON]);
-  const tableName = spec.data && (spec.data as { name: string }).name || 'output';
-  const data = results[tableName] || [];
+  const specs = React.useMemo(() => parseSpecs(inputJSON), [inputJSON]);
+  const charts = React.useMemo(() => {
+    return Object.keys(specs).map((chartName) => {
+      const spec = specs[chartName];
+      const tableName = spec.data && (spec.data as { name: string }).name || 'output';
+      const data = results[tableName] || [];
+      return (
+        <div key={chartName}>
+          <Vega data={data} spec={spec} tableName={tableName} />;
+        </div>
+      );
+    });
+  }, [results, specs]);
 
+  const layout = React.useMemo(() => {
+    return buildLayout(specs as VisualizationSpecMap).charts.map((chart) => {
+      const pos = chart.position;
+      return {
+        ...pos,
+        i: chart.vegaKey,
+      };
+    });
+  }, [specs]);
 
   const resize = React.useCallback(() => {
     // Dispatch a window resize event to signal the chart to redraw. As suggested in:
@@ -23,10 +47,14 @@ const Canvas = () => {
   return (
     <AutoSizer onResize={resize}>
       {({ height, width }) => (
-        // Grid placeholder
-        <div style={{ width, height }}>
-          <Vega data={data} spec={spec} tableName={tableName} />
-        </div>
+        <GridLayout
+          style={{ width, height }}
+          width={width}
+          layout={layout}
+          onLayoutChange={resize}
+        >
+          {charts}
+        </GridLayout>
       )}
     </AutoSizer >
   );
