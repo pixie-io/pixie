28edf6208646fb6b4c4da5c67f1a8155bc81ba4c
Michelle Nguyen <michelle@pixielabs.ai>
2020-03-03 11:24:23 -0800

    Implement metadata queue reader
    Summary:
    The metadata queue reader reads from the DurableMetadataUpdate channel sends the metadata updates in order to the indexer.
    if it finds that it receives an out of order metadata update, it requests for the correct metadata updates through nats.
    
    currently there is no code which calls StartVizierUpdates, so nothing actually runs yet. that will come in a later diff.
    
    Test Plan: unit tests, ran skaffold
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3712
    
    GitOrigin-RevId: 00686154c5873c0a7ba3a0a107acb3420c664407

---
+++ src/cloud/vzmgr/controller/metadata_reader.go
@@ -0,0 +1,320 @@
+package controller
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/types"
+	"github.com/jmoiron/sqlx"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
+	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+
+	"pixielabs.ai/pixielabs/src/cloud/shared/vzshard"
+	"pixielabs.ai/pixielabs/src/shared/cvmsgspb"
+	metadatapb "pixielabs.ai/pixielabs/src/shared/k8s/metadatapb"
+)
+
+// The topic on which to make metadata requests.
+const metadataRequestTopic = "MetadataRequest"
+
+// The topic on which to listen to metadata responses.
+const metadataResponseTopic = "MetadataResponse"
+
+// The topic on which to listen for streaming metadata updates.
+const streamingMetadataTopic = "DurableMetadataUpdates"
+
+// The topic on which to write updates to.
+const indexerMetadataTopic = "MetadataIndex"
+
+// VizierState contains all state necessary to process metadata updates for the given vizier.
+type VizierState struct {
+	id              uuid.UUID // The Vizier's ID.
+	resourceVersion string    // The Vizier's last applied resource version.
+
+	liveSub stan.Subscription // The subcription for the live metadata updates.
+	liveCh  chan *stan.Msg
+
+	quitCh chan bool // Channel to sign a stop for a particular vizier
+}
+
+// MetadataReader reads updates from the NATS durable queue and sends updates to the indexer.
+type MetadataReader struct {
+	db *sqlx.DB
+
+	sc stan.Conn
+	nc *nats.Conn
+
+	viziers map[uuid.UUID]*VizierState // Map of Vizier ID to its state.
+	mu      sync.Mutex                 // Mutex for viziers map.
+
+	quitCh chan bool // Channel to signal a stop for all viziers
+}
+
+// NewMetadataReader creates a new MetadataReader.
+func NewMetadataReader(db *sqlx.DB, sc stan.Conn, nc *nats.Conn) (*MetadataReader, error) {
+	viziers := make(map[uuid.UUID]*VizierState)
+
+	m := &MetadataReader{db: db, sc: sc, nc: nc, viziers: viziers}
+	err := m.loadState()
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *MetadataReader) loadState() error {
+	// TOOD(michelle): This should get all currently running Viziers and run StartVizierUpdates() for each of them.
+	// This will come in a followup diff.
+	return nil
+}
+
+// StartVizierUpdates starts listening to the metadata update channel for a given vizier.
+func (m *MetadataReader) StartVizierUpdates(id uuid.UUID, rv string) error {
+	// TODO(michelle): We currently don't have to signal when a Vizier has disconnected. When we have that
+	// functionality, we should clean up the Vizier map and stop its STAN subscriptions.
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if _, ok := m.viziers[id]; ok {
+		log.WithField("vizier_id", id.String()).Info("Already listening to metadata updates from Vizier")
+		return nil
+	}
+
+	vzState := VizierState{
+		id:              id,
+		resourceVersion: rv,
+		liveCh:          make(chan *stan.Msg),
+		quitCh:          make(chan bool),
+	}
+
+	// Subscribe to STAN topic for streaming updates.
+	topic := fmt.Sprintf("v2c.%s.%s.%s", vzshard.VizierIDToShard(id), id.String(), streamingMetadataTopic)
+	log.WithField("topic", topic).Info("Subscribing to STAN")
+	liveSub, err := m.sc.Subscribe(topic, func(msg *stan.Msg) {
+		vzState.liveCh <- msg
+	}, stan.SetManualAckMode())
+	if err != nil {
+		close(vzState.liveCh)
+		close(vzState.quitCh)
+		return err
+	}
+
+	vzState.liveSub = liveSub
+
+	m.viziers[id] = &vzState
+
+	go m.ProcessVizierUpdates(&vzState)
+
+	return nil
+}
+
+// StopVizierUpdates stops listening to the metadata update channel for a given vizier.
+func (m *MetadataReader) StopVizierUpdates(id uuid.UUID) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if vz, ok := m.viziers[id]; ok {
+		vz.liveSub.Unsubscribe()
+		close(vz.liveCh)
+		close(vz.quitCh)
+
+		delete(m.viziers, id)
+		return nil
+	}
+
+	return errors.New("Vizier doesn't exist")
+}
+
+// ProcessVizierUpdates reads from the metadata updates and sends them to the indexer in order.
+func (m *MetadataReader) ProcessVizierUpdates(vzState *VizierState) error {
+	// Clean up if any error has occurred.
+	defer m.StopVizierUpdates(vzState.id)
+
+	for {
+		select {
+		case <-m.quitCh:
+			return nil
+		case <-vzState.quitCh:
+			return nil
+		case msg := <-vzState.liveCh:
+			err := m.processVizierUpdate(msg, vzState)
+			if err != nil {
+				log.WithError(err).Error("Error processing Vizier metadata updates")
+				return err
+			}
+		}
+	}
+}
+
+func readMetadataUpdate(data []byte) (*cvmsgspb.MetadataUpdate, error) {
+	v2cMsg := &cvmsgspb.V2CMessage{}
+	err := proto.Unmarshal(data, v2cMsg)
+	if err != nil {
+		return nil, err
+	}
+	updateMsg := &cvmsgspb.MetadataUpdate{}
+	err = types.UnmarshalAny(v2cMsg.Msg, updateMsg)
+	if err != nil {
+		log.WithError(err).Error("Could not unmarshal metadata update message")
+		return nil, err
+	}
+	return updateMsg, nil
+}
+
+func readMetadataResponse(data []byte) (*cvmsgspb.MetadataResponse, error) {
+	v2cMsg := &cvmsgspb.V2CMessage{}
+	err := proto.Unmarshal(data, v2cMsg)
+	if err != nil {
+		return nil, err
+	}
+	reqUpdates := &cvmsgspb.MetadataResponse{}
+	err = types.UnmarshalAny(v2cMsg.Msg, reqUpdates)
+	if err != nil {
+		log.WithError(err).Error("Could not unmarshal metadata response message")
+		return nil, err
+	}
+	return reqUpdates, nil
+}
+
+func wrapMetadataRequest(vizierID uuid.UUID, req *cvmsgspb.MetadataRequest) ([]byte, error) {
+	reqAnyMsg, err := types.MarshalAny(req)
+	if err != nil {
+		return nil, err
+	}
+	c2vMsg := cvmsgspb.C2VMessage{
+		VizierID: vizierID.String(),
+		Msg:      reqAnyMsg,
+	}
+	b, err := c2vMsg.Marshal()
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (m *MetadataReader) processVizierUpdate(msg *stan.Msg, vzState *VizierState) error {
+	if msg == nil {
+		return nil
+	}
+
+	updateMsg, err := readMetadataUpdate(msg.Data)
+	if err != nil {
+		return err
+	}
+	update := updateMsg.Update
+
+	if update.PrevResourceVersion == vzState.resourceVersion {
+		// Update is in correct order, so send off the update.
+		err := m.applyMetadataUpdates(vzState, []*metadatapb.ResourceUpdate{update})
+		if err != nil {
+			return err
+		}
+	} else if update.PrevResourceVersion > vzState.resourceVersion {
+		// We received an update later than we one we need next. Send out a request for the missing updates.
+		updatesMsg, err := m.getMissingUpdates(vzState.resourceVersion, update.ResourceVersion, vzState)
+		if err != nil {
+			return err
+		}
+
+		reqUpdates, err := readMetadataResponse(updatesMsg.Data)
+		if err != nil {
+			return err
+		}
+
+		allUpdates := append(reqUpdates.Updates, update)
+		err = m.applyMetadataUpdates(vzState, allUpdates)
+		if err != nil {
+			return err
+		}
+	}
+
+	// It's possible that we've missed the message's 30-second ACK timeline, if it took a while to receive
+	// missing metadata updates. This message will be sent back on STAN for reprocessing, but we will
+	// ignore it since the ResourceVersion will be less than the current resource version.
+	msg.Ack()
+	return nil
+}
+
+func (m *MetadataReader) getMissingUpdates(from string, to string, vzState *VizierState) (*nats.Msg, error) {
+	log.WithField("vizier", vzState.id.String()).WithField("from", from).WithField("to", to).Info("Making request for missing metadata updates")
+	mdReq := &cvmsgspb.MetadataRequest{
+		From: from,
+		To:   to,
+	}
+	reqBytes, err := wrapMetadataRequest(vzState.id, mdReq)
+	if err != nil {
+		return nil, err
+	}
+
+	// Subscribe to topic that the response will be sent on.
+	subCh := make(chan *nats.Msg)
+	sub, err := m.nc.ChanSubscribe(fmt.Sprintf("v2c.%s.%s.%s", vzshard.VizierIDToShard(vzState.id), vzState.id.String(), metadataResponseTopic), subCh)
+	defer sub.Unsubscribe()
+
+	pubTopic := fmt.Sprintf("c2v.%s.%s", vzState.id.String(), metadataRequestTopic)
+	err = m.nc.Publish(pubTopic, reqBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		select {
+		case <-m.quitCh:
+			return nil, errors.New("Quit signaled")
+		case <-vzState.quitCh:
+			return nil, errors.New("Vizier removed")
+		case msg := <-subCh:
+			return msg, nil
+		case <-time.After(20 * time.Minute):
+			// Our previous request shouldn't have gotten lost on NATS, so if there is a subscriber for the metadata
+			// requests we shouldn't actually need to resend the request.
+			log.WithField("vizier", vzState.id.String()).Info("Retrying missing metadata request")
+			err = m.nc.Publish(pubTopic, reqBytes)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+}
+
+func (m *MetadataReader) applyMetadataUpdates(vzState *VizierState, updates []*metadatapb.ResourceUpdate) error {
+	for i, u := range updates {
+		if u.ResourceVersion <= vzState.resourceVersion {
+			continue // Don't send the update.
+		}
+
+		// Publish update to the indexer.
+		b, err := updates[i].Marshal()
+		if err != nil {
+			return err
+		}
+
+		log.WithField("topic", indexerMetadataTopic).WithField("rv", u.ResourceVersion).Info("Publishing metadata update to indexer")
+
+		err = m.sc.Publish(indexerMetadataTopic, b)
+		if err != nil {
+			return err
+		}
+
+		vzState.resourceVersion = u.ResourceVersion
+
+		// Update index state in Postgres.
+		query := `
+	    UPDATE vizier_index_state
+	    SET resource_version = $2
+	    WHERE cluster_id = $1`
+		row, err := m.db.Queryx(query, vzState.id, u.ResourceVersion)
+		if err != nil {
+			return err
+		}
+		defer row.Close()
+	}
+	return nil
+}
---
+++ src/cloud/vzmgr/controller/metadata_reader_test.go
@@ -0,0 +1,183 @@
+package controller_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/types"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/stretchr/testify/assert"
+	"pixielabs.ai/pixielabs/src/cloud/shared/vzshard"
+	"pixielabs.ai/pixielabs/src/cloud/vzmgr/controller"
+	"pixielabs.ai/pixielabs/src/shared/cvmsgspb"
+	metadatapb "pixielabs.ai/pixielabs/src/shared/k8s/metadatapb"
+	"pixielabs.ai/pixielabs/src/utils/testingutils"
+)
+
+func TestMetadataReader_ProcessVizierUpdate(t *testing.T) {
+	tests := []struct {
+		name                    string                       // Name of the test
+		startingRV              string                       // The resourceVersion that the metadata reader is waiting for.
+		updatePrevRV            string                       // The prevResourceVersion of the update the metadata reader will receive.
+		updateRV                string                       // The resourceVersion of the update the metadata reader will receive.
+		expectMetadataRequest   bool                         // Whether to expect a request for missing metadata.
+		metadataRequestFrom     string                       // The "from" value of the metadata req.
+		metadataRequestTo       string                       // The "to" value of the metadata req.
+		metadataResponse        *cvmsgspb.MetadataResponse   // The metadata response to send.
+		expectedMetadataUpdates []*metadatapb.ResourceUpdate // The updates that should be sent to the indexer.
+		endingRV                string                       // The new resource version after the update.
+	}{
+		{
+			name:                  "In-order update",
+			startingRV:            "1234",
+			updatePrevRV:          "1234",
+			updateRV:              "1235",
+			expectMetadataRequest: false,
+			expectedMetadataUpdates: []*metadatapb.ResourceUpdate{
+				&metadatapb.ResourceUpdate{ResourceVersion: "1235", PrevResourceVersion: "1234"},
+			},
+			endingRV: "1235",
+		},
+		{
+			name:                  "out-of-order update",
+			startingRV:            "12",
+			updatePrevRV:          "16",
+			updateRV:              "17",
+			expectMetadataRequest: true,
+			metadataRequestFrom:   "12",
+			metadataRequestTo:     "17",
+			metadataResponse: &cvmsgspb.MetadataResponse{
+				Updates: []*metadatapb.ResourceUpdate{
+					&metadatapb.ResourceUpdate{ResourceVersion: "12", PrevResourceVersion: "11"},
+					&metadatapb.ResourceUpdate{ResourceVersion: "13", PrevResourceVersion: "12"},
+					&metadatapb.ResourceUpdate{ResourceVersion: "15", PrevResourceVersion: "13"},
+					&metadatapb.ResourceUpdate{ResourceVersion: "16", PrevResourceVersion: "15"},
+				},
+			},
+			expectedMetadataUpdates: []*metadatapb.ResourceUpdate{
+				&metadatapb.ResourceUpdate{ResourceVersion: "13", PrevResourceVersion: "12"},
+				&metadatapb.ResourceUpdate{ResourceVersion: "15", PrevResourceVersion: "13"},
+				&metadatapb.ResourceUpdate{ResourceVersion: "16", PrevResourceVersion: "15"},
+				&metadatapb.ResourceUpdate{ResourceVersion: "17", PrevResourceVersion: "16"},
+			},
+			endingRV: "17",
+		},
+		{
+			name:                    "duplicateUpdate",
+			startingRV:              "12",
+			updatePrevRV:            "1",
+			updateRV:                "2",
+			expectMetadataRequest:   false,
+			expectedMetadataUpdates: []*metadatapb.ResourceUpdate{},
+			endingRV:                "12",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			vzID := uuid.NewV4()
+
+			db, teardown := setupTestDB(t)
+			defer teardown()
+
+			// Set up initial DB state.
+			insertIdxStateQuery := `INSERT INTO vizier_index_state(cluster_id, resource_version) VALUES ($1, $2)`
+			db.MustExec(insertIdxStateQuery, vzID, test.startingRV)
+
+			natsPort, natsCleanup := testingutils.StartNATS(t)
+			nc, err := nats.Connect(testingutils.GetNATSURL(natsPort))
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer natsCleanup()
+
+			_, sc, stanCleanup := testingutils.StartStan(t, "test-stan", "test-client")
+			defer stanCleanup()
+
+			idxCh := make(chan *stan.Msg)
+			indexerSub, err := sc.Subscribe("MetadataIndex", func(msg *stan.Msg) {
+				idxCh <- msg
+			})
+			defer indexerSub.Unsubscribe()
+
+			if test.expectMetadataRequest {
+				mdSub, err := nc.Subscribe(fmt.Sprintf("c2v.%s.MetadataRequest", vzID.String()), func(msg *nats.Msg) {
+					c2vMsg := &cvmsgspb.C2VMessage{}
+					err := proto.Unmarshal(msg.Data, c2vMsg)
+					assert.Nil(t, err)
+					req := &cvmsgspb.MetadataRequest{}
+					err = types.UnmarshalAny(c2vMsg.Msg, req)
+					assert.Nil(t, err)
+					assert.Equal(t, test.metadataRequestTo, req.To)
+					assert.Equal(t, test.metadataRequestFrom, req.From)
+
+					// Send response.
+					anyUpdates, err := types.MarshalAny(test.metadataResponse)
+					assert.Nil(t, err)
+					v2cMsg := cvmsgspb.V2CMessage{
+						Msg: anyUpdates,
+					}
+					b, err := v2cMsg.Marshal()
+					assert.Nil(t, err)
+					nc.Publish(fmt.Sprintf("v2c.%s.%s.MetadataResponse", vzshard.VizierIDToShard(vzID), vzID.String()), b)
+				})
+				assert.Nil(t, err)
+				defer mdSub.Unsubscribe()
+			}
+
+			mr, err := controller.NewMetadataReader(db, sc, nc)
+			mr.StartVizierUpdates(vzID, test.startingRV)
+			defer mr.StopVizierUpdates(vzID)
+
+			// Publish update to STAN channel.
+			initialUpdate := &cvmsgspb.MetadataUpdate{
+				Update: &metadatapb.ResourceUpdate{
+					ResourceVersion:     test.updateRV,
+					PrevResourceVersion: test.updatePrevRV,
+				},
+			}
+			anyInitUpdate, err := types.MarshalAny(initialUpdate)
+			assert.Nil(t, err)
+			v2cMsg := cvmsgspb.V2CMessage{
+				Msg: anyInitUpdate,
+			}
+			b, err := v2cMsg.Marshal()
+			assert.Nil(t, err)
+
+			sc.Publish(fmt.Sprintf("v2c.%s.%s.DurableMetadataUpdates", vzshard.VizierIDToShard(vzID), vzID.String()), b)
+
+			if len(test.expectedMetadataUpdates) > 0 {
+				numUpdates := 0
+				prev := test.startingRV
+				for numUpdates < len(test.expectedMetadataUpdates) {
+					select {
+					case idxMessage := <-idxCh:
+						u := &metadatapb.ResourceUpdate{}
+						err := proto.Unmarshal(idxMessage.Data, u)
+						assert.Nil(t, err)
+						assert.Equal(t, prev, u.PrevResourceVersion)
+						prev = u.ResourceVersion
+						numUpdates++
+					case <-time.After(2 * time.Second):
+						t.Fatal("Timed out")
+					}
+				}
+			} else {
+				run := true
+				for run {
+					select {
+					case _ = <-idxCh:
+						t.Fatal("Unpexected index message")
+					case <-time.After(2 * time.Second):
+						run = false
+					}
+				}
+			}
+		})
+	}
+}
