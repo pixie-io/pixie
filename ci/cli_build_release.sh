72005151d27f2fc4cb298c812afe90029b66f853
Zain Asgar <zasgar@pixielabs.ai>
2020-03-05 13:44:27 -0800

    Make CLI publically accessible, put latest release in latest
    Summary: This will allow us to have a curl based install.
    
    Test Plan: Tested locally, will test on jenkins.
    
    Reviewers: michelle, nserrino, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3771
    
    GitOrigin-RevId: 868d78189b4bdb4b4cde64abc38915ebca8fea87

--- ci/cli_build_release.sh
+++ ci/cli_build_release.sh
@@ -17,14 +17,31 @@ bazel build -c opt --stamp //src/utils/pixie_cli:px_darwin
 
 bazel build -c opt --stamp //src/utils/pixie_cli:px
 
-mac_binary=bazel-bin/src/utils/pixie_cli/darwin_amd64_pure_stripped/px_darwin
-linux_binary=bazel-bin/src/utils/pixie_cli/linux_amd64_stripped/px
+copy_artifact_to_gcs() {
+    output_path="$1"
+    binary_path="$2"
+    name="$3"
+
+    sha256sum "${binary_path}" | awk '{print $1}' > sha
+    gsutil -h 'Content-Disposition:filename=px' cp "${binary_path}" "${output_path}/${name}"
+    gsutil cp sha "${output_path}/${name}.sha256"
+    gsutil acl ch -u allUsers:READER "${output_path}/${name}"
+    gsutil acl ch -u allUsers:READER "${output_path}/${name}.sha256"
+
+}
+
+write_artifacts_to_gcs() {
+    output_path=$1
+    mac_binary=bazel-bin/src/utils/pixie_cli/darwin_amd64_pure_stripped/px_darwin
+    linux_binary=bazel-bin/src/utils/pixie_cli/linux_amd64_stripped/px
+    copy_artifact_to_gcs "$output_path" "$mac_binary" "cli_darwin_amd64"
+    copy_artifact_to_gcs "$output_path" "$linux_binary" "cli_linux_amd64"
+}
+
 output_path="gs://pixie-prod-artifacts/cli/${release_tag}"
-
-sha256sum ${mac_binary} | awk '{print $1}' > sha
-gsutil -h 'Content-Disposition:filename=px' cp ${mac_binary} "${output_path}/cli_darwin_amd64"
-gsutil cp sha "${output_path}/cli_darwin_amd64.sha256"
-
-sha256sum ${linux_binary} | awk '{print $1}' > sha
-gsutil -h 'Content-Disposition:filename=px' cp ${linux_binary} "${output_path}/cli_linux_amd64"
-gsutil cp sha "${output_path}/cli_linux_amd64.sha256"
+write_artifacts_to_gcs "$output_path"
+# Check to see if it's production build. If so we should also write it to the latest directory.
+if [[ ! "$release_tag" == *"-"* ]]; then
+    output_path="gs://pixie-prod-artifacts/cli/latest"
+    write_artifacts_to_gcs "$output_path"
+fi
