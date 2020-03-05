b9e6134487f17391168eb2e7eeb1459c7611d510
Michelle Nguyen <michelle@pixielabs.ai>
2020-03-05 15:52:09 -0800

    Fix minor UI bugs
    Summary:
    - the placement of the loading dialog was off, since now the dialog box is wrapped in center-content. removed the padding so it doesnt look so low on the page
    - the background color of the auth_complete page was not white
    
    Test Plan: ran webpack
    
    Reviewers: malthus, #engineering, zasgar
    
    Reviewed By: #engineering, zasgar
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3777
    
    GitOrigin-RevId: 3063810811e212e99a9cfce29f7e226fa841448e

--- src/ui/src/containers/login/auth-complete.tsx
+++ src/ui/src/containers/login/auth-complete.tsx
@@ -4,17 +4,19 @@ import * as React from 'react';
 
 export const AuthComplete = ({ location }) => {
   const { err, siteName } = QueryString.parse(location.search);
-  return (<DialogBox>
-    <p className='pixie-auth-complete-msg'>
-      {!err ? 'Authentication successful. Please close this page.' :
-        err === 'token' ?
-          (<>
-            Authentication failed. Please make sure the account has
-            <br />
-            permission to use the site <strong>{siteName}</strong>.
-          </>) :
-          'Authentication failed. Please try again later.'
-      }
-    </p>
-  </DialogBox >);
+  return (<div className='center-content' style={{ background: 'white' }}>
+    <DialogBox>
+      <p className='pixie-auth-complete-msg'>
+        {!err ? 'Authentication successful. Please close this page.' :
+          err === 'token' ?
+            (<>
+              Authentication failed. Please make sure the account has
+              <br />
+              permission to use the site <strong>{siteName}</strong>.
+            </>) :
+            'Authentication failed. Please try again later.'
+        }
+      </p>
+    </DialogBox >
+  </div>);
 };
