b60c0d49d2b52ed51e2e68ac9af0047e5a20809d
James Bartlett <james@pixielabs.ai>
2020-03-09 16:31:00 -0700

    Update NATS watcher to handle V2C messages
    Summary:
    Depends on D3789.
    This updates the Vizier NATS watcher to handle V2CMessages as well as VizierMessages.
    
    Test Plan: Ran skaffold_nats_watcher and made sure I could see V2CMessages being printed out.
    
    Reviewers: #engineering, zasgar
    
    Reviewed By: #engineering, zasgar
    
    Subscribers: zasgar
    
    Differential Revision: https://phab.c.pixielabs.ai/D3790
    
    GitOrigin-RevId: ab836069de138f2245f597cb6c18cb6653496daf

--- src/vizier/utils/nats_watcher/nats_watcher.go
+++ src/vizier/utils/nats_watcher/nats_watcher.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 	"time"
+
+	messages "pixielabs.ai/pixielabs/src/vizier/messages/messagespb"
+
+	"pixielabs.ai/pixielabs/src/shared/cvmsgspb"
 
 	"github.com/fatih/color"
 	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/types"
 	"github.com/nats-io/nats.go"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 
 	"pixielabs.ai/pixielabs/src/shared/services"
-	messages "pixielabs.ai/pixielabs/src/vizier/messages/messagespb"
 )
 
 func connectNATS() *nats.Conn {
@@ -38,6 +44,50 @@ func connectNATS() *nats.Conn {
 	}
 
 	return nc
+}
+
+func getProtoFromAny(any *types.Any) (proto.Message, bool) {
+	slash := strings.LastIndex(any.TypeUrl, "/")
+	if slash < 0 {
+		log.Errorf("Invalid any type url: %s", any.TypeUrl)
+		return nil, false
+	}
+	typeName := any.TypeUrl[slash+1:]
+	messageType := proto.MessageType(typeName)
+	if messageType == nil {
+		log.Errorf("Cannot find %s type in protobuf registries, probably missing a proto import", typeName)
+	}
+	pb := reflect.New(messageType.Elem()).Interface().(proto.Message)
+	return pb, true
+}
+
+func handleV2CMessage(m *nats.Msg) {
+	pb := &cvmsgspb.V2CMessage{}
+	err := proto.Unmarshal(m.Data, pb)
+	if err != nil {
+		log.WithError(err).Error("Failed to unmarshal V2CMessage.")
+		return
+	}
+
+	if pb.Msg == nil {
+		log.Errorf("Invalid msg: %s", proto.MarshalTextString(pb))
+	}
+	innerPb, ok := getProtoFromAny(pb.Msg)
+	if !ok {
+		return
+	}
+
+	if err := proto.Unmarshal(pb.Msg.Value, innerPb); err != nil {
+		log.WithError(err).Error("Failed to unmarshal inner message.")
+		return
+	}
+
+	red := color.New(color.FgRed).SprintfFunc()
+
+	fmt.Printf("----------------------------------------------------------\n")
+	fmt.Printf("%s=%s\n", red("Subject"), m.Subject)
+	fmt.Printf("%s\n", proto.MarshalTextString(innerPb))
+	fmt.Printf("----------------------------------------------------------\n")
 }
 
 func handleNATSMessage(m *nats.Msg) {
@@ -67,7 +117,12 @@ func main() {
 	nc := connectNATS()
 	defer nc.Close()
 
-	// Listen to all messages.
+	// Listen to all V2C messages.
+	if _, err := nc.Subscribe("v2c.*", handleV2CMessage); err != nil {
+		log.WithError(err).Fatal("Could not subscribe to NATS")
+	}
+
+	// Listen to all messages without prefixes.
 	if _, err := nc.Subscribe("*", handleNATSMessage); err != nil {
 		log.WithError(err).Fatal("Could not subscribe to NATS")
 	}
