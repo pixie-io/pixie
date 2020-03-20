386dfea72b1503de1d256d171243205ec68c4876
Zain Asgar <zasgar@pixielabs.ai>
2020-03-19 20:16:49 -0700

    Add get viziers command to the cli
    Summary: TSIA
    
    Test Plan: `px get viziers`
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D3990
    
    GitOrigin-RevId: 53c33b17d6a7d4c0ef16a4a6ae8e0dbb2f99c989

--- src/utils/pixie_cli/cmd/get.go
+++ src/utils/pixie_cli/cmd/get.go
@@ -2,30 +2,31 @@ package cmd
 
 import (
 	"context"
+	"os"
 	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"pixielabs.ai/pixielabs/src/utils"
+	"pixielabs.ai/pixielabs/src/utils/pixie_cli/pkg/components"
 	"pixielabs.ai/pixielabs/src/utils/pixie_cli/pkg/vizier"
 )
 
 func init() {
-	GetCmd.Flags().StringP("output", "o", "", "Output format: one of: json|proto")
+	GetCmd.PersistentFlags().StringP("output", "o", "", "Output format: one of: json|proto")
+
+	GetCmd.AddCommand(GetPEMsCmd)
+	GetCmd.AddCommand(GetViziersCmd)
 }
 
-// GetCmd is the "get" command.
-var GetCmd = &cobra.Command{
-	Use:   "get",
-	Short: "Get information about cluster/edge modules",
+// GetPEMsCmd is the "get pem" command.
+var GetPEMsCmd = &cobra.Command{
+	Use:     "pems",
+	Aliases: []string{"agents", "pem"},
+	Short:   "Get information about running pems",
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO(zasgar): Improvement needed after we spec out multiple vizier/agents support.
-		// Placeholder function until we spec out vizier/agent listing.
-		if len(args) != 1 || args[0] != "pem" {
-			log.Fatalln("only a single argument pem is allowed")
-		}
-
 		cloudAddr := viper.GetString("cloud_addr")
 		format, _ := cmd.Flags().GetString("output")
 		format = strings.ToLower(format)
@@ -43,4 +44,42 @@ var GetCmd = &cobra.Command{
 		tw := vizier.NewVizierStreamOutputAdapter(ctx, resp, format)
 		tw.Finish()
 	},
+}
+
+// GetViziersCmd is the "get viziers" command.
+var GetViziersCmd = &cobra.Command{
+	Use:     "viziers",
+	Aliases: []string{"clusters"},
+	Short:   "Get information about registered viziers",
+	Run: func(cmd *cobra.Command, args []string) {
+		cloudAddr := viper.GetString("cloud_addr")
+		format, _ := cmd.Flags().GetString("output")
+		format = strings.ToLower(format)
+
+		l, err := vizier.NewLister(cloudAddr)
+		if err != nil {
+			log.WithError(err).Fatal("Failed to create Vizier lister")
+		}
+		vzs, err := l.GetViziersInfo()
+		if err != nil {
+			log.WithError(err).Fatalln("Failed to get vizier information")
+		}
+
+		w := components.CreateStreamWriter(format, os.Stdout)
+		defer w.Finish()
+		w.SetHeader("viziers", []string{"ID", "Status", "LastHeartbeat", "Passthrough"})
+		for _, vz := range vzs {
+			passthrough := false
+			if vz.Config != nil {
+				passthrough = vz.Config.PassthroughEnabled
+			}
+			_ = w.Write([]interface{}{utils.UUIDFromProtoOrNil(vz.ID), vz.Status, vz.LastHeartbeatNs, passthrough})
+		}
+	},
+}
+
+// GetCmd is the "get" command.
+var GetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get information about cluster/edge modules",
 }
