b597132918c616d7e07340bee8d13b14e90b7a44
Omid Azizi <oazizi@pixielabs.ai>
2020-04-09 09:52:53 -0700

    Stirling profiling script
    Summary:
    This script automatically runs the Linux perf script on Stirling, and creates a perf report.
    
    The script makes sure only to profile the section of Stirling after its initialization.
    
    Depends on D4218.
    
    Test Plan: Manual.
    
    Reviewers: yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    JIRA Issues: PL-1749
    
    Differential Revision: https://phab.corp.pixielabs.ai/D4220
    
    GitOrigin-RevId: 76047988c08d68b3a57b8c98579ddf9f72fdbd1c

---
+++ src/stirling/scripts/profile_stirling_wrapper.sh
@@ -0,0 +1,95 @@
+#!/bin/bash -e
+
+# This script profiles stirling using the linux perf tool.
+
+###############################################################################
+# Arguments
+###############################################################################
+
+script_dir=$(dirname "$0")
+
+# shellcheck disable=SC1090
+source "$script_dir"/utils.sh
+
+usage() {
+  echo "This script profiles stirling using the linux perf tool."
+  echo ""
+  echo "Usage: $0 [-t=<runtime in seconds>] -- [<arguments to pass to stirling>]"
+  echo ""
+  echo "The time parameter controls the duration for which Stirling should be run and profiled."
+  echo "The duration applies only after Stirling has initialized."
+  exit
+}
+
+parse_args() {
+  # Set defaults here.
+  Tseconds=60
+  outfile=perf-$(date '+%Y%m%d%H%M%S').data
+
+  # Process the command line arguments.
+  while getopts "t:o:h" opt; do
+    case ${opt} in
+      t)
+        Tseconds=$OPTARG
+        ;;
+      o)
+        outfile=$OPTARG
+        ;;
+      :)
+        echo "Invalid option: $OPTARG requires an argument" 1>&2
+        ;;
+      h)
+        usage
+        ;;
+      *)
+        usage
+        ;;
+    esac
+  done
+  shift $((OPTIND -1))
+}
+
+parse_args "$@"
+shift $((OPTIND -1))
+
+###############################################################################
+# Build Stirling
+###############################################################################
+
+bazel_flags="-c opt"
+
+# shellcheck disable=SC2086
+bazel build $bazel_flags //src/stirling:stirling_wrapper
+
+# shellcheck disable=SC2086
+cmd=$(bazel info $bazel_flags bazel-bin)/src/stirling/stirling_wrapper
+
+###############################################################################
+# Run Stirling
+###############################################################################
+
+# This funky syntax runs stirling in the background,
+# while redirecting its output to file descriptor 3.
+# This allows us to run stirling asynchronously, while still capturing its output.
+exec 3< <(sudo "$cmd" --timeout_secs="$Tseconds" 2>&1)
+
+# Extract the Stirling PID.
+pid_line=$(grep -m 1 'Stirling Wrapper PID:' <&3)
+pid=$(echo "$pid_line" | awk -F"PID: " '{print $2}' | awk -F" " '{print $1}')
+echo "Stirling PID = $pid"
+
+# Now wait to see that Stirling has passed its initialization phase.
+grep -m 1 'Probes successfully deployed.' <&3 &> /dev/null
+
+###############################################################################
+# Run Perf profiler
+###############################################################################
+
+# Finally, we can attach perf, such that it monitors Stirling post-init,
+# so the perf numbers are not tainted with the init, which is intensive.
+echo "Stirling has deployed with a runtime of $Tseconds seconds."
+echo "Now attaching perf."
+sudo perf record -o "$outfile" --call-graph lbr -p "$pid"
+
+echo "Done collecting perf data. To view results, try running:"
+echo "  sudo perf report -f -i $outfile"
