25544acb052f7e40cbdf286dad45520e3576ef1a
Michelle Nguyen <michelle@pixielabs.ai>
2020-04-13 11:25:49 -0700

    Add arc unit test for checking pb breaking changes
    Summary:
    Adding unit test for prototool to check for any API breaking changes.
    the test is currently not enabled, because i need to update chef to include prototool.
    
    the ugliest part is that we have to copy any vendor module protos to a tmp directory with the correct structure/naming. but after some thought, this didnt seem so bad since we only use gogoproto.
    
    at some point we can enable the proto linter if we want, but this only uses the `prototool breaking changes` tool.
    
    Test Plan: ran it locally with some api breaking changes to make sure it complains.
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D4078
    
    GitOrigin-RevId: 8ebb5462da8f3876470dd4b12486dab071da43e6

---
+++ linters/engine/PrototoolCheckerTestEngine.php
@@ -0,0 +1,61 @@
+<?php
+
+final class PrototoolCheckerTest {
+    private $project_root;
+    private $files;
+    private $go_modules = array(
+        'github.com/gogo/protobuf' => 'com_github_gogo_protobuf'
+    );
+    private $vendorPath = '/tmp/pixie/vendor';
+
+    public function __construct($project_root, $files) {
+        $this->project_root = $project_root;
+        $this->files = $files;
+    }
+
+    public function run() {
+        $test_results = array();
+
+        chdir($this->project_root);
+
+        # Check that .proto files have corresponding .pb.go files.
+        $protoFiles = array_filter($this->files, function($f) {
+            return substr($f, -6) == '.proto';
+        });
+
+        if (count($protoFiles) == 0) {
+            return $test_results; 
+        }
+
+        // Build proto files.
+        exec('bazel query "kind(\'go_proto_library rule\', //...)"', $output, $return_var);
+
+        foreach ($output as $o) {
+            exec('bazel build ' . $o . '> /dev/null 2>&1');
+        }
+
+        // Copy over vendor files to a tmp directory.
+        exec('rm -rf ' . $this->vendorPath);
+        foreach ($this->go_modules as $m => $p) {
+            $cpModuleDir = $this->vendorPath . '/' . $m;
+            exec('mkdir -p ' . dirname($cpModuleDir));
+
+            // Copy over the directory from bazel-pixielabs/external/.
+            exec(sprintf('cp -r %s/bazel-pixielabs/external/%s %s', $this->project_root, $p, $cpModuleDir));
+            exec('chmod -R 755 '. $cpModuleDir);
+        }
+
+        exec('prototool break check .', $output, $return_var);
+        $updatedRes = new ArcanistUnitTestResult();
+        $updatedRes->setName('Protobuf Breaking API check');
+        if ($return_var == 0) {
+            $updatedRes->setResult(ArcanistUnitTestResult::RESULT_PASS);
+        } else {
+            $updatedRes->setUserData(implode("\n", $output));
+            $updatedRes->setResult(ArcanistUnitTestResult::RESULT_FAIL);   
+        }
+
+        $test_results[] = $updatedRes;           
+        return $test_results;
+    }
+}
