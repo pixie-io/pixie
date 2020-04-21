e44910c515487cd60c1010dd413ba6e587ccaab2
Malthus Yau <malthus@pixielabs.ai>
2020-04-21 12:11:20 -0700

    Add pixie cloud version to segment
    Summary: Tell segment/sentry what version of the UI we are running so we can debug with the correct source maps.
    
    Test Plan: tested manually and the release tag shows up in sentry
    
    Reviewers: zasgar, michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    JIRA Issues: PC-57
    
    Differential Revision: https://phab.corp.pixielabs.ai/D4456
    
    GitOrigin-RevId: f37d0596d7e2e627395e17c52cc944ad211e34ef

--- src/ui/src/utils/analytics.ts
+++ src/ui/src/utils/analytics.ts
@@ -1,9 +1,13 @@
 /// <reference types="@types/segment-analytics" />
 
 import {SEGMENT_UI_WRITE_KEY} from 'containers/constants';
+import {PIXIE_CLOUD_VERSION} from 'utils/env';
 
 declare global {
-    interface Window { analytics: SegmentAnalytics.AnalyticsJS; }
+    interface Window {
+        analytics: SegmentAnalytics.AnalyticsJS;
+        __pixie_cloud_version__: string;
+    }
 }
 
 function isValidSegmentKey(k) {
@@ -21,6 +25,7 @@ class Analytics {
     }
 
     load() {
+        window.__pixie_cloud_version__ = PIXIE_CLOUD_VERSION;
         window.analytics.load(SEGMENT_UI_WRITE_KEY);
     }
 
