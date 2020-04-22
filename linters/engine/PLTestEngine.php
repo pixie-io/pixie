af3b78f840a3d5f3fd7075392227f8496a2f76d3
Michelle Nguyen <michelle@pixielabs.ai>
2020-04-22 10:44:12 -0700

    Enable prototool in arc unit
    Summary:
    this diff enables prototool for the arc unit test.
    the tool requires a prototool.yaml in master in order to work properly.
    now that the previous diff has landed and we do that a prototool.yaml, this should work.
    
    Test Plan: modified protofiles with correct/invalid changes
    
    Reviewers: #engineering, zasgar
    
    Reviewed By: #engineering, zasgar
    
    Differential Revision: https://phab.corp.pixielabs.ai/D4306
    
    GitOrigin-RevId: 114b6504a9652458fec9d3cc0cf54a5d9678294d

--- linters/engine/PLTestEngine.php
+++ linters/engine/PLTestEngine.php
@@ -4,6 +4,7 @@ include 'FileCheckerTestEngine.php';
 include 'GazelleCheckerTestEngine.php';
 include 'ExpCheckerTestEngine.php';
 include 'GoGenerateCheckerTestEngine.php';
+include 'PrototoolCheckerTestEngine.php';
 
 final class PLTestEngine extends ArcanistUnitTestEngine {
     private $project_root;
@@ -28,6 +29,9 @@ final class PLTestEngine extends ArcanistUnitTestEngine {
         // TODO(michelle): Determine a more robust check.
         // $go_generate_checker = new GoGenerateCheckerTest($this->project_root, $this->files);
         // $test_results = array_merge($test_results, $go_generate_checker->run());
+
+    	$prototool_checker = new PrototoolCheckerTest($this->project_root, $this->files);
+    	$test_results = array_merge($test_results, $prototool_checker->run());
 
         return $test_results;
     }
