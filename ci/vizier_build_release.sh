fc029c14187d91ad3f3e2116acb3d70d45b0d646
Zain Asgar <zasgar@pixielabs.ai>
2020-04-27 21:12:27 -0700

    Split Vizier into Vizier + Vizier Bootstrap
    Summary: This splits vizier as described above. It also fixes a bug where we weren't packaging prod vizier.
    
    Test Plan: Tested on skaffold
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D4576
    
    GitOrigin-RevId: 20a84511b268940512e50cfa610b922774c94e8f

--- ci/vizier_build_release.sh
+++ ci/vizier_build_release.sh
@@ -15,10 +15,15 @@ bazel run -c opt //src/utils/artifacts/versions_gen:versions_gen -- \
 
 bazel run --stamp -c opt --define BUNDLE_VERSION="${release_tag}" --stamp //k8s/vizier:vizier_images_push
 bazel build --stamp -c opt --define BUNDLE_VERSION="${release_tag}" --stamp //k8s/vizier:vizier_yamls
+bazel build --stamp -c opt --define BUNDLE_VERSION="${release_tag}" --stamp //k8s/vizier:vizier_bootstrap_yamls
 
 output_path="gs://pixie-prod-artifacts/vizier/${release_tag}"
 yamls_tar="${repo_path}/bazel-bin/k8s/vizier/vizier_yamls.tar"
+bootstrap_yamls_tar="${repo_path}/bazel-bin/k8s/vizier/vizier_bootstrap_yamls.tar"
 
 sha256sum "${yamls_tar}" | awk '{print $1}' > sha
-gsutil cp "${yamls_tar}" "${output_path}/vizier_yamls.tar"
+sha256sum "${bootstrap_yamls_tar}" | awk '{print $1}' > sha_bootstrap
+gsutil cp "${bootstrap_yamls_tar}" "${output_path}/vizier_yamls.tar"
+gsutil cp "${bootstrap_yamls_tar}" "${output_path}/vizier_bootstrap_yamls.tar"
 gsutil cp sha "${output_path}/vizier_yamls.tar.sha256"
+gsutil cp sha_bootstrap "${output_path}/vizier_bootstrap_yamls.tar.sha256"
--- k8s/vizier/images.bzl
+++ k8s/vizier/images.bzl
@@ -22,10 +22,33 @@ def generate_vizier_yamls(name, srcs, out, image_map, **kwargs):
         cp -aL k8s/vizier $$T
 
         # Update the bundle versions.
-        pushd $$T/vizier/base
+        pushd $$T/vizier/prod
         {0}
         popd
 
-        kustomize build $$T/vizier/base -o $@
+        kustomize build $$T/vizier/prod -o $@
+        """.format(merged_edits),
+    )
+
+def generate_vizier_bootstrap_yamls(name, srcs, out, image_map, **kwargs):
+    kustomize_edits = []
+    for k in image_map.keys():
+        kustomize_edits.append("kustomize edit set image {0}={1}:{2}".format(k, k, "$(BUNDLE_VERSION)"))
+
+    merged_edits = "\n".join(kustomize_edits)
+    native.genrule(
+        name = name,
+        srcs = srcs,
+        outs = [out],
+        cmd = """
+        T=`mktemp -d`
+        cp -aL k8s/vizier_bootstrap $$T
+
+        # Update the bundle versions.
+        pushd $$T/vizier_bootstrap/prod
+        {0}
+        popd
+
+        kustomize build $$T/vizier_bootstrap/prod -o $@
         """.format(merged_edits),
     )
