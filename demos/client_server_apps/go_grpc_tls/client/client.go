a9d4d207d14b2e58e0b6d364fc4b797b9e63b179
Omid Azizi <oazizi@pixielabs.ai>
2020-06-04 23:19:59 -0700

    Delve Benchmarking
    Summary:
    Enables Delve benchmarking.
    
    Next step will be to add some scripts to automate the benchmark.
    
    Test Plan: Manual
    
    Reviewers: yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    JIRA Issues: PP-1979
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5093
    
    GitOrigin-RevId: b40190b57cd7ba626e82124f78b185fba900a651

--- demos/client_server_apps/go_grpc_tls/client/client.go
+++ demos/client_server_apps/go_grpc_tls/client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"runtime"
 	"time"
 
@@ -22,18 +23,36 @@ const (
 
 func main() {
 	pflag.Int("max_procs", 1, "The maximum number of OS threads created by the golang runtime.")
-	pflag.Int("iters", 1000, "Number of iterations.")
-	pflag.Int("sub_iters", 1000, "Number of sub-iterations with same TLS config.")
-	pflag.Int("sleep_time", 1000, "Sleep time in microseconds per iteration.")
+	pflag.Uint32("iters", 100000, "Number of requests to make for each TLS connection.")
+	pflag.Uint32("rounds", 1, "Number of connection rounds to perform.")
+	pflag.Uint32("sleep_time", 1000, "Sleep time in microseconds per iteration.")
+	pflag.Bool("benchmark", false, "Whether to run in benchmarking mode")
+	pflag.Uint32("run_time_seconds", 0, "Number of seconds for which to run")
 	pflag.Parse()
 
 	viper.BindPFlags(pflag.CommandLine)
 
 	runtime.GOMAXPROCS(viper.GetInt("max_procs"))
 
+	benchmarkMode := viper.GetBool("benchmark")
 	sleepTime := time.Duration(viper.GetInt("sleep_time")) * time.Millisecond
+	iters := viper.GetInt("iters")
+	rounds := viper.GetInt("rounds")
+	runTimeSeconds := time.Duration(viper.GetInt("run_time_seconds")) * time.Second
 
-	for i := 0; i < viper.GetInt("iters"); i++ {
+	if benchmarkMode {
+		log.Printf("Running in benchmark mode. sleep_time and run_time_seconds will be overridden.")
+		sleepTime = 0
+		runTimeSeconds = time.Duration(10) * time.Second
+	}
+
+	// When running by time, set the number of iterations to max/infinity.
+	if runTimeSeconds != 0 {
+		log.Printf("Running by duration. iters flag will be ignored.")
+		iters = math.MaxUint32
+	}
+
+	for i := 0; i < rounds; i++ {
 		// Set up a connection to the server.
 		tlsConfig := &tls.Config{InsecureSkipVerify: true}
 		creds := credentials.NewTLS(tlsConfig)
@@ -44,7 +63,10 @@ func main() {
 		defer conn.Close()
 		c := pb.NewGreeterClient(conn)
 
-		for j := 0; j < viper.GetInt("sub_iters"); j++ {
+		start := time.Now()
+		statRequests := 0
+
+		for j := 0; j < iters; j++ {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 			defer cancel()
 
@@ -53,11 +75,25 @@ func main() {
 			if err != nil {
 				log.Errorf("could not greet: %v", err)
 			} else {
-				log.Printf("Greeting: %s", resp.Message)
+				if benchmarkMode {
+					statRequests++
+				} else {
+					log.Printf("Greeting: %s", resp.Message)
+				}
 			}
 			if sleepTime != 0 {
 				time.Sleep(sleepTime)
 			}
+
+			if runTimeSeconds != 0 && time.Since(start) > runTimeSeconds {
+				break
+			}
 		}
+
+		// Print summary stats.
+		duration := time.Since(start)
+		fmt.Println("Elapsed time = ", duration.Seconds())
+		fmt.Println("Requests per second = ", float64(statRequests)/duration.Seconds())
 	}
+
 }
