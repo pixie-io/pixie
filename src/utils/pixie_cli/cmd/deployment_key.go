9693f2f512aa5a3474632467e4bf366d2b5daff7
Michelle Nguyen <michelle@pixielabs.ai>
2020-06-05 17:57:43 -0700

    Add list deploykeys in CLI
    Summary: px deploy-key list
    
    Test Plan: ran it
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5124
    
    GitOrigin-RevId: 240e53a4d73169e19c734c606fd5138c200bfe80

--- src/utils/pixie_cli/cmd/deployment_key.go
+++ src/utils/pixie_cli/cmd/deployment_key.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
+	"strings"
 
 	uuid "github.com/satori/go.uuid"
 	log "github.com/sirupsen/logrus"
@@ -13,18 +15,23 @@ import (
 	"pixielabs.ai/pixielabs/src/cloud/cloudapipb"
 	utils2 "pixielabs.ai/pixielabs/src/utils"
 	"pixielabs.ai/pixielabs/src/utils/pixie_cli/pkg/auth"
+	"pixielabs.ai/pixielabs/src/utils/pixie_cli/pkg/components"
 	"pixielabs.ai/pixielabs/src/utils/pixie_cli/pkg/utils"
 )
 
 func init() {
 	DeployKeyCmd.AddCommand(CreateDeployKeyCmd)
 	DeployKeyCmd.AddCommand(DeleteDeployKeyCmd)
+	DeployKeyCmd.AddCommand(ListDeployKeyCmd)
 
 	CreateDeployKeyCmd.Flags().StringP("desc", "d", "", "A description for the deploy key")
 	viper.BindPFlag("desc", CreateDeployKeyCmd.Flags().Lookup("desc"))
 
 	DeleteDeployKeyCmd.Flags().StringP("id", "i", "", "The deploy key to delete")
 	viper.BindPFlag("id", DeleteDeployKeyCmd.Flags().Lookup("id"))
+
+	ListDeployKeyCmd.Flags().StringP("output", "o", "", "Output format: one of: json|proto")
+	viper.BindPFlag("output", ListDeployKeyCmd.Flags().Lookup("output"))
 }
 
 // DeployKeyCmd is the deploy-key sub-command of the CLI.
@@ -78,6 +85,30 @@ var DeleteDeployKeyCmd = &cobra.Command{
 	},
 }
 
+// ListDeployKeyCmd is the List sub-command of DeployKey.
+var ListDeployKeyCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all deploy key for Pixie",
+	Run: func(cmd *cobra.Command, args []string) {
+		cloudAddr := viper.GetString("cloud_addr")
+		format, _ := cmd.Flags().GetString("output")
+		format = strings.ToLower(format)
+
+		keys, err := listDeployKeys(cloudAddr)
+		if err != nil {
+			log.WithError(err).Fatal("Failed to list deployment keys")
+		}
+		// Throw keys into table.
+		w := components.CreateStreamWriter(format, os.Stdout)
+		defer w.Finish()
+		w.SetHeader("deployment-keys", []string{"ID", "Key", "CreatedAt", "Description"})
+		for _, k := range keys {
+			_ = w.Write([]interface{}{utils2.UUIDFromProtoOrNil(k.ID), k.Key, k.CreatedAt,
+				k.Desc})
+		}
+	},
+}
+
 func getClientAndContext(cloudAddr string) (cloudapipb.VizierDeploymentKeyManagerClient, context.Context, error) {
 	// Get grpc connection to cloud.
 	cloudConn, err := utils.GetCloudClientConnection(cloudAddr)
@@ -120,4 +151,18 @@ func deleteDeployKey(cloudAddr string, keyID uuid.UUID) error {
 
 	_, err = deployMgrClient.Delete(ctxWithCreds, utils2.ProtoFromUUID(&keyID))
 	return err
+}
+
+func listDeployKeys(cloudAddr string) ([]*cloudapipb.DeploymentKey, error) {
+	deployMgrClient, ctxWithCreds, err := getClientAndContext(cloudAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := deployMgrClient.List(ctxWithCreds, &cloudapipb.ListDeploymentKeyRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Keys, nil
 }
