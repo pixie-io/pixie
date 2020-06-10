e66938e37e6c1a0882341793346e0d3de99f6e26
Michelle Nguyen <michelle@pixielabs.ai>
2020-06-09 18:20:36 -0700

    Increase etcd db size, set up autocompaction
    Summary:
    Although our pv was 10gb, our etcd db was still the default size (2GB). 8 is the max-recommended size, so this diff updates to that size.
    also set up etcd auto-compaction. however, this won't be useful until we set up regular defragging.
    After playing around with an etcd instance which ran out of space, here is what I saw:
    - Etcd instance was out of space, with 2.1GB used.
    - Compacted to the last revision. At this point, 2.1GB was still used.
    - Ran etcd defrag -> space usage dropped to 25MB.
    
    also updated the etcdctl script to work with the new etcd statefulset.
    
    Test Plan: n/a
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5159
    
    GitOrigin-RevId: 6739fb95130872e2c5eb67428af7162e421f6407

--- scripts/run_etcdctl.sh
+++ scripts/run_etcdctl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
 
 endpoint="https://localhost:2379"
-cert="/etc/etcdtls/operator/etcd-tls/etcd-client.crt"
-key="/etc/etcdtls/operator/etcd-tls/etcd-client.key"
-cacert="/etc/etcdtls/operator/etcd-tls/etcd-client-ca.crt"
+cert="/clientcerts/etcd-client.crt"
+key="/clientcerts/etcd-client.key"
+cacert="/clientcerts/etcd-client-ca.crt"
 
 kubectl exec -t -i \
  $(kubectl get pod --namespace pl \
-    --selector="app=etcd,etcd_cluster=pl-etcd" --output jsonpath='{.items[0].metadata.name}') \
+    --selector="vizier-dep=etcd" --output jsonpath='{.items[0].metadata.name}') \
  -n=pl -- /bin/sh -c \
  "ETCDCTL_API=3 etcdctl \
   --endpoints=$endpoint \
