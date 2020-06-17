8f804b1bcf5cfb8a83e4dc6dd33a259ecc3c3974
Natalie Serrino <nserrino@pixielabs.ai>
2020-06-16 19:56:01 -0700

    PC-304: Add entity centric urls for pods, services, nodes.
    Summary:
    Depends on D5237. Adds the routes for the above entity live views.
    doesn't do the cluster one yet because it's unclear how the url for that should difffer from the generic live view url.
    Note that some special handling of pod and service is required because in Carnot we prepend namespace to those names,
    but we don't in our entity url scheme.
    
    Test Plan: added/existing/ran the various urls
    
    Reviewers: malthus, michelle, zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    JIRA Issues: PC-304
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5241
    
    GitOrigin-RevId: dc1a58029089c8de14c795b9ff5a637fd99a44f9

--- src/ui/src/containers/live/utils/live-view-params-test.tsx
+++ src/ui/src/containers/live/utils/live-view-params-test.tsx
@@ -21,6 +21,70 @@ describe('matchLiveViewEntity test', () => {
     });
   });
 
+  it('should correctly match nodes entity page', () => {
+    const entity = matchLiveViewEntity('/live/clusters/gke%3Afoobar/nodes')
+    expect(entity).toStrictEqual({
+      page: LiveViewPage.Nodes,
+      params: {},
+      clusterName: 'gke:foobar',
+    });
+  });
+
+  it('should correctly match node entity page', () => {
+    const entity = matchLiveViewEntity('/live/clusters/gke%3Afoobar/nodes/node-123')
+    expect(entity).toStrictEqual({
+      page: LiveViewPage.Node,
+      params: {
+        node: 'node-123',
+      },
+      clusterName: 'gke:foobar',
+    });
+  });
+
+  it('should correctly match pods entity page', () => {
+    const entity = matchLiveViewEntity('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/pods')
+    expect(entity).toStrictEqual({
+      page: LiveViewPage.Pods,
+      params: {
+        namespace: 'px-sock-shop',
+      },
+      clusterName: 'gke:foobar',
+    });
+  });
+
+  it('should correctly match pod entity page', () => {
+    const entity = matchLiveViewEntity('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/pods/orders-123')
+    expect(entity).toStrictEqual({
+      page: LiveViewPage.Pod,
+      params: {
+        pod: 'px-sock-shop/orders-123',
+      },
+      clusterName: 'gke:foobar',
+    });
+  });
+
+  it('should correctly match services entity page', () => {
+    const entity = matchLiveViewEntity('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/services')
+    expect(entity).toStrictEqual({
+      page: LiveViewPage.Services,
+      params: {
+        namespace: 'px-sock-shop',
+      },
+      clusterName: 'gke:foobar',
+    });
+  });
+
+  it('should correctly match service entity page', () => {
+    const entity = matchLiveViewEntity('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/services/orders')
+    expect(entity).toStrictEqual({
+      page: LiveViewPage.Service,
+      params: {
+        service: 'px-sock-shop/orders',
+      },
+      clusterName: 'gke:foobar',
+    });
+  });
+
   it('should correctly match a default view with no cluster', () => {
     const entity = matchLiveViewEntity('/live')
     expect(entity).toStrictEqual({
@@ -58,6 +122,70 @@ describe('toEntityPathname test', () => {
       clusterName: 'gke:foobar',
     };
     expect(toEntityPathname(entity)).toEqual('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop');
+  });
+
+  it('should generate the url for the nodes page', () => {
+    const entity = {
+      page: LiveViewPage.Nodes,
+      params: {},
+      clusterName: 'gke:foobar',
+    };
+    expect(toEntityPathname(entity)).toEqual('/live/clusters/gke%3Afoobar/nodes');
+  });
+
+  it('should generate the url for the node page', () => {
+    const entity = {
+      page: LiveViewPage.Node,
+      params: {
+        node: 'node-123'
+      },
+      clusterName: 'gke:foobar',
+    };
+    expect(toEntityPathname(entity)).toEqual('/live/clusters/gke%3Afoobar/nodes/node-123');
+  });
+
+  it('should generate the url for the pods page', () => {
+    const entity = {
+      page: LiveViewPage.Pods,
+      params: {
+        namespace: 'px-sock-shop',
+      },
+      clusterName: 'gke:foobar',
+    };
+    expect(toEntityPathname(entity)).toEqual('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/pods');
+  });
+
+  it('should generate the url for the pod page', () => {
+    const entity = {
+      page: LiveViewPage.Pod,
+      params: {
+        pod: 'px-sock-shop/orders-123',
+      },
+      clusterName: 'gke:foobar',
+    };
+    expect(toEntityPathname(entity)).toEqual('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/pods/orders-123');
+  });
+
+  it('should generate the url for the services page', () => {
+    const entity = {
+      page: LiveViewPage.Services,
+      params: {
+        namespace: 'px-sock-shop',
+      },
+      clusterName: 'gke:foobar',
+    };
+    expect(toEntityPathname(entity)).toEqual('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/services');
+  });
+
+  it('should generate the url for the service page', () => {
+    const entity = {
+      page: LiveViewPage.Service,
+      params: {
+        service: 'px-sock-shop/orders',
+      },
+      clusterName: 'gke:foobar',
+    };
+    expect(toEntityPathname(entity)).toEqual('/live/clusters/gke%3Afoobar/namespaces/px-sock-shop/services/orders');
   });
 
   it('should generate the url for the default page', () => {
--- src/ui/src/containers/live/utils/live-view-params.tsx
+++ src/ui/src/containers/live/utils/live-view-params.tsx
@@ -3,14 +3,32 @@ import { matchPath } from 'react-router';
 export enum LiveViewPage {
   Default,
   Namespace,
-  Namespaces
+  Namespaces,
+  Node,
+  Nodes,
+  Pod,
+  Pods,
+  Service,
+  Services,
 }
 
 interface NamespaceURLParams {
   namespace: string;
 }
 
-export type EntityURLParams = {} | NamespaceURLParams;
+interface NodeURLParams {
+  node: string;
+}
+
+interface PodURLParams {
+  pod: string;
+}
+
+interface ServiceURLParams {
+  service: string;
+}
+
+export type EntityURLParams = {} | NamespaceURLParams | NodeURLParams | PodURLParams | ServiceURLParams;
 
 export interface EntityURL {
   clusterName?: string;
@@ -22,44 +40,111 @@ export const LiveViewPageRoutes = {
   [LiveViewPage.Default]: '/live',
   [LiveViewPage.Namespace]: '/live/clusters/:cluster/namespaces/:namespace',
   [LiveViewPage.Namespaces]: '/live/clusters/:cluster/namespaces',
+  [LiveViewPage.Node]: '/live/clusters/:cluster/nodes/:node',
+  [LiveViewPage.Nodes]: '/live/clusters/:cluster/nodes',
+  [LiveViewPage.Pod]: '/live/clusters/:cluster/namespaces/:namespace/pods/:pod',
+  [LiveViewPage.Pods]: '/live/clusters/:cluster/namespaces/:namespace/pods',
+  [LiveViewPage.Service]: '/live/clusters/:cluster/namespaces/:namespace/services/:service',
+  [LiveViewPage.Services]: '/live/clusters/:cluster/namespaces/:namespace/services',
 };
 
 export const LiveViewPageScriptIds = {
   [LiveViewPage.Namespace]: 'px/namespace',
   [LiveViewPage.Namespaces]: 'px/namespaces',
+  [LiveViewPage.Node]: 'px/node',
+  [LiveViewPage.Nodes]: 'px/nodes',
+  [LiveViewPage.Pod]: 'px/pod',
+  [LiveViewPage.Pods]: 'px/pods',
+  [LiveViewPage.Service]: 'px/service',
+  [LiveViewPage.Services]: 'px/services',
 };
 
 interface WithCluster {
   cluster: string;
 }
 
+function matchAndExtractEntity<T>(path: string, page: LiveViewPage) {
+  const match = matchPath<WithCluster & T>(path, {
+    path: LiveViewPageRoutes[page],
+    exact: true
+  });
+  if (!match) {
+    return null;
+  }
+  const {cluster, ...params} = match.params as WithCluster & T;
+  return {
+    clusterName: decodeURIComponent(cluster),
+    page,
+    params,
+  }
+}
+
 export function matchLiveViewEntity(path: string): EntityURL {
-  const namespacesMatch = matchPath<WithCluster>(path, {
-    path: LiveViewPageRoutes[LiveViewPage.Namespaces],
-    exact: true,
-  });
-  if (namespacesMatch) {
-    return {
-      clusterName: decodeURIComponent(namespacesMatch.params.cluster),
-      page: LiveViewPage.Namespaces,
-      params: {},
-    }
-  }
-
-  const namespaceMatch = matchPath<WithCluster & NamespaceURLParams>(path, {
-    path: LiveViewPageRoutes[LiveViewPage.Namespace],
-    exact: true,
-  });
+  // namespaces
+  const namespaceMatch = matchAndExtractEntity<NamespaceURLParams>(
+    path, LiveViewPage.Namespace
+  );
   if (namespaceMatch) {
-    return {
-      clusterName: decodeURIComponent(namespaceMatch.params.cluster),
-      page: LiveViewPage.Namespace,
-      params: {
-        namespace: namespaceMatch.params.namespace,
-      },
-    }
+    return namespaceMatch;
   }
-
+  const namespacesMatch = matchAndExtractEntity<{}>(
+    path, LiveViewPage.Namespaces
+  );
+  if (namespacesMatch) {
+    return namespacesMatch;
+  }
+  // nodes
+  const nodeMatch = matchAndExtractEntity<NodeURLParams>(
+    path, LiveViewPage.Node
+  );
+  if (nodeMatch) {
+    return nodeMatch;
+  }
+  const nodesMatch = matchAndExtractEntity<{}>(
+    path, LiveViewPage.Nodes
+  );
+  if (nodesMatch) {
+    return nodesMatch;
+  }
+  // pods
+  const podMatch = matchAndExtractEntity<NamespaceURLParams & PodURLParams>(
+    path, LiveViewPage.Pod
+  );
+  if (podMatch) {
+    return {
+      ...podMatch,
+      params: {
+        // TODO(nserrino): remove this logic when we separate pod and namespace in the backend.
+        pod: `${podMatch.params.namespace}/${podMatch.params.pod}`,
+      },
+    };
+  }
+  const podsMatch = matchAndExtractEntity<NamespaceURLParams>(
+    path, LiveViewPage.Pods
+  );
+  if (podsMatch) {
+    return podsMatch;
+  }
+  // services
+  const serviceMatch = matchAndExtractEntity<NamespaceURLParams & ServiceURLParams>(
+    path, LiveViewPage.Service
+  );
+  if (serviceMatch) {
+    return {
+      ...serviceMatch,
+      params: {
+        // TODO(nserrino): remove this logic when we separate service and namespace in the backend.
+        service: `${serviceMatch.params.namespace}/${serviceMatch.params.service}`,
+      },
+    };
+  }
+  const servicesMatch = matchAndExtractEntity<NamespaceURLParams>(
+    path, LiveViewPage.Services
+  );
+  if (servicesMatch) {
+    return servicesMatch;
+  }
+  // non-exact cluster match.
   const clusterMatch = matchPath<WithCluster>(path, {
     path: '/live/clusters/:cluster',
     exact: false,
@@ -71,7 +156,6 @@ export function matchLiveViewEntity(path: string): EntityURL {
       params: {},
     }
   }
-
   return {
     page: LiveViewPage.Default,
     params: {},
@@ -87,6 +171,31 @@ export function toEntityPathname(entity: EntityURL): string {
     }
     case LiveViewPage.Namespaces: {
       return `/live/clusters/${encodedCluster}/namespaces`;
+    }
+    case LiveViewPage.Node: {
+      const { node } = entity.params as NodeURLParams;
+      return `/live/clusters/${encodedCluster}/nodes/${node}`;
+    }
+    case LiveViewPage.Nodes: {
+      return `/live/clusters/${encodedCluster}/nodes`;
+    }
+    case LiveViewPage.Pod: {
+      const { pod } = entity.params as PodURLParams;
+      const [ namespace, podName ] = pod.split('/');
+      return `/live/clusters/${encodedCluster}/namespaces/${namespace}/pods/${podName}`;
+    }
+    case LiveViewPage.Pods: {
+      const { namespace } = entity.params as NamespaceURLParams;
+      return `/live/clusters/${encodedCluster}/namespaces/${namespace}/pods`;
+    }
+    case LiveViewPage.Service: {
+      const { service } = entity.params as ServiceURLParams;
+      const [ namespace, serviceName ] = service.split('/');
+      return `/live/clusters/${encodedCluster}/namespaces/${namespace}/services/${serviceName}`;
+    }
+    case LiveViewPage.Services: {
+      const { namespace } = entity.params as NamespaceURLParams;
+      return `/live/clusters/${encodedCluster}/namespaces/${namespace}/services`;
     }
     case LiveViewPage.Default:
     default:
