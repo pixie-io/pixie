4e9454fd6a530d8916de50bac84d695f99c2050b
Zain Asgar <zasgar@pixielabs.ai>
2020-07-14 14:31:37 -0700

    Defer segment load until browser is idle
    Summary:
    This improves the performance by not making segment render blocking, significantly improving
    our lighthouse score.
    
    Test Plan: N/A
    
    Reviewers: michelle, nserrino, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5586
    
    GitOrigin-RevId: 14f081e5ab6d978e581257bc852166bce53ae9e5

--- src/ui/src/segment.js
+++ src/ui/src/segment.js
@@ -1,2 +1,43 @@
-!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://__SEGMENT_ANALYTICS_JS_DOMAIN__/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-}}();
+!function() {
+  var analytics = window.analytics = window.analytics || [];
+  if (!analytics.initialize) {
+    if (analytics.invoked) {
+      window.console && console.error && console.error('Segment snippet included twice.');
+    } else {
+      analytics.invoked = !0;
+      analytics.methods = ['trackSubmit', 'trackClick', 'trackLink', 'trackForm', 'pageview', 'identify', 'reset', 'group', 'track', 'ready', 'alias', 'debug', 'page', 'once', 'off', 'on'];
+      analytics.factory = function(t) {
+        return function() {
+          var e = Array.prototype.slice.call(arguments);
+          e.unshift(t);
+          analytics.push(e);
+          return analytics;
+        };
+      };
+      for (var t = 0; t < analytics.methods.length; t++) {
+        var e = analytics.methods[t];
+        analytics[e] = analytics.factory(e);
+      }
+      /*******************************************
+       * PLEASE NOTE!!!!
+       *******************************************/
+      // They function defers the loading of segment until the browser is idle,
+      // followed by waiting another 5 seconds. This significantly helps with page performance
+      // because it defers loading of external resources.
+      analytics.load = function(t, e) {
+        window.requestIdleCallback(() => {
+          setTimeout(() => {
+            var n = document.createElement('script');
+            n.type = 'text/javascript';
+            n.async = !0;
+            n.src = 'https://__SEGMENT_ANALYTICS_JS_DOMAIN__/analytics.js/v1/' + t + '/analytics.min.js';
+            var a = document.getElementsByTagName('script')[0];
+            a.parentNode.insertBefore(n, a);
+            analytics._loadOptions = e;
+          }, 5000);
+        });
+      };
+      analytics.SNIPPET_VERSION = "4.1.0";
+    }
+  }
+}();
