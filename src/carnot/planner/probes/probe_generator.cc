0e438fbdb0ae4505e526427d17cec3bc7ada8f2c
Phillip Kuznetsov <philkuz@pixielabs.ai>
2020-07-20 13:44:46 -0700

    Adding probe executable
    Summary:
    @oazizi requested an executable to write probe queries and test out stirling dynamic tracing capabilities
    Usage:
    
    `bazel run //src/carnot/planner/probes:probe_generator -- --query="$(cat $(pwd)/test.pxl)" --output_file="$(pwd)/output.pbtxt"`
    
    *test.pxl*
    ```
    import pxtrace
    import px
    
    @pxtrace.goprobe("MyFunc")
    def probe_func():
        id = pxtrace.ArgExpr('id')
        return "http_return_table", [{'id': id},
                {'err': pxtrace.RetExpr('$0.a')},
                {'latency': pxtrace.FunctionLatency()}]
    
    pxtrace.UpsertProbe('http_return',
                        probe_func,
                        px.uint128("123e4567-e89b-12d3-a456-426655440000"),
                        "5m")
    ```
    
    *output.pbtxt* (after execution)
    ```
    binary_spec {
      language: GOLANG
    }
    outputs {
      name: "http_return_table"
      fields: "id"
      fields: "err"
      fields: "latency"
    }
    probes {
      name: "http_return"
      trace_point {
        symbol: "MyFunc"
      }
      args {
        id: "arg0"
        expr: "id"
      }
      ret_vals {
        id: "ret0"
      }
      function_latency {
        id: "lat0"
      }
      output_actions {
        output_name: "http_return_table"
        variable_name: "arg0"
        variable_name: "ret0"
        variable_name: "lat0"
      }
    }
    ```
    
    Test Plan: ran through usage, output the above
    
    Reviewers: oazizi, yzhao, zasgar, michelle, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Subscribers: oazizi
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5661
    
    GitOrigin-RevId: 1261b221db07e9ab8b3f602b5b6b7e7c11fd6a26

---
+++ src/carnot/planner/probes/probe_generator.cc
@@ -0,0 +1,71 @@
+#include <fstream>
+#include <iostream>
+
+#include "src/carnot/planner/compiler/ast_visitor.h"
+#include "src/carnot/planner/parser/parser.h"
+#include "src/carnot/planner/probes/probes.h"
+#include "src/common/base/file.h"
+
+namespace pl {
+namespace carnot {
+namespace planner {
+namespace compiler {
+
+StatusOr<std::shared_ptr<DynamicTraceIR>> CompileProbeScript(std::string_view query) {
+  std::shared_ptr<DynamicTraceIR> dynamic_trace = std::make_shared<DynamicTraceIR>();
+
+  Parser parser;
+  PL_ASSIGN_OR_RETURN(auto ast, parser.Parse(query));
+
+  IR ir;
+  ModuleHandler module_handler;
+
+  RegistryInfo registry_info;
+  CompilerState compiler_state(
+      std::make_unique<RelationMap>(), &registry_info,
+      // Time now isn't used to generate probes, but we still need to pass one in.
+      /*time_now*/ 1552607213931245000,
+      /*max_output_rows_per_table*/ 10000);
+  PL_ASSIGN_OR_RETURN(auto ast_walker,
+                      ASTVisitorImpl::Create(&ir, dynamic_trace.get(), &compiler_state,
+                                             &module_handler, false, {}, {}));
+  PL_RETURN_IF_ERROR(ast_walker->ProcessModuleNode(ast));
+  return dynamic_trace;
+}
+
+}  // namespace compiler
+}  // namespace planner
+}  // namespace carnot
+}  // namespace pl
+
+DEFINE_string(query, "", "The query to run.");
+DEFINE_string(
+    output_file, "",
+    "The file path to write the output protobuf representation of the dynamic tracing protobuf.");
+
+void PrintUsageAndExit(std::string_view name) {
+  LOG(ERROR) << absl::Substitute(
+      "Usage: $0 --query <query_string> --output_file <file_to_output_protobuf_to>", name);
+  exit(1);
+}
+
+int main(int argc, char** argv) {
+  pl::EnvironmentGuard env_guard(&argc, argv);
+  std::string query = FLAGS_query;
+  std::string output_file = FLAGS_output_file;
+  if (query.empty()) {
+    LOG(ERROR) << "Query must not be empty";
+    PrintUsageAndExit(argv[0]);
+  }
+
+  if (output_file.empty()) {
+    LOG(ERROR) << absl::Substitute("Invalid output file: '$0'", output_file);
+    PrintUsageAndExit(argv[0]);
+  }
+
+  auto probe = pl::carnot::planner::compiler::CompileProbeScript(query).ConsumeValueOrDie();
+  pl::stirling::dynamic_tracing::ir::logical::Program probe_pb;
+  PL_CHECK_OK(probe->ToProto(&probe_pb));
+  PL_CHECK_OK(pl::WriteFileFromString(output_file, probe_pb.DebugString()));
+  return 0;
+}
