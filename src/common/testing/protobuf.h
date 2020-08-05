b1b48e963d114293eb2599fe422daf22bc51399e
Phillip Kuznetsov <philkuz@pixielabs.ai>
2020-08-05 13:11:11 -0700

    Adding new WithDifferencer matcher that takes in differencer as argument
    Summary: Need a way to use the machinery in the protobuf stuff but I need to ignore the order of some fields. This exposes the most variability.
    
    Test Plan: Tested in D5734.
    
    Reviewers: oazizi, yzhao, zasgar, #engineering
    
    Reviewed By: oazizi, zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5881
    
    GitOrigin-RevId: a3497b3133f8c21b5dbcaf430be3a500767e08e2

--- src/common/testing/protobuf.h
+++ src/common/testing/protobuf.h
@@ -17,8 +17,9 @@ namespace pl {
 namespace testing {
 namespace proto {
 
-struct EqualsProtoMatcher {
-  explicit EqualsProtoMatcher(std::string text_pb) : expected_text_pb_(std::move(text_pb)) {}
+struct ProtoMatcher {
+  ProtoMatcher(google::protobuf::util::MessageDifferencer* differencer, std::string text_pb)
+      : expected_text_pb_(std::move(text_pb)), differencer_(differencer) {}
 
   template <typename PBType>
   bool MatchAndExplain(const PBType& pb, ::testing::MatchResultListener* listener) const {
@@ -27,42 +28,54 @@ struct EqualsProtoMatcher {
       (*listener) << "The input cannot be parsed as protobuf, got:\n" << pb.DebugString();
       return false;
     }
-    google::protobuf::util::MessageDifferencer differencer;
     std::string diff_report;
-    differencer.ReportDifferencesToString(&diff_report);
+    differencer_->ReportDifferencesToString(&diff_report);
     if (optional_scope_.has_value()) {
-      differencer.set_scope(optional_scope_.value());
+      differencer_->set_scope(optional_scope_.value());
     }
-    if (!differencer.Compare(*expected_pb, pb)) {
+    if (!differencer_->Compare(*expected_pb, pb)) {
       (*listener) << "got:\n" << pb.DebugString() << "diff:\n" << diff_report;
       return false;
     }
     return true;
   }
 
-  void DescribeTo(::std::ostream* os) const {
+  virtual void DescribeTo(::std::ostream* os) const {
     *os << "equals to text probobuf: " << expected_text_pb_;
   }
 
-  void DescribeNegationTo(::std::ostream* os) const {
+  virtual void DescribeNegationTo(::std::ostream* os) const {
     *os << "does not equal to text protobuf: " << expected_text_pb_;
   }
 
   std::string expected_text_pb_;
+  google::protobuf::util::MessageDifferencer* differencer_;
   std::optional<google::protobuf::util::MessageDifferencer::Scope> optional_scope_;
 };
 
-struct PartiallyEqualsProtoMatcher : public EqualsProtoMatcher {
-  explicit PartiallyEqualsProtoMatcher(std::string text_pb)
-      : EqualsProtoMatcher(std::move(text_pb)) {
+struct EqualsProtoMatcher : public ProtoMatcher {
+  explicit EqualsProtoMatcher(std::string text_pb) : ProtoMatcher(nullptr, std::move(text_pb)) {
+    vanilla_differencer_ = std::make_shared<google::protobuf::util::MessageDifferencer>();
+    differencer_ = vanilla_differencer_.get();
+  }
+  // NOTE: using a shared_ptr here instead of as a regular object because MessageDifferencer
+  // does not have a copy constructor and MakePolymorphicMatccher copies the passed in
+  // Matcher object (which then copies this member).
+  std::shared_ptr<google::protobuf::util::MessageDifferencer> vanilla_differencer_;
+};
+
+struct PartiallyEqualsProtoMatcher : public ProtoMatcher {
+  PartiallyEqualsProtoMatcher(google::protobuf::util::MessageDifferencer* differencer,
+                              std::string text_pb)
+      : ProtoMatcher(differencer, std::move(text_pb)) {
     optional_scope_ = google::protobuf::util::MessageDifferencer::Scope::PARTIAL;
   }
 
-  void DescribeTo(::std::ostream* os) const {
+  void DescribeTo(::std::ostream* os) const override {
     *os << "partially equals to text probobuf: " << expected_text_pb_;
   }
 
-  void DescribeNegationTo(::std::ostream* os) const {
+  void DescribeNegationTo(::std::ostream* os) const override {
     *os << "does not partially equal to text protobuf: " << expected_text_pb_;
   }
 };
@@ -74,7 +87,20 @@ inline ::testing::PolymorphicMatcher<EqualsProtoMatcher> EqualsProto(std::string
 inline ::testing::PolymorphicMatcher<PartiallyEqualsProtoMatcher> Partially(
     const ::testing::PolymorphicMatcher<EqualsProtoMatcher>& matcher) {
   return ::testing::MakePolymorphicMatcher(
-      PartiallyEqualsProtoMatcher(matcher.impl().expected_text_pb_));
+      PartiallyEqualsProtoMatcher(matcher.impl().differencer_, matcher.impl().expected_text_pb_));
+}
+
+inline ::testing::PolymorphicMatcher<PartiallyEqualsProtoMatcher> Partially(
+    const ::testing::PolymorphicMatcher<ProtoMatcher>& matcher) {
+  return ::testing::MakePolymorphicMatcher(
+      PartiallyEqualsProtoMatcher(matcher.impl().differencer_, matcher.impl().expected_text_pb_));
+}
+
+inline ::testing::PolymorphicMatcher<ProtoMatcher> WithDifferencer(
+    google::protobuf::util::MessageDifferencer* differencer,
+    const ::testing::PolymorphicMatcher<EqualsProtoMatcher>& matcher) {
+  return ::testing::MakePolymorphicMatcher(
+      ProtoMatcher(differencer, matcher.impl().expected_text_pb_));
 }
 
 }  // namespace proto
