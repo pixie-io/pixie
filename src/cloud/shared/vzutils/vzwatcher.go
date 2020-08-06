2c3ff352d00e303dfebe3752b94030f2a3c7ef64
Michelle Nguyen <michelle@pixielabs.ai>
2020-08-06 12:46:33 -0700

    Implementation for vizier watcher
    Summary:
    the vizier watcher listens to the nats channel for any newly connected viziers.
    if a VizierFunction is registered, it will fetch all currently active viziers and execute the function for each of those viziers.
    for any newly connected viziers, it will also execute the provided function.
    
    Test Plan: unit test
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D5899
    
    GitOrigin-RevId: d7e58168ee673d48e891c3326dd8550fcc54a325

--- src/cloud/shared/vzutils/vzwatcher.go
+++ src/cloud/shared/vzutils/vzwatcher.go
@@ -1,10 +1,21 @@
 package vzutils
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/gogo/protobuf/proto"
 	"github.com/nats-io/nats.go"
 	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc/metadata"
 
+	"pixielabs.ai/pixielabs/src/cloud/shared/messages"
+	messagespb "pixielabs.ai/pixielabs/src/cloud/shared/messagespb"
 	"pixielabs.ai/pixielabs/src/cloud/vzmgr/vzmgrpb"
+	svcutils "pixielabs.ai/pixielabs/src/shared/services/utils"
+	"pixielabs.ai/pixielabs/src/utils"
 )
 
 // VizierHandlerFn is the function signature for the function that is run for active and newly connected Viziers.
@@ -22,42 +33,112 @@ type Watcher struct {
 	vizierHandlerFn VizierHandlerFn
 	errorHandlerFn  ErrorHandlerFn
 
-	quitCh chan bool
+	quitCh      chan bool
+	ch          chan *nats.Msg
+	sub         *nats.Subscription
+	toShardID   string
+	fromShardID string
 }
 
 // NewWatcher creates a new vizier watcher.
-func NewWatcher(nc *nats.Conn, vzmgrClient vzmgrpb.VZMgrServiceClient) *Watcher {
+func NewWatcher(nc *nats.Conn, vzmgrClient vzmgrpb.VZMgrServiceClient, fromShardID string, toShardID string) (*Watcher, error) {
+	ch := make(chan *nats.Msg, 1024)
+	sub, err := nc.ChanSubscribe(messages.VizierConnectedChannel, ch)
+	if err != nil {
+		close(ch)
+		return nil, err
+	}
+
 	vw := &Watcher{
 		nc:          nc,
 		vzmgrClient: vzmgrClient,
 		quitCh:      make(chan bool),
+		ch:          ch,
+		sub:         sub,
+		toShardID:   toShardID,
+		fromShardID: fromShardID,
 	}
 
 	go vw.runWatch()
 
-	return vw
+	return vw, nil
 }
 
 // runWatch subscribes to the NATS channel for any newly connected viziers, and executes the registered task for
 // each.
 func (w *Watcher) runWatch() {
+	defer w.sub.Unsubscribe()
+	defer close(w.ch)
+	for {
+		select {
+		case <-w.quitCh:
+			log.Info("Quit signaled")
+			return
+		case msg := <-w.ch:
+			vcMsg := &messagespb.VizierConnected{}
+			err := proto.Unmarshal(msg.Data, vcMsg)
+			if err != nil {
+				log.WithError(err).Error("Could not unmarshal VizierConnected msg")
+				continue
+			}
+			vzID := utils.UUIDFromProtoOrNil(vcMsg.VizierID)
+			orgID := utils.UUIDFromProtoOrNil(vcMsg.OrgID)
+			w.onVizier(vzID, orgID, vcMsg.K8sUID, vcMsg.ResourceVersion)
+		}
+	}
+}
+
+func (w *Watcher) onVizier(id uuid.UUID, orgID uuid.UUID, uid string, resourceVersion string) {
+	if w.vizierHandlerFn == nil {
+		return
+	}
+
+	err := w.vizierHandlerFn(id, orgID, uid, resourceVersion)
+	if err != nil && w.errorHandlerFn != nil {
+		w.errorHandlerFn(id, orgID, uid, resourceVersion, err)
+	}
 }
 
 // RegisterVizierHandler registers the function that should be called on all currently active Viziers, and any newly
 // connected Viziers.
 func (w *Watcher) RegisterVizierHandler(fn VizierHandlerFn) error {
-	// Make an RPC call to Vzmgr to get all active Viziers. Call the VizierHandlerFn on all those viziers.
+	w.vizierHandlerFn = fn
 
-	// Set w.registeredFn = fn, so that runWatch calls fn for each newly connected vizier.
+	// Call VizierHandlerFn on all current-active viziers.
+	serviceAuthToken, err := getServiceCredentials(viper.GetString("jwt_signing_key"))
+	if err != nil {
+		return err
+	}
+	ctx := metadata.AppendToOutgoingContext(context.Background(), "authorization",
+		fmt.Sprintf("bearer %s", serviceAuthToken))
+	vzmgrResp, err := w.vzmgrClient.GetViziersByShard(ctx, &vzmgrpb.GetViziersByShardRequest{
+		FromShardID: w.fromShardID,
+		ToShardID:   w.toShardID,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, vz := range vzmgrResp.Viziers {
+		vzID := utils.UUIDFromProtoOrNil(vz.VizierID)
+		orgID := utils.UUIDFromProtoOrNil(vz.OrgID)
+		w.onVizier(vzID, orgID, vz.K8sUID, vz.ResourceVersion)
+	}
+
 	return nil
 }
 
 // RegisterErrorHandler registers the function that should be called when the VizierHandler returns an error.
 func (w *Watcher) RegisterErrorHandler(fn ErrorHandlerFn) {
-
+	w.errorHandlerFn = fn
 }
 
 // Stop stops the watcher.
 func (w *Watcher) Stop() {
 	close(w.quitCh)
+}
+
+func getServiceCredentials(signingKey string) (string, error) {
+	claims := svcutils.GenerateJWTForService("vzwatcher")
+	return svcutils.SignJWTClaims(claims, signingKey)
 }
---
+++ src/cloud/shared/vzutils/vzwatcher_test.go
@@ -0,0 +1,128 @@
+package vzutils_test
+
+import (
+	"errors"
+	"sync"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/nats-io/nats.go"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+
+	"pixielabs.ai/pixielabs/src/cloud/shared/messages"
+	messagespb "pixielabs.ai/pixielabs/src/cloud/shared/messagespb"
+	"pixielabs.ai/pixielabs/src/cloud/shared/vzutils"
+	"pixielabs.ai/pixielabs/src/cloud/vzmgr/vzmgrpb"
+	mock_vzmgrpb "pixielabs.ai/pixielabs/src/cloud/vzmgr/vzmgrpb/mock"
+	"pixielabs.ai/pixielabs/src/utils"
+	"pixielabs.ai/pixielabs/src/utils/testingutils"
+)
+
+func TestVzWatcher(t *testing.T) {
+	tests := []struct {
+		name        string
+		expectError bool
+	}{
+		{
+			name:        "no error",
+			expectError: false,
+		},
+		{
+			name:        "error",
+			expectError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			mockVZMgr := mock_vzmgrpb.NewMockVZMgrServiceClient(ctrl)
+
+			existingVzID := uuid.NewV4()
+			existingOrgID := uuid.NewV4()
+			existingK8sUID := "testUID"
+			existingResourceVersion := "1234"
+
+			mockVZMgr.
+				EXPECT().
+				GetViziersByShard(gomock.Any(), &vzmgrpb.GetViziersByShardRequest{
+					FromShardID: "00",
+					ToShardID:   "bb",
+				}).
+				Return(&vzmgrpb.GetViziersByShardResponse{
+					Viziers: []*vzmgrpb.GetViziersByShardResponse_VizierInfo{
+						&vzmgrpb.GetViziersByShardResponse_VizierInfo{
+							VizierID:        utils.ProtoFromUUID(&existingVzID),
+							OrgID:           utils.ProtoFromUUID(&existingOrgID),
+							ResourceVersion: existingResourceVersion,
+							K8sUID:          existingK8sUID,
+						},
+					},
+				}, nil)
+
+			natsPort, natsCleanup := testingutils.StartNATS(t)
+			nc, err := nats.Connect(testingutils.GetNATSURL(natsPort))
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer natsCleanup()
+
+			w, err := vzutils.NewWatcher(nc, mockVZMgr, "00", "bb")
+			assert.Nil(t, err)
+
+			var wg sync.WaitGroup
+			wg.Add(2)
+
+			if test.expectError {
+				wg.Add(1)
+			}
+
+			defer wg.Wait()
+
+			newVzID := uuid.NewV4()
+			newOrgID := uuid.NewV4()
+			newK8sUID := "testUID"
+			newResourceVersion := "1234"
+
+			w.RegisterErrorHandler(func(id uuid.UUID, orgID uuid.UUID, uid string, rv string, err error) {
+				defer wg.Done()
+				assert.Equal(t, existingVzID, id)
+				assert.Equal(t, existingOrgID, orgID)
+				assert.Equal(t, existingK8sUID, uid)
+				assert.Equal(t, existingResourceVersion, rv)
+			})
+
+			w.RegisterVizierHandler(func(id uuid.UUID, orgID uuid.UUID, uid string, rv string) error {
+				defer wg.Done()
+
+				if id == existingVzID {
+					assert.Equal(t, existingOrgID, orgID)
+					assert.Equal(t, existingK8sUID, uid)
+					assert.Equal(t, existingResourceVersion, rv)
+					if test.expectError {
+						return errors.New("Some error")
+					}
+				} else if id == newVzID {
+					assert.Equal(t, newOrgID, orgID)
+					assert.Equal(t, newK8sUID, uid)
+					assert.Equal(t, newResourceVersion, rv)
+				} else {
+					t.Fatal("Called Vizier handler with unexpected vizier")
+				}
+				return nil
+			})
+
+			msg := &messagespb.VizierConnected{
+				VizierID:        utils.ProtoFromUUID(&newVzID),
+				OrgID:           utils.ProtoFromUUID(&newOrgID),
+				ResourceVersion: newResourceVersion,
+				K8sUID:          newK8sUID,
+			}
+			b, err := msg.Marshal()
+			assert.Nil(t, err)
+			nc.Publish(messages.VizierConnectedChannel, b)
+		})
+	}
+}
