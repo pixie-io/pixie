0849cd31807cd0a4050b6e27d8d4f7a4daac7c07
Michelle Nguyen <michelle@pixielabs.ai>
2020-09-08 13:49:56 -0700

    Etcd batch utils: should limit batches by max num txns and byte size
    Summary:
    some of our customers clusters were hitting a problem where the number of bytes we were flushing in the cache was too much in a single txn.
    we are currently batching requests by a max number of txns, however it is possible to have really large requests with less than the max number of txns.
    this updates our etcd batcher util to take the max number of bytes into account as well.
    
    Test Plan: unit test
    
    Reviewers: zasgar, #engineering, nserrino
    
    Reviewed By: #engineering, nserrino
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6178
    
    GitOrigin-RevId: 12b4cf9a3ee5ce1b082effe34b930cd5dcc21120

--- src/vizier/services/metadata/controllers/etcd/utils.go
+++ src/vizier/services/metadata/controllers/etcd/utils.go
@@ -2,7 +2,7 @@ package etcd
 
 import (
 	"context"
-	"math"
+	"errors"
 
 	v3 "github.com/coreos/etcd/clientv3"
 	etcdpb "github.com/coreos/etcd/etcdserver/etcdserverpb"
@@ -11,20 +11,44 @@ import (
 // The maximum number of operations that can be done in a single transaction, set by the etcd cluster.
 var maxTxnOps = 128
 
+// The maximum number of bytes we should include a in single transaction (max allowed by etcd: 1.5MiB).
+var maxNumBytes = 1048576 // (1 MiB)
+
 // BatchOps performs the given transaction operations in batches.
 func BatchOps(client *v3.Client, ops []v3.Op) ([]*etcdpb.ResponseOp, error) {
-	batch := 0
 	var totalOutput []*etcdpb.ResponseOp
-	for batch*maxTxnOps < len(ops) {
+	currBatch := make([]v3.Op, 0)
+	currBatchBytes := 0
 
-		batchSlice := ops[batch*maxTxnOps : int(math.Min(float64((batch+1)*maxTxnOps), float64(len(ops))))]
-		output, err := client.Txn(context.TODO()).If().Then(batchSlice...).Commit()
+	for _, op := range ops {
+		opBytes := len(op.ValueBytes())
+		if opBytes > maxNumBytes {
+			return nil, errors.New("Etcd operation bytes larger than max request bytes")
+		}
+
+		if len(currBatch) == maxTxnOps || (currBatchBytes+opBytes > maxNumBytes) {
+			output, err := client.Txn(context.TODO()).If().Then(currBatch...).Commit()
+			if err != nil {
+				return nil, err
+			}
+			totalOutput = append(totalOutput, output.Responses...)
+
+			currBatch = make([]v3.Op, 0)
+			currBatchBytes = 0
+		}
+
+		currBatch = append(currBatch, op)
+		currBatchBytes = currBatchBytes + opBytes
+	}
+
+	if len(currBatch) > 0 {
+		output, err := client.Txn(context.TODO()).If().Then(currBatch...).Commit()
 		if err != nil {
 			return nil, err
 		}
 		totalOutput = append(totalOutput, output.Responses...)
-
-		batch++
 	}
+
 	return totalOutput, nil
+
 }
--- src/vizier/services/metadata/controllers/etcd/utils_test.go
+++ src/vizier/services/metadata/controllers/etcd/utils_test.go
@@ -2,6 +2,7 @@ package etcd_test
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 	"testing"
 
@@ -50,4 +51,45 @@ func TestLargerThanOneBatch(t *testing.T) {
 
 func TestTwoBatches(t *testing.T) {
 	testBatchSize(t, 256)
+}
+
+func testBatchBytesSize(t *testing.T, size int, valueSize int) {
+	clearEtcd(t)
+
+	i := 0
+	keyValues := make(map[int][]byte)
+	ops := make([]clientv3.Op, size)
+	for i < size {
+		randValue := make([]byte, valueSize)
+		_, err := rand.Read(randValue)
+		assert.Nil(t, err)
+		keyValues[i] = randValue
+		ops[i] = clientv3.OpPut("/"+fmt.Sprint(i), string(randValue))
+		i++
+	}
+
+	resp, err := etcd.BatchOps(etcdClient, ops)
+	assert.Equal(t, size, len(resp))
+	assert.Nil(t, err)
+
+	i = 0
+	for i < size {
+		val, err := etcdClient.Get(context.Background(), "/"+fmt.Sprint(i))
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(val.Kvs))
+		assert.Equal(t, keyValues[i], val.Kvs[0].Value)
+		i++
+	}
+}
+
+func TestSmallerBytes(t *testing.T) {
+	testBatchBytesSize(t, 127, 1)
+}
+
+func TestLargerBytes(t *testing.T) {
+	testBatchBytesSize(t, 16, 131072)
+}
+
+func TestTwoByteBatches(t *testing.T) {
+	testBatchBytesSize(t, 3, 1048576)
 }
