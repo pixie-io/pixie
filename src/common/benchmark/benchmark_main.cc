1acfa0459fb4aa50ffbfd0bead687d397cb7ccd1
Phillip Kuznetsov <philkuz@pixielabs.ai>
2020-09-17 10:01:18 -0700

    Benchmark Args in the wrong order w/ env_guard
    Summary: Fix that makes benchmark args actually available, toherwise args would cause errors when running benchmarks.
    
    Test Plan:
    Tested by passing ```
    bazel run -c opt //src/carnot/planner:logical_planner_benchmark -- --benchmark_min_time=5
    ```
    
    Reviewers: zasgar, oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6239
    
    GitOrigin-RevId: 08de9c2796e03b76be625bf0c80b4d0df297cd62

--- src/common/benchmark/benchmark_main.cc
+++ src/common/benchmark/benchmark_main.cc
@@ -2,8 +2,11 @@
 #include "src/common/base/base.h"
 
 int main(int argc, char** argv) {
-  pl::EnvironmentGuard env_guard(&argc, argv);
+  // Initialize must come before env_guard otherwise the arguments are not declared and env_guard
+  // throws an error if you pass in benchmark args. Note the arguments that benchmark uses are also
+  // consumed but doesn't affect other arguments.
   benchmark::Initialize(&argc, argv);
+  pl::EnvironmentGuard env_guard(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
   return 0;
 }
