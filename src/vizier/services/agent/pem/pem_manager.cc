pixie-io/pixie

commit ef02f5384d2ffc73b772894c988167de069d2141

TableStore: AddTable interface changes
Summary: Just some follow-up to a previous change. Simplifying the levels of indirection by further by tweaking the interface.

Test Plan: Existing tests.

Reviewers: michelle, #engineering, zasgar

Reviewed By: #engineering, zasgar

Differential Revision: https://phab.corp.pixielabs.ai/D6341

GitOrigin-RevId: 61cb42962c0b2c89950b9b94243af992b25d421b

src/vizier/services/agent/pem/pem_manager.cc
@@ -48,16 +48,16 @@ Status PEMManager::InitSchemas() {
   // This should eventually be done by subscribe requests.
   auto relation_info_vec = ConvertSubscribePBToRelationInfo(subscribe_pb);
   for (const auto& relation_info : relation_info_vec) {
+    // Make http_events hold 512Mi. This is a hack and will be removed once we have proactive
+    // backup.
+    std::shared_ptr<table_store::Table> table_ptr;
     if (relation_info.name == "http_events") {
-      // Make http_events hold 512Mi. This is a hack and will be removed once we have proactive
-      // backup.
-      auto t = std::shared_ptr<table_store::Table>(
-          new table_store::Table(relation_info.relation, 1024 * 1024 * 512));
-      table_store()->AddTable(relation_info.id, relation_info.name, t);
+      std::make_shared<table_store::Table>(relation_info.relation, 1024 * 1024 * 512);
     } else {
-      table_store()->AddTable(relation_info.id, relation_info.name,
-                              table_store::Table::Create(relation_info.relation));
+      table_store::Table::Create(relation_info.relation);
     }
+
+    table_store()->AddTable(std::move(table_ptr), relation_info.name, relation_info.id);
     PL_RETURN_IF_ERROR(relation_info_manager()->AddRelationInfo(relation_info));
   }
   return Status::OK();

src/carnot/exec/memory_source_node_test.cc
@@ -213,7 +213,7 @@ class MemorySourceNodeTabletTest : public ::testing::Test {
     std::shared_ptr<Table> tablet = Table::Create(rel);
     AddValuesToTable(tablet.get());
 
-    exec_state_->table_store()->AddTable(table_id_, table_name_, tablet_id_, tablet);
+    exec_state_->table_store()->AddTable(tablet, table_name_, table_id_, tablet_id_);
   }
 
   void AddValuesToTable(Table* table) {

src/table_store/table/table_store.cc
@@ -93,9 +93,8 @@ void TableStore::RegisterTableID(uint64_t table_id, TableInfo table_info,
   id_to_table_map_[key] = table;
 }
 
-void TableStore::AddTableImpl(std::optional<uint64_t> table_id, const std::string& table_name,
-                              const types::TabletID& tablet_id,
-                              std::shared_ptr<table_store::Table> table) {
+void TableStore::AddTable(std::shared_ptr<table_store::Table> table, const std::string& table_name,
+                          std::optional<uint64_t> table_id, const types::TabletID& tablet_id) {
   const auto& table_relation = table->GetRelation();
 
   // Register the table by name.

src/table_store/table/table_store.h
@@ -92,28 +92,22 @@ class TableStore {
   /**
    * Add a table under the given name and optionally tablet id.
    *
-   * @ param table_id: unique ID to associate with the table.
-   * @ param table_name: the name of the table to add this under.
    * @ param table: the table to store.
-   * @ param tablet_id: the optional id of the tablet to assign this to.
+   * @ param table_name: the name of the table to add this under.
+   * @ param table_id: optional unique ID to for the table name, for quick-lookups.
+   * @ param tablet_id: the tablet id to assign this to.
+   *
+   * Unlike most interfaces, this has the value first (table), and the key second (table_name,
+   * table_id and tablet_id are all essentially keys). This is done because to accommodate
+   * optional fields.
    */
-  void AddTable(uint64_t table_id, const std::string& table_name, const types::TabletID& tablet_id,
-                std::shared_ptr<table_store::Table> table) {
-    return AddTableImpl(table_id, table_name, tablet_id, table);
-  }
+  void AddTable(std::shared_ptr<table_store::Table> table, const std::string& table_name,
+                std::optional<uint64_t> table_id = std::nullopt,
+                const types::TabletID& tablet_id = kDefaultTablet);
 
-  void AddTable(uint64_t table_id, const std::string& table_name,
-                std::shared_ptr<table_store::Table> table) {
-    return AddTableImpl(table_id, table_name, kDefaultTablet, table);
-  }
-
-  void AddTable(const std::string& table_name, const types::TabletID& tablet_id,
-                std::shared_ptr<table_store::Table> table) {
-    return AddTableImpl(std::nullopt, table_name, tablet_id, table);
-  }
-
+  // Old interface: Deprecated.
   void AddTable(const std::string& table_name, std::shared_ptr<table_store::Table> table) {
-    return AddTableImpl(std::nullopt, table_name, kDefaultTablet, table);
+    return AddTable(std::move(table), table_name);
   }
 
   /**
@@ -154,17 +148,6 @@ class TableStore {
   }
 
  private:
-  /**
-   * Add a table under the given name and optionally tablet id.
-   *
-   * @ param table_name: the name of the table to add this under.
-   * @ param table_id: optional unique ID to associate with the table.
-   * @ param table: the table to store.
-   * @ param tablet_id: the optional id of the tablet to assign this to.
-   */
-  void AddTableImpl(std::optional<uint64_t> table_id, const std::string& table_name,
-                    const types::TabletID& tablet_id, std::shared_ptr<table_store::Table> table);
-
   void RegisterTableName(const std::string& table_name, const types::TabletID& tablet_id,
                          const schema::Relation& table_relation,
                          std::shared_ptr<table_store::Table> table);

src/table_store/table/table_store_test.cc
@@ -52,8 +52,8 @@ class TableStoreTest : public ::testing::Test {
 
 TEST_F(TableStoreTest, basic) {
   auto table_store = TableStore();
-  table_store.AddTable("a", table1);
-  table_store.AddTable("b", table2);
+  table_store.AddTable(table1, "a");
+  table_store.AddTable(table2, "b");
 
   auto lookup = table_store.GetRelationMap();
   EXPECT_EQ(2, lookup->size());
@@ -71,8 +71,8 @@ TEST_F(TableStoreTest, basic) {
 
 TEST_F(TableStoreTest, get_table_ids) {
   auto table_store = TableStore();
-  table_store.AddTable(1, "a", table1);
-  table_store.AddTable(20, "b", table2);
+  table_store.AddTable(table1, "a", 1);
+  table_store.AddTable(table2, "b", 20);
 
   EXPECT_THAT(table_store.GetTableIDs(), ::testing::UnorderedElementsAre(1, 20));
 }
@@ -83,7 +83,7 @@ TEST_F(TableStoreTest, table_id_aliasing) {
   const uint64_t kTableID = 1;
   const uint64_t kAliasID = 5;
 
-  table_store.AddTable(kTableID, "a", table1);
+  table_store.AddTable(table1, "a", kTableID);
   EXPECT_OK(table_store.AddTableAlias(kAliasID, "a"));
 
   Table* table;
@@ -132,14 +132,14 @@ TEST_F(TableStoreTest, table_id_aliasing) {
 using TableStoreDeathTest = TableStoreTest;
 TEST_F(TableStoreDeathTest, rewrite_fails) {
   auto table_store = TableStore();
-  table_store.AddTable("a", table1);
+  table_store.AddTable(table1, "a");
 
   auto lookup = table_store.GetRelationMap();
   EXPECT_EQ(1, lookup->size());
   EXPECT_EQ(table1->GetRelation(), lookup->at("a"));
   EXPECT_FALSE(table2->GetRelation() == lookup->at("a"));
 
-  EXPECT_DEBUG_DEATH(table_store.AddTable("a", table2), "name_to_relation_map_iter->second == .*");
+  EXPECT_DEBUG_DEATH(table_store.AddTable(table2, "a"), "name_to_relation_map_iter->second == .*");
 }
 
 const char* kTableStoreProto = R"proto(
@@ -176,8 +176,8 @@ relation_map {
 )proto";
 TEST_F(TableStoreTest, to_proto) {
   auto table_store = TableStore();
-  table_store.AddTable("a", table1);
-  table_store.AddTable("b", table1);
+  table_store.AddTable(table1, "a");
+  table_store.AddTable(table1, "b");
   schemapb::Schema actual_schema;
   schemapb::Schema expected_schema;
   EXPECT_OK(table_store.SchemaAsProto(&actual_schema));
@@ -208,8 +208,8 @@ TEST_F(TableStoreTabletsTest, tablet_test) {
   types::TabletID tablet2_id = "789";
 
   // Create the containing table and add the tablets to the table.
-  table_store.AddTable(table_id, "a", tablet1_id, tablet1_1);
-  table_store.AddTable(table_id, "a", tablet2_id, tablet1_2);
+  table_store.AddTable(tablet1_1, "a", table_id, tablet1_id);
+  table_store.AddTable(tablet1_2, "a", table_id, tablet2_id);
 
   Table* tablet1 = table_store.GetTable("a", tablet1_id);
   EXPECT_EQ(tablet1->NumBytes(), 0);
@@ -241,7 +241,7 @@ TEST_F(TableStoreTabletsTest, add_tablet_on_append_data) {
   types::TabletID tablet2_id = "789";
 
   // Only add tablet 2.
-  table_store.AddTable(table_id, "a", tablet2_id, tablet1_1);
+  table_store.AddTable(tablet1_1, "a", table_id, tablet2_id);
 
   Table* tablet2 = table_store.GetTable("a", tablet2_id);
   EXPECT_EQ(tablet2->NumBytes(), 0);
@@ -267,9 +267,9 @@ TEST_F(TableStoreTabletsDeathTest, tablet_test) {
   types::TabletID tablet2_id = "789";
   types::TabletID tablet3_id = "654";
 
-  table_store.AddTable(table_id, "a", tablet1_id, tablet1_1);
-  table_store.AddTable(table_id, "a", tablet2_id, tablet1_2);
-  EXPECT_DEBUG_DEATH(table_store.AddTable(table_id, tablet3_id, tablet2_1), "");
+  table_store.AddTable(tablet1_1, "a", table_id, tablet1_id);
+  table_store.AddTable(tablet1_2, "a", table_id, tablet2_id);
+  EXPECT_DEBUG_DEATH(table_store.AddTable(tablet2_1, "a", table_id, tablet3_id), "");
 }
 
 }  // namespace table_store

src/vizier/services/agent/pem/tracepoint_manager.cc
@@ -204,8 +204,8 @@ Status TracepointManager::UpdateSchema(const stirling::stirlingpb::Publish& publ
   // figure out how to handle this as part of the data model refactor project.
   for (const auto& relation_info : relation_info_vec) {
     if (!relation_info_manager_->HasRelation(relation_info.name)) {
-      table_store_->AddTable(relation_info.id, relation_info.name,
-                             table_store::Table::Create(relation_info.relation));
+      table_store_->AddTable(table_store::Table::Create(relation_info.relation), relation_info.name,
+                             relation_info.id);
       PL_RETURN_IF_ERROR(relation_info_manager_->AddRelationInfo(relation_info));
     } else {
       if (relation_info.relation != table_store_->GetTable(relation_info.name)->GetRelation()) {

commit 4ca7968a538762c54496360fd78b45427d1acd86

Fix segfault
Summary:
Fix silly mistake introduced in D6341.

Apparently we have no test coverage for this though.

Test Plan: Manual

Reviewers: zasgar, #engineering

Reviewed By: zasgar, #engineering

Differential Revision: https://phab.corp.pixielabs.ai/D6351

GitOrigin-RevId: cc0c096bdc40208ea6966c7a71dba023cb2b022c

src/vizier/services/agent/pem/pem_manager.cc
@@ -48,13 +48,13 @@ Status PEMManager::InitSchemas() {
   // This should eventually be done by subscribe requests.
   auto relation_info_vec = ConvertSubscribePBToRelationInfo(subscribe_pb);
   for (const auto& relation_info : relation_info_vec) {
-    // Make http_events hold 512Mi. This is a hack and will be removed once we have proactive
-    // backup.
     std::shared_ptr<table_store::Table> table_ptr;
     if (relation_info.name == "http_events") {
-      std::make_shared<table_store::Table>(relation_info.relation, 1024 * 1024 * 512);
+      // Make http_events hold 512Mi. This is a hack and will be removed once we have proactive
+      // backup.
+      table_ptr = std::make_shared<table_store::Table>(relation_info.relation, 1024 * 1024 * 512);
     } else {
-      table_store::Table::Create(relation_info.relation);
+      table_ptr = table_store::Table::Create(relation_info.relation);
     }
 
     table_store()->AddTable(std::move(table_ptr), relation_info.name, relation_info.id);