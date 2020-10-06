1ef65c339729fd0e7b3a2116779921e8c113fc00
Zain Asgar <zasgar@pixielabs.ai>
2020-10-06 09:34:49 -0700

    Add RPM and DEB packages for px
    Summary: TSIA
    
    Test Plan: N/A
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6459
    
    GitOrigin-RevId: 7a841eeda2be31bf9f4774eb6196284a78512c8b

--- ci/cli_build_release.sh
+++ ci/cli_build_release.sh
@@ -9,9 +9,12 @@ repo_path=$(bazel info workspace)
 printenv
 
 release_tag=${TAG_NAME##*/v}
+arch=x86_64
+pkg_prefix="pixie-px-${release_tag}.${arch}"
 versions_file="${repo_path}/src/utils/artifacts/artifact_db_updater/VERSIONS.json"
 
 echo "The release tag is: ${release_tag}"
+linux_binary=bazel-bin/src/utils/pixie_cli/px_/px
 
 bazel run -c opt //src/utils/artifacts/versions_gen:versions_gen -- \
       --repo_path "${repo_path}" --artifact_name cli --versions_file "${versions_file}"
@@ -20,9 +23,40 @@ bazel build -c opt --build_event_text_file=/tmp/darwin_build --stamp //src/utils
 
 bazel build -c opt --stamp //src/utils/pixie_cli:px
 
+# Create and push docker image.
+bazel run -c opt --stamp //src/utils/pixie_cli:push_px_image
+
 if [[ ! "$release_tag" == *"-"* ]]; then
-    bazel run -c opt --stamp //src/utils/pixie_cli:push_px_image
-else
+    # Create rpm package.
+    docker run \
+           -v "${repo_path}/${linux_binary}:/src/px" \
+           -v "${repo_path}:/image" \
+           cdrx/fpm-fedora:24 \
+           fpm \
+           -f \
+           -p "/image/${pkg_prefix}.rpm" \
+           -s dir \
+           -t rpm \
+           -n pixie-px \
+           -v "${release_tag}" \
+           --prefix /usr/local/bin \
+           px
+
+    # Create deb package.
+    docker run \
+           -v "${repo_path}/${linux_binary}:/src/px" \
+           -v "${repo_path}:/image" \
+           cdrx/fpm-ubuntu:18.04 \
+           fpm \
+           -f \
+           -p "/image/${pkg_prefix}.deb" \
+           -s dir \
+           -t deb \
+           -n pixie-px \
+           -v "${release_tag}" \
+           --prefix /usr/local/bin \
+           px
+
     # Push officially releases to docker hub.
     bazel run -c opt --stamp //src/utils/pixie_cli:push_px_image_to_docker
 fi
@@ -30,9 +64,14 @@ fi
 write_artifacts_to_gcs() {
     output_path=$1
     mac_binary=$(grep -oP -m 1 '(?<=pl\/).*px_darwin(?=\")' /tmp/darwin_build)
-    linux_binary=bazel-bin/src/utils/pixie_cli/px_/px
     copy_artifact_to_gcs "$output_path" "$mac_binary" "cli_darwin_amd64_unsigned"
     copy_artifact_to_gcs "$output_path" "$linux_binary" "cli_linux_amd64"
+
+    if [[ ! "$release_tag" == *"-"* ]]; then
+        # RPM/DEB only exists for release builds.
+        copy_artifact_to_gcs "$output_path" "${repo_path}/${pkg_prefix}.deb" "${pkg_prefix}.deb"
+        copy_artifact_to_gcs "$output_path" "${repo_path}/${pkg_prefix}.rpm" "${pkg_prefix}.rpm"
+    fi
 }
 
 output_path="gs://pixie-prod-artifacts/cli/${release_tag}"
