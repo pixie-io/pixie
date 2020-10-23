33557af0eb649ab4061c38b19c5459632a2b4d5f
Zain Asgar <zasgar@pixielabs.ai>
2020-10-22 21:50:18 -0700

    Add some heap debug UDTFs
    Summary: UDTFs that will help debug memory issues.
    
    Test Plan: N/A
    
    Reviewers: michelle, vihang, philkuz, #engineering
    
    Reviewed By: philkuz, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6576
    
    GitOrigin-RevId: a0d7dfcb05842e65fd3ab26054e132da20012236

---
+++ src/vizier/funcs/internal/debug.h
@@ -0,0 +1,102 @@
+#pragma once
+#include <string>
+
+#include "src/carnot/udf/registry.h"
+#include "src/carnot/udf/udf.h"
+#include "src/common/base/base.h"
+
+#ifdef TCMALLOC
+#include <gperftools/malloc_extension.h>
+#endif
+
+namespace pl {
+namespace vizier {
+namespace funcs {
+
+constexpr int kMaxBufferSize = 1024 * 1024;
+
+class HeapStatsUDTF final : public carnot::udf::UDTF<HeapStatsUDTF> {
+ public:
+  static constexpr auto Executor() { return carnot::udfspb::UDTFSourceExecutor::UDTF_ALL_AGENTS; }
+
+  static constexpr auto OutputRelation() {
+    return MakeArray(ColInfo("asid", types::DataType::INT64, types::PatternType::GENERAL,
+                             "The short ID of the agent"),
+                     ColInfo("heap", types::DataType::STRING, types::PatternType::GENERAL,
+                             "The pretty heap stats"));
+  }
+
+  bool NextRecord(FunctionContext* ctx, RecordWriter* rw) {
+#ifdef TCMALLOC
+    std::string buf(kMaxBufferSize, '\0');
+    MallocExtension::instance()->GetStats(&buf[0], buf.size());
+    buf.resize(strlen(buf.c_str()));
+
+    rw->Append<IndexOf("asid")>(ctx->metadata_state()->asid());
+    rw->Append<IndexOf("heap")>(buf);
+#else
+    rw->Append<IndexOf("asid")>(ctx->metadata_state()->asid());
+    rw->Append<IndexOf("heap")>("Only supported with tcmalloc.");
+#endif
+
+    return false;
+  }
+};
+
+class HeapSampleUDTF final : public carnot::udf::UDTF<HeapSampleUDTF> {
+ public:
+  static constexpr auto Executor() { return carnot::udfspb::UDTFSourceExecutor::UDTF_ALL_AGENTS; }
+
+  static constexpr auto OutputRelation() {
+    return MakeArray(ColInfo("asid", types::DataType::INT64, types::PatternType::GENERAL,
+                             "The short ID of the agent"),
+                     ColInfo("heap", types::DataType::STRING, types::PatternType::GENERAL,
+                             "The pretty heap stats"));
+  }
+
+  bool NextRecord(FunctionContext* ctx, RecordWriter* rw) {
+#ifdef TCMALLOC
+    std::string buf;
+    MallocExtension::instance()->GetHeapSample(&buf);
+
+    rw->Append<IndexOf("asid")>(ctx->metadata_state()->asid());
+    rw->Append<IndexOf("heap")>(buf);
+#else
+    rw->Append<IndexOf("asid")>(ctx->metadata_state()->asid());
+    rw->Append<IndexOf("heap")>("Only supported with tcmalloc.");
+#endif
+
+    return false;
+  }
+};
+
+class HeapGrowthStacksUDTF final : public carnot::udf::UDTF<HeapGrowthStacksUDTF> {
+ public:
+  static constexpr auto Executor() { return carnot::udfspb::UDTFSourceExecutor::UDTF_ALL_AGENTS; }
+
+  static constexpr auto OutputRelation() {
+    return MakeArray(ColInfo("asid", types::DataType::INT64, types::PatternType::GENERAL,
+                             "The short ID of the agent"),
+                     ColInfo("heap", types::DataType::STRING, types::PatternType::GENERAL,
+                             "The pretty heap stats"));
+  }
+
+  bool NextRecord(FunctionContext* ctx, RecordWriter* rw) {
+#ifdef TCMALLOC
+    std::string buf;
+    MallocExtension::instance()->GetHeapGrowthStacks(&buf);
+
+    rw->Append<IndexOf("asid")>(ctx->metadata_state()->asid());
+    rw->Append<IndexOf("heap")>(buf);
+#else
+    rw->Append<IndexOf("asid")>(ctx->metadata_state()->asid());
+    rw->Append<IndexOf("heap")>("Only supported with tcmalloc.");
+#endif
+
+    return false;
+  }
+};
+
+}  // namespace funcs
+}  // namespace vizier
+}  // namespace pl
--- src/vizier/funcs/internal/internal_impl.cc
+++ src/vizier/funcs/internal/internal_impl.cc
@@ -1,4 +1,5 @@
 #include "src/vizier/funcs/internal/internal_impl.h"
+#include "src/vizier/funcs/internal/debug.h"
 #include "src/vizier/funcs/internal/stack_trace.h"
 
 namespace pl {
@@ -9,6 +10,9 @@ namespace internal {
 void RegisterFuncsOrDie(carnot::udf::Registry* registry) {
   registry->RegisterOrDie<StackTracerUDTF>("_DebugStackTrace");
   registry->RegisterOrDie<KelvinVersionUDTF>("Version");
+  registry->RegisterOrDie<HeapStatsUDTF>("_HeapStats");
+  registry->RegisterOrDie<HeapSampleUDTF>("_HeapSample");
+  registry->RegisterOrDie<HeapGrowthStacksUDTF>("_HeapGrowthStacks");
 }
 
 }  // namespace internal
