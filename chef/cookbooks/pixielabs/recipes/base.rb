2dcca660a7aeb6f26ea5f947b22b73cf10cbfda7
Zain Asgar <zasgar@pixielabs.ai>
2020-11-22 00:28:26 -0800

    Revert "Move to globally installed eslint"
    Summary:
    This reverts commit 3d60b2c830b7ed6847e86ac376eee81887ddcc83.
    Needs to be script to handle installing JS deps.
    
    Test Plan: Jenkins
    
    Reviewers: vihang, michelle, #engineering
    
    Reviewed By: vihang, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6825
    
    GitOrigin-RevId: 1174916c3a2cc4aef011fecc8cb1f13dedc7c454

--- chef/cookbooks/pixielabs/recipes/base.rb
+++ chef/cookbooks/pixielabs/recipes/base.rb
@@ -19,7 +19,7 @@ include_recipe 'pixielabs::golang'
 
 execute 'install node packages' do
   command %(/opt/node/bin/npm install -g \
-            tslint@5.11.0 typescript@3.0.1 yarn@1.22.10 webpack@5.5.0 jshint@2.11.0 jest@26.6.3 eslint@6.8.0)
+            tslint@5.11.0 typescript@3.0.1 yarn@1.22.4 webpack@4.42.0 jshint@2.11.0 jest@23.4.2)
 end
 
 directory '/opt/pixielabs' do
---
+++ linters/eslint_ui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+uipath="${SCRIPTPATH}/../src/ui"
+eslint=./node_modules/.bin/eslint
+
+cd "${uipath}"
+if [[ ! -x "${eslint}" ]]; then
+    yarn install
+fi
+
+
+"${eslint}" "$@"
