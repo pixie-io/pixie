f22ad0e234d1ce277de89b694bf86fafd3159552
Phillip Kuznetsov <philkuz@pixielabs.ai>
2020-11-24 09:53:25 -0800

    [PC-689] Bypass cache on bundle scripts
    Summary: Bundle scripts set by user should not go to the cache, instead should always look at the URL. Supports development
    
    Test Plan: Tested and made sure it bypasses cache even when cache is turned on.
    
    Reviewers: nick, michelle, nserrino, #engineering, zasgar
    
    Reviewed By: #engineering, zasgar
    
    Subscribers: zasgar
    
    JIRA Issues: PC-689
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6856
    
    GitOrigin-RevId: 7f69b0767cc99ed2428cf6ec6415e9578e553890

--- src/ui/src/utils/script-bundle.tsx
+++ src/ui/src/utils/script-bundle.tsx
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import { isStaging } from 'utils/env';
+import * as QueryString from 'query-string';
 
 const PROD_SCRIPTS = 'https://storage.googleapis.com/pixie-prod-artifacts/script-bundles/bundle-core.json';
 const OSS_SCRIPTS = 'https://storage.googleapis.com/pixie-prod-artifacts/script-bundles/bundle-oss.json';
@@ -23,11 +24,27 @@ interface ScriptJSON {
   orgName: string;
 }
 
+// bypassCacheURL adds a timestamp to the URL to bypass the disk-cache.
+// Necessary because disabling cache in headers doesn't work.
+// https://stackoverflow.com/questions/49263559/using-javascript-axios-fetch-can-you-disable-browser-cache
+function bypassCacheURL(url: string) {
+  const queryURL = QueryString.parseUrl(url);
+  queryURL.query.timestamp = `${new Date().getTime()}`;
+  return QueryString.stringifyUrl(queryURL);
+}
+
 export function GetPxScripts(orgName: string): Promise<Script[]> {
-  const bundlePath = localStorage.getItem('px-custom-core-bundle-path')
-    || (isStaging() ? STAGING_SCRIPTS : PROD_SCRIPTS);
-  const ossBundlePath = localStorage.getItem('px-custom-oss-bundle-path') || OSS_SCRIPTS;
-  const fetchPromises = [Axios({ method: 'get', url: bundlePath }), Axios({ method: 'get', url: ossBundlePath })];
+  let localStorageCoreBundle = localStorage.getItem('px-custom-core-bundle-path');
+  let localStorageOSSBundle = localStorage.getItem('px-custom-oss-bundle-path');
+  if (localStorageCoreBundle) {
+    localStorageCoreBundle = bypassCacheURL(localStorageCoreBundle);
+  }
+  if (localStorageOSSBundle) {
+    localStorageOSSBundle = bypassCacheURL(localStorageOSSBundle);
+  }
+  const coreBundlePath = localStorageCoreBundle || (isStaging() ? STAGING_SCRIPTS : PROD_SCRIPTS);
+  const ossBundlePath = localStorageOSSBundle || OSS_SCRIPTS;
+  const fetchPromises = [Axios({ method: 'get', url: coreBundlePath }), Axios({ method: 'get', url: ossBundlePath })];
   return Promise.all(fetchPromises).then((response) => {
     const scripts = [];
     response.forEach((resp) => {
