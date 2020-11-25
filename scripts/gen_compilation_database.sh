4a02111e6147ec3459634f7d7fdbc3ae26aeaa0b
Vihang Mehta <vihang@pixielabs.ai>
2020-11-25 00:00:14 -0800

    Patch generate-compilation-db
    Summary: TSIA
    
    Test Plan: N/A
    
    Reviewers: zasgar, #engineering
    
    Reviewed By: zasgar, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6871
    
    GitOrigin-RevId: dd30a0792af5b1e8f6dbf1c3dfbb5e88364ab7c1

--- scripts/gen_compilation_database.sh
+++ scripts/gen_compilation_database.sh
@@ -2,8 +2,16 @@
 
 RELEASE_VERSION=0.3.5
 
+workspace=$(bazel info workspace 2> /dev/null)
+pushd "${workspace}" || exit
+
 if [[ ! -d bazel-compilation-database-${RELEASE_VERSION} ]]; then
   curl -L https://github.com/grailbio/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
+  pushd bazel-compilation-database-${RELEASE_VERSION} || exit
+  patch -p1 < "${workspace}/third_party/bazel_compilation_database.patch"
+  popd || exit
 fi
 
-bazel-compilation-database-${RELEASE_VERSION}/generate.sh $@
+bazel-compilation-database-${RELEASE_VERSION}/generate.sh "$@"
+
+popd || return
