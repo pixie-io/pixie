e228f6ab9bfaa7d1dd64bf363e7fc8bd7bb8a9de
Nick Lanam <nick@pixielabs.ai>
2020-12-07 18:18:13 -0800

    User settings support in the UI
    Summary: This specifies the available settings (currently just one - whether the tour has been seen) and makes them available for read/write. Accessed via React hook.
    
    Test Plan: Unit tests only right now. The next change for this will add a use for the `tourSeen` setting to show the tour once to each new user.
    
    Reviewers: nserrino, michelle, vihang, #engineering
    
    Reviewed By: nserrino, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6888
    
    GitOrigin-RevId: 91dbc658d85abf0a7b9dfa351f283813ac04b399

---
+++ src/ui/src/common/use-setting.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import gql from 'graphql-tag';
+import { useMutation, useQuery } from '@apollo/react-hooks';
+import { SetStateFunc } from 'context/common';
+
+export interface UserSettings {
+  tourSeen: boolean;
+}
+
+type TData = { userSettings: Array<{key: string; value: string}> };
+
+/** Default values for every user setting, which will be returned from useSetting if there is not a stored value. */
+export const DEFAULT_USER_SETTINGS: UserSettings = Object.freeze({
+  tourSeen: false,
+});
+
+/** Exported only for test mocks */
+export const GET_ALL_USER_SETTINGS = gql`
+{
+  userSettings(keys: [${Object.keys(DEFAULT_USER_SETTINGS).join(', ')}]) {
+    key
+    value
+  }
+}`;
+
+/** Exported only for test mocks */
+export const SAVE_USER_SETTING = gql`
+  mutation UpdateUserSetting($key: String!, $value: String!) {
+    UpdateUserSettings(keys: [$key], values: [$value])
+  }
+`;
+
+/**
+ * Similar to useState, provides the value and a setter for a given (recognized) user setting.
+ * If the user has not set that setting before or it's still loading, a default value will be provided in its stead.
+ * Returns the value as described above, a setter, and the loading state.
+ *
+ * Usage:
+ * ```
+ * const [tourSeen, setTourSeen, loading] = useSetting('tourSeen'); // tourSeen is a boolean (others have other types).
+ * ```
+ */
+export function useSetting(
+  key: keyof UserSettings,
+): [UserSettings[typeof key], SetStateFunc<UserSettings[typeof key]>, boolean] {
+  type ValueType = UserSettings[typeof key];
+
+  const [setting, setSetting] = React.useState<ValueType>(DEFAULT_USER_SETTINGS[key]);
+  const [storeSetting] = useMutation(SAVE_USER_SETTING);
+  const { loading, data, error } = useQuery<TData>(GET_ALL_USER_SETTINGS, { fetchPolicy: 'cache-and-network' });
+
+  // Upstream, settings are stored as strings no matter what they contain. Since the data type is not stored there, we
+  // make some compromises at runtime when trying to parse them out.
+  const storedString = !loading && !error && data.userSettings.find((s) => s.key === key).value;
+  let stored: ValueType|undefined;
+  if (!loading && !error) {
+    try {
+      // Compromise 1: Strings that happen to look like valid JSON (for example, 'false') are treated as such
+      stored = JSON.parse(storedString);
+    } catch {
+      // Compromise 2: Malformed data gets treated as a string even if it isn't supposed to be
+      stored = storedString as any as ValueType;
+    }
+  }
+
+  const setter: SetStateFunc<ValueType> = (actionOrValue) => {
+    const value: ValueType = typeof actionOrValue === 'function'
+      ? actionOrValue(setting)
+      : actionOrValue;
+    if (value !== stored) {
+      setSetting(value);
+      storeSetting({ variables: { key, value: JSON.stringify(value) } }).then();
+    }
+  };
+
+  // Triggered when the initial state finishes loading, and (redundantly) when a setting is updated in Apollo's cache.
+  React.useEffect(() => {
+    if (!loading && !error && stored !== setting && stored !== undefined) {
+      setSetting(stored);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading, stored]);
+
+  return [setting, setter, loading];
+}
