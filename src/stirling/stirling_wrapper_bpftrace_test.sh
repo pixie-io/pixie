e284f6bec98af23ae0200fea5be35ef156899e00
Omid Azizi <oazizi@pixielabs.ai>
2020-12-11 11:14:08 -0800

    BPFTrace + stirling_wrapper test
    Summary: Run a bpftrace pxl script through stirling to get more coverage.
    
    Test Plan: This is a new test.
    
    Reviewers: yzhao, #engineering
    
    Reviewed By: yzhao, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6919
    
    GitOrigin-RevId: 6e67d33a2c4c18b0c3652722c609ad9041f9af97

---
+++ src/stirling/stirling_wrapper_bpftrace_test.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -e
+
+# Clean-up any spawned background processes on exit.
+trap 'kill $(jobs -p) &> /dev/null || true' SIGINT SIGTERM EXIT
+
+script_dir="$(dirname "$0")"
+
+# shellcheck source=./src/stirling/scripts/utils.sh
+source "$script_dir"/scripts/utils.sh
+
+# shellcheck source=./src/stirling/scripts/test_utils.sh
+source "$script_dir"/scripts/test_utils.sh
+
+if [ -z "$BUILD_WORKSPACE_DIRECTORY" ] && [ -z "$TEST_TMPDIR" ]; then
+    # If the script was run in a stand-alone way, then build and set paths.
+    pixie_root="$script_dir"/../..
+    stirling_wrapper=$pixie_root/$(bazel_build //src/stirling:stirling_wrapper)
+    trace_script=$pixie_root/src/stirling/testing/tcpdrop.bpftrace.pxl
+else
+    # If the script was run through bazel, the locations are passed as arguments.
+    stirling_wrapper=$1
+    trace_script=$2
+fi
+
+###############################################################################
+# Main test: Run stirling_wrapper.
+###############################################################################
+
+echo "Running stirling_wrapper"
+flags="--timeout_secs=60 --trace=$trace_script"
+# shellcheck disable=SC2086
+out=$(run_prompt_sudo "$stirling_wrapper" $flags 2>&1)
+
+###############################################################################
+# Check output for errors/warnings.
+###############################################################################
+
+check_dynamic_trace_deployment() {
+  out=$1
+  echo "$out"
+
+  # Look for GLOG errors or warnings, which start with E or W respectively.
+  err_count=$(echo "$out" | grep -c -e ^E -e ^W || true)
+  echo "Error/Warning count = $err_count"
+  if [ "$err_count" -ne "0" ]; then
+    echo "Test FAILED"
+    return 1
+  fi
+
+  success_msg=$(echo "$out" | grep -c -e "Successfully deployed dynamic trace!" || true)
+  if [ "$success_msg" -ne "1" ]; then
+    echo "Could not find success message"
+    echo "Test FAILED"
+    return 1
+  fi
+
+  record_count=$(echo "$out" | grep -c -e "^\[tcp_drop_table\]" || true)
+  echo "Record count = $record_count"
+  if [ "$record_count" -eq "0" ]; then
+    echo "Test FAILED"
+    return 1
+  fi
+
+  echo "Test PASSED"
+  return 0
+}
+
+check_dynamic_trace_deployment "$out"
