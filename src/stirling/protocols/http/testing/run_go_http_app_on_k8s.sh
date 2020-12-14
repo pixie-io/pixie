a30ccb6323b7942540f3f1e68c105c547ee2e249
Yaxiong Zhao <yzhao@pixielabs.ai>
2020-12-14 14:36:12 -0800

    [CLEANUP] A trick to ignore ALREADY_EXISTS error
    Summary:
    This uses the `kubectl create` to produce a yaml file, and then pipe to
    `kubectl apply`.
    
    Create is imperative, so it fails if the target resource already exists.
    Apply is declarative, which ignores the same error.
    
    Test Plan: Manual test
    
    Reviewers: oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D6942
    
    GitOrigin-RevId: a9a5f7d2f9ef5686d2ef6cf86ab7c39983364bdb

--- src/stirling/protocols/http/testing/run_go_http_app_on_k8s.sh
+++ src/stirling/protocols/http/testing/run_go_http_app_on_k8s.sh
@@ -11,7 +11,8 @@ kubectl --namespace "${namespace_name}" create secret docker-registry image-pull
   --docker-server=https://gcr.io \
   --docker-username=_json_key \
   --docker-email="${USER}@pixielabs.ai" \
-  --docker-password="$(sops -d credentials/k8s/dev/image-pull-secrets.encrypted.json)"
+  --docker-password="$(sops -d credentials/k8s/dev/image-pull-secrets.encrypted.json)" \
+  --dry-run=true --output=yaml | kubectl apply -f -
 
 sed "s/{{USER}}/${USER}/" src/stirling/protocols/http/testing/go_http_server/deployment.yaml | \
   kubectl apply -f -
