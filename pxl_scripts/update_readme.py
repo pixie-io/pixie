3d1a5af10ada52542126d797a6814869138f35bc
Hannah Troisi <htroisi@pixielabs.ai>
2020-12-18 22:56:33 -0800

    Update Travis Build to enforce README.md updates if px/* files change.

--- pxl_scripts/update_readme.py
+++ pxl_scripts/update_readme.py
@@ -1,12 +1,14 @@
 import os
 import yaml
 
+if os.getcwd().split("/").pop() == "pixie":
+	os.chdir("./pxl_scripts")
 
 def script_list_format(folder, script, desc):
 	github_url = "https://github.com/pixie-labs/pixie/tree/main/pxl_scripts"
 	return f"- {folder}/[{script}]({github_url}/{folder}/{script}): {desc}"
 
-def get_script_list(directory="px"):
+def get_script_list(directory="./px"):
 	scripts = []
 	for folder in sorted(os.listdir(directory)):
 		path = os.path.join(directory,folder,"manifest.yaml")
@@ -35,7 +37,7 @@ You can contribute a PxL script by forking our repo, adding a new script then cr
 To learn in more details, you can review this tutorial on [Contributing PxL Scripts](https://docs.pixielabs.ai/using-pixie/scripts/contributing-pxl-scripts/)
 """
 
-with open("README.md", "w") as f:
+with open("./README.md", "w") as f:
 	f.write(header_text)
 	for script in get_script_list():
 		f.write(script + "\n")
