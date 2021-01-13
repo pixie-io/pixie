3fb07531abd3b908e22f957688013fe79d37f75d
Yaxiong Zhao <yzhao@pixielabs.ai>
2021-01-13 10:01:19 -0800

    Add more tests in redis_trace_bpf_test.cc
    Test Plan: Jenkins
    
    Reviewers: jps, oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7089
    
    GitOrigin-RevId: b60f526de45763e8bebd9f6364a899f92de4ad71

--- src/stirling/socket_tracer/redis_trace_bpf_test.cc
+++ src/stirling/socket_tracer/redis_trace_bpf_test.cc
@@ -6,6 +6,7 @@
 namespace pl {
 namespace stirling {
 
+using ::testing::ElementsAre;
 using ::testing::SizeIs;
 using ::testing::StrEq;
 
@@ -19,20 +20,90 @@ class RedisContainer : public ContainerRunner {
   static constexpr std::string_view kReadyMessage = "# Server initialized";
 };
 
-class RedisTraceBPFTest : public testing::SocketTraceBPFTest</* TClientSideTracing */ false> {
+struct RedisTraceTestCase {
+  std::string cmd;
+  std::string req;
+  std::string resp;
+};
+
+class RedisTraceBPFTest : public testing::SocketTraceBPFTest</* TClientSideTracing */ false>,
+                          public ::testing::WithParamInterface<RedisTraceTestCase> {
  protected:
   RedisTraceBPFTest() { PL_CHECK_OK(container_.Run(150, {})); }
 
   RedisContainer container_;
 };
 
-// A place holder to verify the setup of the redis and redis_cli container.
-TEST_F(RedisTraceBPFTest, VerifyFlushAllCommand) {
+struct RedisTraceRecord {
+  std::string req;
+  std::string resp;
+};
+
+std::ostream& operator<<(std::ostream& os, const RedisTraceRecord& record) {
+  os << "req: " << record.req << " resp: " << record.resp << std::endl;
+  return os;
+}
+
+bool operator==(const RedisTraceRecord& lhs, const RedisTraceRecord& rhs) {
+  return lhs.req == rhs.req && lhs.resp == rhs.resp;
+}
+
+std::vector<RedisTraceRecord> GetRedisTraceRecords(
+    const types::ColumnWrapperRecordBatch& record_batch) {
+  std::vector<RedisTraceRecord> res;
+  for (size_t i = 0; i < record_batch[kRedisReqIdx]->Size(); ++i) {
+    res.push_back(
+        RedisTraceRecord{std::string(record_batch[kRedisReqIdx]->Get<types::StringValue>(i)),
+                         std::string(record_batch[kRedisRespIdx]->Get<types::StringValue>(i))});
+  }
+  return res;
+}
+
+// Verifies that batched commands can be traced correctly.
+TEST_F(RedisTraceBPFTest, VerifyBatchedCommands) {
+  constexpr std::string_view kRedisDockerCmdTmpl =
+      R"(docker run --rm --network=container:$0 redis bash -c "echo '$1' | redis-cli")";
+  constexpr std::string_view kRedisCmds = R"(
+    set foo 100
+    bitcount foo 0 0
+    incr foo
+    append foo xxx
+    get foo
+  )";
   const std::string redis_cli_cmd =
-      absl::Substitute("docker run --rm --network=container:$0 redis redis-cli flushall",
-                       container_.container_name());
+      absl::Substitute(kRedisDockerCmdTmpl, container_.container_name(), kRedisCmds);
   ASSERT_OK_AND_ASSIGN(const std::string output, pl::Exec(redis_cli_cmd));
-  EXPECT_THAT(output, StrEq("OK\n"));
+  ASSERT_FALSE(output.empty());
+
+  DataTable data_table(kRedisTable);
+  source_->TransferData(ctx_.get(), SocketTraceConnector::kRedisTableNum, &data_table);
+  std::vector<TaggedRecordBatch> tablets = data_table.ConsumeRecords();
+
+  ASSERT_FALSE(tablets.empty());
+
+  types::ColumnWrapperRecordBatch record_batch = tablets[0].records;
+  std::vector<RedisTraceRecord> redis_trace_records = GetRedisTraceRecords(record_batch);
+
+  // redis-cli sends a 'command' req to query all available commands from server.
+  // The response is too long to test meaningfully, so we ignore them.
+  redis_trace_records.erase(redis_trace_records.begin());
+
+  EXPECT_THAT(redis_trace_records, ElementsAre(RedisTraceRecord{"[set, foo, 100]", "OK"},
+                                               RedisTraceRecord{"[bitcount, foo, 0, 0]", "3"},
+                                               RedisTraceRecord{"[incr, foo]", "101"},
+                                               RedisTraceRecord{"[append, foo, xxx]", "6"},
+                                               RedisTraceRecord{"[get, foo]", "101xxx"}));
+}
+
+// Verifies individual commands.
+TEST_P(RedisTraceBPFTest, VerifyCommand) {
+  std::string_view redis_cmd = GetParam().cmd;
+
+  std::string redis_cli_cmd =
+      absl::Substitute("docker run --rm --network=container:$0 redis redis-cli $1",
+                       container_.container_name(), redis_cmd);
+  ASSERT_OK_AND_ASSIGN(const std::string output, pl::Exec(redis_cli_cmd));
+  ASSERT_FALSE(output.empty());
 
   DataTable data_table(kRedisTable);
   source_->TransferData(ctx_.get(), SocketTraceConnector::kRedisTableNum, &data_table);
@@ -42,17 +113,17 @@ TEST_F(RedisTraceBPFTest, VerifyFlushAllCommand) {
 
   types::ColumnWrapperRecordBatch record_batch = tablets[0].records;
 
-  const std::vector<size_t> target_record_indices =
-      testing::FindRecordIdxMatchesPID(record_batch, kRedisUPIDIdx, container_.process_pid());
-  ASSERT_THAT(target_record_indices, SizeIs(1));
-
-  EXPECT_THAT(
-      std::string(record_batch[kRedisReqIdx]->Get<types::StringValue>(target_record_indices[0])),
-      StrEq("[flushall]"));
-  EXPECT_THAT(
-      std::string(record_batch[kRedisRespIdx]->Get<types::StringValue>(target_record_indices[0])),
-      StrEq("OK"));
+  EXPECT_THAT(GetRedisTraceRecords(record_batch),
+              ElementsAre(RedisTraceRecord{GetParam().req, GetParam().resp}));
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    Commands, RedisTraceBPFTest,
+    // Add new commands here.
+    ::testing::Values(RedisTraceTestCase{"lpush ilist 100", "[lpush, ilist, 100]", "1"},
+                      RedisTraceTestCase{"rpush ilist 200", "[rpush, ilist, 200]", "1"},
+                      RedisTraceTestCase{"lrange ilist 0 1", "[lrange, ilist, 0, 1]", "[]"},
+                      RedisTraceTestCase{"flushall", "[flushall]", "OK"}));
 
 }  // namespace stirling
 }  // namespace pl
