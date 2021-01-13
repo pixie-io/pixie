ac72e6f03ce50ce8284960ed0ea5b0e8999af3c4
Yaxiong Zhao <yzhao@pixielabs.ai>
2021-01-13 09:54:34 -0800

    Add a script to deploy to stirling perf cluster
    Test Plan: Manual push
    
    Reviewers: jps, oazizi, #engineering
    
    Reviewed By: oazizi, #engineering
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7096
    
    GitOrigin-RevId: 75ad836d15b8d4e17cfa4e621c4fb6fed7c06395

---
+++ src/stirling/scripts/update_gke_stirling_perf_cluster.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+if [[ $(git rev-parse --abbrev-ref HEAD) != "main" ]]; then
+  echo "You are not on main branch, exit ..."
+  exit 1
+fi
+
+if [[ $(git status -s) != "" ]]; then
+  echo "Your repository is not clean, make sure there isn't any change, exit ..."
+  exit 1
+fi
+
+gke_cluster_context="gke_pl-pixies_us-west1-a_dev-cluster-stirling-perf"
+if [[ $(kubectl config current-context) != "${gke_cluster_context}" ]]; then
+  echo "Your kubectl context is wrong, should be ${gke_cluster_context}, exit ..."
+fi
+
+echo "Note down current head commit: "
+git rev-parse HEAD
+
+# TODO(yzhao): We
+if (( $(kubectl get pods -n pl --no-headers | wc -l) < 5 )); then
+  echo "Must be upgrading an existing Vizier deployment, but did not found enough pods" \
+       "in the 'pl' namespace, exit ..."
+  exit 1
+fi
+
+echo "You must be upgrading an existing Vizier deployment. Launching skaffold ..."
+skaffold run -f skaffold/skaffold_vizier.yaml -p opt
