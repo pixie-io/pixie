23e463041810f720402410000603afc5d717e97d
Hannah Troisi <htroisi@pixielabs.ai>
2021-02-08 10:01:51 -0800

    address zasgar comments

--- demos/slack-alert-app/slackbot.py
+++ demos/slack-alert-app/slackbot.py
@@ -21,8 +21,24 @@ from slack.web.client import WebClient
 from slack.errors import SlackApiError
 
 
-# Pixie PxL script:
-# This script ouputs a table of the HTTP requests count and
+logging.basicConfig(level=logging.INFO)
+
+# The slackbot requires the following configs, which are specified
+# using environment variables. For directions on how to find these
+# config values, see: https://docs.pixielabs.ai/tutorials/slackbot-alert
+if "PIXIE_API_KEY" not in os.environ:
+    logging.error("Missing `PIXIE_API_KEY` environment variable.")
+pixie_api_key = os.environ['PIXIE_API_KEY']
+
+if "PIXIE_CLUSTER_ID" not in os.environ:
+    logging.error("Missing `PIXIE_CLUSTER_ID` environment variable.")
+pixie_cluster_id = os.environ['PIXIE_CLUSTER_ID']
+
+if "SLACK_BOT_TOKEN" not in os.environ:
+    logging.error("Missing `SLACK_BOT_TOKEN` environment variable.")
+slack_bot_token = os.environ['SLACK_BOT_TOKEN']
+
+# This PxL script ouputs a table of the HTTP total requests count and
 # HTTP error (>4xxx) count for each service in the `px-sock-shop` namespace.
 PXL_SCRIPT = """
 import px
@@ -45,38 +61,32 @@ df = df.groupby(['service']).agg(
     total_requests=('http_resp_status', px.count)
 )
 
-px.display(df, "status")
+px.display(df, "http_table")
 """
 
-logging.basicConfig(level=logging.DEBUG)
 
-
-# Get data from the Pixie API.
 def get_pixie_data(cluster_conn):
 
-    # Execute the PxL script.
+    msg = ["*Recent 4xx+ Spikes in last 5 minutes:*"]
+
     script = cluster_conn.prepare_script(PXL_SCRIPT)
-    logging.debug("Pixie cluster executed script.")
 
-    service_stats_msg = ["*Recent 4xx+ Spikes in last 5 minutes:*"]
+    # If you change the PxL script, you'll need to change the
+    # columns this script looks for in the result table.
+    for row in script.results("http_table"):
+        msg.append(format_message(row["service"],
+                                  row["total_requests"],
+                                  row["error_count"]))
 
-    # Process table output rows to construct slack message.
-    for row in script.results("status"):
-        service_stats_msg.append(format_message(row["service"],
-                                                row["total_requests"],
-                                                row["error_count"]))
-
-    return "\n\n".join(service_stats_msg)
+    return "\n\n".join(msg)
 
 
-# Format Pixie API table row data.
 def format_message(service, request_count, error_count):
     return (f"`{service}` \t ---> {error_count} (>4xx)"
             f" errors out of {request_count} requests.")
 
 
-# Send Slack message through the Slack client.
-def send_message(slack_client, channel, cluster_conn):
+def send_slack_message(slack_client, channel, cluster_conn):
 
     # Get data from the Pixie API.
     msg = get_pixie_data(cluster_conn)
@@ -91,47 +101,32 @@ def send_message(slack_client, channel, cluster_conn):
         logging.error(e.response)
 
 
-if __name__ == "__main__":
+def main():
 
-    # Get Pixie API key.
-    if "PIXIE_API_KEY" not in os.environ:
-        logging.error("Missing `PIXIE_API_KEY` environment variable.")
-    PIXIE_API_KEY = os.environ['PIXIE_API_KEY']
-
-    # Create a Pixie client.
     logging.debug("Authorizing Pixie client.")
-    px_client = pxapi.Client(token=PIXIE_API_KEY)
+    px_client = pxapi.Client(token=pixie_api_key)
 
-    # Get Pixie cluster ID.
-    if "PIXIE_CLUSTER_ID" not in os.environ:
-        logging.error("Missing `PIXIE_CLUSTER_ID` environment variable.")
-    PIXIE_CLUSTER_ID = os.environ['PIXIE_CLUSTER_ID']
-
-    # Connect to cluster.
-    cluster_conn = px_client.connect_to_cluster(PIXIE_CLUSTER_ID)
+    cluster_conn = px_client.connect_to_cluster(pixie_cluster_id)
     logging.debug("Pixie client connected to %s cluster.", cluster_conn.name())
 
-    # Get Slackbot access token.
-    if "SLACK_BOT_TOKEN" not in os.environ:
-        logging.error("Missing `SLACK_BOT_TOKEN` environment variable.")
-    SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
-
-    # Create a Slack client.
     logging.debug("Authorizing Slack client.")
-    slack_client = WebClient(SLACK_BOT_TOKEN)
+    slack_client = WebClient(slack_bot_token)
 
-    # Slack channel to post in. Slack App must be a member of this channel.
+    # Slack channel for Slackbot to post in.
+    # Slack App must be a member of this channel.
     SLACK_CHANNEL = "#pixie-alerts"
 
     # Schedule sending a Slack channel message every 5 minutes.
-    schedule.every(5).minutes.do(lambda: send_message(slack_client,
-                                                      SLACK_CHANNEL,
-                                                      cluster_conn))
+    schedule.every(5).minutes.do(lambda: send_slack_message(slack_client,
+                                                            SLACK_CHANNEL,
+                                                            cluster_conn))
 
     logging.info("Message scheduled for %s Slack channel.", SLACK_CHANNEL)
 
     while True:
         schedule.run_pending()
-
-        # Sleep for 5 seconds between checks on the scheduler.
         time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
