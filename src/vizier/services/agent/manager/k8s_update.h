af608bb377d9eee1378e99a4ddea547735a6a89a
Natalie Serrino <nserrino@pixielabs.ai>
2021-02-09 16:43:26 -0800

    PP-2372: Add logic to re-request missing updates to K8s update handler in agent.
    Summary:
    Note: the k8s update handler (modified below) isn't currently hooked in. Those changes will happen next when k8s updates stop being sent over heartbeats.
    
    This diff is the agent side of our new k8s update rerequesting logic. Currently, agents blindly apply all updates they receive, without regard to order. This can potentially lead to incorrect state if a message is delayed or lost. Also, if updates are lost, the agent has no way (currently) to get them back.
    
    This diff addresses that by having the agent update handler keep track of its update versions and rerequest missing ones. There are some edge cases to handle here, like if the rerequest times out, or if we get duplicate data back, which should be addressed in the diff. The general approach is, if a discontinuity is detected, to make a missing metadata request if there hasn't been one made already. Then, wait 5 seconds for the state to catch up. if it does not catch up within that time, make a new missing metadata rerquest because updates likely got lost in the previous request.
    
    Test Plan: added lots of tests
    
    Reviewers: vihang, michelle, zasgar, #engineering
    
    Reviewed By: vihang, #engineering
    
    Subscribers: vjain
    
    JIRA Issues: PP-2372
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7316
    
    GitOrigin-RevId: 6f73705d6397a26be5461f81d31ed4abd62b8908

--- src/vizier/services/agent/manager/k8s_update.h
+++ src/vizier/services/agent/manager/k8s_update.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <deque>
 #include <memory>
+#include <queue>
+#include <string>
 
 #include "src/vizier/services/agent/manager/manager.h"
 
@@ -16,9 +19,14 @@ using ::pl::shared::k8s::metadatapb::ResourceUpdate;
 class K8sUpdateHandler : public Manager::MessageHandler {
  public:
   K8sUpdateHandler() = delete;
+
   K8sUpdateHandler(Dispatcher* d, pl::md::AgentMetadataStateManager* mds_manager, Info* agent_info,
                    Manager::VizierNATSConnector* nats_conn)
-      : MessageHandler(d, agent_info, nats_conn), mds_manager_(mds_manager) {}
+      : K8sUpdateHandler(d, mds_manager, agent_info, nats_conn, kDefaultMaxUpdateBacklogQueueSize) {
+  }
+
+  K8sUpdateHandler(Dispatcher* d, pl::md::AgentMetadataStateManager* mds_manager, Info* agent_info,
+                   Manager::VizierNATSConnector* nats_conn, size_t max_update_queue_size);
 
   ~K8sUpdateHandler() override = default;
 
@@ -26,8 +34,27 @@ class K8sUpdateHandler : public Manager::MessageHandler {
 
  private:
   Status HandleMissingK8sMetadataResponse(const messages::MissingK8sMetadataResponse& update);
+  Status HandleK8sUpdate(const ResourceUpdate& update);
+  Status AddK8sUpdate(const ResourceUpdate& update);
+  void RequestMissingMetadata();
 
   pl::md::AgentMetadataStateManager* mds_manager_;
+
+  // The most recent update version passed to the state manager.
+  // If there are resources in the backlog, this will be a lower version than those updates.
+  bool initial_metadata_received_ = false;
+  int64_t current_update_version_ = 0;
+
+  // Logic for re-requesting missing metadata.
+  pl::event::TimerUPtr missing_metadata_request_timer_;
+  static constexpr std::chrono::seconds kMissingMetadataTimeout{5};
+
+  // logic/variables for the backlog update queue.
+  std::priority_queue<ResourceUpdate, std::deque<ResourceUpdate>,
+                      std::function<bool(ResourceUpdate, ResourceUpdate)>>
+      update_backlog_;
+  const size_t max_update_queue_size_;
+  static constexpr size_t kDefaultMaxUpdateBacklogQueueSize{1000};
 };
 
 }  // namespace agent
--- src/vizier/services/agent/manager/k8s_update_test.cc
+++ src/vizier/services/agent/manager/k8s_update_test.cc
@@ -4,28 +4,18 @@
 #include <vector>
 
 #include "src/common/event/api_impl.h"
-#include "src/common/event/libuv.h"
 #include "src/common/event/nats.h"
 #include "src/common/system/config_mock.h"
 #include "src/common/testing/event/simulated_time_system.h"
 #include "src/common/testing/testing.h"
-#include "src/shared/metadatapb/metadata.pb.h"
-#include "src/vizier/messages/messagespb/messages.pb.h"
 #include "src/vizier/services/agent/manager/k8s_update.h"
-#include "src/vizier/services/agent/manager/manager.h"
 #include "src/vizier/services/agent/manager/test_utils.h"
 
 namespace pl {
 namespace vizier {
 namespace agent {
 
-using ::pl::table_store::schema::Relation;
 using ::pl::testing::proto::EqualsProto;
-using ::pl::testing::proto::Partially;
-using shared::metadatapb::MetadataType;
-using ::testing::Return;
-using ::testing::ReturnRef;
-using ::testing::UnorderedElementsAreArray;
 
 constexpr char kUpdate0[] = R"(
   namespace_update {
@@ -33,7 +23,33 @@ constexpr char kUpdate0[] = R"(
     uid: "namespace_1"
     start_timestamp_ns: 999
   }
+  update_version: $0
+  prev_update_version: $1
 )";
+
+std::string TestResourceUpdate(int64_t curr_version, int64_t prev_version) {
+  return absl::Substitute(kUpdate0, curr_version, prev_version);
+}
+
+std::unique_ptr<messages::VizierMessage> TestK8sUpdateMsg(const std::string& update) {
+  auto sent_update = std::make_unique<messages::VizierMessage>();
+  google::protobuf::TextFormat::MergeFromString(
+      update, sent_update->mutable_k8s_metadata_message()->mutable_k8s_metadata_update());
+  return sent_update;
+}
+
+std::unique_ptr<messages::VizierMessage> TestMissingUpdateResp(
+    const std::vector<std::string> updates, int64_t earliest_version, int64_t latest_version) {
+  auto sent_update = std::make_unique<messages::VizierMessage>();
+  auto resp = sent_update->mutable_k8s_metadata_message()->mutable_missing_k8s_metadata_response();
+  for (const auto& update_str : updates) {
+    auto update = resp->add_updates();
+    google::protobuf::TextFormat::MergeFromString(update_str, update);
+  }
+  resp->set_first_update_available(earliest_version);
+  resp->set_last_update_available(latest_version);
+  return sent_update;
+}
 
 class K8sUpdateHandlerTest : public ::testing::Test {
  protected:
@@ -49,13 +65,19 @@ class K8sUpdateHandlerTest : public ::testing::Test {
     nats_conn_ = std::make_unique<FakeNATSConnector<pl::vizier::messages::VizierMessage>>();
     auto sys_config = system::MockConfig();
     EXPECT_CALL(sys_config, KernelTicksPerSecond()).WillRepeatedly(::testing::Return(10000000));
-    EXPECT_CALL(sys_config, HasConfig()).WillRepeatedly(Return(true));
-    EXPECT_CALL(sys_config, proc_path()).WillRepeatedly(ReturnRef(proc_path_));
-    EXPECT_CALL(sys_config, sysfs_path()).WillRepeatedly(ReturnRef(sysfs_path_));
+    EXPECT_CALL(sys_config, HasConfig()).WillRepeatedly(::testing::Return(true));
+    EXPECT_CALL(sys_config, proc_path()).WillRepeatedly(::testing::ReturnRef(proc_path_));
+    EXPECT_CALL(sys_config, sysfs_path()).WillRepeatedly(::testing::ReturnRef(sysfs_path_));
 
     fake_mds_manager_ = std::make_unique<FakeAgentMetadataStateManager>();
     k8s_update_handler_ = std::make_unique<K8sUpdateHandler>(
-        dispatcher_.get(), fake_mds_manager_.get(), &agent_info_, nats_conn_.get());
+        dispatcher_.get(), fake_mds_manager_.get(), &agent_info_, nats_conn_.get(), 2);
+
+    // Intentionally using nonconsecutive resource numbers.
+    // 10, 20, 30, 40, 50, 60, 70
+    updates_ = {TestResourceUpdate(10, 0),  TestResourceUpdate(20, 10), TestResourceUpdate(30, 20),
+                TestResourceUpdate(40, 30), TestResourceUpdate(50, 40), TestResourceUpdate(60, 50),
+                TestResourceUpdate(70, 60)};
   }
 
   event::MonotonicTimePoint start_monotonic_time_;
@@ -69,17 +91,319 @@ class K8sUpdateHandlerTest : public ::testing::Test {
   agent::Info agent_info_;
   std::filesystem::path proc_path_;
   std::filesystem::path sysfs_path_;
+  std::vector<std::string> updates_;
 };
 
 TEST_F(K8sUpdateHandlerTest, HandleK8sUpdate) {
-  auto sent_update = std::make_unique<messages::VizierMessage>();
-  CHECK(google::protobuf::TextFormat::MergeFromString(
-      kUpdate0, sent_update->mutable_k8s_metadata_message()->mutable_k8s_metadata_update()));
+  auto update = TestResourceUpdate(1, 0);
+  auto sent_update = TestK8sUpdateMsg(update);
   auto s = k8s_update_handler_->HandleMessage(std::move(sent_update));
   EXPECT_OK(s);
   EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
   auto recv_update = fake_mds_manager_->k8s_update(0);
-  EXPECT_THAT(*recv_update, EqualsProto(kUpdate0));
+  EXPECT_THAT(*recv_update, EqualsProto(update));
+}
+
+TEST_F(K8sUpdateHandlerTest, RequestInitialK8sUpdates) {
+  // Check that the handler automatically requests the backlog of updates.
+  EXPECT_EQ(0, nats_conn_->published_msgs().size());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_);
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+  auto req = nats_conn_->published_msgs()[0];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  auto missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(0, missing_md.from_update_version());
+  EXPECT_EQ(0, missing_md.to_update_version());
+
+  // No requests in backlog.
+  auto sent_missing = TestMissingUpdateResp({}, 0, 0);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_missing)));
+  EXPECT_EQ(0, fake_mds_manager_->num_k8s_updates());
+
+  // New updates go through.
+  auto sent_update = TestK8sUpdateMsg(updates_[0]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+}
+
+TEST_F(K8sUpdateHandlerTest, RequestInitialK8sUpdatesTimeout) {
+  // Check that the handler automatically requests the backlog of updates.
+  EXPECT_EQ(0, nats_conn_->published_msgs().size());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_);
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+  auto req = nats_conn_->published_msgs()[0];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  auto missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(0, missing_md.from_update_version());
+  EXPECT_EQ(0, missing_md.to_update_version());
+
+  // Push forward the clock so that the new request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_ + std::chrono::seconds(10));
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  EXPECT_EQ(2, nats_conn_->published_msgs().size());
+  req = nats_conn_->published_msgs()[1];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(0, missing_md.from_update_version());
+  EXPECT_EQ(0, missing_md.to_update_version());
+}
+
+TEST_F(K8sUpdateHandlerTest, RequestInitialK8sUpdatesTimeoutMiddleMsg) {
+  // Check that the handler automatically requests the backlog of updates.
+  EXPECT_EQ(0, nats_conn_->published_msgs().size());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_);
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+  auto req = nats_conn_->published_msgs()[0];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  auto missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(0, missing_md.from_update_version());
+  EXPECT_EQ(0, missing_md.to_update_version());
+
+  // Send the third message
+  auto third = TestMissingUpdateResp({updates_[2]}, 10, 30);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(third)));
+  EXPECT_EQ(0, fake_mds_manager_->num_k8s_updates());
+
+  // Send the first message
+  auto first = TestMissingUpdateResp({updates_[0]}, 10, 30);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(first)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_ + std::chrono::seconds(6));
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  EXPECT_EQ(2, nats_conn_->published_msgs().size());
+  req = nats_conn_->published_msgs()[1];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(11, missing_md.from_update_version());
+  EXPECT_EQ(30, missing_md.to_update_version());
+}
+
+TEST_F(K8sUpdateHandlerTest, RequestMissingK8sUpdates) {
+  // Check flow for re-requesting missing updates.
+  // Makes sure that missing updates paginated across multiple responses work.
+  // Makes sure that updates that come in before the backlog is resolved get
+  // added to the backlog.
+  // Makes sure that unnecessary requests to NATS don't get made.
+  // Makes sure that the updates are applied in the correct order.
+
+  // Send update 0
+  auto sent_update = TestK8sUpdateMsg(updates_[0]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Send update 4
+  sent_update = TestK8sUpdateMsg(updates_[4]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_);
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  // Check that NATS has received a missing metadata request.
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+  auto req = nats_conn_->published_msgs()[0];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  auto missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(11, missing_md.from_update_version());
+  EXPECT_EQ(50, missing_md.to_update_version());
+
+  // Missing MD response part 1/2
+  auto sent_missing = TestMissingUpdateResp({updates_[0], updates_[1], updates_[2]}, 10, 40);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_missing)));
+  EXPECT_EQ(3, fake_mds_manager_->num_k8s_updates());
+
+  // Send update 5. Should go to backlog.
+  sent_update = TestK8sUpdateMsg(updates_[5]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(3, fake_mds_manager_->num_k8s_updates());
+
+  // Missing Metadata response part 2/2. Backlog gets written.
+  sent_missing = TestMissingUpdateResp({updates_[3]}, 10, 40);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_missing)));
+  // Check that backlog is flushed.
+  EXPECT_EQ(6, fake_mds_manager_->num_k8s_updates());
+
+  // Update 6 goes through now that we have resolved the backlog.
+  sent_update = TestK8sUpdateMsg(updates_[6]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(7, fake_mds_manager_->num_k8s_updates());
+
+  // Check that no more re-request messages were sent to NATS.
+  time_system_->SetMonotonicTime(start_monotonic_time_ + std::chrono::seconds(6));
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+
+  // Check that the messages were sent in the right order.
+  for (const auto& [i, expected_update] : Enumerate(updates_)) {
+    EXPECT_THAT(*(fake_mds_manager_->k8s_update(i)), EqualsProto(expected_update));
+  }
+}
+
+TEST_F(K8sUpdateHandlerTest, RequestMissingK8sUpdatesTimeoutRerequest) {
+  // Check for handling of timeout after rerequesting missing K8s updates.
+
+  // Send update 0
+  auto sent_update = TestK8sUpdateMsg(updates_[0]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Send update 0
+  sent_update = TestK8sUpdateMsg(updates_[4]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_);
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  // Check that NATS has received a missing metadata request.
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+  auto req = nats_conn_->published_msgs()[0];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  auto missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(11, missing_md.from_update_version());
+  EXPECT_EQ(50, missing_md.to_update_version());
+
+  // Push forward the clock so that the re-request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_ + std::chrono::seconds(6));
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  // Check that the re-request got sent.
+  EXPECT_EQ(2, nats_conn_->published_msgs().size());
+  req = nats_conn_->published_msgs()[1];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(11, missing_md.from_update_version());
+  EXPECT_EQ(50, missing_md.to_update_version());
+}
+
+TEST_F(K8sUpdateHandlerTest, RequestMissingK8sUpdatesMDSTruncated) {
+  // Check that when the metadata service says it is missing a chunk of updates,
+  // the k8s update handler can tolerate that condition and proceed forward.
+
+  // Send update 0
+  auto sent_update = TestK8sUpdateMsg(updates_[0]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Send update 4
+  sent_update = TestK8sUpdateMsg(updates_[4]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_);
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  // Check that NATS has received a missing metadata request.
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+  auto req = nats_conn_->published_msgs()[0];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  auto missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(11, missing_md.from_update_version());
+  EXPECT_EQ(50, missing_md.to_update_version());
+
+  // Missing MD response part 1/1
+  auto sent_missing = TestMissingUpdateResp({updates_[2], updates_[3]}, 30, 40);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_missing)));
+  EXPECT_EQ(4, fake_mds_manager_->num_k8s_updates());
+
+  // Check that no more re-request messages were sent to NATS.
+  time_system_->SetMonotonicTime(start_monotonic_time_ + std::chrono::seconds(6));
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+
+  // Check that the messages were sent in the right order.
+  std::vector<std::string> expected_updates = {updates_[0], updates_[2], updates_[3]};
+  for (const auto& [i, expected_update] : Enumerate(expected_updates)) {
+    EXPECT_THAT(*(fake_mds_manager_->k8s_update(i)), EqualsProto(expected_update));
+  }
+}
+
+TEST_F(K8sUpdateHandlerTest, RequestMissingK8sUpdatesDropBacklog) {
+  // Request missing k8s updates, adding new items to backlog to be flushed,
+  // but then they need to be rerequested.
+
+  // Send update 0
+  auto sent_update = TestK8sUpdateMsg(updates_[0]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Send update 2
+  sent_update = TestK8sUpdateMsg(updates_[2]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  // Send update 3
+  sent_update = TestK8sUpdateMsg(updates_[3]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  // Last 2 should be in the backlog.
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Push forward the clock so that the request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_);
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  // Check that NATS has received a missing metadata request.
+  EXPECT_EQ(1, nats_conn_->published_msgs().size());
+  auto req = nats_conn_->published_msgs()[0];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  auto missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(11, missing_md.from_update_version());
+  EXPECT_EQ(30, missing_md.to_update_version());
+
+  // Send update 4. This should push update 2 (30) out of the backlog.
+  // We will need to rerequest it.
+  sent_update = TestK8sUpdateMsg(updates_[4]);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_update)));
+  // Last 3 should be in the backlog.
+  EXPECT_EQ(1, fake_mds_manager_->num_k8s_updates());
+
+  // Missing MD response part 1/1, req 1
+  auto sent_missing = TestMissingUpdateResp({updates_[0], updates_[1]}, 10, 20);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_missing)));
+  // Update 2 (30) shouldn't have been written yet, it should have been dropped..
+  // We only have updates 0 and 1 due to the backlog capacity.
+  EXPECT_EQ(2, fake_mds_manager_->num_k8s_updates());
+
+  // Push forward the clock so that the 2nd dropped backlog request gets sent.
+  time_system_->SetMonotonicTime(start_monotonic_time_ + std::chrono::seconds(6));
+  dispatcher_->Run(event::Dispatcher::RunType::NonBlock);
+
+  // Check that NATS has received a missing metadata request.
+  EXPECT_EQ(2, nats_conn_->published_msgs().size());
+  req = nats_conn_->published_msgs()[1];
+  EXPECT_TRUE(req.k8s_metadata_message().has_missing_k8s_metadata_request());
+  missing_md = req.k8s_metadata_message().missing_k8s_metadata_request();
+  EXPECT_EQ(21, missing_md.from_update_version());
+  EXPECT_EQ(40, missing_md.to_update_version());
+
+  // Missing MD response part 1/1, req 2
+  sent_missing = TestMissingUpdateResp({updates_[1], updates_[2]}, 20, 30);
+  ASSERT_OK(k8s_update_handler_->HandleMessage(std::move(sent_missing)));
+
+  // Now all 5 updates should be received.
+  EXPECT_EQ(5, fake_mds_manager_->num_k8s_updates());
+
+  // Check that the messages were sent in the right order.
+  for (size_t i = 0; i < 5; ++i) {
+    EXPECT_THAT(*(fake_mds_manager_->k8s_update(i)), EqualsProto(updates_[i]));
+  }
 }
 
 }  // namespace agent
