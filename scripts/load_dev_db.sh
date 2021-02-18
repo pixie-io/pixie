fdc54001e917e5052cd3011dd05cc0e599d5fe3d
Phillip Kuznetsov <philkuz@pixielabs.ai>
2021-02-18 14:55:22 -0800

    [PC-758] [PC-757] Adding hydra and kratos deployments
    Summary: Adding deployments for hydra and kratos. Currently are not added to the rest of our deployment flow because we dont' want it to be included in future releases. Available in skaffold under the hydra profile.
    
    Test Plan: Tested and could run through atuh flow with the self-service ui that ORY developed.
    
    Reviewers: michelle, #engineering
    
    Reviewed By: michelle, #engineering
    
    JIRA Issues: PC-757, PC-758
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7412
    
    GitOrigin-RevId: b1261e653278710a54d1c293b7c578a79a0b226c

--- scripts/load_dev_db.sh
+++ scripts/load_dev_db.sh
@@ -1,14 +1,46 @@
 #!/bin/bash -ex
 
+
+function usage() {
+  # Disable the printing on each echo.
+  set +x
+  echo "Usage:"
+  echo "$0 namespace [--load_oss_auth] --help"
+  echo ""
+  echo "    namespace             The namespace where we want to load the db"
+  echo "                              Required."
+  echo "    --load_oss_auth       Whether to load the oss auth database or not."
+  echo "                              Optional."
+  echo ""
+  exit 1
+}
+
 if [ $# -lt 1 ]; then
-  echo "This script requires exactly one argument: <namespace>"
+  usage
   exit
 fi
-namespace=$1
 
+namespace=""
+load_oss_auth=0
 repo_path=$(pwd)
 versions_file="$(pwd)/src/utils/artifacts/artifact_db_updater/VERSIONS.json"
 certs_path=$(pwd)/credentials/certs
+while true; do
+    if [[ "$1" == "--help" ]]; then
+        usage
+        exit 1
+    elif [[ "$1" == "--load_oss_auth" ]]; then
+        load_oss_auth=1
+    else
+        namespace=$1
+    fi
+    shift
+
+    if [[ -z "$1" ]]; then
+        break
+    fi
+done
+
 
 # Port-forward the postgres pod.
 postgres_pod=$(kubectl get pod --namespace "$namespace" --selector="name=postgres" \
@@ -32,6 +64,12 @@ git checkout main "$versions_file"
 # Update database with SSL certs.
 bazel run -c opt //src/cloud/dnsmgr/load_certs:load_certs -- \
     --certs_path "${certs_path}" --postgres_db "pl"
+
+# Run the kratos and hydra migrate jobs.
+if [[ $load_oss_auth -ne 0 ]]; then
+    kubectl apply -n "${namespace}" -f "${repo_path}/k8s/cloud/base/kratos/kratos_migrate.yaml"
+    kubectl apply -n "${namespace}" -f "${repo_path}/k8s/cloud/base/hydra/hydra_migrate.yaml"
+fi
 
 # Kill kubectl port-forward.
 kill -15 "$!"
