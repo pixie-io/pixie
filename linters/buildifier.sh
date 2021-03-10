b55d5f63791f52f6d37fe0ce3bd7173a278268e4
Zain Asgar <zasgar@pixielabs.ai>
2021-03-10 08:50:52 -0800

    Disable BES on buildifier
    Summary: Stop spamming buildbuddy
    
    Test Plan: N/A
    
    Reviewers: vihang
    
    Reviewed By: vihang
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7636
    
    GitOrigin-RevId: 321289a58fcd27114208771a489ac83da1579986

--- linters/buildifier.sh
+++ linters/buildifier.sh
@@ -6,4 +6,4 @@ workspace=$(bazel info workspace)
 buildifier_args="--path=${workspace} ${workspace}/$1"
 
 # Auto-fix the build files.
-bazel run --direct_run @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=fix ${buildifier_args}
+bazel run --bes_backend="" --bes_results_url="" --direct_run @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=fix ${buildifier_args}
