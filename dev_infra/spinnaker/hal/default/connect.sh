56f24171284edd9c98272aa6fa14714ad8fb233b
Zain Asgar <zasgar@pixielabs.ai>
2021-03-10 21:03:29 -0800

    Removing unused dev_infra
    Summary: TSIA
    
    Test Plan: N/A
    
    Reviewers: michelle, vihang
    
    Reviewed By: vihang
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7656
    
    GitOrigin-RevId: f62ebd230d440fd8bb71e853d9d9c6600518b222

--- dev_infra/spinnaker/hal/default/connect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubectl -n=spinnaker port-forward $(kubectl -n=spinnaker get po -l=cluster=spin-deck -o=jsonpath='{.items[0].metadata.name}') 9000 &
-kubectl -n=spinnaker port-forward $(kubectl -n=spinnaker get po -l=cluster=spin-gate -o=jsonpath='{.items[0].metadata.name}') 8084
--- dev_infra/spinnaker/hal/default/profiles/settings-local.js
+++
@@ -1,3 +0,0 @@
-window.spinnakerSettings.feature.kustomizeEnabled = true;
-window.spinnakerSettings.feature.artifactsRewrite = true;
-
