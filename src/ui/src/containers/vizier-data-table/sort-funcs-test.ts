04ff554e326cbde059606cc67b614925a9fdeeea
Nick Lanam <nick@pixielabs.ai>
2021-03-22 17:44:23 -0700

    [PC-793] Use a more complete sorting model for fields
    Summary: When sorting complex objects, the function we used to sort them by a specific field failed to account for types that aren't numbers. It now handles this, and gives specific error messages when data can't be reasonably sorted. Also, a little bit of trivia in the unit tests.
    
    Test Plan: Run the script in `PC-793`. The resulting table should now sort properly for both columns, rather than only the `pod` column.
    
    Reviewers: michelle, vihang
    
    Reviewed By: michelle
    
    JIRA Issues: PC-793
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7804
    
    GitOrigin-RevId: ea821da85475c5f8a9b7aa48137921f2fd69d5b3

--- src/ui/src/containers/vizier-data-table/sort-funcs-test.ts
+++ src/ui/src/containers/vizier-data-table/sort-funcs-test.ts
@@ -40,4 +40,130 @@ describe('fieldSortFunc', () => {
       null,
     ]);
   });
+
+  it('correctly sorts boolean values', () => {
+    const f = fieldSortFunc('label', true);
+    const rows = [
+      { label: false },
+      { label: true },
+      { label: false },
+      { label: true },
+      { label: false },
+      { label: false },
+    ];
+    expect(rows.sort(f)).toStrictEqual([
+      { label: false },
+      { label: false },
+      { label: false },
+      { label: false },
+      { label: true },
+      { label: true },
+    ]);
+  });
+
+  it('correctly sorts bigint values', () => {
+    const f = fieldSortFunc('label', true);
+    const rows = [
+      { label: BigInt(3) },
+      { label: BigInt(1) },
+      { label: BigInt(0) },
+      { label: BigInt(2) },
+      { label: BigInt(4) },
+      { label: BigInt(5) },
+    ];
+    expect(rows.sort(f)).toStrictEqual([
+      { label: BigInt(0) },
+      { label: BigInt(1) },
+      { label: BigInt(2) },
+      { label: BigInt(3) },
+      { label: BigInt(4) },
+      { label: BigInt(5) },
+    ]);
+  });
+
+  it('correctly sorts string values', () => {
+    const f = fieldSortFunc('label', true);
+    // The late Jurassic period, 164 to 145 million years ago, had very few (known) species considered to be dinosaurs.
+    const rows = [
+      /*
+       * They _do_ move in herds! Nowadays, we don't have sauropods like this. At least giraffes are friendly!
+       * Specimens have been found in Algeria, Portugal, Tanzania, and the United States.
+       */
+      { label: 'Brachiosaurus' },
+      /*
+       * The crocodile-sized gargoyle lizard lacks the morningstar at the tip of its Anklyosaurus cousin's tail.
+       * Specimens have been found in the United States.
+       */
+      { label: 'Gargoyleosaurus' },
+      /*
+       * Visual evidence that chickens evolved from raptors. Feathered omnivores, just 1.7m long.
+       * Specimens have been found in Canada.
+       */
+      { label: 'Chirostenotes' },
+      /*
+       * Tyrannosaurus Rex was not alive during the late Jurassic era. Its smaller cousin, Allosaurus, was.
+       * Specimens have been found in Portugal and the United States.
+       */
+      { label: 'Allosaurus' },
+      /*
+       * A dwarf sauropod measuring about 6.2 meters snout to tail, with a recognizable bump atop its head.
+       * Specimens have been found in Germany.
+       */
+      { label: 'Europasaurus' },
+      /*
+       * Also known as the "oak lizard", Dryosaurus is a bipedal dinosaur about four meters snout to tail.
+       * Specimens have been found in Tanzania and the United States.
+       */
+      { label: 'Dryosaurus' },
+    ];
+    expect(rows.sort(f)).toStrictEqual([
+      // Surprisingly, there are no dinosaurs from the late Jurassic period whose names start with an F.
+      { label: 'Allosaurus' },
+      { label: 'Brachiosaurus' },
+      { label: 'Chirostenotes' },
+      { label: 'Dryosaurus' },
+      { label: 'Europasaurus' },
+      { label: 'Gargoyleosaurus' },
+    ]);
+  });
+
+  it('throws when trying to compare disparate types', () => {
+    const f = fieldSortFunc('label', true);
+    const rows = [
+      { label: 'I am not a number.' },
+      { label: 12345 },
+    ];
+    expect(() => rows.sort(f)).toThrowError();
+  });
+
+  it('throws when trying to compare types with no reasonable comparison method', () => {
+    const f = fieldSortFunc('label', true);
+    expect(() => [
+      { label: function noSortingMe() {} },
+      { label: function notMeEither() {} },
+    ].sort(f)).toThrowError();
+
+    expect(() => [
+      { label: {} },
+      { label: {} },
+    ].sort(f)).toThrowError();
+
+    expect(() => [
+      { label: Symbol('Symbols cannot be compared reasonably') },
+      { label: Symbol('Because doing so would be quite dirty') },
+    ].sort(f)).toThrowError();
+  });
+
+  it('sorts nulls to the end of the list', () => {
+    const f = fieldSortFunc('label', true);
+    expect([
+      { label: 'foo' },
+      { label: null },
+      { label: 'bar' },
+    ].sort(f)).toStrictEqual([
+      { label: 'bar' },
+      { label: 'foo' },
+      { label: null },
+    ]);
+  });
 });
--- src/ui/src/containers/vizier-data-table/sort-funcs.ts
+++ src/ui/src/containers/vizier-data-table/sort-funcs.ts
@@ -1,23 +1,61 @@
 import { SortDirection, SortDirectionType } from 'react-virtualized';
 import { getDataSortFunc } from 'utils/format-data';
 import { SemanticType } from 'types/generated/vizierapi_pb';
+import { checkExhaustive } from 'utils/check-exhaustive';
 import { ColumnDisplayInfo, QuantilesDisplayState } from './column-display-info';
 
 // Sort funcs for semantic column types (when the pure data type alone doesn't produce
 // the sort result that we want.
 
-// Sorts an object on a specific field name.
+/**
+ * Returns a sorting function that extracts and compares a specific field from the compared objects.
+ * Sorts missing (null, undefined) values to the bottom when in ascending order.
+ *
+ * @param fieldName What field to look at when comparing two objects that both may have that field.
+ * @param ascending If true, "bigger" values are sorted to the bottom. If false, they're sorted to the top.
+ */
 export function fieldSortFunc(fieldName: string, ascending: boolean) {
   return (a, b) => {
-    const aNull = !(a && a[fieldName] != null) ? 1 : 0;
-    const bNull = !(b && b[fieldName] != null) ? 1 : 0;
+    const aVal = a?.[fieldName];
+    const bVal = b?.[fieldName];
+
+    // Number(true) returns 1, Number(false) returns 0.
+    const aNull = Number(aVal == null);
+    const bNull = Number(bVal == null);
 
     // Nulls come last.
     if (aNull || bNull) {
       return aNull - bNull;
     }
 
-    const result = a[fieldName] - b[fieldName];
+    // The type must be in a const, rather than directly referenced, for checkExhaustive
+    // and switch(typeof aVal) to realize they're referring to the same (narrowed) type.
+    const aType = typeof aVal;
+    const bType = typeof bVal;
+    if (aType !== bType) {
+      throw new Error(`Cannot compare disparate types '${aType}' and '${bType}' (a=${aVal}; b=${bVal})`);
+    }
+
+    let result: number;
+    switch (aType) {
+      case 'number':
+      case 'bigint':
+      case 'boolean':
+        // Number(true) is 1, Number(false) and Number(null) are 0.
+        result = Number(aVal) - Number(bVal);
+        break;
+      case 'string':
+        result = aVal.localeCompare(bVal);
+        break;
+      case 'object':
+      case 'function':
+      case 'symbol':
+      case 'undefined':
+        throw new Error(`Cannot compare values of type '${aType}'`);
+      default:
+        checkExhaustive(aType);
+    }
+
     return (ascending ? result : -result);
   };
 }
