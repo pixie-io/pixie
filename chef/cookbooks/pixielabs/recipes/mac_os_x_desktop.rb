809a6306912722d679798e242ea5450fbe219751
Vihang Mehta <vihang@pixielabs.ai>
2021-03-30 15:46:28 -0700

    Cleanup unused chef submodules
    Summary: Cleanup some old jank
    
    Test Plan: Run chef
    
    Reviewers: zasgar, #third_party_approvers
    
    Reviewed By: zasgar, #third_party_approvers
    
    Differential Revision: https://phab.corp.pixielabs.ai/D7934
    
    GitOrigin-RevId: c674ffb99346ead29eefb69951a856750e03dc5d

--- chef/cookbooks/pixielabs/recipes/mac_os_x_desktop.rb
+++ chef/cookbooks/pixielabs/recipes/mac_os_x_desktop.rb
@@ -1,5 +1,3 @@
 homebrew_cask 'google-chrome'
 homebrew_cask 'iterm2'
 homebrew_cask 'tunnelblick'
-# TODO(zasgar): PL-122 doesn't work on Mojave because VB is doing sketchy kernel extension things.
-# homebrew_cask "virtualbox"
--- chef/roles/pl_base_dev.rb
+++ chef/roles/pl_base_dev.rb
@@ -2,6 +2,5 @@ name 'pl_base_dev'
 description 'This role installs all relevant developer desktop apps'
 
 run_list(
-  'recipe[build-essential]',
   'recipe[pixielabs::base]',
 )
