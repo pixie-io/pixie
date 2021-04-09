9ed0618d25693557c51d283a183248047f8b8562
Michelle Nguyen <michelle@pixielabs.ai>
2021-04-09 09:36:20 -0700

    Revert "PP-2445: move limit pushdown rule from optimizer to presplit optimizer in order to add PEM-awareness"
    Summary: this diff somehow breaks df.ctx in http_data
    
    Test Plan: ran in skaffold, confirm that df.ctx works again
    
    Reviewers: nserrino, zasgar
    
    Reviewed By: nserrino
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8091
    
    GitOrigin-RevId: 547f8c6a6c84417b8c504331e4c6823bc0464a91

--- src/carnot/planner/distributed/presplit_optimizer.h
+++ src/carnot/planner/distributed/presplit_optimizer.h
@@ -27,15 +27,11 @@ class PreSplitOptimizer : public RuleExecutor<IR> {
  private:
   explicit PreSplitOptimizer(CompilerState* compiler_state) : compiler_state_(compiler_state) {}
 
-  void CreateLimitPushdownBatch() {
-    RuleBatch* limit_pushdown = CreateRuleBatch<FailOnMax>("LimitPushdown", 2);
-    limit_pushdown->AddRule<LimitPushdownRule>(compiler_state_);
-  }
-
   Status Init() {
-    CreateLimitPushdownBatch();
+    PL_UNUSED(compiler_state_);
     return Status::OK();
   }
+
   CompilerState* compiler_state_;
 };
 }  // namespace distributed
