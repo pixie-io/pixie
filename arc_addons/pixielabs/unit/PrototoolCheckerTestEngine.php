dd15007581d8b4ad562a87ff7ca7d5662e92f7c5
Vihang Mehta <vihang@pixielabs.ai>
2021-04-10 22:47:09 -0700

    Cleanup prototool invocation
    Summary:
    Since we import `gogo.proto` but the dependency is fetched by bazel
    we end up having to copy the bazel fetched gogo lib as a workaround to run
    prototool. Having the file checked in for prototool to use simplifies this
    greatly. We will still use the bazel fetched proto for building and hence
    care must be taken to avoid divergence, but `gogo.proto` has not changed in
    nearly 2.5 years.
    
    Test Plan: `prototool` still works.
    
    Reviewers: zasgar, michelle, #third_party_approvers
    
    Reviewed By: zasgar, michelle, #third_party_approvers
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8123
    
    GitOrigin-RevId: f96ad8ddcb01bb7b6ab066e1930274d07a1d753c

--- arc_addons/pixielabs/unit/PrototoolCheckerTestEngine.php
+++ arc_addons/pixielabs/unit/PrototoolCheckerTestEngine.php
@@ -3,10 +3,6 @@
 final class PrototoolCheckerTestEngine {
     private $project_root;
     private $files;
-    private $go_modules = array(
-        'github.com/gogo/protobuf' => 'com_github_gogo_protobuf'
-    );
-    private $vendorPath = '/tmp/pixie/vendor';
 
     public function __construct($project_root, $files) {
         $this->project_root = $project_root;
@@ -25,24 +21,6 @@ final class PrototoolCheckerTestEngine {
 
         if (count($protoFiles) == 0) {
             return $test_results;
-        }
-
-        // Build proto files.
-        exec('bazel query "kind(\'go_proto_library rule\', //src/...)"', $output, $return_var);
-
-        foreach ($output as $o) {
-            exec('bazel build ' . $o . '> /dev/null 2>&1');
-        }
-
-        // Copy over vendor files to a tmp directory.
-        exec('rm -rf ' . $this->vendorPath);
-        foreach ($this->go_modules as $m => $p) {
-            $cpModuleDir = $this->vendorPath . '/' . $m;
-            exec('mkdir -p ' . dirname($cpModuleDir));
-
-            // Copy over the directory from bazel-pixielabs/external/.
-            exec(sprintf('cp -r %s/bazel-pixielabs/external/%s %s', $this->project_root, $p, $cpModuleDir));
-            exec('chmod -R 755 '. $cpModuleDir);
         }
 
         $output = array();
