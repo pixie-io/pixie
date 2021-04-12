69ece9b8ff3e9acfb58d846d616ace455c7877c3
Nick Lanam <nick@pixielabs.ai>
2021-04-12 15:33:31 -0700

    Teach Arcanist to use the nearest .eslintrc.json to lint targets.
    Summary: Rather than always using `//src/ui/.eslintrc.json`, check if there is another `.eslintrc.json` somewhere closer in the lint target's ancestry. Useful for Yarn workspaces that have their own configs.
    
    Test Plan: Try `arc lint` on this diff!
    
    Reviewers: michelle, vihang
    
    Reviewed By: vihang
    
    JIRA Issues: PC-837
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8136
    
    GitOrigin-RevId: 126d2de59ba45fb3af2cbadc8775054635fb3bcd

--- linters/eslint_ui.sh
+++ linters/eslint_ui.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 set -e
 
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-uipath="${SCRIPTPATH}/../src/ui"
+# This script is invoked with a line like:
+# linters/eslint_ui.sh '--format=json' --no-color --config .eslintrc.json src/ui/.../something.ts
+# That assumes that there's a .eslintrc.json in the working directory. We need the nearest matching ancestor directory.
 
-pushd "${uipath}" > /dev/null
-./node_modules/.bin/eslint "$@"
+scriptDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+uiDir="${scriptDir}/../src/ui"
+
+# Walk up the lint target's directory ancestry until finding .eslintrc.json, or until reaching the UI root or /.
+workDir=$(dirname "$5")
+while [ ! "$workDir" = '/' ] && [ ! "$workDir" = "$uiDir" ] && [ ! -f "${workDir}/.eslintrc.json" ]; do
+  workDir=$(dirname "$workDir")
+done
+
+if [ ! -f "${workDir}/.eslintrc.json" ]; then
+  echo "Could not find .eslintrc when walking up the directory tree from $(dirname "$5")" >&2
+  echo "Last location checked was $workDir" >&2
+  exit 1
+fi
+
+pushd "${workDir}" > /dev/null
+"${uiDir}/node_modules/.bin/eslint" "$@"
 popd > /dev/null
--- src/ui/packages/pixie-api-react/src/testing/utils.ts
+++ src/ui/packages/pixie-api-react/src/testing/utils.ts
@@ -1,7 +1,3 @@
-// TODO(nick): Arcanist's lint config is using the root config instead of the local one, causing this to trip.
-//  The .eslintrc.json that goes with this package is properly set up for this file and is the one we should be using.
-//  For now, just silence the warning since it's a false positive.
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { act } from '@testing-library/react';
 
 export const waitNoAct = async (t = 0): Promise<void> => new Promise((resolve) => {
