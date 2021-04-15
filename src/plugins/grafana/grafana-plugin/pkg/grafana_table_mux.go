cfdf49d3b40ee0853c9d91f493667d55887afed2
Vishal Jain <vjain@pixielabs.ai>
2021-04-15 10:50:55 -0700

    Grafana Pixie table Muxer with Unit test. Can handle more than 1 column and more than 1 table.
    Summary:
    Need a to implement TableMuxer and RecordHandler when collecting data from Pixie tables. We add this data to Grafana
    data frames.
    
    Test Plan: Manual testing with Grafana + Unit testing
    
    Reviewers: nserrino, michelle, #engineering, vihang
    
    Reviewed By: #engineering, vihang
    
    JIRA Issues: PC-832
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8177
    
    GitOrigin-RevId: f4938af0b90c58786a8446ccbb0c7d0b601a44ab

---
+++ src/plugins/grafana/grafana-plugin/pkg/grafana_table_mux.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+
+	"pixielabs.ai/pixielabs/src/api/go/pxapi"
+	"pixielabs.ai/pixielabs/src/api/go/pxapi/types"
+	vizierapipb "pixielabs.ai/pixielabs/src/api/public/vizierapipb"
+)
+
+// PixieToGrafanaTablePrinter satisfies the TableRecordHandler interface.
+type PixieToGrafanaTablePrinter struct {
+	// Grafana Frame for Pixie Table. Holds fields.
+	frame *data.Frame
+}
+
+// HandleInit creates a new Grafana field for each column in a table.
+func (t *PixieToGrafanaTablePrinter) HandleInit(ctx context.Context, metadata types.TableMetadata) error {
+	// Create new data frame for new table.
+	t.frame = data.NewFrame(metadata.Name)
+
+	// Create new fields (columns) for the frame.
+	for _, col := range metadata.ColInfo {
+		switch colType := col.Type; colType {
+		case vizierapipb.BOOLEAN:
+			t.frame.Fields = append(t.frame.Fields,
+				data.NewField(col.Name, nil, []bool{}))
+		case vizierapipb.INT64:
+			t.frame.Fields = append(t.frame.Fields,
+				data.NewField(col.Name, nil, []int64{}))
+		case vizierapipb.TIME64NS:
+			t.frame.Fields = append(t.frame.Fields,
+				data.NewField(col.Name, nil, []time.Time{}))
+		case vizierapipb.FLOAT64:
+			t.frame.Fields = append(t.frame.Fields,
+				data.NewField(col.Name, nil, []float64{}))
+		case vizierapipb.STRING:
+			t.frame.Fields = append(t.frame.Fields,
+				data.NewField(col.Name, nil, []string{}))
+		case vizierapipb.UINT128:
+			// Use a UUID style string representation for uint128
+			// since Grafana fields do not support uint128
+			t.frame.Fields = append(t.frame.Fields,
+				data.NewField(col.Name, nil, []string{}))
+		}
+	}
+	return nil
+}
+
+// HandleRecord goes through the record adding the data to the appropriate
+// field.
+func (t *PixieToGrafanaTablePrinter) HandleRecord(ctx context.Context, r *types.Record) error {
+	for colIdx, d := range r.Data {
+		switch d.Type() {
+		case vizierapipb.BOOLEAN:
+			t.frame.Fields[colIdx].Append(d.(*types.BooleanValue).Value())
+		case vizierapipb.INT64:
+			t.frame.Fields[colIdx].Append(d.(*types.Int64Value).Value())
+		case vizierapipb.UINT128:
+			t.frame.Fields[colIdx].Append(d.(*types.UInt128Value).String())
+		case vizierapipb.FLOAT64:
+			t.frame.Fields[colIdx].Append(d.(*types.Float64Value).Value())
+		case vizierapipb.STRING:
+			t.frame.Fields[colIdx].Append(d.(*types.StringValue).Value())
+		case vizierapipb.TIME64NS:
+			t.frame.Fields[colIdx].Append(d.(*types.Time64NSValue).Value())
+		}
+	}
+
+	return nil
+}
+
+// HandleDone is run when all record processing is complete.
+func (t *PixieToGrafanaTablePrinter) HandleDone(ctx context.Context) error {
+	return nil
+}
+
+// PixieToGrafanaTableMux satisfies the TableMuxer interface.
+type PixieToGrafanaTableMux struct {
+	// pxTablePrinterLst is a list of the table printers.
+	pxTablePrinterLst []*PixieToGrafanaTablePrinter
+}
+
+// AcceptTable adds the table printer to the list of table printers.
+func (s *PixieToGrafanaTableMux) AcceptTable(ctx context.Context, metadata types.TableMetadata) (pxapi.TableRecordHandler, error) {
+	tablePrinter := &PixieToGrafanaTablePrinter{}
+	s.pxTablePrinterLst = append(s.pxTablePrinterLst, tablePrinter)
+	return tablePrinter, nil
+}
