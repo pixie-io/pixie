c9bf44f00147b42cd3572beb95c83f0a8891b964
Zain Asgar <zasgar@pixielabs.ai>
2021-04-18 12:59:52 -0700

    PP-2565 - Add license headers to all stirling files
    Summary:
    Splitting this out to make sure the licenes are applied correctly. Please ensure:
     1. Correct license is used
     2. We aren't accidentaly claiming license on files that aren't ours
     3. All relevant files have licenses.
    
    Test Plan: manual inspection
    
    Reviewers: michelle, vihang, oazizi, #third_party_approvers
    
    Reviewed By: michelle, oazizi, #third_party_approvers
    
    JIRA Issues: PP-2565
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8241
    
    GitOrigin-RevId: d57369f194d32cf126527f4464d447732995cc10

--- src/stirling/source_connectors/jvm_stats/testing/run_hello_world_on_gke.sh
+++ src/stirling/source_connectors/jvm_stats/testing/run_hello_world_on_gke.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright 2018- The Pixie Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 bazel run //src/stirling/source_connectors/jvm_stats/testing:push_hello_world_image
 
 # kubectl apply does not force re-pulling the image, as the tag has not changed.
