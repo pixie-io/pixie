3938387c59596e6642485f38fb1488716ab5601b
Vihang Mehta <vihang@pixielabs.ai>
2021-04-19 21:01:13 -0700

    Fixup cpplint
    Summary: TSIA
    
    Test Plan: N/A
    
    Reviewers: zasgar, michelle, #third_party_approvers
    
    Reviewed By: zasgar, #third_party_approvers
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8294
    
    GitOrigin-RevId: 64c3bfc1e206c62313b1bbfd27ea133d3d7bd4b1

--- third_party/cpplint/cpplint.py
+++ third_party/cpplint/cpplint.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright 2018- The Pixie Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-
-#
 # Copyright (c) 2009 Google Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -44,6 +27,8 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# SPDX-License-Identifier: BSD-3-Clause
 
 """Does google-lint on c++ files.
 
