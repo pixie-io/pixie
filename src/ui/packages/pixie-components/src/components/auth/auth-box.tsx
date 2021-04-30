f4d36ed290898e0d88cd15a1665d2a1585244e2c
Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
2021-04-29 17:00:04 -0700

    Revert "Styling tweak for sign up page"
    Summary: This reverts commit 6016cb5af425c12dd03635d39abc9bf3b4a19032. Styling was only useful with the rest of the changes.
    
    Test Plan: Tested on yarn dev.
    
    Reviewers: michelle
    
    Reviewed By: michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8479
    
    GitOrigin-RevId: 791605b25aba343f2762d9bfbe85de2882ccbbbf

--- src/ui/packages/pixie-components/src/components/auth/auth-box.tsx
+++ src/ui/packages/pixie-components/src/components/auth/auth-box.tsx
@@ -39,7 +39,7 @@ const useStyles = makeStyles(({ spacing, palette }: Theme) => createStyles({
   title: {
     color: palette.foreground.two,
     paddingTop: spacing(1),
-    paddingBottom: spacing(2),
+    paddingBottom: spacing(3),
     marginBottom: spacing(1.25),
   },
   subtitle: {
@@ -54,7 +54,7 @@ const useStyles = makeStyles(({ spacing, palette }: Theme) => createStyles({
     flexFlow: 'row nowrap',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: spacing(1),
+    marginTop: spacing(3),
     paddingTop: spacing(1),
     borderTop: `1px solid ${palette.foreground.grey1}`,
   },
