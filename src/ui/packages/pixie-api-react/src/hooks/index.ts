55768a1e4970f4119d3f0f96f482b8ff98be8010
Vihang Mehta <vihang@pixielabs.ai>
2021-05-30 20:54:52 -0700

    Remove unused queries from api-react
    Summary: Now unused.
    
    Test Plan: still builds and runs.
    
    Reviewers: philkuz, michelle, nlanam, zasgar
    
    Reviewed By: philkuz
    
    JIRA Issues: PC-968
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8797
    
    GitOrigin-RevId: 705f971717849662d4c859aa7731d916288b13e3

--- src/ui/packages/pixie-api-react/src/hooks/index.ts
+++ src/ui/packages/pixie-api-react/src/hooks/index.ts
@@ -17,13 +17,10 @@
  */
 
 export * from './use-api-keys';
-export * from './use-autocomplete';
 export * from './use-autocomplete-field';
-export * from './use-cluster-control-plane-pods';
 export * from './use-deployment-keys';
 export * from './use-is-authenticated';
 export * from './use-invitation';
-export * from './use-list-clusters';
 export * from './use-query';
 export * from './use-setting';
 export * from './use-user-info';
