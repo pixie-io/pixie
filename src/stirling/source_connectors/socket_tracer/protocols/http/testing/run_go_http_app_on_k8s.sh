f4844d9cc717d66e046aef640717312eaa09539a
Omid Azizi <oazizi@pixielabs.ai>
2021-06-02 15:17:54 -0700

    High-throughput client-server: as k8s load
    Summary: A client-server pair that produces a lot of traffic, so we can study the impact on Stirling/PEM performance.
    
    Test Plan: Manual
    
    Reviewers: #stirling, yzhao
    
    Reviewed By: #stirling, yzhao
    
    Differential Revision: https://phab.corp.pixielabs.ai/D8828
    
    GitOrigin-RevId: 6a508b6490c675a4a98b19fa4af51d0a7fe91963

--- src/stirling/source_connectors/socket_tracer/protocols/http/testing/run_go_http_app_on_k8s.sh
+++ src/stirling/source_connectors/socket_tracer/protocols/http/testing/run_go_http_app_on_k8s.sh
@@ -16,21 +16,23 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+script_dir="$(dirname "$0")"
+pixie_root=$(bazel info workspace)
+
 bazel run //src/stirling/source_connectors/socket_tracer/protocols/http/testing/go_http_client:push_image
 bazel run //src/stirling/source_connectors/socket_tracer/protocols/http/testing/go_http_server:push_image
 
-namespace_name="px-http-test"
+ns="px-http-test"
 
-kubectl create namespace "${namespace_name}"
+# Create namespace if it does not exist.
+kubectl create namespace "$ns" --dry-run -o yaml | kubectl apply -f -
 
-kubectl -n "$namespace_name" create secret docker-registry image-pull-secret \
+kubectl -n "$ns" create secret docker-registry image-pull-secret \
   --docker-server=https://gcr.io \
   --docker-username=_json_key \
   --docker-email="${USER}@pixielabs.ai" \
-  --docker-password="$(sops -d credentials/k8s/dev/image-pull-secrets.encrypted.json)" \
+  --docker-password="$(sops -d "$pixie_root"/credentials/k8s/dev/image-pull-secrets.encrypted.json)" \
   --dry-run=true --output=yaml | kubectl apply -f -
 
-sed "s/{{USER}}/${USER}/" src/stirling/source_connectors/socket_tracer/protocols/http/testing/go_http_server/deployment.yaml | \
-  kubectl -n "$namespace_name" apply -f -
-sed "s/{{USER}}/${USER}/" src/stirling/source_connectors/socket_tracer/protocols/http/testing/go_http_client/deployment.yaml | \
-  kubectl -n "$namespace_name" apply -f -
+sed "s/{{USER}}/${USER}/" "$script_dir"/go_http_server/deployment.yaml | kubectl apply -n "$ns" -f -
+sed "s/{{USER}}/${USER}/" "$script_dir"/go_http_client/deployment.yaml | kubectl apply -n "$ns" -f -
