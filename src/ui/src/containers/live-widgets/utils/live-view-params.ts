299918e26c6c8eb2cad7332074220e05e8e025f8
Vihang Mehta <vihang@pixielabs.ai>
2021-06-30 17:26:44 -0700

    Remove some unused code
    Summary: TSIA
    
    Test Plan: Existing tests + build
    
    Reviewers: michelle, nlanam
    
    Reviewed By: michelle
    
    Differential Revision: https://phab.corp.pixielabs.ai/D9021
    
    GitOrigin-RevId: 073394d8d21aa2dac0ff19758b917e2f00831e56

--- src/ui/src/containers/live-widgets/utils/live-view-params.ts
+++ src/ui/src/containers/live-widgets/utils/live-view-params.ts
@@ -17,7 +17,6 @@
  */
 
 import * as QueryString from 'query-string';
-import { matchPath } from 'react-router';
 
 import { SemanticType } from 'app/types/generated/vizierapi_pb';
 import { Arguments } from 'app/utils/args-utils';
@@ -65,19 +64,6 @@ export interface EntityPage {
   params: EntityURLParams;
 }
 
-// For live view entity routes.
-export const LiveViewPageRoutes = new Map<LiveViewPage, string>([
-  [LiveViewPage.Cluster, '/live/clusters/:cluster'],
-  [LiveViewPage.Namespace, '/live/clusters/:cluster/namespaces/:namespace'],
-  [LiveViewPage.Namespaces, '/live/clusters/:cluster/namespaces'],
-  [LiveViewPage.Node, '/live/clusters/:cluster/nodes/:node'],
-  [LiveViewPage.Nodes, '/live/clusters/:cluster/nodes'],
-  [LiveViewPage.Pod, '/live/clusters/:cluster/namespaces/:namespace/pods/:pod'],
-  [LiveViewPage.Pods, '/live/clusters/:cluster/namespaces/:namespace/pods'],
-  [LiveViewPage.Service, '/live/clusters/:cluster/namespaces/:namespace/services/:service'],
-  [LiveViewPage.Services, '/live/clusters/:cluster/namespaces/:namespace/services'],
-]);
-
 export const LiveViewPageScriptIds = new Map<LiveViewPage, string>([
   [LiveViewPage.Cluster, 'px/cluster'],
   [LiveViewPage.Namespace, 'px/namespace'],
@@ -110,161 +96,6 @@ export const LiveViewEntityParams = new Map<LiveViewPage, Set<string>>([
   [LiveViewPage.Service, new Set(['service'])],
   [LiveViewPage.Services, new Set(['namespace'])],
 ]);
-
-interface WithCluster {
-  cluster: string;
-}
-
-function matchAndExtractEntity<T>(path: string, page: LiveViewPage) {
-  const match = matchPath<WithCluster & T>(path, {
-    path: LiveViewPageRoutes.get(page),
-    exact: true,
-  });
-  if (!match) {
-    return null;
-  }
-  const { cluster, ...params } = match.params as WithCluster & T;
-  return {
-    clusterName: decodeURIComponent(cluster),
-    page,
-    params,
-  };
-}
-
-export function matchLiveViewEntity(path: string): EntityPage {
-  // cluster
-  const clusterMatch = matchAndExtractEntity<Record<string, never>>(
-    path, LiveViewPage.Cluster,
-  );
-  if (clusterMatch) {
-    return clusterMatch;
-  }
-
-  // namespaces
-  const namespaceMatch = matchAndExtractEntity<NamespaceURLParams>(
-    path, LiveViewPage.Namespace,
-  );
-  if (namespaceMatch) {
-    return namespaceMatch;
-  }
-  const namespacesMatch = matchAndExtractEntity<Record<string, never>>(
-    path, LiveViewPage.Namespaces,
-  );
-  if (namespacesMatch) {
-    return namespacesMatch;
-  }
-  // nodes
-  const nodeMatch = matchAndExtractEntity<NodeURLParams>(
-    path, LiveViewPage.Node,
-  );
-  if (nodeMatch) {
-    return nodeMatch;
-  }
-  const nodesMatch = matchAndExtractEntity<Record<string, never>>(
-    path, LiveViewPage.Nodes,
-  );
-  if (nodesMatch) {
-    return nodesMatch;
-  }
-  // pods
-  const podMatch = matchAndExtractEntity<NamespaceURLParams & PodURLParams>(
-    path, LiveViewPage.Pod,
-  );
-  if (podMatch) {
-    return {
-      ...podMatch,
-      params: {
-        // TODO(nserrino): remove this logic when we separate pod and namespace in the backend.
-        pod: `${podMatch.params.namespace}/${podMatch.params.pod}`,
-      },
-    };
-  }
-  const podsMatch = matchAndExtractEntity<NamespaceURLParams>(
-    path, LiveViewPage.Pods,
-  );
-  if (podsMatch) {
-    return podsMatch;
-  }
-  // services
-  const serviceMatch = matchAndExtractEntity<NamespaceURLParams & ServiceURLParams>(
-    path, LiveViewPage.Service,
-  );
-  if (serviceMatch) {
-    return {
-      ...serviceMatch,
-      params: {
-        // TODO(nserrino): remove this logic when we separate service and namespace in the backend.
-        service: `${serviceMatch.params.namespace}/${serviceMatch.params.service}`,
-      },
-    };
-  }
-  const servicesMatch = matchAndExtractEntity<NamespaceURLParams>(
-    path, LiveViewPage.Services,
-  );
-  if (servicesMatch) {
-    return servicesMatch;
-  }
-
-  // cluster script match.
-  const scriptMatch = matchPath<WithCluster>(path, {
-    path: '/live/clusters/:cluster/script',
-    exact: false,
-  });
-  if (scriptMatch) {
-    return {
-      clusterName: decodeURIComponent(scriptMatch.params.cluster),
-      page: LiveViewPage.Default,
-      params: {},
-    };
-  }
-
-  return {
-    page: LiveViewPage.Default,
-    params: {},
-  };
-}
-
-export function getLiveViewTitle(defaultTitle: string, page: LiveViewPage,
-  params: EntityURLParams, clusterPrettyName: string): string {
-  switch (page) {
-    case LiveViewPage.Cluster: {
-      return clusterPrettyName;
-    }
-    case LiveViewPage.Namespace: {
-      const { namespace } = params as NamespaceURLParams;
-      return `${namespace}` || defaultTitle;
-    }
-    case LiveViewPage.Namespaces: {
-      return `${clusterPrettyName} namespaces`;
-    }
-    case LiveViewPage.Node: {
-      const { node } = params as NodeURLParams;
-      return `${node}` || defaultTitle;
-    }
-    case LiveViewPage.Nodes: {
-      return `${clusterPrettyName} nodes`;
-    }
-    case LiveViewPage.Pod: {
-      const { pod } = params as PodURLParams;
-      return `${pod}` || defaultTitle;
-    }
-    case LiveViewPage.Pods: {
-      const { namespace } = params as NamespaceURLParams;
-      return `${namespace}/pods`;
-    }
-    case LiveViewPage.Service: {
-      const { service } = params as ServiceURLParams;
-      return `${service}` || defaultTitle;
-    }
-    case LiveViewPage.Services: {
-      const { namespace } = params as NamespaceURLParams;
-      return `${namespace}/services`;
-    }
-    case LiveViewPage.Default:
-    default:
-      return defaultTitle;
-  }
-}
 
 export function toEntityPathname(entity: EntityPage, isEmbedded: boolean): string {
   const pathPrefix = isEmbedded ? '/embed/live' : '/live';
@@ -399,23 +230,4 @@ export function toSingleEntityPage(entityName: string, semanticType: SemanticTyp
     default:
       return null;
   }
-}
-
-// If we are in a pod or a service view, this function extracts the namespace
-// from the entity params so that when we switch views, the namespace is maintained.
-export function optionallyGetNamespace(args: Arguments): string {
-  let valToSplit;
-  if (args.pod) {
-    valToSplit = args.pod;
-  }
-  if (args.service) {
-    valToSplit = args.service;
-  }
-  if (valToSplit) {
-    const split = valToSplit.split('/');
-    if (split.length >= 2) {
-      return split[0];
-    }
-  }
-  return null;
 }
