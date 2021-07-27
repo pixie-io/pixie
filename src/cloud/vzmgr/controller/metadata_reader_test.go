dbc9a406766d5c3292f474cb9ecb035ada5ec2a7
Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
2021-07-27 11:26:57 -0700

    Refactor metadata_reader_tests for better reuse
    Summary: @vjain left a comment that I forgot to properly address in the diff that added this test. I ran into a bug in a later diff that would've been fixed if this was easier to maintain and the tests were combined. Therefore, decided to dedupe the tests and combine them together.
    
    Test Plan: Ran the test, should pass
    
    Reviewers: vihang, michelle, vjain
    
    Reviewed By: vjain
    
    Subscribers: vjain
    
    Differential Revision: https://phab.corp.pixielabs.ai/D9329
    
    GitOrigin-RevId: ab5e511192d79173eecd1ef7f248e7655a640bcb

--- src/cloud/vzmgr/controller/metadata_reader_test.go
+++ src/cloud/vzmgr/controller/metadata_reader_test.go
@@ -47,11 +47,12 @@ type MetadataRequest struct {
 
 func TestMetadataReader_ProcessVizierUpdate(t *testing.T) {
 	tests := []struct {
-		name                   string                       // Name of the test
-		stanMetadataUpdates    []*metadatapb.ResourceUpdate // The updates sent on STAN
-		missingMetadataCalls   []*MetadataRequest           // The expected metadata request and responses.
-		expectedIndexerUpdates []*metadatapb.ResourceUpdate // The updates that should be sent to the indexer.
-		vizierStatus           string
+		name                    string                       // Name of the test
+		oldMetadataIndexUpdates []int64                      // Updates that already exist on the MetadataIndex channel.
+		stanMetadataUpdates     []*metadatapb.ResourceUpdate // The updates sent on STAN
+		missingMetadataCalls    []*MetadataRequest           // The expected metadata request and responses.
+		expectedIndexerUpdates  []*metadatapb.ResourceUpdate // The updates that should be sent to the indexer.
+		vizierStatus            string
 	}{
 		{
 			name: "In-order update",
@@ -196,6 +197,97 @@ func TestMetadataReader_ProcessVizierUpdate(t *testing.T) {
 			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{},
 			vizierStatus:           "HEALTHY",
 		},
+		{
+			name: "Metadata missing but k8s also missing the metadata on restart",
+			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
+				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
+			},
+			missingMetadataCalls: []*MetadataRequest{
+				{
+					from: 1001,
+					to:   1003,
+					responses: []*metadatapb.MissingK8SMetadataResponse{
+						{
+							FirstUpdateAvailable: 1003,
+						},
+					},
+				},
+			},
+			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{
+				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
+			},
+			oldMetadataIndexUpdates: []int64{999, 1000, 1001},
+			vizierStatus:            "HEALTHY",
+		},
+		{
+			name: "Expect Full Metadata Update requests on restart",
+			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
+				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
+			},
+			missingMetadataCalls: []*MetadataRequest{
+				{
+					from: 1001,
+					to:   1005,
+					responses: []*metadatapb.MissingK8SMetadataResponse{
+						{
+							FirstUpdateAvailable: 1001,
+							Updates: []*metadatapb.ResourceUpdate{
+								{UpdateVersion: 1001, PrevUpdateVersion: 1000},
+								{UpdateVersion: 1002, PrevUpdateVersion: 1001},
+								{UpdateVersion: 1003, PrevUpdateVersion: 1002},
+								{UpdateVersion: 1004, PrevUpdateVersion: 1003},
+							},
+						},
+					},
+				},
+			},
+			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{
+				{UpdateVersion: 1002, PrevUpdateVersion: 1001},
+				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
+				{UpdateVersion: 1004, PrevUpdateVersion: 1003},
+				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
+			},
+			oldMetadataIndexUpdates: []int64{999, 1000, 1001},
+			vizierStatus:            "HEALTHY",
+		},
+		{
+			name: "Expect Partial Metadata Update requests on restart",
+			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
+				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
+			},
+			missingMetadataCalls: []*MetadataRequest{
+				{
+					from: 1001,
+					to:   1005,
+					responses: []*metadatapb.MissingK8SMetadataResponse{
+						{
+							FirstUpdateAvailable: 1003,
+							Updates: []*metadatapb.ResourceUpdate{
+								{UpdateVersion: 1003, PrevUpdateVersion: 1002},
+								{UpdateVersion: 1004, PrevUpdateVersion: 1003},
+							},
+						},
+					},
+				},
+			},
+			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{
+				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
+				{UpdateVersion: 1004, PrevUpdateVersion: 1003},
+				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
+			},
+			oldMetadataIndexUpdates: []int64{999, 1000, 1001},
+			vizierStatus:            "HEALTHY",
+		},
+		{
+			name: "Receive update already sent to indexer and do nothing on restart",
+			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
+				{UpdateVersion: 1000, PrevUpdateVersion: 999},
+			},
+			missingMetadataCalls:    []*MetadataRequest{},
+			expectedIndexerUpdates:  []*metadatapb.ResourceUpdate{},
+			oldMetadataIndexUpdates: []int64{999, 1000, 1001},
+			vizierStatus:            "HEALTHY",
+		},
 	}
 
 	for _, test := range tests {
@@ -216,6 +308,19 @@ func TestMetadataReader_ProcessVizierUpdate(t *testing.T) {
 
 			_, sc, stanCleanup := testingutils.MustStartTestStan(t, "test-stan", "test-client")
 			defer stanCleanup()
+
+			// First we add  entries in the indexer stream. This ensures that when we init the Vizier
+			// update manager, we are peeking the bottom of the stream's queue.
+			for _, updateV := range test.oldMetadataIndexUpdates {
+				update := &metadatapb.ResourceUpdate{UpdateVersion: updateV}
+
+				b, err := update.Marshal()
+				require.NoError(t, err)
+
+				// Push the update in.
+				err = sc.Publish("MetadataIndex.test", b)
+				require.NoError(t, err)
+			}
 
 			idxCh := make(chan *stan.Msg)
 			indexerSub, err := sc.Subscribe("MetadataIndex.test", func(msg *stan.Msg) {
@@ -320,237 +425,6 @@ func TestMetadataReader_ProcessVizierUpdate(t *testing.T) {
 				}
 			}
 
-			wg.Wait()
-		})
-	}
-}
-
-func TestMetadataReader_RestartMetadataReader(t *testing.T) {
-	tests := []struct {
-		name                   string                       // Name of the test
-		stanMetadataUpdates    []*metadatapb.ResourceUpdate // The updates sent on STAN
-		missingMetadataCalls   []*MetadataRequest           // The expected metadata request and responses.
-		expectedIndexerUpdates []*metadatapb.ResourceUpdate // The updates that should be sent to the indexer.
-		vizierStatus           string
-	}{
-		{
-			name: "Metadata missing but k8s also missing the metadata",
-			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
-				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
-			},
-			missingMetadataCalls: []*MetadataRequest{
-				{
-					from: 1001,
-					to:   1003,
-					responses: []*metadatapb.MissingK8SMetadataResponse{
-						{
-							FirstUpdateAvailable: 1003,
-						},
-					},
-				},
-			},
-			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{
-				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
-			},
-			vizierStatus: "HEALTHY",
-		},
-		{
-			name: "Expect Full Metadata Update requests",
-			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
-				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
-			},
-			missingMetadataCalls: []*MetadataRequest{
-				{
-					from: 1001,
-					to:   1005,
-					responses: []*metadatapb.MissingK8SMetadataResponse{
-						{
-							FirstUpdateAvailable: 1001,
-							Updates: []*metadatapb.ResourceUpdate{
-								{UpdateVersion: 1001, PrevUpdateVersion: 1000},
-								{UpdateVersion: 1002, PrevUpdateVersion: 1001},
-								{UpdateVersion: 1003, PrevUpdateVersion: 1002},
-								{UpdateVersion: 1004, PrevUpdateVersion: 1003},
-							},
-						},
-					},
-				},
-			},
-			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{
-				{UpdateVersion: 1002, PrevUpdateVersion: 1001},
-				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
-				{UpdateVersion: 1004, PrevUpdateVersion: 1003},
-				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
-			},
-			vizierStatus: "HEALTHY",
-		},
-		{
-			name: "Expect Partial Metadata Update requests",
-			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
-				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
-			},
-			missingMetadataCalls: []*MetadataRequest{
-				{
-					from: 1001,
-					to:   1005,
-					responses: []*metadatapb.MissingK8SMetadataResponse{
-						{
-							FirstUpdateAvailable: 1003,
-							Updates: []*metadatapb.ResourceUpdate{
-								{UpdateVersion: 1003, PrevUpdateVersion: 1002},
-								{UpdateVersion: 1004, PrevUpdateVersion: 1003},
-							},
-						},
-					},
-				},
-			},
-			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{
-				{UpdateVersion: 1003, PrevUpdateVersion: 1002},
-				{UpdateVersion: 1004, PrevUpdateVersion: 1003},
-				{UpdateVersion: 1005, PrevUpdateVersion: 1004},
-			},
-			vizierStatus: "HEALTHY",
-		},
-		{
-			name: "Receive update already sent to indexer and do nothing",
-			stanMetadataUpdates: []*metadatapb.ResourceUpdate{
-				{UpdateVersion: 1000, PrevUpdateVersion: 999},
-			},
-			missingMetadataCalls:   []*MetadataRequest{},
-			expectedIndexerUpdates: []*metadatapb.ResourceUpdate{},
-			vizierStatus:           "HEALTHY",
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			vzID := uuid.Must(uuid.NewV4())
-			orgID := uuid.Must(uuid.NewV4())
-
-			mustLoadTestData(db)
-
-			// Set up initial DB state.
-			insertClusterQuery := `INSERT INTO vizier_cluster(id, org_id, cluster_uid) VALUES ($1, $2, 'test')`
-			db.MustExec(insertClusterQuery, vzID, orgID)
-			insertClusterInfoQuery := `INSERT INTO vizier_cluster_info(vizier_cluster_id, status) VALUES ($1, $2)`
-			db.MustExec(insertClusterInfoQuery, vzID, test.vizierStatus)
-
-			nc, natsCleanup := testingutils.MustStartTestNATS(t)
-			defer natsCleanup()
-
-			_, sc, stanCleanup := testingutils.MustStartTestStan(t, "test-stan", "test-client")
-			defer stanCleanup()
-
-			// First we add  3 entries 999-1001 in the indexer stream. This ensures that when we init the Vizier
-			// update manager, we are peeking the bottom of the stream's queue.
-			oldUpdates := []int64{999, 1000, 1001}
-			for _, updateV := range oldUpdates {
-				update := &metadatapb.ResourceUpdate{UpdateVersion: updateV}
-
-				b, err := update.Marshal()
-				require.NoError(t, err)
-
-				// Push the update in.
-				err = sc.Publish("MetadataIndex.test", b)
-				require.NoError(t, err)
-			}
-
-			idxCh := make(chan *stan.Msg)
-			indexerSub, err := sc.Subscribe("MetadataIndex.test", func(msg *stan.Msg) {
-				idxCh <- msg
-			})
-			if err != nil {
-				t.Fatalf("failed to subscribe to stan: %v", err)
-			}
-			defer func() {
-				err = indexerSub.Unsubscribe()
-				require.NoError(t, err)
-			}()
-
-			batch := 0
-			if test.missingMetadataCalls != nil {
-				mdSub, err := nc.Subscribe(vzshard.C2VTopic("MetadataRequest", vzID), func(msg *nats.Msg) {
-					c2vMsg := &cvmsgspb.C2VMessage{}
-					err := proto.Unmarshal(msg.Data, c2vMsg)
-					require.NoError(t, err)
-					req := &metadatapb.MissingK8SMetadataRequest{}
-					err = types.UnmarshalAny(c2vMsg.Msg, req)
-					require.NoError(t, err)
-					if len(test.missingMetadataCalls) <= batch {
-						assert.FailNow(t, "unexpected missingmetadatarequest call", req)
-					}
-					assert.Equal(t, test.missingMetadataCalls[batch].to, req.ToUpdateVersion)
-					assert.Equal(t, test.missingMetadataCalls[batch].from, req.FromUpdateVersion)
-
-					responseTopic := fmt.Sprintf("%s:%s", "MetadataResponse", req.CustomTopic)
-
-					// Send response.
-					for _, r := range test.missingMetadataCalls[batch].responses {
-						anyUpdates, err := types.MarshalAny(r)
-						require.NoError(t, err)
-						v2cMsg := cvmsgspb.V2CMessage{
-							Msg: anyUpdates,
-						}
-						b, err := v2cMsg.Marshal()
-						require.NoError(t, err)
-						err = nc.Publish(vzshard.V2CTopic(responseTopic, vzID), b)
-						require.NoError(t, err)
-					}
-
-					batch++
-				})
-				require.NoError(t, err)
-				defer func() {
-					err = mdSub.Unsubscribe()
-					require.NoError(t, err)
-				}()
-			}
-
-			mdr, err := controller.NewMetadataReader(db, sc, nc)
-			require.NoError(t, err)
-			defer mdr.Stop()
-
-			var wg sync.WaitGroup
-
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-
-				for _, update := range test.stanMetadataUpdates {
-					// Publish update to STAN channel.
-					anyInitUpdate, err := types.MarshalAny(update)
-					require.NoError(t, err)
-					v2cMsg := cvmsgspb.V2CMessage{
-						Msg: anyInitUpdate,
-					}
-					b, err := v2cMsg.Marshal()
-					require.NoError(t, err)
-
-					err = sc.Publish(vzshard.V2CTopic("DurableMetadataUpdates", vzID), b)
-					require.NoError(t, err)
-				}
-			}()
-
-			numUpdates := 0
-			for numUpdates < len(test.expectedIndexerUpdates) {
-				select {
-				case idxMessage := <-idxCh:
-					u := &metadatapb.ResourceUpdate{}
-					err := proto.Unmarshal(idxMessage.Data, u)
-					require.NoError(t, err)
-					numUpdates++
-				case <-time.After(2 * time.Second):
-					t.Fatal("Timed out")
-				}
-			}
-			run := true
-			for run {
-				select {
-				case <-idxCh:
-					t.Fatal("Unpexected index message")
-				case <-time.After(2 * time.Second):
-					run = false
-				}
-			}
 			wg.Wait()
 		})
 	}
