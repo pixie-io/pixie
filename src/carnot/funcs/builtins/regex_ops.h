accc914adfc91281ae9e4d926753a94fe535cf94
Hannah Stepanek <hstepanek@newrelic.com>
2021-10-04 09:56:01 -0700

    Add MatchRegexRule UDF and unit tests
    * Add MatchRegexRule UDF to regex_ops
    * Use rapidjson to parse stringified json into map
    * Return invalid argument status on json parse failure
    
    Signed-off-by: Hannah Stepanek <hstepanek@newrelic.com>

--- src/carnot/funcs/builtins/regex_ops.h
+++ src/carnot/funcs/builtins/regex_ops.h
@@ -22,6 +22,8 @@
 #include <algorithm>
 #include <memory>
 #include <string>
+#include <regex>
+#include <rapidjson/document.h>
 #include "re2/re2.h"
 #include "src/carnot/udf/registry.h"
 #include "src/common/base/utils.h"
@@ -108,6 +110,48 @@ class RegexReplaceUDF : public udf::ScalarUDF {
 
  private:
   std::unique_ptr<re2::RE2> regex_;
+};
+
+class MatchRegexRule : public udf::ScalarUDF {
+ public:
+  Status Init(FunctionContext*, StringValue encodedRegexRules) {
+    rapidjson::ParseResult ok = regex_rules.Parse(encodedRegexRules.data());
+    // TODO(zasgar/michellenguyen, PP-419): Replace with null when available.
+    if (ok == nullptr) {
+      return Status(statuspb::Code::INVALID_ARGUMENT, "unable to convert to json");
+    }
+    return Status::OK();
+  }
+  types::StringValue Exec(FunctionContext*, StringValue value) {
+    RegexMatchUDF regex_match; 
+    for (rapidjson::Value::ConstMemberIterator itr = regex_rules.MemberBegin(); itr != regex_rules.MemberEnd(); ++itr) {
+        auto name = itr->name.GetString();
+        PL_UNUSED(regex_match.Init(nullptr, itr->value.GetString()));
+        auto is_match = regex_match.Exec(nullptr, value).val;
+        if (is_match) {
+            return name; 
+        }
+    }
+    return "";
+  }
+
+  static udf::ScalarUDFDocBuilder Doc() {
+    return udf::ScalarUDFDocBuilder("Check for a match to a json of regex pattern rules in a string.")
+        .Details(
+            "This function checks the input string (second arg) for a match with the regex pattern rules"
+            "(first arg). "
+            "The regex pattern must match the full string. For example, the pattern 'abc' doesn't "
+            "match the string 'abcd' but the pattern 'abc*' does match that string. "
+            "We support google RE2 syntax. More details on that syntax can be found "
+            "[here](https://github.com/google/re2/wiki/Syntax). ")
+        .Example("df.is_match = px.regex_match('{\"rule1\": \".*my_regex_pattern.*\"}', df.resp_body)")
+        .Arg("arg1", "The encoded json of regex patterns to match.")
+        .Arg("arg2", "The string column to match the pattern against.")
+        .Returns("string representing the name of the first rule that matched or an empty string if no match.");
+  }
+
+ private:
+  rapidjson::Document regex_rules;
 };
 
 void RegisterRegexOpsOrDie(udf::Registry* registry);
