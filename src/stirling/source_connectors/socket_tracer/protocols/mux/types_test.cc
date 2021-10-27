fa92c212c314c0fca522273b8c633a383cd5c7f9
Dom Del Nano <ddelnano@gmail.com>
2021-10-26 22:17:26 -0700

    Use ASSERT_DEATH to test when GetMatchingRespType cannot match a request
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- src/stirling/source_connectors/socket_tracer/protocols/mux/types_test.cc
+++ src/stirling/source_connectors/socket_tracer/protocols/mux/types_test.cc
@@ -45,8 +45,7 @@ TEST(GetMatchingRespType, ReturnsCorrespondingResponseTypes) {
   ASSERT_OK_AND_EQ(GetMatchingRespType(Type::kTdrain), Type::kRdrain);
   ASSERT_OK_AND_EQ(GetMatchingRespType(Type::kTdispatch), Type::kRdispatch);
   ASSERT_OK_AND_EQ(GetMatchingRespType(Type::kTreq), Type::kRreq);
-  // TODO(ddelnano): This does not pass because of the LOG(DFATAL) statement in GetMatchingRespType
-  /* ASSERT_NOT_OK(GetMatchingRespType(static_cast<Type>(0))); */
+  ASSERT_DEATH([&] { GetMatchingRespType(static_cast<Type>(0)); }(), "Unexpected request type");
 }
 
 }  // namespace mux
