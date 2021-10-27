64f05b9a8a7f08bc38840cc4e110f5dfb3d0b36b
James Bartlett <jamesbartlett@pixielabs.ai>
2021-10-27 13:28:29 -0700

    [PP-3103] Use PL_DATA_ACCESS env var in data_privacy.
    Summary: Instead of pulling from the CRD directly, this diff changes data_privacy to use the environment variable that is templated into the qb yaml by the operator based on the CRD.
    
    Test Plan: Tried in skaffold. Can redact data by specifying 'Restricted' in the qb deployment yaml.
    
    Reviewers: philkuz, nserrino
    
    Reviewed By: philkuz
    
    JIRA Issues: PP-3103
    
    Signed-off-by: James Bartlett <jamesbartlett@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D10071
    
    GitOrigin-RevId: a75851a489bfa895cbdd1385efbe4ff9c49d719a

--- src/vizier/services/query_broker/controllers/data_privacy.go
+++ src/vizier/services/query_broker/controllers/data_privacy.go
@@ -20,48 +20,16 @@ package controllers
 
 import (
 	"context"
-	"errors"
+	"fmt"
 
-	log "github.com/sirupsen/logrus"
-	k8serr "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/rest"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	pixie "px.dev/pixie/src/operator/apis/px.dev/v1alpha1"
-	"px.dev/pixie/src/operator/client/versioned"
 )
 
-var (
-	// ErrVizierNotFound occurs when the k8s API does not return any Viziers because none exist
-	// or the viziers CRD is not defined.
-	ErrVizierNotFound = errors.New("Vizier CustomResource not found")
-)
-
-// getVizierCRD gets the Vizier CRD for the running Vizier, if running using an operator.
-func getVizierCRD(ns string) (*pixie.Vizier, error) {
-	kubeConfig, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	// Create k8s client.
-	vzCrdClient, err := versioned.NewForConfig(kubeConfig)
-	if err != nil {
-		log.WithError(err).Error("Failed to initialize vizier CRD client")
-		return nil, err
-	}
-
-	viziers, err := vzCrdClient.PxV1alpha1().Viziers(ns).List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		if k8serr.IsNotFound(err) {
-			return nil, ErrVizierNotFound
-		}
-		return nil, err
-	}
-	if len(viziers.Items) > 0 {
-		return &viziers.Items[0], nil
-	}
-	return nil, ErrVizierNotFound
+func init() {
+	pflag.String("data_access", "Full", "The data access level for queries. Options are 'Full' or 'Restricted'")
 }
 
 type vizierCachedDataPrivacy struct {
@@ -75,19 +43,12 @@ func (dp *vizierCachedDataPrivacy) ShouldRedactSensitiveColumns(ctx context.Cont
 
 // CreateDataPrivacyManager creates a privacy manager for the namespace.
 func CreateDataPrivacyManager(ns string) (DataPrivacy, error) {
-	vz, err := getVizierCRD(ns)
-	// If the vizier was not found, that means this cluster is not using the Operator version - we only provide Full DataAcces.
-	if err == ErrVizierNotFound {
-		return &vizierCachedDataPrivacy{
-			dataAccess: pixie.DataAccessFull,
-		}, nil
-	} else if err != nil {
-		log.WithError(err).Warn("Failure trying to getVizierCRD, starting in restricted mode")
-		return &vizierCachedDataPrivacy{
-			dataAccess: pixie.DataAccessRestricted,
-		}, nil
+	dataAccessStr := viper.GetString("data_access")
+	dataAccess := pixie.DataAccessLevel(dataAccessStr)
+	switch dataAccess {
+	case pixie.DataAccessFull, pixie.DataAccessRestricted:
+		return &vizierCachedDataPrivacy{dataAccess}, nil
+	default:
+		return nil, fmt.Errorf("Invalid DataAccess: '%s'", dataAccessStr)
 	}
-	return &vizierCachedDataPrivacy{
-		dataAccess: vz.Spec.DataAccess,
-	}, nil
 }
