1f9ab5ad08aaf3aa7783657ad163a2b4e7b8d6fa
Natalie Serrino <nserrino@pixielabs.ai>
2021-11-05 15:13:19 -0700

    Remove VizierHeartbeat from analytics events
    Summary: We don't sent this event anymore (and don't want to because it's TMI for analytics)
    
    Test Plan: n/a
    
    Reviewers: michelle, zasgar, vihang
    
    Reviewed By: michelle
    
    Signed-off-by: Natalie Serrino <nserrino@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D10116
    
    GitOrigin-RevId: b22c6d3bf55e75187253fbe6281f2ff4c56ed2cc

--- src/shared/services/events/events.go
+++ src/shared/services/events/events.go
@@ -34,8 +34,6 @@ const (
 	VizierCreated = "Vizier Created"
 	// VizierStatusChange is an event for when a Vizier cluster's status changes.
 	VizierStatusChange = "Vizier Status Change"
-	// VizierHeartbeat is an event for when Vizier sends a heartbeat to the cloud.
-	VizierHeartbeat = "Vizier Heartbeat"
 	// APIRequest is an event for when a request is made to the Pixie API using an API token.
 	APIRequest = "API Request"
 )
