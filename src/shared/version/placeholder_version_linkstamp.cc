fef2b10ca72e382e3c35e0416cc3dfe5911326ad
Zain Asgar <zasgar@pixielabs.ai>
2022-01-12 16:22:49 -0800

    Fix bazel caching issues with the version file
    Summary:
    This fixes bazel issues related to the use of the version file.
    We still need to do some cleanup where version information is pulled.
    
    Test Plan: Jenkins
    
    Reviewers: vihang, michelle, oazizi
    
    Reviewed By: vihang
    
    Signed-off-by: Zain Asgar <zasgar@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D10479
    
    GitOrigin-RevId: 3ee7f2196aad5dfd299aac0653ff0aa03a10f473

---
+++ src/shared/version/placeholder_version_linkstamp.cc
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018- The Pixie Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <cstdint>
+
+extern const char* kBuildSCMStatus;
+extern const char* kBuildSCMRevision;
+extern const int64_t kBuildTimeStamp;
+extern const char* kBuildSemver;
+extern const char* kBuildNumber;
+
+const char* kBuildSCMStatus = "Modified";
+const char* kBuildSCMRevision = "0000000";
+const int64_t kBuildTimeStamp = 0;       // UNIX TIMESTAMP seconds.
+const char* kBuildSemver = "0.0.0-dev";  // Semver string.
+const char* kBuildNumber = "0";          // Build number..
