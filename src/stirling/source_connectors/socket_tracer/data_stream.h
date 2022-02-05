pixie-io/pixie

commit cf6b471b2897a4b2bda1c482cb1969c180fe5b9d

New DataStreamBuffer Resync mechanism
Summary:
The current way of purging the old data in data_stream_buffer is based on stuck_count (which increments every iteration). This diff changes to a new cleanup mechanism, where frames and buffer in a data_stream is purged if:
1. Some time has passed since the last progress was made in the parser. The timeout for frames and data buffers are different and configurable.
2. The frames or data buffer has reached a size limit. The size limits are also configurable.

Test Plan: Updated old tests.

Reviewers: #stirling, oazizi

Reviewed By: #stirling, oazizi

Subscribers: oazizi, yzhao

Signed-off-by: Ryan Cheng <rcheng@pixielabs.ai>

Differential Revision: https://phab.corp.pixielabs.ai/D10644

GitOrigin-RevId: 976a6990db8a691b4a4f940c0407e443a20e1c1a

src/stirling/source_connectors/socket_tracer/data_stream.h
@@ -32,6 +32,9 @@ DECLARE_uint32(datastream_buffer_spike_size);
 DECLARE_uint32(datastream_buffer_max_gap_size);
 DECLARE_uint32(datastream_buffer_allow_before_gap_size);
 
+DECLARE_uint32(buffer_resync_duration_secs);
+DECLARE_uint32(buffer_expiration_duration_secs);
+
 namespace px {
 namespace stirling {
 
@@ -137,11 +140,25 @@ class DataStream : NotCopyMoveable {
                                     std::get<std::deque<TFrameType>>(frames_).empty());
   }
 
+  /**
+   * If buffer has not been successfully processed in the past kSyncTimeout duration,
+   * run ParseFrames() with a search for a new message boundary.
+   * A long stuck time implies there is something unparsable at the head. It is neither returning
+   * ParseState::kInvalid nor ParseState::kSuccess, but instead is returning
+   * ParseState::kNeedsMoreData.
+   * @return true if a resync will be performed in the next ParseFrames().
+   */
+  bool IsSyncRequired() const {
+    const auto kSyncTimeout = std::chrono::seconds(FLAGS_buffer_resync_duration_secs);
+
+    return (std::chrono::steady_clock::now() - last_progress_time_) >= kSyncTimeout;
+  }
+
   int stat_invalid_frames() const { return stat_invalid_frames_; }
   int stat_valid_frames() const { return stat_valid_frames_; }
   int stat_raw_data_gaps() const { return stat_raw_data_gaps_; }
 
-  void UpdateLastProgressTime() { last_progress_time_ = std::chrono::steady_clock::now(); }
+  void ResetLastProgressTimeToNow() { last_progress_time_ = std::chrono::steady_clock::now(); }
 
   /**
    * Fraction of frame parsing attempts that resulted in an invalid frame.
@@ -195,12 +212,12 @@ class DataStream : NotCopyMoveable {
                      std::chrono::time_point<std::chrono::steady_clock> expiry_timestamp) {
     // We are assuming that when this stream is stuck, we clear the data buffer.
     if (last_progress_time_ < expiry_timestamp) {
-      Reset();
+      data_buffer_.Reset();
+      has_new_events_ = false;
+      ResetLastProgressTimeToNow();
       return true;
     }
 
-    // If the size of the buffer after the processing loop is bigger than capacity, truncate the
-    // head.
     if (data_buffer_.size() > size_limit_bytes) {
       data_buffer_.RemovePrefix(data_buffer_.size() - size_limit_bytes);
     }
@@ -249,11 +266,9 @@ class DataStream : NotCopyMoveable {
   // changed.
   bool has_new_events_ = false;
 
-  // Number of consecutive calls to ProcessToRecords(), where there are a non-zero number of events,
-  // but no parsed messages are produced.
-  // Note: unlike the monotonic stats below, this resets when the stuck condition is cleared.
-  // Thus it is a state, not a statistic.
-  int stuck_count_ = 0;
+  // The timestamp when progress was last made in the data buffer. It's used in CleanupEvents().
+  std::chrono::time_point<std::chrono::steady_clock> last_progress_time_ =
+      std::chrono::steady_clock::now();
 
   // The timestamp when progress was last made in the data buffer. It's used in CleanupEvents().
   std::chrono::time_point<std::chrono::steady_clock> last_progress_time_ =

src/stirling/source_connectors/socket_tracer/data_stream.cc
@@ -34,6 +34,14 @@ DEFINE_uint32(datastream_buffer_allow_before_gap_size,
               "After a PL_DATASTREAM_BUFFER_MAX_GAP_SIZE gap occurs, we allow for this amount of "
               "data to come in before (byte position wise) the event that caused the large gap.");
 
+DEFINE_uint32(buffer_resync_duration_secs, 5,
+              "The duration, in seconds, after which a buffer resync will happen if there has been "
+              "no progress in the parser.");
+DEFINE_uint32(
+    buffer_expiration_duration_secs, 10,
+    "The duration, in seconds, after which the buffer will be cleared if there is no progress in "
+    "the parser.");
+
 namespace px {
 namespace stirling {
 
@@ -46,34 +54,6 @@ void DataStream::AddData(std::unique_ptr<SocketDataEvent> event) {
 
   has_new_events_ = true;
 }
-
-namespace {
-
-bool IsSyncRequired(int64_t stuck_count) {
-  ECHECK_GE(stuck_count, 0);
-
-  // Stuck counts where we switch the sync policy.
-  static constexpr int64_t kBasicSyncThreshold = 1;
-
-  // Thresholds must be in increasing order.
-  static_assert(kBasicSyncThreshold > 0);
-
-  if (stuck_count <= kBasicSyncThreshold) {
-    // If stuck_count == 0, then no reason to sync.
-    // If stuck_count != 0, but is low, it could mean we have partial data (i.e. kNeedsMoreData).
-    // The rest of the data could now be avilable in this new iteration,
-    // so still don't try to search for a message boundary yet.
-    return false;
-  }
-
-  // Multiple stuck cycles implies there is something unparseable at the head.
-  // It is neither returning ParseState::kInvalid nor ParseState::kSuccess.
-  // It constantly is returning ParseState::kNeedsMoreData.
-  // Run ParseFrames() with a search for a new message boundary;
-  return true;
-}
-
-}  // namespace
 
 // ProcessBytesToFrames() processes the raw data in the DataStream to extract parsed frames.
 //
@@ -99,8 +79,8 @@ void DataStream::ProcessBytesToFrames(message_type_t type, TStateType* state) {
 
   // A description of some key variables in this function:
   //
-  // - stuck_count_: Number of calls to where no new frames were produced.
-  //                 indicates an unparseable event at the head that is blocking progress.
+  // - last_progress_time_: The timestamp of when progress was made in the parser. It's used to
+  //                        calculate if we are stuck.
   //
   // - has_new_events_: An optimization to avoid the expensive call to ParseFrames() when
   //                    nothing has changed in the DataStream. Note that we *do* want to call
@@ -117,8 +97,8 @@ void DataStream::ProcessBytesToFrames(message_type_t type, TStateType* state) {
   //                 new events, because we have hit the threshold to attempt a stream recovery.
   //                 Used for the first iteration only.
 
-  // We appear to be stuck with an an unparseable sequence of events blocking the head.
-  bool attempt_sync = IsSyncRequired(stuck_count_);
+  // We appear to be stuck with an an unparsable sequence of events blocking the head.
+  bool attempt_sync = IsSyncRequired();
 
   bool keep_processing = has_new_events_ || attempt_sync;
 
@@ -130,18 +110,14 @@ void DataStream::ProcessBytesToFrames(message_type_t type, TStateType* state) {
     size_t contiguous_bytes = data_buffer_.Head().size();
 
     // Now parse the raw data.
-    parse_result = protocols::ParseFrames(type, data_buffer_, &typed_messages,
-                                          IsSyncRequired(stuck_count_), state);
-
+    parse_result =
+        protocols::ParseFrames(type, data_buffer_, &typed_messages, IsSyncRequired(), state);
     if (contiguous_bytes != data_buffer_.size()) {
       // We weren't able to submit all bytes, which means we ran into a missing event.
       // We don't expect missing events to arrive in the future, so just cut our losses.
       // Drop all events up to this point, and then try to resume.
       data_buffer_.RemovePrefix(contiguous_bytes);
       data_buffer_.Trim();
-
-      // Update stuck count so we use the correct sync type on the next iteration.
-      stuck_count_ = 0;
 
       keep_processing = (parse_result.state != ParseState::kEOS);
     } else {
@@ -150,7 +126,6 @@ void DataStream::ProcessBytesToFrames(message_type_t type, TStateType* state) {
       // If anything was processed at all, reset stuck count.
       if (parse_result.end_position != 0) {
         data_buffer_.RemovePrefix(parse_result.end_position);
-        stuck_count_ = 0;
       }
 
       keep_processing = false;
@@ -163,15 +138,13 @@ void DataStream::ProcessBytesToFrames(message_type_t type, TStateType* state) {
 
   // Check to see if we are blocked on parsing.
   // Note that missing events is handled separately (not considered stuck).
-  bool events_but_no_progress = !data_buffer_.empty() && (data_buffer_.position() == orig_pos);
-  if (events_but_no_progress) {
-    ++stuck_count_;
-  } else {
-    UpdateLastProgressTime();
+  bool made_progress = data_buffer_.empty() || (data_buffer_.position() != orig_pos);
+  if (made_progress) {
+    ResetLastProgressTimeToNow();
   }
 
   if (parse_result.state == ParseState::kEOS) {
-    ECHECK(!events_but_no_progress);
+    ECHECK(made_progress);
   }
 
   // If parse state is kInvalid, then no amount of waiting is going to help us.
@@ -182,8 +155,7 @@ void DataStream::ProcessBytesToFrames(message_type_t type, TStateType* state) {
 
     // TODO(oazizi): A dedicated data_buffer_.Flush() implementation would be more efficient.
     data_buffer_.RemovePrefix(data_buffer_.size());
-    stuck_count_ = 0;
-    UpdateLastProgressTime();
+    ResetLastProgressTimeToNow();
   }
 
   // Shrink the data buffer's allocated memory to fit just what is retained.
@@ -221,8 +193,7 @@ template void DataStream::ProcessBytesToFrames<protocols::nats::Message, protoco
 void DataStream::Reset() {
   data_buffer_.Reset();
   has_new_events_ = false;
-  stuck_count_ = 0;
-  UpdateLastProgressTime();
+  ResetLastProgressTimeToNow();
 
   frames_ = std::monostate();
 }

src/stirling/source_connectors/socket_tracer/data_stream_test.cc
@@ -25,6 +25,7 @@
 
 #include "src/common/testing/testing.h"
 #include "src/stirling/source_connectors/socket_tracer/testing/event_generator.h"
+#include "src/stirling/testing/common.h"
 
 namespace px {
 namespace stirling {
@@ -130,8 +131,9 @@ TEST_F(DataStreamTest, StuckTooLong) {
   stream.ProcessBytesToFrames<http::Message>(message_type_t::kRequest, &state);
   EXPECT_THAT(stream.Frames<http::Message>(), IsEmpty());
 
-  stream.ProcessBytesToFrames<http::Message>(message_type_t::kRequest, &state);
-  EXPECT_THAT(stream.Frames<http::Message>(), IsEmpty());
+  // Sleep for buffer_resync_duration_secs to a trigger resync starting from pos 1.
+  SET_TEST_FLAG(FLAGS_buffer_resync_duration_secs, 1);
+  sleep(FLAGS_buffer_resync_duration_secs);
 
   // Remaining data does not arrive in time, so stuck recovery has already removed req0a.
   // req0b will be noticed as invalid and cleared out as well.
@@ -227,11 +229,12 @@ TEST_F(DataStreamTest, LateArrivalPlusMissingEvents) {
   stream.ProcessBytesToFrames<http::Message>(message_type_t::kRequest, &state);
   ASSERT_THAT(stream.Frames<http::Message>(), IsEmpty());
 
-  stream.ProcessBytesToFrames<http::Message>(message_type_t::kRequest, &state);
-  ASSERT_THAT(stream.Frames<http::Message>(), IsEmpty());
+  // Setting buffer_expiry_timestamp to now to simulate a large delay.
+  int buffer_size_limit = 10000;
+  auto buffer_expiry_timestamp = std::chrono::steady_clock::now();
 
-  stream.ProcessBytesToFrames<http::Message>(message_type_t::kRequest, &state);
-  ASSERT_THAT(stream.Frames<http::Message>(), IsEmpty());
+  stream.CleanupEvents(buffer_size_limit, buffer_expiry_timestamp);
+  EXPECT_TRUE(stream.Empty<http::Message>());
 
   stream.AddData(std::move(req0b));
   stream.AddData(std::move(req1a));

commit 97451462cb5d96c9baf2909be3306d4a0c007bd9

Fix build error introduced in D10644.
Summary: D10644 accidentally introduced a compile error to main caused by a merge failure. This is the fix.

Test Plan: Existing test.

Reviewers: #stirling, oazizi

Reviewed By: #stirling, oazizi

Signed-off-by: Ryan Cheng <rcheng@pixielabs.ai>

Differential Revision: https://phab.corp.pixielabs.ai/D10689

GitOrigin-RevId: c8c17e475d70be92c2b43d24488766a42629d85a

src/stirling/source_connectors/socket_tracer/data_stream.h
@@ -270,10 +270,6 @@ class DataStream : NotCopyMoveable {
   std::chrono::time_point<std::chrono::steady_clock> last_progress_time_ =
       std::chrono::steady_clock::now();
 
-  // The timestamp when progress was last made in the data buffer. It's used in CleanupEvents().
-  std::chrono::time_point<std::chrono::steady_clock> last_progress_time_ =
-      std::chrono::steady_clock::now();
-
   // Keep some stats on ParseFrames() attempts.
   int stat_valid_frames_ = 0;
   int stat_invalid_frames_ = 0;