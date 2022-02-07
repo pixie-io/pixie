a52d547a67fb86a8a6170dc04f6cf603a3a6aaa8
Natalie Serrino <nserrino@pixielabs.ai>
2022-02-07 13:46:20 -0800

    Add GRPC service for load testing
    Summary: We want to be able to load test GRPC services. This creates a GRPC load tester that should match what we are doing with the HTTP load tester. For the 4 types of GRPC endpoint (unary, client streaming, server streaming, and bidirectional streaming) we have an endpoint that takes sequence IDs and payloads/bytes requested, and the server will return the requested volume of data.
    
    Test Plan: Ran it locally with bazel, need to more thoroughly test the streaming endpoints once I add the client.
    
    Reviewers: zasgar, vihang, jamesbartlett, oazizi, #third_party_approvers
    
    Reviewed By: zasgar, #third_party_approvers
    
    JIRA Issues: PP-3238
    
    Signed-off-by: Natalie Serrino <nserrino@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D10695
    
    GitOrigin-RevId: d0255aa9ba888da0cc31f893f1b14b0b789cf8b2

---
+++ src/e2e_test/grpc_loadtest/server/main.go
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2018- The Pixie Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+
+	"google.golang.org/grpc"
+
+	loadtestpb "px.dev/pixie/src/e2e_test/grpc_loadtest/server/loadtestpb"
+)
+
+type loadTestServer struct {
+}
+
+// Implements the Unary loadtest endpoint.
+func (lt *loadTestServer) Unary(ctx context.Context, req *loadtestpb.UnaryRequest) (*loadtestpb.UnaryReply, error) {
+	return &loadtestpb.UnaryReply{
+		SeqId:   req.SeqId,
+		Payload: strings.Repeat("u", int(req.BytesRequested)),
+	}, nil
+}
+
+// Implements the ClientStreaming loadtest endpoint.
+func (lt *loadTestServer) ClientStreaming(s loadtestpb.LoadTester_ClientStreamingServer) error {
+	seq := int64(0)
+	bytesRequested := 0
+
+	for {
+		select {
+		case <-s.Context().Done():
+			return nil
+		default:
+			msg, err := s.Recv()
+			if msg != nil {
+				bytesRequested = int(msg.BytesRequested)
+				seq = msg.SeqId
+			}
+			if err != nil && err == io.EOF {
+				return s.SendAndClose(&loadtestpb.ClientStreamingReply{
+					SeqId:   seq,
+					Payload: strings.Repeat("c", bytesRequested),
+				})
+			}
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+// Implements the ServerStreaming loadtest endpoint.
+func (lt *loadTestServer) ServerStreaming(req *loadtestpb.ServerStreamingRequest, s loadtestpb.LoadTester_ServerStreamingServer) error {
+	payload := strings.Repeat("s", int(req.BytesRequestedPerMessage))
+	for i := int32(0); i < req.MessagesRequested; i++ {
+		resp := &loadtestpb.ServerStreamingReply{
+			SeqId:       req.SeqId,
+			StreamSeqId: int64(i),
+			Payload:     payload,
+		}
+		if err := s.Send(resp); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Implements the BidirectionalStreaming loadtest endpoint.
+func (lt *loadTestServer) BidirectionalStreaming(s loadtestpb.LoadTester_BidirectionalStreamingServer) error {
+	msgsSent := int64(0)
+	for {
+		in, err := s.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		resp := &loadtestpb.BidirectionalStreamingReply{
+			SeqId:             in.SeqId,
+			StreamSeqIdServer: msgsSent,
+			Payload:           strings.Repeat("b", int(in.BytesRequestedNextMessage)),
+		}
+		msgsSent++
+		if err := s.Send(resp); err != nil {
+			return err
+		}
+	}
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		panic(fmt.Sprintf("failed to listen: %v", err))
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	loadtestpb.RegisterLoadTesterServer(grpcServer, &loadTestServer{})
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		panic(err)
+	}
+}
