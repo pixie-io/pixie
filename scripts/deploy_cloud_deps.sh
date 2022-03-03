7f5508bab43cc6d278082466fc0c78f0be8ec8fe
Vihang Mehta <vihang@pixielabs.ai>
2022-03-03 10:24:28 -0800

    Fix yamls to run elastic operator in elastic-system
    Summary:
    The elastic operator should run in it's own namespace. Pull it out of
    the kustomize and apply elastic yamls explicitly to make this clear.
    
    Test Plan:
    Deploy dev cloud. All the deps including the elastic operator and the
    elastic cluster run as expected.
    
    Reviewers: michelle, nserrino
    
    Reviewed By: michelle
    
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D10926
    
    GitOrigin-RevId: bc3f2a9d17f4cfe24867394e442afab11434d8bb

--- scripts/deploy_cloud_deps.sh
+++ scripts/deploy_cloud_deps.sh
@@ -29,6 +29,7 @@ source "${workspace}"/scripts/script_utils.sh
 
 cloud_deps_deploy() {
   # Can't use kubectl -k because bundled kustomize is v2.0.3 as of kubectl v1.16, vs kustomize which is v3.5.4
+  kustomize build "${workspace}"/k8s/cloud_deps/base/elastic/operator | kubectl apply -f -
   kustomize build "${workspace}"/k8s/cloud_deps/"${deploy_env}" | kubectl apply -f -
 }
 
