e54f37fe269cda6637529638aded0d78a07b1d97
Michelle Nguyen <michellenguyen@pixielabs.ai>
2022-03-24 18:52:55 -0700

    PC-1462  Handle CronScriptUpdates in querybroker
    Summary:
    As users add/remove cronscripts in the cloud, this will trigger updates to individual viziers.
    The querybroker is responsible for keeping the running scripts up-to-date. this diff handles those incoming messages, and instantiates a runner class which is reponsible for running the scripts at the specified frequency
    
    Test Plan: Unit tests
    
    Reviewers: vihang, nserrino, philkuz
    
    Reviewed By: vihang
    
    JIRA Issues: PC-1462
    
    Signed-off-by: Michelle Nguyen <michellenguyen@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11053
    
    GitOrigin-RevId: 8dac1af6e2e54fb5eb8bc8329c61abd03d341ce4

--- src/vizier/services/query_broker/script_runner/script_runner.go
+++ src/vizier/services/query_broker/script_runner/script_runner.go
@@ -34,6 +34,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"px.dev/pixie/src/shared/cvmsgspb"
+	"px.dev/pixie/src/utils"
 	"px.dev/pixie/src/vizier/services/metadata/metadatapb"
 	"px.dev/pixie/src/vizier/utils/messagebus"
 )
@@ -47,26 +48,45 @@ var (
 	GetCronScriptsRequestChannel = messagebus.V2CTopic("GetCronScriptsRequest")
 	// GetCronScriptsResponseChannel is the NATS channel that script responses are published to.
 	GetCronScriptsResponseChannel = messagebus.C2VTopic("GetCronScriptsResponse")
-	natsWaitTimeout               = 2 * time.Minute
+	// CronScriptUpdatesChannel is the NATS channel that any cron script updates are published to.
+	CronScriptUpdatesChannel = messagebus.C2VTopic("CronScriptsUpdates")
+	// CronScriptUpdatesResponseChannel is the NATS channel that script updates are published to.
+	CronScriptUpdatesResponseChannel = messagebus.V2CTopic("CronScriptsUpdatesResponse")
+	natsWaitTimeout                  = 2 * time.Minute
 )
 
 // ScriptRunner tracks registered cron scripts and runs them according to schedule.
 type ScriptRunner struct {
-	nc       *nats.Conn
-	csClient metadatapb.CronScriptStoreServiceClient
+	nc          *nats.Conn
+	csClient    metadatapb.CronScriptStoreServiceClient
+	runnerMap   map[uuid.UUID]*runner
+	runnerMapMu sync.Mutex
 
 	done chan struct{}
 	once sync.Once
+
+	updatesCh  chan *nats.Msg
+	updatesSub *nats.Subscription
 }
 
 // New creates a new script runner.
-func New(nc *nats.Conn, csClient metadatapb.CronScriptStoreServiceClient) *ScriptRunner {
-	return &ScriptRunner{nc: nc, csClient: csClient, done: make(chan struct{})}
+func New(nc *nats.Conn, csClient metadatapb.CronScriptStoreServiceClient) (*ScriptRunner, error) {
+	updatesCh := make(chan *nats.Msg, 4096)
+	sub, err := nc.ChanSubscribe(CronScriptUpdatesChannel, updatesCh)
+	if err != nil {
+		log.WithError(err).Error("Failed to listen for cron script updates")
+		return nil, err
+	}
+
+	sr := &ScriptRunner{nc: nc, csClient: csClient, done: make(chan struct{}), updatesCh: updatesCh, updatesSub: sub, runnerMap: make(map[uuid.UUID]*runner)}
+	return sr, nil
 }
 
 // Stop performs any necessary cleanup before shutdown.
 func (s *ScriptRunner) Stop() {
 	s.once.Do(func() {
+		close(s.updatesCh)
+		s.updatesSub.Unsubscribe()
 		close(s.done)
 	})
 }
@@ -95,15 +115,136 @@ func (s *ScriptRunner) SyncScripts() error {
 			log.WithError(err).Error("Failed to fetch scripts from cloud")
 			return err
 		}
-		scripts = cloudScripts
-		_, err = s.csClient.SetScripts(context.Background(), &metadatapb.SetScriptsRequest{Scripts: cloudScripts})
+		// Clear out persisted scripts.
+		_, err = s.csClient.SetScripts(context.Background(), &metadatapb.SetScriptsRequest{Scripts: make(map[string]*cvmsgspb.CronScript)})
 		if err != nil {
-			log.WithError(err).Error("Failed to persist current cron scripts")
+			log.WithError(err).Error("Failed to delete scripts from store")
+			return err
+		}
+		scripts = cloudScripts
+	}
+
+	// Add runners.
+	for k, v := range scripts {
+		err = s.upsertScript(uuid.FromStringOrNil(k), v)
+		if err != nil {
+			log.WithError(err).Error("Failed to upsert script, skipping...")
 		}
 	}
-	_ = scripts
+	go s.processUpdates()
+	return nil
+}
 
-	// Listen for changes to any new scripts.
+func (s *ScriptRunner) processUpdates() {
+	for {
+		select {
+		case <-s.done:
+			return
+		case msg := <-s.updatesCh:
+			c2vMsg := &cvmsgspb.C2VMessage{}
+			err := proto.Unmarshal(msg.Data, c2vMsg)
+			if err != nil {
+				log.WithError(err).Error("Failed to unmarshal c2v message")
+				continue
+			}
+			resp := &cvmsgspb.CronScriptUpdate{}
+			err = types.UnmarshalAny(c2vMsg.Msg, resp)
+			if err != nil {
+				log.WithError(err).Error("Failed to unmarshal c2v message")
+				continue
+			}
+			switch resp.Msg.(type) {
+			case *cvmsgspb.CronScriptUpdate_UpsertReq:
+				uResp := resp.GetUpsertReq()
+				err := s.upsertScript(utils.UUIDFromProtoOrNil(uResp.Script.ID), uResp.Script)
+				if err != nil {
+					log.WithError(err).Error("Failed to upsert script")
+				}
+				// Send response.
+				r := &cvmsgspb.RegisterOrUpdateCronScriptResponse{}
+				reqAnyMsg, err := types.MarshalAny(r)
+				if err != nil {
+					log.WithError(err).Error("Failed to marshal update script response")
+					continue
+				}
+				v2cMsg := cvmsgspb.V2CMessage{
+					Msg: reqAnyMsg,
+				}
+				// Publish request.
+				b, err := v2cMsg.Marshal()
+				if err != nil {
+					log.WithError(err).Error("Failed to marshal update script response")
+					continue
+				}
+				err = s.nc.Publish(fmt.Sprintf("%s:%s", CronScriptUpdatesResponseChannel, resp.RequestID), b)
+				if err != nil {
+					log.WithError(err).Error("Failed to publish update script response")
+				}
+			case *cvmsgspb.CronScriptUpdate_DeleteReq:
+				dResp := resp.GetDeleteReq()
+				err := s.deleteScript(utils.UUIDFromProtoOrNil(dResp.ScriptID))
+				if err != nil {
+					log.WithError(err).Error("Failed to delete script")
+				}
+				// Send response.
+				r := &cvmsgspb.DeleteCronScriptResponse{}
+				reqAnyMsg, err := types.MarshalAny(r)
+				if err != nil {
+					log.WithError(err).Error("Failed to marshal update script response")
+					continue
+				}
+				v2cMsg := cvmsgspb.V2CMessage{
+					Msg: reqAnyMsg,
+				}
+				// Publish request.
+				b, err := v2cMsg.Marshal()
+				if err != nil {
+					log.WithError(err).Error("Failed to marshal update script response")
+					continue
+				}
+				err = s.nc.Publish(fmt.Sprintf("%s:%s", CronScriptUpdatesResponseChannel, resp.RequestID), b)
+				if err != nil {
+					log.WithError(err).Error("Failed to publish update script response")
+				}
+			default:
+				log.Error("Received unknown message for cronScriptUpdate")
+			}
+		}
+	}
+}
+
+func (s *ScriptRunner) upsertScript(id uuid.UUID, script *cvmsgspb.CronScript) error {
+	s.runnerMapMu.Lock()
+	defer s.runnerMapMu.Unlock()
+
+	if v, ok := s.runnerMap[id]; ok {
+		v.stop()
+		delete(s.runnerMap, id)
+	}
+	r := newRunner(script)
+	s.runnerMap[id] = r
+	go r.start()
+	_, err := s.csClient.AddOrUpdateScript(context.Background(), &metadatapb.AddOrUpdateScriptRequest{Script: script})
+	if err != nil {
+		log.WithError(err).Error("Failed to upsert script in metadata")
+	}
+
+	return nil
+}
+
+func (s *ScriptRunner) deleteScript(id uuid.UUID) error {
+	s.runnerMapMu.Lock()
+	defer s.runnerMapMu.Unlock()
+	v, ok := s.runnerMap[id]
+	if !ok {
+		return nil
+	}
+	v.stop()
+	delete(s.runnerMap, id)
+	_, err := s.csClient.DeleteScript(context.Background(), &metadatapb.DeleteScriptRequest{ScriptID: utils.ProtoFromUUID(id)})
+	if err != nil {
+		log.WithError(err).Error("Failed to delete script from metadata")
+	}
 
 	return nil
 }
@@ -217,4 +358,27 @@ func checksumFromScriptMap(scripts map[string]*cvmsgspb.CronScript) (string, err
 	h.Write([]byte(scriptStr))
 
 	return string(h.Sum(nil)), nil
+}
+
+// Logic for "runners" which handle the script execution.
+type runner struct {
+	cronScript *cvmsgspb.CronScript
+
+	done chan struct{}
+	once sync.Once
+}
+
+func newRunner(script *cvmsgspb.CronScript) *runner {
+	return &runner{
+		cronScript: script, done: make(chan struct{}),
+	}
+}
+
+func (r *runner) start() {
+}
+
+func (r *runner) stop() {
+	r.once.Do(func() {
+		close(r.done)
+	})
 }
--- src/vizier/services/query_broker/script_runner/script_runner_test.go
+++ src/vizier/services/query_broker/script_runner/script_runner_test.go
@@ -19,18 +19,23 @@
 package scriptrunner
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"testing"
 
+	"github.com/gofrs/uuid"
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/types"
 	"github.com/nats-io/nats.go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 
 	"px.dev/pixie/src/shared/cvmsgspb"
 	"px.dev/pixie/src/utils"
 	"px.dev/pixie/src/utils/testingutils"
+	"px.dev/pixie/src/vizier/services/metadata/metadatapb"
 )
 
 func TestScriptRunner_CompareScriptState(t *testing.T) {
@@ -143,7 +148,8 @@ func TestScriptRunner_CompareScriptState(t *testing.T) {
 			nc, natsCleanup := testingutils.MustStartTestNATS(t)
 			defer natsCleanup()
 
-			sr := New(nc, nil)
+			sr, err := New(nc, &fakeCronStore{scripts: map[uuid.UUID]*cvmsgspb.CronScript{}})
+			require.NoError(t, err)
 
 			// Subscribe to request channel.
 			mdSub, err := nc.Subscribe(CronScriptChecksumRequestChannel, func(msg *nats.Msg) {
@@ -187,7 +193,8 @@ func TestScriptRunner_GetCloudScripts(t *testing.T) {
 	nc, natsCleanup := testingutils.MustStartTestNATS(t)
 	defer natsCleanup()
 
-	sr := New(nc, nil)
+	sr, err := New(nc, nil)
+	require.NoError(t, err)
 
 	scripts := map[string]*cvmsgspb.CronScript{
 		"223e4567-e89b-12d3-a456-426655440000": &cvmsgspb.CronScript{
@@ -236,4 +243,413 @@ func TestScriptRunner_GetCloudScripts(t *testing.T) {
 	resp, err := sr.getCloudScripts()
 	require.Nil(t, err)
 	assert.Equal(t, scripts, resp)
+}
+
+type fakeCronStore struct {
+	scripts map[uuid.UUID]*cvmsgspb.CronScript
+}
+
+// GetScripts fetches all scripts in the cron script store.
+func (s *fakeCronStore) GetScripts(ctx context.Context, req *metadatapb.GetScriptsRequest, opts ...grpc.CallOption) (*metadatapb.GetScriptsResponse, error) {
+	m := make(map[string]*cvmsgspb.CronScript)
+	for k, v := range s.scripts {
+		m[k.String()] = v
+	}
+
+	return &metadatapb.GetScriptsResponse{
+		Scripts: m,
+	}, nil
+}
+
+// AddOrUpdateScript updates or adds a cron script to the store, based on ID.
+func (s *fakeCronStore) AddOrUpdateScript(ctx context.Context, req *metadatapb.AddOrUpdateScriptRequest, opts ...grpc.CallOption) (*metadatapb.AddOrUpdateScriptResponse, error) {
+	s.scripts[utils.UUIDFromProtoOrNil(req.Script.ID)] = req.Script
+
+	return &metadatapb.AddOrUpdateScriptResponse{}, nil
+}
+
+// DeleteScript deletes a cron script from the store by ID.
+func (s *fakeCronStore) DeleteScript(ctx context.Context, req *metadatapb.DeleteScriptRequest, opts ...grpc.CallOption) (*metadatapb.DeleteScriptResponse, error) {
+	_, ok := s.scripts[utils.UUIDFromProtoOrNil(req.ScriptID)]
+	if ok {
+		delete(s.scripts, utils.UUIDFromProtoOrNil(req.ScriptID))
+	}
+
+	return &metadatapb.DeleteScriptResponse{}, nil
+}
+
+// SetScripts sets the list of all cron scripts to match the given set of scripts.
+func (s *fakeCronStore) SetScripts(ctx context.Context, req *metadatapb.SetScriptsRequest, opts ...grpc.CallOption) (*metadatapb.SetScriptsResponse, error) {
+	m := make(map[uuid.UUID]*cvmsgspb.CronScript)
+	for k, v := range req.Scripts {
+		m[uuid.FromStringOrNil(k)] = v
+	}
+	s.scripts = m
+
+	return &metadatapb.SetScriptsResponse{}, nil
+}
+
+func TestScriptRunner_SyncScripts(t *testing.T) {
+	tests := []struct {
+		name             string
+		persistedScripts map[string]*cvmsgspb.CronScript
+		cloudScripts     map[string]*cvmsgspb.CronScript
+		updates          []*cvmsgspb.CronScriptUpdate
+		expectedScripts  map[string]*cvmsgspb.CronScript
+	}{
+		{
+			name: "initial match",
+			persistedScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440000": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440000"),
+					Script:     "px.display()",
+					Configs:    "config1",
+					FrequencyS: 5,
+				},
+				"223e4567-e89b-12d3-a456-426655440001": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+					Script:     "test script",
+					Configs:    "config2",
+					FrequencyS: 22,
+				},
+			},
+			cloudScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440000": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440000"),
+					Script:     "px.display()",
+					Configs:    "config1",
+					FrequencyS: 5,
+				},
+				"223e4567-e89b-12d3-a456-426655440001": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+					Script:     "test script",
+					Configs:    "config2",
+					FrequencyS: 22,
+				},
+			},
+			updates: []*cvmsgspb.CronScriptUpdate{
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_UpsertReq{
+						UpsertReq: &cvmsgspb.RegisterOrUpdateCronScriptRequest{
+							Script: &cvmsgspb.CronScript{
+								ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+								Script:     "test script 2",
+								Configs:    "config3",
+								FrequencyS: 123,
+							},
+						},
+					},
+					RequestID: "1",
+				},
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_DeleteReq{
+						DeleteReq: &cvmsgspb.DeleteCronScriptRequest{
+							ScriptID: utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+						},
+					},
+					RequestID: "2",
+				},
+			},
+			expectedScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440000": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440000"),
+					Script:     "px.display()",
+					Configs:    "config1",
+					FrequencyS: 5,
+				},
+				"223e4567-e89b-12d3-a456-426655440002": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+					Script:     "test script 2",
+					Configs:    "config3",
+					FrequencyS: 123,
+				},
+			},
+		},
+		{
+			name: "initial mismatch",
+			persistedScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440000": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440000"),
+					Script:     "px.display()",
+					Configs:    "config1",
+					FrequencyS: 5,
+				},
+				"223e4567-e89b-12d3-a456-426655440001": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+					Script:     "test script",
+					Configs:    "config2",
+					FrequencyS: 22,
+				},
+			},
+			cloudScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440001": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+					Script:     "test script",
+					Configs:    "config2",
+					FrequencyS: 22,
+				},
+			},
+			updates: []*cvmsgspb.CronScriptUpdate{
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_UpsertReq{
+						UpsertReq: &cvmsgspb.RegisterOrUpdateCronScriptRequest{
+							Script: &cvmsgspb.CronScript{
+								ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+								Script:     "test script 2",
+								Configs:    "config3",
+								FrequencyS: 123,
+							},
+						},
+					},
+					RequestID: "1",
+				},
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_DeleteReq{
+						DeleteReq: &cvmsgspb.DeleteCronScriptRequest{
+							ScriptID: utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+						},
+					},
+					RequestID: "2",
+				},
+			},
+			expectedScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440002": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+					Script:     "test script 2",
+					Configs:    "config3",
+					FrequencyS: 123,
+				},
+			},
+		},
+		{
+			name:             "persisted empty",
+			persistedScripts: map[string]*cvmsgspb.CronScript{},
+			cloudScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440001": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+					Script:     "test script",
+					Configs:    "config2",
+					FrequencyS: 22,
+				},
+			},
+			updates: []*cvmsgspb.CronScriptUpdate{
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_UpsertReq{
+						UpsertReq: &cvmsgspb.RegisterOrUpdateCronScriptRequest{
+							Script: &cvmsgspb.CronScript{
+								ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+								Script:     "test script 2",
+								Configs:    "config3",
+								FrequencyS: 123,
+							},
+						},
+					},
+					RequestID: "1",
+				},
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_DeleteReq{
+						DeleteReq: &cvmsgspb.DeleteCronScriptRequest{
+							ScriptID: utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440003"),
+						},
+					},
+					RequestID: "2",
+				},
+			},
+			expectedScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440001": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440001"),
+					Script:     "test script",
+					Configs:    "config2",
+					FrequencyS: 22,
+				},
+				"223e4567-e89b-12d3-a456-426655440002": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+					Script:     "test script 2",
+					Configs:    "config3",
+					FrequencyS: 123,
+				},
+			},
+		},
+		{
+			name:             "cloud empty",
+			persistedScripts: map[string]*cvmsgspb.CronScript{},
+			cloudScripts:     map[string]*cvmsgspb.CronScript{},
+			updates: []*cvmsgspb.CronScriptUpdate{
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_UpsertReq{
+						UpsertReq: &cvmsgspb.RegisterOrUpdateCronScriptRequest{
+							Script: &cvmsgspb.CronScript{
+								ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+								Script:     "test script 2",
+								Configs:    "config3",
+								FrequencyS: 123,
+							},
+						},
+					},
+					RequestID: "1",
+				},
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_UpsertReq{
+						UpsertReq: &cvmsgspb.RegisterOrUpdateCronScriptRequest{
+							Script: &cvmsgspb.CronScript{
+								ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440003"),
+								Script:     "test script 2",
+								Configs:    "config3",
+								FrequencyS: 123,
+							},
+						},
+					},
+					RequestID: "2",
+				},
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_UpsertReq{
+						UpsertReq: &cvmsgspb.RegisterOrUpdateCronScriptRequest{
+							Script: &cvmsgspb.CronScript{
+								ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440004"),
+								Script:     "test script 2",
+								Configs:    "config3",
+								FrequencyS: 123,
+							},
+						},
+					},
+					RequestID: "3",
+				},
+				&cvmsgspb.CronScriptUpdate{
+					Msg: &cvmsgspb.CronScriptUpdate_DeleteReq{
+						DeleteReq: &cvmsgspb.DeleteCronScriptRequest{
+							ScriptID: utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440003"),
+						},
+					},
+					RequestID: "4",
+				},
+			},
+			expectedScripts: map[string]*cvmsgspb.CronScript{
+				"223e4567-e89b-12d3-a456-426655440002": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440002"),
+					Script:     "test script 2",
+					Configs:    "config3",
+					FrequencyS: 123,
+				},
+				"223e4567-e89b-12d3-a456-426655440004": &cvmsgspb.CronScript{
+					ID:         utils.ProtoFromUUIDStrOrNil("223e4567-e89b-12d3-a456-426655440004"),
+					Script:     "test script 2",
+					Configs:    "config3",
+					FrequencyS: 123,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			nc, natsCleanup := testingutils.MustStartTestNATS(t)
+			defer natsCleanup()
+
+			initialScripts := make(map[uuid.UUID]*cvmsgspb.CronScript)
+			for k, v := range test.persistedScripts {
+				initialScripts[uuid.FromStringOrNil(k)] = v
+			}
+
+			fcs := &fakeCronStore{scripts: initialScripts}
+			sr, err := New(nc, fcs)
+			require.NoError(t, err)
+
+			var wg sync.WaitGroup
+			wg.Add(len(test.updates))
+
+			// Subscribe to request channel.
+			mdSub, err := nc.Subscribe(CronScriptChecksumRequestChannel, func(msg *nats.Msg) {
+				v2cMsg := &cvmsgspb.V2CMessage{}
+				err := proto.Unmarshal(msg.Data, v2cMsg)
+				require.NoError(t, err)
+				req := &cvmsgspb.GetCronScriptsChecksumRequest{}
+				err = types.UnmarshalAny(v2cMsg.Msg, req)
+				require.NoError(t, err)
+				topic := req.Topic
+				checksum, err := checksumFromScriptMap(test.cloudScripts)
+				require.NoError(t, err)
+				resp := &cvmsgspb.GetCronScriptsChecksumResponse{
+					Checksum: checksum,
+				}
+				anyMsg, err := types.MarshalAny(resp)
+				require.NoError(t, err)
+				c2vMsg := cvmsgspb.C2VMessage{
+					Msg: anyMsg,
+				}
+				b, err := c2vMsg.Marshal()
+				require.NoError(t, err)
+
+				err = nc.Publish(fmt.Sprintf("%s:%s", CronScriptChecksumResponseChannel, topic), b)
+				require.NoError(t, err)
+			})
+			defer func() {
+				err = mdSub.Unsubscribe()
+				require.NoError(t, err)
+			}()
+
+			md2Sub, err := nc.Subscribe(GetCronScriptsRequestChannel, func(msg *nats.Msg) {
+				v2cMsg := &cvmsgspb.V2CMessage{}
+				err := proto.Unmarshal(msg.Data, v2cMsg)
+				require.NoError(t, err)
+				req := &cvmsgspb.GetCronScriptsRequest{}
+				err = types.UnmarshalAny(v2cMsg.Msg, req)
+				require.NoError(t, err)
+				topic := req.Topic
+
+				resp := &cvmsgspb.GetCronScriptsResponse{
+					Scripts: test.cloudScripts,
+				}
+				anyMsg, err := types.MarshalAny(resp)
+				require.NoError(t, err)
+				c2vMsg := cvmsgspb.C2VMessage{
+					Msg: anyMsg,
+				}
+				b, err := c2vMsg.Marshal()
+				require.NoError(t, err)
+
+				err = nc.Publish(fmt.Sprintf("%s:%s", GetCronScriptsResponseChannel, topic), b)
+				require.NoError(t, err)
+			})
+			defer func() {
+				err = md2Sub.Unsubscribe()
+				require.NoError(t, err)
+			}()
+
+			for _, u := range test.updates {
+				md3Sub, err := nc.Subscribe(fmt.Sprintf("%s:%s", CronScriptUpdatesResponseChannel, u.RequestID), func(msg *nats.Msg) {
+					wg.Done()
+				})
+				defer func() {
+					err = md3Sub.Unsubscribe()
+					require.NoError(t, err)
+				}()
+
+				anyMsg, err := types.MarshalAny(u)
+				require.NoError(t, err)
+				c2vMsg := cvmsgspb.C2VMessage{
+					Msg: anyMsg,
+				}
+				b, err := c2vMsg.Marshal()
+				require.NoError(t, err)
+				err = nc.Publish(CronScriptUpdatesChannel, b)
+			}
+
+			err = sr.SyncScripts()
+			wg.Wait()
+			require.NoError(t, err)
+			assert.Equal(t, len(test.expectedScripts), len(fcs.scripts))
+			for k, v := range test.expectedScripts {
+				val, ok := fcs.scripts[uuid.FromStringOrNil(k)]
+				assert.True(t, ok)
+				assert.Equal(t, v, val)
+			}
+
+			assert.Equal(t, len(test.expectedScripts), len(sr.runnerMap))
+			for k, v := range test.expectedScripts {
+				val, ok := sr.runnerMap[uuid.FromStringOrNil(k)]
+				assert.True(t, ok)
+				assert.Equal(t, v, val.cronScript)
+			}
+		})
+	}
 }
