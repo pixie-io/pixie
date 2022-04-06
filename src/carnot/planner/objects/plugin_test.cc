51db6a606055a489229a5d7199ce9aa905c1cc12
Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
2022-04-06 14:59:14 -0700

    PC-1488: Create plugin module in PxL with start_time attribute
    Summary:
    Plugin module in PxL allows users to leave the start_time specification to the plugin.
    That way we can hide plugin writers can hide the impl details from the user, making a better
    overall experience.
    
    Test Plan: Added a test.
    
    Reviewers: michelle, vihang, jamesbartlett
    
    Reviewed By: michelle
    
    JIRA Issues: PC-1488
    
    Signed-off-by: Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11166
    
    GitOrigin-RevId: a67204c75e46be4a8dde0baddff0ae76fbe9bb22

---
+++ src/carnot/planner/objects/plugin_test.cc
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018- The Pixie Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <absl/strings/ascii.h>
+#include <gtest/gtest.h>
+#include <memory>
+
+#include <absl/container/flat_hash_map.h>
+
+#include "src/carnot/planner/objects/plugin.h"
+// #include "src/carnot/planner/objects/dataframe.h"
+// #include "src/carnot/planner/objects/exporter.h"
+// #include "src/carnot/planner/objects/otel.h"
+#include "src/carnot/planner/objects/test_utils.h"
+// #include "src/carnot/planner/objects/var_table.h"
+
+namespace px {
+namespace carnot {
+namespace planner {
+namespace compiler {
+
+class PluginTest : public QLObjectTest {};
+
+TEST_F(PluginTest, get_start_time) {
+  PluginConfig plugin_config{1234};
+
+  ASSERT_OK_AND_ASSIGN(auto plugin,
+                       PluginModule::Create(&plugin_config, ast_visitor.get(), graph.get()));
+  var_table->Add("plugin", plugin);
+
+  ASSERT_OK_AND_ASSIGN(auto start_time_ns, ParseExpression("plugin.start_time"));
+  auto start_time_expr = static_cast<ExprObject*>(start_time_ns.get());
+  EXPECT_EQ(static_cast<TimeIR*>(start_time_expr->expr())->val(), 1234);
+}
+
+TEST_F(PluginTest, null_plugin_config_throws_compiler_error) {
+  ASSERT_OK_AND_ASSIGN(auto plugin, PluginModule::Create(nullptr, ast_visitor.get(), graph.get()));
+  var_table->Add("plugin", plugin);
+
+  EXPECT_THAT(
+      ParseExpression("plugin.start_time").status(),
+      HasCompilerError("No plugin config found. Make sure the script is run in a plugin context."));
+}
+
+}  // namespace compiler
+}  // namespace planner
+}  // namespace carnot
+}  // namespace px
