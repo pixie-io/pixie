d3c884f5512e85c77badc7c6de978000fadd73fa
Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
2022-04-14 10:36:46 -0700

    Fix default arguments test
    Summary: tsia, broke with plugin in pixie module diff.
    
    Test Plan: test passes
    
    Reviewers: michelle
    
    Reviewed By: michelle
    
    Signed-off-by: Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11229
    
    GitOrigin-RevId: a8ebee0f28af00e77199a5edb52d29d749d9ad63

--- src/carnot/planner/objects/default_arguments_test.cc
+++ src/carnot/planner/objects/default_arguments_test.cc
@@ -72,7 +72,9 @@ class DefaultArgumentsTest : public OperatorTests {
         SetUpRelMap(), /* sensitive_columns */ SensitiveColumnMap{}, info_.get(),
         /* time_now */ time_now_,
         /* max_output_rows_per_table */ 0, "result_addr", "result_ssl_targetname",
-        /* redaction_options */ RedactionOptions{}, nullptr, nullptr);
+
+        /* redaction_options */ RedactionOptions{}, nullptr,
+        std::unique_ptr<PluginConfig>(new PluginConfig{0}));
 
     ast_visitor_ = ASTVisitorImpl::Create(graph.get(), &dynamic_trace_, compiler_state_.get(),
                                           &module_handler_)
