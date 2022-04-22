b75845fbd85dbc2c984e45bf1b1e4a5db01b5142
Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
2022-04-22 15:14:32 -0700

    Fix OTel export error to make it more readable.
    Summary:
    The OTel export error would just use the integer enum value instead of showing the
    string name of the enum. That was not readable and made debugging difficult.
    
    I decided to fix that and change some wording around the error to make it easier to understand and
    take action.
    
    Test Plan: Added tests and happened to run it in skaffold deploy as well
    
    Reviewers: michelle, jamesbartlett, vihang, nserrino
    
    Reviewed By: jamesbartlett
    
    Signed-off-by: Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11292
    
    GitOrigin-RevId: 19884838469e53ba2ce28313d055b7980e113f69

--- src/carnot/exec/otel_export_sink_node.cc
+++ src/carnot/exec/otel_export_sink_node.cc
@@ -193,6 +193,12 @@ void ReplicateData(const std::vector<planpb::OTelAttribute>& attributes_spec,
   }
 }
 
+Status FormatOTelStatus(int64_t id, const grpc::Status& status) {
+  return error::Internal(absl::Substitute(
+      "OTel export (carnot node_id=$0) failed with error '$1'. Details: $2 $3", id,
+      magic_enum::enum_name(status.error_code()), status.error_message(), status.error_details()));
+}
+
 using ::opentelemetry::proto::metrics::v1::ResourceMetrics;
 Status OTelExportSinkNode::ConsumeMetrics(const RowBatch& rb) {
   grpc::ClientContext context;
@@ -271,10 +277,7 @@ Status OTelExportSinkNode::ConsumeMetrics(const RowBatch& rb) {
 
   grpc::Status status = metrics_service_stub_->Export(&context, request, &metrics_response_);
   if (!status.ok()) {
-    return error::Internal(absl::Substitute(
-        "OTelExportSinkNode $0 encountered error code $1 "
-        "exporting data, message: $2 $3",
-        plan_node_->id(), status.error_code(), status.error_message(), status.error_details()));
+    return FormatOTelStatus(plan_node_->id(), status);
   }
   return Status::OK();
 }
@@ -389,10 +392,7 @@ Status OTelExportSinkNode::ConsumeSpans(const RowBatch& rb) {
 
   grpc::Status status = trace_service_stub_->Export(&context, request, &trace_response_);
   if (!status.ok()) {
-    return error::Internal(absl::Substitute(
-        "OTelExportSinkNode $0 encountered error code $1 "
-        "exporting data, message: $2 $3",
-        plan_node_->id(), status.error_code(), status.error_message(), status.error_details()));
+    return FormatOTelStatus(plan_node_->id(), status);
   }
   return Status::OK();
 }
