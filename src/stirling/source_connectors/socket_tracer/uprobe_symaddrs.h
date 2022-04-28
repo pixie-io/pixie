0ed04fc1bb41002269153e9d0610f774a640f064
Dom Del Nano <ddelnano@twitter.com>
2022-04-28 07:34:26 -0700

    Enhance openssl tracing to fallback to function pointers when dlsym fails
    Summary:
    This is a step towards adding TLS tracing support to netty.
    The netty-tcnative shared library does not use a dynamic OpenSSL_num_version symbol.
    This means the symbol ends up in the symtab rather than dynsym. The former is private and incompatible with dlsym, while the latter is public and is supported by the existing openssl tracing.
    
    Test Plan: Updated openssl_trace_bpf_test to cover the new cases.
    
    Reviewers: #stirling, yzhao
    
    Reviewed By: #stirling, yzhao
    
    Signed-off-by: Dom Del Nano <ddelnano@twitter.com>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11327
    
    GitOrigin-RevId: 20abc6c7636f923e3d587e5467748397ee4c97da

--- src/stirling/source_connectors/socket_tracer/uprobe_symaddrs.h
+++ src/stirling/source_connectors/socket_tracer/uprobe_symaddrs.h
@@ -18,14 +18,41 @@
 
 #pragma once
 
+#include <string>
+
 #include "src/common/base/base.h"
+#include "src/common/system/proc_parser.h"
 #include "src/stirling/obj_tools/dwarf_reader.h"
 #include "src/stirling/obj_tools/elf_reader.h"
 #include "src/stirling/source_connectors/socket_tracer/bcc_bpf_intf/symaddrs.h"
 #include "src/stirling/utils/detect_application.h"
 
+DECLARE_bool(openssl_force_raw_fptrs);
+DECLARE_bool(openssl_raw_fptrs_enabled);
+
 namespace px {
 namespace stirling {
+
+using system::ProcParser;
+
+class RawFptrManager : NotCopyMoveable {
+ public:
+  explicit RawFptrManager(obj_tools::ElfReader* elf_reader, system::ProcParser* proc_parser,
+                          std::string lib_path);
+  ~RawFptrManager();
+
+  template <class T>
+  StatusOr<T*> RawSymbolToFptr(const std::string& symbol_name);
+  StatusOr<bool> Init();
+
+ private:
+  obj_tools::ElfReader* elf_reader_;
+  ProcParser* proc_parser_;
+  uint64_t text_segment_offset_;
+  void* dlopen_handle_;
+  std::string lib_path_;
+  ProcParser::ProcessSMaps map_entry_;
+};
 
 /**
  * Uses ELF and DWARF information to return the locations of all relevant symbols for general Go
@@ -52,7 +79,9 @@ StatusOr<struct go_tls_symaddrs_t> GoTLSSymAddrs(obj_tools::ElfReader* elf_reade
  * Detects the version of OpenSSL to return the locations of all relevant symbols for OpenSSL uprobe
  * deployment.
  */
-StatusOr<struct openssl_symaddrs_t> OpenSSLSymAddrs(const std::filesystem::path& openssl_lib);
+StatusOr<struct openssl_symaddrs_t> OpenSSLSymAddrs(RawFptrManager* fptrManager,
+                                                    const std::filesystem::path& openssl_lib,
+                                                    uint32_t pid);
 
 /**
  * Returns the corresponding symbol offsets of the input Nodejs executable.
