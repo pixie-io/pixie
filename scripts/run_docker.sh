14a9c0d685a6f3771021873df4550ed607946a6e
Zain Asgar <zasgar@pixielabs.ai>
2022-06-09 12:09:35 -0700

    Docker containers can now use caches while running bazel
    Summary: We can now use buildbuddy or file cache. In addition the user get's their own shell, etc.
    
    Test Plan: N/A
    
    Reviewers: michelle, vihang, #third_party_approvers
    
    Reviewed By: vihang, #third_party_approvers
    
    Signed-off-by: Zain Asgar <zasgar@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11556
    
    GitOrigin-RevId: 4cafbf15f95425f5e46a5c05a0160d835d2fed3d

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -22,32 +22,81 @@
 script_dir="$(dirname "$0")"
 workspace_root=$(realpath "${script_dir}/../")
 
+# Some colors to use.
+red="\e[0;91m"
+green="\e[0;92m"
+bold="\e[1m"
+resetcolor="\e[0m"
+
 # Docker image information.
 docker_image_base=gcr.io/pixie-oss/pixie-dev-public/dev_image_with_extras
 version=$(grep DOCKER_IMAGE_TAG "${workspace_root}/docker.properties" | cut -d= -f2)
 docker_image_with_tag="${docker_image_base}:${version}"
+docker_host_gid=$(getent group docker | awk -F: '{print $3}')
+
+if [[ -z "${docker_host_gid}" ]]; then
+    echo "${red}Docker needs to be enabled in order to run the dev container.{$resetcolor}"
+    exit 1
+fi
+
+shell=/bin/bash
+if [[ "${SHELL}" == *"zsh" ]]; then
+    shell=/usr/bin/zsh
+fi
+
+pushd "${workspace_root}/tools/docker/user_dev_image" > /dev/null
+dev_image_name="px_dev_image:${version}"
+docker build -q -t "${dev_image_name}" \
+       --build-arg BASE_IMAGE="${docker_image_with_tag}" \
+       --build-arg USER_NAME="${USER}" \
+       --build-arg USER_ID="$(id -u)" \
+       --build-arg GROUP_ID="$(id -g)" \
+       --build-arg DOCKER_ID="${docker_host_gid}" \
+       --build-arg SHELL="${shell}" \
+       .
+popd > /dev/null
+
+# Check if build buddy cache exists. If so, add the appropriate arguments.
+build_buddy_args=()
+if grep -q "^build.*remote_cache.*grpc.*9998" /etc/bazelrc; then
+    build_buddy_args=(
+        "--add-host=$(hostname -f):127.0.0.1"
+        "--volume=/etc/bazelrc:/etc/bazelrc")
+fi
 
 IFS=' '
 # Read the environment variable and set it to an array. This allows
 # us to use an array access in the later command.
 read -ra PX_RUN_DOCKER_EXTRA_ARGS <<< "${PX_RUN_DOCKER_EXTRA_ARGS}"
 
-configs=(-v "$HOME/.config:/root/.config" \
-  -v "$HOME/.ssh:/root/.ssh" \
-  -v "$HOME/.kube:/root/.kube" \
-  -v "$HOME/.gitconfig:/root/.gitconfig" \
-  -v "$HOME/.arcrc:/root/.arcrc")
+configs=(
+    # Mount the home directory.
+    -v "${HOME}:/home/${USER}"
+    # Allow docker to run within the container.
+    -v /var/run/docker.sock:/var/run/docker.sock
+    # Mount the workspace root within the build directory.
+    -v "${workspace_root}:/pl/src/px.dev/pixie"
+    # Use the host network so we can easily access build caches, etc.
+    "--network=host"
+)
 
-exec_cmd=("/usr/bin/bash")
+exec_cmd=("${shell}")
 if [ $# -ne 0 ]; then
   exec_cmd=("${exec_cmd[@]}" "-c" "$*")
 fi
 
+echo -e "${bold}Run Parameters:${resetcolor}"
+if [ ${#build_buddy_args[@]} -ne 0 ]; then
+    echo -e "\t${bold}Build Buddy: \t\t ${green}Enabled${resetcolor}"
+else
+    echo -e "\t${bold}Build Buddy: \t\t ${red}Disabled${resetcolor}"
+fi
+echo -e "\t${bold}Shell: \t\t\t ${green}${shell}${resetcolor}"
+
+
 docker run --rm -it \
   "${configs[@]}" \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v "${workspace_root}:/pl/src/px.dev/pixie" \
-  --network host \
+  "${build_buddy_args[@]}" \
   "${PX_RUN_DOCKER_EXTRA_ARGS[@]}" \
-  "${docker_image_with_tag}" \
+  "${dev_image_name}" \
   "${exec_cmd[@]}"
