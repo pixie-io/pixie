287f5025809e61a46fb3c4f91d18ae7c64af8770
Vihang Mehta <vihang@pixielabs.ai>
2022-06-16 21:18:14 -0700

    Pass build opts to cquery in sudo bazel run
    Summary:
    This ensures that cquery finds the correct binary instead of just
    the fastbuild binary
    
    Test Plan:
    Removed the fastbuild binary, ran sudo bazel run with `-c dbg`. Fails
    before this change, works after.
    
    Reviewers: zasgar, oazizi
    
    Reviewed By: zasgar
    
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11626
    
    GitOrigin-RevId: f25f778ec06782f32a58aa2c392886658f22be15
    (cherry picked from commit 8f87b07ffa4f233a297c7c4d33116401b0676f3a)
    Signed-off-by: Dom Del Nano <ddelnano@twitter.com>

--- scripts/sudo_bazel_run.sh
+++ scripts/sudo_bazel_run.sh
@@ -89,7 +89,7 @@ echo "Pass through env. vars: ${pass_thru_env_vars[*]}"
 # Perform the build as user (not as root).
 bazel build --remote_download_toplevel "${options[@]}" "$target"
 
-target_executable=$(bazel cquery "${target}" --output starlark --starlark:expr "target.files.to_list()[0].path" 2>/dev/null)
+target_executable=$(bazel cquery "${options[@]}" "${target}" --output starlark --starlark:expr "target.files.to_list()[0].path" 2>/dev/null)
 
 extra_env_args=()
 extra_env_args+=("RUNFILES_MANIFEST_FILE=${target_executable}.runfiles/MANIFEST")
