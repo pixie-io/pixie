8bf18010ac11dbe1ce0d1791cbdab866825eddb5
Nick Lanam <nlanam@pixielabs.ai>
2022-06-22 12:28:25 -0700

    [PC-1542]: Prevent unintended horizontal scrollbars
    Summary:
    Scrollbars are not consistent between browsers, operating systems, and settings for either.
    
    Customizing scrollbars I even more complex, and makes detection of their dimensions somehow even less consistent.
    
    This diff guarantees that `useScrollbarSize` will detect the actual dimensions of the scrollbar (and if it's overlay vs inlay) in Safari, Chrome, and Firefox on macOS, Linux, and Windows, and especially for macOS users who change their system settings for scrollbars (General -> Show scroll bars -> defaults to "When scrolling").
    
    Result is that data tables should no longer fail to reserve space for scrollbars when they should.
    
    This diff also deletes a long-unused file that was still laying around.
    
    Test Plan:
    Run any PxL script with tables in Chrome on macOS with system preference of "When scrolling".
    Do the same in Firefox and also try changing the system setting (reload between changes).
    Try Firefox and Chrome on Ubuntu as well.
    
    In all cases, there should not be any _unexpected_ horizontal scrollbars (tables with too many columns for the space available will still show one).
    
    Reviewers: michelle, vihang
    
    Reviewed By: michelle
    
    JIRA Issues: PC-1542
    
    Signed-off-by: Nick Lanam <nlanam@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11657
    
    GitOrigin-RevId: cd96499fcd87f1e99b1f9189aeb51be31ab9c2d4

--- src/ui/src/utils/use-scrollbar-size.ts
+++ src/ui/src/utils/use-scrollbar-size.ts
@@ -18,29 +18,61 @@
 
 import * as React from 'react';
 
+import { Theme } from '@mui/material/styles';
+import { createStyles, makeStyles } from '@mui/styles';
+
+import { scrollbarStyles } from 'app/components';
+
+/**
+ * Each combination of OS, browser, and system settings can change not only overlay/inlay mode, but dimensions.
+ * To guarantee we get the right answer for the default scroll testing element, we need to force a few settings.
+ */
+const useDefaultStyles = makeStyles((theme: Theme) => createStyles({
+  outer: {
+    ...scrollbarStyles(theme),
+    position: 'absolute',
+    top: '-100vh',
+    left: '-100vw',
+    pointerEvents: 'none',
+  },
+  inner: {
+    width: '100vw',
+    height: '100vh',
+    overflow: 'scroll',
+  },
+}), { name: 'ScrollSizeTester' });
+
+const scrollDim = (el: HTMLElement) => ({
+  width: el.offsetWidth - el.clientWidth,
+  height: el.offsetHeight - el.clientHeight,
+});
+
 /**
  * Determines the dimensions of the browser scrollbars on the given element, if it's currently showing them.
  * If no element is provided, determines the default dimensions of the browser scrollbars when they would show.
  * Browsers may overlay scrollbars rather than putting them in-layout. In those cases they're treated as 0-width/height.
  */
 export function useScrollbarSize(el?: HTMLElement): { width: number, height: number } {
+  const classes = useDefaultStyles();
+
   const overflow = el && (el.offsetWidth > el.clientWidth || el.offsetHeight > el.clientHeight);
 
   return React.useMemo(() => {
     if (el) {
-      return {
-        width: el.offsetWidth - el.clientWidth,
-        height: el.offsetHeight - el.clientHeight,
-      };
+      return scrollDim(el);
     }
 
-    const scroller = document.createElement('div');
-    scroller.setAttribute('style', 'width: 100vw; height: 100vh; overflow: scroll; position: absolute; top: -100vh;');
-    document.body.appendChild(scroller);
-    const width = scroller.offsetWidth - scroller.clientWidth;
-    const height = scroller.offsetHeight - scroller.clientHeight;
-    document.body.removeChild(scroller);
-    return { width, height };
+    const outer = document.createElement('div');
+    outer.classList.add(classes.outer);
+    const inner = document.createElement('div');
+    inner.classList.add(classes.inner);
+    inner.appendChild(document.createTextNode('foobarbaz'));
+    outer.appendChild(inner);
+    document.body.appendChild(outer);
+
+    const dim = scrollDim(inner);
+    document.body.removeChild(outer);
+    return dim;
 
     // We also want to check if the element is overflowing - that changes its scrollbar dimensions.
     // eslint-disable-next-line react-hooks/exhaustive-deps
