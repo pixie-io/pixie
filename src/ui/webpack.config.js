8ba67e25672cc6e0494646f19609a8b407c420c0
Vihang Mehta <vihang@pixielabs.ai>
2022-06-27 22:23:41 -0700

    Cleanup and speed up webpack build
    Summary:
    Drop `image-webpack-loader` since it's deps are mostly no longer
    maintained and seem to have lots of CVEs. Instead, just minimize the images
    and check them im.
    Disable a couple more extra favicons that were being generated but are unused.
    Fixup the stars background on the login page.
    
    This speeds up the yarn install some amount? and the webpack by a couple of seconds.
    
    Test Plan: yarn dev, still words. yarn build_prod still works.
    
    Reviewers: zasgar, michelle, nlanam, #third_party_approvers
    
    Reviewed By: michelle, nlanam, #third_party_approvers
    
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11690
    
    GitOrigin-RevId: 1856906d198d43ae47b4f12e06d5bff8ae2a0d1e

--- src/ui/webpack.config.js
+++ src/ui/webpack.config.js
@@ -142,14 +142,6 @@ const webpackConfig = {
         },
       })),
       {
-        test: /\.(jpg|png|gif|svg)$/,
-        loader: 'image-webpack-loader',
-        // Specify enforce: 'pre' to apply the loader
-        // before url-loader/svg-url-loader
-        // and not duplicate it in rules with them
-        enforce: 'pre',
-      },
-      {
         test: /\.(svg)$/i,
         loader: require.resolve('svg-url-loader'),
         options: {
