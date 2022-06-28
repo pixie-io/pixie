de3e9a171bffcb4abc40a43559b9b0f516b312ab
Vihang Mehta <vihang@pixielabs.ai>
2022-06-28 09:50:16 -0700

    Drop some more unused favicon generation
    Summary: TSIA
    
    Test Plan: Ran yarn build_prod and examined the bundle
    
    Reviewers: michelle, nlanam
    
    Reviewed By: michelle, nlanam
    
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D11692
    
    GitOrigin-RevId: 646b8c85dd63d33baa9c1492f8321c61ce0818e0

--- src/ui/webpack.config.js
+++ src/ui/webpack.config.js
@@ -44,7 +44,9 @@ const plugins = [
         android: false,
         appleIcon: false,
         appleStartup: false,
+        coast: false,
         favicons: true,
+        firefox: false,
         windows: false,
         yandex: false
       }
