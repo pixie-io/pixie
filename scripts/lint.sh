0436bb0c8c5467be245f52881a595438b432ed04
Zain Asgar <zasgar@pixielabs.ai>
2022-10-18 13:33:35 -0700

    Add lint.sh that can run our linters
    Summary: This can be used to easily run our linters outside of machines that have arc.
    
    Test Plan: `./scripts/lint.sh`
    
    Reviewers: vihang, michelle
    
    Reviewed By: vihang, michelle
    
    Signed-off-by: Zain Asgar <zasgar@pixielabs.ai>
    
    Differential Revision: https://phab.corp.pixielabs.ai/D12425
    
    GitOrigin-RevId: 173e1756ccb884c22fd2008cd251484274f50fe5

---
+++ scripts/lint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# Copyright 2018- The Pixie Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Script Usage:
+# lint.sh runs all the linters for pixie. Needs Docker.
+script_dir="$(dirname "$0")"
+
+# Check if docker exists and works.
+if ! docker version &> /dev/null
+then
+    echo "Docker is not working. This might be because you don't have enough permissions, or the docker is not installed."
+    exit $?
+fi
+
+"${script_dir}/run_docker.sh" arc lint
