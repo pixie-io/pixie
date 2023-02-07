e4117e379117e8139a3d023b36430684e9547d46
Vihang Mehta <vihang@pixielabs.ai>
2023-02-06 22:47:05 -0800

    Add a checker to ensure necessary generated files are updated in a PR (#791)
    Summary: This linter runs generators for the various types of generated
    files that checked into our codebase and fails if any of the files
    are changed. i.e. they should have been regen for a PR.
    
    Relevant Issues: N/A
    
    Type of change: /kind cleanup
    
    Test Plan: Will trigger the linter with failures on this diff.
    
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>

--- src/cloud/api/controllers/schema/update.sh
+++ src/cloud/api/controllers/schema/update.sh
@@ -16,15 +16,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# Update the schema assets.
-go-bindata -modtime=1 -ignore=\.go -ignore=\.sh -pkg=complete -o=complete/bindata.gen.go .
-go-bindata -modtime=1 -ignore=\.go -ignore=\.sh -ignore=^auth_schema.graphql -pkg=noauth -o=noauth/bindata.gen.go .
-
 tot=$(git rev-parse --show-toplevel)
-pushd "${tot}/src/ui"
+
+pushd "${tot}/src/cloud/api/controllers/schema" > /dev/null
+# Update the schema assets.
+go-bindata -modtime=1 -mode=436 -ignore=\.go -ignore=\.sh -pkg=complete -o=complete/bindata.gen.go .
+go-bindata -modtime=1 -mode=436 -ignore=\.go -ignore=\.sh -ignore=^auth_schema.graphql -pkg=noauth -o=noauth/bindata.gen.go .
+popd > /dev/null
+
+pushd "${tot}/src/ui" > /dev/null
+yarn install
 # Note: this _should_ be schema.d.ts, not schema.ts. However, since we build the UI in isolatedModules mode, we must
 # also use normal enums instead of const enums (see https://www.typescriptlang.org/tsconfig#isolatedModules).
 # A strict declarations file (.d.ts) can't export values (like normal enums), so we must rename the file as well.
 yarn graphql-schema-typescript generate-ts "${tot}/src/cloud/api/controllers/schema" --output src/types/schema.ts
 sed -i 's/export const enum/export enum/g' src/types/schema.ts
-popd
+popd > /dev/null
