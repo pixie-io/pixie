a57100ea9d47521c3230ee3f141cb280805798d6
James Bartlett <jamesbartlett@pixielabs.ai>
2023-02-14 11:06:20 -0800

    Support pushing multi-arch images in vizier_build_release.sh (#843)
    Summary: Adds pushing multi-arch images in the vizier release process.
    Builds each architecture's images separately and pushes those
    individually. Then pushes a manifest list pointing to each architecture
    for each vizier image.
    
    Relevant Issues: #147.
    
    Type of change: /kind cleanup
    
    Test Plan: Created an RC with these changes, saw that it built
    multi-arch images.
    Ran `px deploy --vizier_version=<RC>` and it successfully deployed
    Pixie.
    
    Changelog Message:
    ```release-note
    Our releases now build multi-arch docker images that support x86_64 and aarch64.
    ARM support is now in an early alpha stage. We'd love for the community to help test out ARM support.
    ```
    
    Signed-off-by: James Bartlett <jamesbartlett@pixielabs.ai>

--- ci/vizier_build_release.sh
+++ ci/vizier_build_release.sh
@@ -41,8 +41,36 @@ fi
 output_path="gs://${bucket}/vizier/${release_tag}"
 latest_output_path="gs://${bucket}/vizier/latest"
 
-bazel run --stamp -c opt --//k8s:image_version="${release_tag}" \
-    --stamp "${build_type}" //k8s/vizier:vizier_images_push "${extra_bazel_args[@]}"
+push_images_for_arch() {
+  arch="$1"
+  bazel run --stamp -c opt --//k8s:image_version="${release_tag}-${arch}" \
+      --config="${arch}_sysroot" \
+      --stamp "${build_type}" //k8s/vizier:vizier_images_push "${extra_bazel_args[@]}" > /dev/null
+}
+
+push_images_for_arch "x86_64"
+push_images_for_arch "aarch64"
+
+push_multiarch_image() {
+  multiarch_image="$1"
+  x86_image="${multiarch_image}-x86_64"
+  aarch64_image="${multiarch_image}-aarch64"
+  echo "Building ${multiarch_image} manifest"
+  # If the multiarch manifest list already exists locally, remove it before building a new one.
+  # otherwise, the docker manifest create step will fail because it can't amend manifests to an existing image.
+  # We could use the --amend flag to `manifest create` but it doesn't seem to overwrite existing images with the same tag,
+  # instead it seems to just ignore images that already exist in the local manifest.
+  docker manifest rm "${multiarch_image}" || true
+  docker manifest create "${multiarch_image}" "${x86_image}" "${aarch64_image}"
+  docker manifest push "${multiarch_image}"
+}
+
+while read -r image;
+do
+  push_multiarch_image "${image}"
+done < <(bazel run --stamp -c opt --//k8s:image_version="${release_tag}" \
+         --stamp "${build_type}" //k8s/vizier:list_image_bundle "${extra_bazel_args[@]}")
+
 bazel build --stamp -c opt --//k8s:image_version="${release_tag}" \
     --stamp "${build_type}" //k8s/vizier:vizier_yamls "${extra_bazel_args[@]}"
 
