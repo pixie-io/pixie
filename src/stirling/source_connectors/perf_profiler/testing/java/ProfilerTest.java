a82c1b5cba66935d47a984956775037cdf1fd4c8
Pete Stevenson <jps@pixielabs.ai>
2023-02-22 10:29:11 -0800

    Better comments.
    Signed-off-by: Pete Stevenson <jps@pixielabs.ai>

--- src/stirling/source_connectors/perf_profiler/testing/java/ProfilerTest.java
+++ src/stirling/source_connectors/perf_profiler/testing/java/ProfilerTest.java
@@ -17,15 +17,28 @@
  */
 
 class ProfilerTest {
+  // Our two leaf functions do not share their code, as we had originally intended. Sharing
+  // would work like this:
+  // leaf2x() { return leaf(5);  }  // Crafted such that leaf(5) does 2x the work of leaf(10).
+  // leaf1x() { return leaf(10); }
+  // i.e. with a shared parameterized leaf function, leaf1x() and leaf2x() are just pass throughs.
+  // Unfortunately, different JVMs inline functions in different ways. Some of them would inline
+  // leaf1x and leaf2x and as a result, we were unable to find the expected symbols.
 
   public static long leaf1x() {
-    long n = 10000;
-    long s = 0;
+    // leaf1x() counts by 10s, and sums the result. leaf2x() does the same, but counts by 5s.
     long m = 10;
+
+    // Outer loop iterations. This function will compute its count (by increments of "m") "n" times.
+    long n = 10000;
+    
+    // The sum, returned when the function completes.
+    long s = 0;
 
     for(long i=0; i < n; i++ ) {
       long k = i % 2 == 0 ? 7 : 11;
       for(long j=m; j <= n; j += m ) {
+        // We do some silly things here to prevent newer more powerful JVMs from inlining.
         if( j % m == 0 ) {
           s += j;
         }
@@ -40,14 +53,19 @@ class ProfilerTest {
   }
 
   public static long leaf2x() {
+    // leaf2x() counts by 5s, and sums the result. leaf1x() does the same, but counts by 10s.
     long m = 5;
 
+    // Outer loop iterations. This function will compute its count (by increments of "m") "n" times.
     long n = 10000;
+
+    // The sum, returned when the function completes.
     long s = 0;
 
     for(long i=0; i < n; i++ ) {
       long k = i % 2 == 0 ? 7 : 11;
       for(long j=m; j <= n; j += m ) {
+        // We do some silly things here to prevent newer more powerful JVMs from inlining.
         if( j % m == 0 ) {
           s += j;
         }
