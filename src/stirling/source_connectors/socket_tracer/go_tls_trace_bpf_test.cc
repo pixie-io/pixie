4461d1832b7e7b2339043511c83982fb57f72e11
Zain Asgar <zasgar@pixielabs.ai>
2023-03-01 04:24:57 +0000

    Fix Go TLS trace test by reducing number of iterations
    Signed-off-by: Zain Asgar <zasgar@pixielabs.ai>

--- src/stirling/source_connectors/socket_tracer/go_tls_trace_bpf_test.cc
+++ src/stirling/source_connectors/socket_tracer/go_tls_trace_bpf_test.cc
@@ -94,7 +94,7 @@ TYPED_TEST(GoTLSTraceTest, BasicHTTP) {
   PX_CHECK_OK(this->client_.Run(
       std::chrono::seconds{10},
       {absl::Substitute("--network=container:$0", this->server_.container_name())},
-      {"--http2=false"}));
+      {"--http2=false", "--iters=2", "--sub_iters=5"}));
   this->client_.Wait();
 
   this->StopTransferDataThread();
@@ -131,7 +131,7 @@ TYPED_TEST(GoTLSTraceTest, BasicHTTP2) {
   PX_CHECK_OK(this->client_.Run(
       std::chrono::seconds{10},
       {absl::Substitute("--network=container:$0", this->server_.container_name())},
-      {"--http2=true"}));
+      {"--http2=true", "--iters=2", "--sub_iters=5"}));
   this->client_.Wait();
 
   this->StopTransferDataThread();
