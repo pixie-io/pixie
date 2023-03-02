b6219ed2430dded9d8bcb61cacf69cd17689c884
Pete Stevenson <jps@pixielabs.ai>
2023-03-02 09:49:53 -0800

    Fix off by one error in amqp/parse.cc.
    Signed-off-by: Pete Stevenson <jps@pixielabs.ai>

--- src/stirling/source_connectors/socket_tracer/protocols/amqp/parse.cc
+++ src/stirling/source_connectors/socket_tracer/protocols/amqp/parse.cc
@@ -90,7 +90,7 @@ ParseState ParseFrame(message_type_t type, std::string_view* buf, Frame* packet)
   packet->frame_type = frame_type;
   packet->channel = channel;
   packet->payload_size = payload_size;
-  if (decoder.BufSize() < payload_size) {
+  if (decoder.BufSize() <= payload_size) {
     return ParseState::kInvalid;
   }
 
