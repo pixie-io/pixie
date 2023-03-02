7cc05fff80bc1bd15b3d7b36cb4dd5f73877229a
Pete Stevenson <jps@pixielabs.ai>
2023-03-02 13:35:42 -0800

    Change parse state to 'needs more data' if the buffer size is less than payload size.
    Signed-off-by: Pete Stevenson <jps@pixielabs.ai>

--- src/stirling/source_connectors/socket_tracer/protocols/amqp/parse.cc
+++ src/stirling/source_connectors/socket_tracer/protocols/amqp/parse.cc
@@ -91,7 +91,7 @@ ParseState ParseFrame(message_type_t type, std::string_view* buf, Frame* packet)
   packet->channel = channel;
   packet->payload_size = payload_size;
   if (decoder.BufSize() <= payload_size) {
-    return ParseState::kInvalid;
+    return ParseState::kNeedsMoreData;
   }
 
   // Check end byte is valid
