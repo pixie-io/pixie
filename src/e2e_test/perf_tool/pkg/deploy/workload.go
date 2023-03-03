fa600d96e4b8fb3c95581d9be523b604106e3163
James Bartlett <jamesbartlett@pixielabs.ai>
2023-03-03 20:56:06 +0000

    [perf_tool/deploy] Implement `Workload` interface
    Signed-off-by: James Bartlett <jamesbartlett@pixielabs.ai>

--- src/e2e_test/perf_tool/pkg/deploy/workload.go
+++ src/e2e_test/perf_tool/pkg/deploy/workload.go
@@ -20,9 +20,17 @@ package deploy
 
 import (
 	"context"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
 
 	"px.dev/pixie/src/e2e_test/perf_tool/experimentpb"
 	"px.dev/pixie/src/e2e_test/perf_tool/pkg/cluster"
+	"px.dev/pixie/src/e2e_test/perf_tool/pkg/deploy/checks"
+	"px.dev/pixie/src/e2e_test/perf_tool/pkg/deploy/steps"
+	"px.dev/pixie/src/e2e_test/perf_tool/pkg/pixie"
+	"px.dev/pixie/src/utils/shared/k8s"
 )
 
 // Workload is the interface for workloads that get deployed to the experiment cluster.
@@ -31,4 +39,97 @@ type Workload interface {
 	Start(*cluster.Context) error
 	WaitForHealthCheck(context.Context, *cluster.Context, *experimentpb.ClusterSpec) error
 	Close() error
+}
+
+type workloadImpl struct {
+	name         string
+	spec         *experimentpb.WorkloadSpec
+	clusterCtx   *cluster.Context
+	pxCtx        *pixie.Context
+	deploySteps  []steps.DeployStep
+	healthChecks []checks.HealthCheck
+
+	namespacesToDelete map[string]bool
+}
+
+// NewWorkload creates a new Workload capable of deploying according to the spec given.
+func NewWorkload(pxCtx *pixie.Context, containerRegistryRepo string, spec *experimentpb.WorkloadSpec) (Workload, error) {
+	// Add deploy steps and healthchecks from spec once those are implemented.
+	deploySteps := make([]steps.DeployStep, len(spec.DeploySteps))
+	healthchecks := make([]checks.HealthCheck, len(spec.Healthchecks))
+	return &workloadImpl{
+		name:               spec.Name,
+		spec:               spec,
+		pxCtx:              pxCtx,
+		deploySteps:        deploySteps,
+		healthChecks:       healthchecks,
+		namespacesToDelete: make(map[string]bool),
+	}, nil
+}
+
+// Prepare the workload by building necessary images and rendering yamls.
+func (w *workloadImpl) Prepare() error {
+	log.WithField("workload", w.name).Trace("Preparing workload for deployment")
+	for _, s := range w.deploySteps {
+		log.WithField("workload", w.name).WithField("step", s.Name()).Trace("preparing deploy step")
+		if err := s.Prepare(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Start starts the workload.
+func (w *workloadImpl) Start(clusterCtx *cluster.Context) error {
+	w.clusterCtx = clusterCtx
+	log.WithField("workload", w.name).Trace("Starting workload deployment")
+	for _, s := range w.deploySteps {
+		log.WithField("workload", w.name).WithField("step", s.Name()).Trace("running deploy step")
+		namespaces, err := s.Deploy(clusterCtx)
+		if err != nil {
+			return err
+		}
+		for _, ns := range namespaces {
+			w.namespacesToDelete[ns] = true
+		}
+	}
+	return nil
+}
+
+// WaitForHealthCheck waits for all of the workload's healthchecks to pass.
+// The ClusterSpec is provided so that the healthcheck can use the number of nodes if necessary.
+func (w *workloadImpl) WaitForHealthCheck(ctx context.Context, clusterCtx *cluster.Context, clusterSpec *experimentpb.ClusterSpec) error {
+	var eg errgroup.Group
+	for _, c := range w.healthChecks {
+		check := c
+		eg.Go(func() error {
+			log.WithField("workload", w.name).WithField("check", check.Name()).Trace("waiting for healthcheck")
+			err := check.Wait(ctx, clusterCtx, clusterSpec)
+			if err != nil {
+				return err
+			}
+			log.WithField("workload", w.name).WithField("check", check.Name()).Trace("healthcheck passed")
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Close stops the workload.
+func (w *workloadImpl) Close() error {
+	for ns := range w.namespacesToDelete {
+		log.WithField("workload", w.name).WithField("namespace", ns).Trace("deleting workload namespace")
+		od := k8s.ObjectDeleter{
+			Namespace:  ns,
+			Clientset:  w.clusterCtx.Clientset(),
+			RestConfig: w.clusterCtx.RestConfig(),
+			Timeout:    5 * time.Minute,
+		}
+		return od.DeleteNamespace()
+	}
+	w.namespacesToDelete = make(map[string]bool)
+	return nil
 }
