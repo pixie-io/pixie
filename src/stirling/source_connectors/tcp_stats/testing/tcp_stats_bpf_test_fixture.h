0efa913bd58879ad148f2e418200e2007b8a07ab
RagalahariP <rpotti@newrelic.com>
2023-03-21 15:22:17 +0000

    Adding new unit testcase
    Signed-off-by: RagalahariP <rpotti@newrelic.com>

--- src/stirling/source_connectors/tcp_stats/testing/tcp_stats_bpf_test_fixture.h
+++ src/stirling/source_connectors/tcp_stats/testing/tcp_stats_bpf_test_fixture.h
@@ -41,13 +41,7 @@ class TcpTraceBPFTestFixture : public ::testing::Test {
     source_.reset(dynamic_cast<TCPStatsConnector*>(source_connector.release()));
     ASSERT_OK(source_->Init());
     source_->set_data_tables(data_tables_.tables());
-    //absl::flat_hash_set<md::UPID> upids;
-    upid = md::UPID(1, 100, 23123);
-    upids.insert(upid);
-    ctx_ = std::make_unique<StandaloneContext>(upids);
-    //StandaloneContext context_(upids);
-    //RefreshContextCore();
-     //StartTransferDataThread();
+    ctx_ = std::make_unique<SystemWideStandaloneContext>();
   }
 
   void TearDown() override { ASSERT_OK(source_->Stop()); }
@@ -63,8 +57,7 @@ class TcpTraceBPFTestFixture : public ::testing::Test {
       transfer_enable_ = true;
       while (transfer_enable_) {
         {
-          //absl::base_internal::SpinLockHolder lock(&tcp_stats_state_lock_);
-          RefreshContext(upids);
+          RefreshContext();
           source_->TransferData(ctx_.get());
         }
         std::this_thread::sleep_for(kTransferDataPeriod);
@@ -75,51 +68,37 @@ class TcpTraceBPFTestFixture : public ::testing::Test {
     }
 
     // Wait for at least one TransferData() call before returning.
-    //std::this_thread::sleep_for(10);
+    std::this_thread::sleep_for(kTransferDataPeriod);
   }
 
   void StopTransferDataThread() {
     // Give enough time for one more TransferData call by transfer_data_thread_,
     // so we make sure we've captured everything.
-    //std::this_thread::sleep_for(2 * kTransferDataPeriod);
-    //std::this_thread::sleep_for(10);
-    LOG(INFO) << "Here in StopTransferDataThread";
-    //absl::base_internal::SpinLockHolder lock(&tcp_stats_state_lock_);
+    std::this_thread::sleep_for(2 * kTransferDataPeriod);
     CHECK(transfer_data_thread_.joinable());
     transfer_enable_ = false;
     transfer_data_thread_.join();
     source_->TransferData(ctx_.get());
-    LOG(INFO) << "end - Here in StopTransferDataThread";
   }
 
   std::vector<TaggedRecordBatch> ConsumeRecords(int table_num) {
     return source_->data_tables()[table_num]->ConsumeRecords();
   }
 
-  md::UPID getUpid() {
-    return upid;
-  }
-
-    void RefreshContext(const absl::flat_hash_set<md::UPID>& upids) {
+  void RefreshContext() {
     absl::base_internal::SpinLockHolder lock(&tcp_stats_state_lock_);
-    ctx_ = std::make_unique<StandaloneContext>(upids);
+    ctx_ = std::make_unique<SystemWideStandaloneContext>();
   }
+
   static constexpr int kTcpstatsTableNum = TCPStatsConnector::kTCPStatsTableNum;
-
+  testing::DataTables data_tables_{TCPStatsConnector::kTables};
+  DataTable* tcp_stats_table_ = data_tables_[kTcpstatsTableNum];
   absl::base_internal::SpinLock tcp_stats_state_lock_;
-  md::UPID upid;
   std::unique_ptr<TCPStatsConnector> source_;
-  //std::unique_ptr<SystemWideStandaloneContext> ctx_;
   std::atomic<bool> transfer_enable_ = false;
-//  StandaloneContext context_;
-  std::unique_ptr<StandaloneContext> ctx_;
-    absl::flat_hash_set<md::UPID> upids;
+  std::unique_ptr<SystemWideStandaloneContext> ctx_;
   std::thread transfer_data_thread_;
-  DataTables data_tables_{TCPStatsConnector::kTables};
   static constexpr std::chrono::milliseconds kTransferDataPeriod{100};
-
- //private:
- // void RefreshContextCore() { ctx_ = std::make_unique<StandaloneContext>(); }
 };
 
 }  // namespace testing
