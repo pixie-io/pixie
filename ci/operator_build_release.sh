f919a54c4d60dce602c996d72e0bd8caa22035a8
Michelle Nguyen <michellenguyen@pixielabs.ai>
2023-03-22 10:44:31 -0700

    update image_utils
    Signed-off-by: Michelle Nguyen <michellenguyen@pixielabs.ai>

--- ci/operator_build_release.sh
+++ ci/operator_build_release.sh
@@ -22,6 +22,9 @@ repo_path=$(pwd)
 release_tag=${TAG_NAME##*/v}
 versions_file="$(pwd)/src/utils/artifacts/artifact_db_updater/VERSIONS.json"
 
+# shellcheck source=ci/image_utils.sh
+. "${repo_path}/ci/image_utils.sh"
+
 echo "The release tag is: ${release_tag}"
 
 bazel run -c opt //src/utils/artifacts/versions_gen:versions_gen -- \
@@ -40,7 +43,7 @@ bucket="pixie-dev-public"
 # The previous version should be the 2nd item in the tags. Since this is a release build,
 # the first item in the tag is the current release.
 prev_tag=$(echo "$tags" | sed -n '2 p')
-
+extra_bazel_args=()
 if [[ $release_tag == *"-"* ]]; then
   build_type="--//k8s:build_type=dev"
   image_path="gcr.io/pixie-oss/pixie-dev/operator/operator_image:${release_tag}"
@@ -53,9 +56,7 @@ if [[ $release_tag == *"-"* ]]; then
   prev_tag=$(echo "$tags" | sed -n '1 p')
 fi
 
-# Push operator image.
-bazel run --stamp -c opt --//k8s:image_version="${release_tag}" \
-    --stamp "${build_type}" //k8s/operator:operator_images_push
+push_all_multiarch_images "//k8s/operator:operator_images_push" "//k8s/operator:list_image_bundle" "${release_tag}" "${build_type}" "${extra_bazel_args[@]}"
 
 # Build operator bundle for OLM.
 tmp_dir="$(mktemp -d)"
