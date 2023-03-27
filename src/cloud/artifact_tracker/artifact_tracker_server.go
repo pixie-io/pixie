4e78803355c45beadd12fbf397700f3cbbff3051
James Bartlett <jamesbartlett@pixielabs.ai>
2023-03-27 21:57:47 +0000

    [artifact manifests] Use `ArtifactManifest` to satisfy requests to the artifact tracker server
    Signed-off-by: James Bartlett <jamesbartlett@pixielabs.ai>

--- src/cloud/artifact_tracker/artifact_tracker_server.go
+++ src/cloud/artifact_tracker/artifact_tracker_server.go
@@ -26,9 +26,7 @@ import (
 	"time"
 
 	"cloud.google.com/go/storage"
-	bindata "github.com/golang-migrate/migrate/source/go_bindata"
 	"github.com/googleapis/google-cloud-go-testing/storage/stiface"
-	"github.com/jmoiron/sqlx"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -39,12 +37,9 @@ import (
 	"px.dev/pixie/src/cloud/artifact_tracker/artifacttrackerenv"
 	atpb "px.dev/pixie/src/cloud/artifact_tracker/artifacttrackerpb"
 	"px.dev/pixie/src/cloud/artifact_tracker/controllers"
-	"px.dev/pixie/src/cloud/artifact_tracker/schema"
-	"px.dev/pixie/src/cloud/shared/pgmigrate"
 	"px.dev/pixie/src/shared/artifacts/manifest"
 	"px.dev/pixie/src/shared/services"
 	"px.dev/pixie/src/shared/services/healthz"
-	"px.dev/pixie/src/shared/services/pg"
 	"px.dev/pixie/src/shared/services/server"
 )
 
@@ -75,17 +70,6 @@ func loadServiceAccountConfig() *jwt.Config {
 	return saCfg
 }
 
-func mustLoadDB() *sqlx.DB {
-	db := pg.MustConnectDefaultPostgresDB()
-
-	err := pgmigrate.PerformMigrationsUsingBindata(db, "artifacts_tracker_service_migrations",
-		bindata.Resource(schema.AssetNames(), schema.Asset))
-	if err != nil {
-		log.WithError(err).Fatal("Failed to apply migrations")
-	}
-	return db
-}
-
 func main() {
 	services.SetupService("artifact-tracker-service", 50750)
 	services.PostFlagSetupAndParse()
@@ -114,10 +98,9 @@ func main() {
 
 	env := artifacttrackerenv.New()
 
-	db := mustLoadDB()
 	bucket := viper.GetString("artifact_bucket")
 	releaseBucket := viper.GetString("release_artifact_bucket")
-	svr := controllers.NewServer(db, stiface.AdaptClient(client), bucket, releaseBucket, saCfg)
+	svr := controllers.NewServer(stiface.AdaptClient(client), bucket, releaseBucket, saCfg)
 
 	// If any versions are not hardcoded, then we need to poll for the artifact manifest.
 	if (viper.GetString("vizier_version") == "") || (viper.GetString("cli_version") == "") || (viper.GetString("operator_version") == "") {
