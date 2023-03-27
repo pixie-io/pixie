d91707c2d2f96fd3ae6596eecef3fc754854e2bd
James Bartlett <jamesbartlett@pixielabs.ai>
2023-03-27 15:00:26 -0700

    [artifact manifests] Add `ArtifactManifest` type to represent a manifest file (#1096)
    Summary: This PR adds `ArtifactManifest` which is a golang
    representation of an artifact manifest file. In the future, the artifact
    tracker will pull a manifest file from GCS, decode it into this type,
    and then query for artifacts from `ArtifactManifest` directly. This PR
    adds support for json serialization/deserialization for artifact
    manifests. Internally, the artifacts in the manifest are stored in
    version sorted order, such that newer versions are first. This will
    enable faster version-ordered queries of manifests in the future.
    
    Type of change: /kind cleanup
    
    Test Plan: Added tests for `ArtifactManifest`
    serialization/deserialization.

---
+++ src/shared/artifacts/manifest/json.go
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018- The Pixie Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package manifest
+
+import (
+	"bytes"
+	"encoding/json"
+	"sort"
+
+	"github.com/gogo/protobuf/jsonpb"
+
+	"px.dev/pixie/src/shared/artifacts/versionspb"
+)
+
+// UnmarshalJSON decodes a json array of artifact sets into an ArtifactManifest.
+func (a *ArtifactManifest) UnmarshalJSON(b []byte) error {
+	a.sets = make(map[string]*sortedArtifactSet)
+	l := []*sortedArtifactSet{}
+	if err := json.Unmarshal(b, &l); err != nil {
+		return err
+	}
+	for _, as := range l {
+		a.sets[as.name] = as
+	}
+	return nil
+}
+
+// MarshalJSON marshals a slice of artifact sets into a json array.
+func (a *ArtifactManifest) MarshalJSON() ([]byte, error) {
+	l := make([]*sortedArtifactSet, 0, len(a.sets))
+	for _, as := range a.sets {
+		l = append(l, as)
+	}
+	// marshal ArtifactSets sorted by Name.
+	sort.Slice(l, func(i, j int) bool {
+		return l[i].name < l[j].name
+	})
+	return json.Marshal(l)
+}
+
+func (as *sortedArtifactSet) UnmarshalJSON(b []byte) error {
+	pb := &versionspb.ArtifactSet{}
+	if err := jsonpb.Unmarshal(bytes.NewReader(b), pb); err != nil {
+		return err
+	}
+	as.name = pb.Name
+	as.artifacts = pb.Artifact
+	sort.Sort(as)
+	return nil
+}
+
+func (as *sortedArtifactSet) MarshalJSON() ([]byte, error) {
+	pb := as.toProto()
+	var b bytes.Buffer
+	if err := (&jsonpb.Marshaler{}).Marshal(&b, pb); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
