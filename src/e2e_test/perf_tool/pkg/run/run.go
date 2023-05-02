7b1ef6aec38572141098f03c41e3dc3d19add9eb
James Bartlett <jamesbartlett@pixielabs.ai>
2023-05-02 10:50:36 -0700

    [perf_tool] Specify experiments in terms of actions. (#1262)
    Summary: Instead of specifying experiment duration and pre-workload
    duration, explicitly specify the actions to run for the experiment and
    the order to run them in. Each action can optionally have a duration
    associated with it, for example, the `RUN` and `BURNIN` actions will
    sleep for the specified duration to allow the experiment to run/burnin.
    This allows for much more complicated experiments to be specified. For
    example, we can now have an experiment where for the first half we run
    workloads without vizier and the second half we run vizier, in order to
    measure overhead on the application caused by vizier.
    
    Type of change: /kind test-infra
    
    Test Plan: Ran a full perf experiment locally, saw that it worked
    without issue.
    
    Signed-off-by: James Bartlett <jamesbartlett@pixielabs.ai>

--- src/e2e_test/perf_tool/pkg/run/run.go
+++ src/e2e_test/perf_tool/pkg/run/run.go
@@ -21,6 +21,7 @@ package run
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"os/exec"
 	"strconv"
@@ -51,11 +52,13 @@ type Runner struct {
 	specTable             *bq.Table
 	containerRegistryRepo string
 
-	clusterCtx     *cluster.Context
-	clusterCleanup func()
-	vizier         deploy.Workload
-	workloads      []deploy.Workload
-	wg             sync.WaitGroup
+	clusterCtx          *cluster.Context
+	clusterCleanup      func()
+	vizier              deploy.Workload
+	wg                  sync.WaitGroup
+	metricsResultCh     chan *metrics.ResultRow
+	metricsBySelector   map[string][]metrics.Recorder
+	workloadsBySelector map[string][]deploy.Workload
 }
 
 // NewRunner creates a new Runner for the given contexts.
@@ -85,12 +88,13 @@ func (r *Runner) RunExperiment(ctx context.Context, expID uuid.UUID, spec *exper
 		return nil
 	})
 
-	metricsResultCh := make(chan *metrics.ResultRow)
+	r.metricsBySelector = make(map[string][]metrics.Recorder)
+	r.metricsResultCh = make(chan *metrics.ResultRow)
 	metricsChCloseOnce := sync.Once{}
-	defer metricsChCloseOnce.Do(func() { close(metricsResultCh) })
+	defer metricsChCloseOnce.Do(func() { close(r.metricsResultCh) })
 
 	r.wg.Add(1)
-	go r.runBQInserter(expID, metricsResultCh)
+	go r.runBQInserter(expID)
 
 	if err := eg.Wait(); err != nil {
 		if r.clusterCleanup != nil {
@@ -104,113 +108,59 @@ func (r *Runner) RunExperiment(ctx context.Context, expID uuid.UUID, spec *exper
 	defer r.clusterCleanup()
 	defer r.clusterCtx.Close()
 
-	log.Info("Deploying Vizier")
-	if err := r.vizier.Start(r.clusterCtx); err != nil {
-		return fmt.Errorf("failed to deploy vizier: %w", err)
-	}
-	defer r.vizier.Close()
-
-	log.Info("Waiting for Vizier HealthCheck")
-	if err := r.vizier.WaitForHealthCheck(ctx, r.clusterCtx, spec.ClusterSpec); err != nil {
-		return err
-	}
-
-	log.Info("Starting metric recorders")
-	// Start the metric recorders.
-	mrs := make([]metrics.Recorder, len(spec.MetricSpecs))
-	for i, ms := range spec.MetricSpecs {
-		recorder := metrics.NewMetricsRecorder(r.pxCtx, ms, metricsResultCh)
-		mrs[i] = recorder
-		if err := recorder.Start(); err != nil {
-			return fmt.Errorf("failed to start metrics recorder: %s", err)
+	canceledErr := backoff.Permanent(context.Canceled)
+	for _, a := range spec.RunSpec.Actions {
+		log.Tracef("started action %s", experimentpb.ActionType_name[int32(a.Type)])
+		if canceled := r.sendActionTimestamp(ctx, a, "begin"); canceled {
+			return canceledErr
 		}
-		defer recorder.Close()
-	}
-
-	// Wait for the PreWorkloadDuration specified in the RunSpec.
-	// During this time, Vizier will be deployed, metrics are recording but no workloads are deployed.
-	preWorkloadDur, err := types.DurationFromProto(spec.RunSpec.PreWorkloadDuration)
-	if err != nil {
-		return err
-	}
-	log.Infof("Waiting %v before deploying workloads", preWorkloadDur)
-	if sleep(ctx, preWorkloadDur) {
-		return nil
-	}
-
-	// Deploy the workloads.
-	log.Info("Deploying workloads")
-	for i, w := range r.workloads {
-		log.WithField("workload", spec.WorkloadSpecs[i].Name).Trace("deploying workload")
-		if err := w.Start(r.clusterCtx); err != nil {
-			return fmt.Errorf("failed to start workload deployment: %w", err)
-		}
-		defer w.Close()
-	}
-
-	// Wait for workload healthchecks.
-	eg = errgroup.Group{}
-	for i, w := range r.workloads {
-		name := spec.WorkloadSpecs[i].Name
-		workload := w
-		eg.Go(func() error {
-			log.WithField("workload", name).Trace("Waiting for workload healthcheck")
-			if err := workload.WaitForHealthCheck(ctx, r.clusterCtx, spec.ClusterSpec); err != nil {
+		switch a.Type {
+		case experimentpb.START_VIZIER:
+			cleanup, err := r.startVizier(ctx, spec)
+			if err != nil {
 				return err
 			}
-			log.WithField("workload", name).Trace("HealthCheck passed")
-			return nil
-		})
-	}
-
-	if err := eg.Wait(); err != nil {
-		return err
-	}
-
-	// Add a row to the results with the time when workloads were deployed.
-	row := &metrics.ResultRow{
-		Timestamp: time.Now(),
-		Name:      "workloads_deployed",
-		Value:     0.0,
-	}
-	select {
-	case <-ctx.Done():
-	case metricsResultCh <- row:
-	}
-
-	// Wait for the experiment duration specified in the RunSpec.
-	// During this time, Vizier and workloads are deployed and metrics are recording.
-	dur, err := types.DurationFromProto(spec.RunSpec.Duration)
-	if err != nil {
-		return err
-	}
-	log.Infof("Experiment running for %v", dur)
-	if sleep(ctx, dur) {
-		return nil
-	}
-	log.Info("Experiment finished tearing down")
-
-	// Teardown the experiment. First teardown metrics, then workloads, then the vizier.
-	// This is a different order than the defers would occur.
-	// Since all of the Close() methods should be idempotent, it should be fine that we call Close() twice.
-	// We report the first error from any of the Close() methods, the defers will take care of tearing down the rest.
-	log.Trace("Tearing down metric recorders")
-	for _, recorder := range mrs {
-		if err := recorder.Close(); err != nil {
-			return err
+			defer cleanup()
+		case experimentpb.START_WORKLOADS:
+			cleanup, err := r.startWorkloads(ctx, spec, a.Name)
+			if err != nil {
+				return err
+			}
+			defer cleanup()
+		case experimentpb.START_METRIC_RECORDERS:
+			cleanup, err := r.startMetricRecorders(ctx, spec, a.Name)
+			if err != nil {
+				return err
+			}
+			defer cleanup()
+		case experimentpb.STOP_VIZIER:
+			if err := r.stopVizier(); err != nil {
+				return err
+			}
+		case experimentpb.STOP_WORKLOADS:
+			if err := r.stopWorkloads(a.Name); err != nil {
+				return err
+			}
+		case experimentpb.STOP_METRIC_RECORDERS:
+			if err := r.stopMetricRecorders(a.Name); err != nil {
+				return err
+			}
+		case experimentpb.RUN, experimentpb.BURNIN:
+			dur, err := types.DurationFromProto(a.Duration)
+			if err != nil {
+				return err
+			}
+			log.WithField("duration", dur).
+				WithField("action_name", a.Name).
+				Infof("Waiting for %s action", experimentpb.ActionType_name[int32(a.Type)])
+			if canceled := sleep(ctx, dur); canceled {
+				return canceledErr
+			}
 		}
-	}
-
-	log.Trace("Tearing down workloads")
-	for _, w := range r.workloads {
-		if err := w.Close(); err != nil {
-			return err
+		log.Tracef("finished action %s", experimentpb.ActionType_name[int32(a.Type)])
+		if canceled := r.sendActionTimestamp(ctx, a, "end"); canceled {
+			return canceledErr
 		}
-	}
-
-	log.Trace("Tearing down vizier")
-	if err := r.vizier.Close(); err != nil {
-		return err
 	}
 
 	// The experiment succeeded so we write the spec to bigquery.
@@ -233,10 +183,124 @@ func (r *Runner) RunExperiment(ctx context.Context, expID uuid.UUID, spec *exper
 		return err
 	}
 
-	metricsChCloseOnce.Do(func() { close(metricsResultCh) })
+	metricsChCloseOnce.Do(func() { close(r.metricsResultCh) })
 	r.wg.Wait()
 
 	return nil
+}
+
+func (r *Runner) startVizier(ctx context.Context, spec *experimentpb.ExperimentSpec) (func(), error) {
+	log.Info("Deploying Vizier")
+	noCleanup := func() {}
+	if err := r.vizier.Start(r.clusterCtx); err != nil {
+		return noCleanup, fmt.Errorf("failed to deploy vizier: %w", err)
+	}
+
+	log.Info("Waiting for Vizier HealthCheck")
+	if err := r.vizier.WaitForHealthCheck(ctx, r.clusterCtx, spec.ClusterSpec); err != nil {
+		_ = r.stopVizier()
+		return noCleanup, err
+	}
+	return func() { _ = r.vizier.Close() }, nil
+}
+
+func (r *Runner) startMetricRecorders(ctx context.Context, spec *experimentpb.ExperimentSpec, selector string) (func(), error) {
+	log.WithField("selector", selector).Infof("Starting metric recorders")
+	noCleanup := func() {}
+	for _, ms := range spec.MetricSpecs {
+		if ms.ActionSelector != selector {
+			continue
+		}
+
+		recorder := metrics.NewMetricsRecorder(r.pxCtx, ms, r.metricsResultCh)
+		r.metricsBySelector[selector] = append(r.metricsBySelector[selector], recorder)
+		if err := recorder.Start(); err != nil {
+			_ = r.stopMetricRecorders(selector)
+			return noCleanup, fmt.Errorf("failed to start metrics recorder: %s", err)
+		}
+	}
+	cleanup := func() { _ = r.stopMetricRecorders(selector) }
+	return cleanup, nil
+}
+
+func (r *Runner) startWorkloads(ctx context.Context, spec *experimentpb.ExperimentSpec, selector string) (func(), error) {
+	log.WithField("selector", selector).Info("Deploying workloads")
+	noCleanup := func() {}
+	for _, w := range r.workloadsBySelector[selector] {
+		log.WithField("workload", w.Name()).Trace("deploying workload")
+		if err := w.Start(r.clusterCtx); err != nil {
+			_ = r.stopWorkloads(selector)
+			return noCleanup, fmt.Errorf("failed to start workload deployment: %w", err)
+		}
+	}
+
+	// Wait for workload healthchecks.
+	eg := errgroup.Group{}
+	for _, w := range r.workloadsBySelector[selector] {
+		workload := w
+		eg.Go(func() error {
+			log.WithField("workload", workload.Name()).Trace("Waiting for workload healthcheck")
+			if err := workload.WaitForHealthCheck(ctx, r.clusterCtx, spec.ClusterSpec); err != nil {
+				return err
+			}
+			log.WithField("workload", workload.Name()).Trace("HealthCheck passed")
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		_ = r.stopWorkloads(selector)
+		return noCleanup, err
+	}
+	cleanup := func() { _ = r.stopWorkloads(selector) }
+	return cleanup, nil
+}
+
+func (r *Runner) stopVizier() error {
+	log.Info("Stopping Vizier")
+	return r.vizier.Close()
+}
+
+func (r *Runner) stopMetricRecorders(selector string) error {
+	log.WithField("selector", selector).Info("Stopping metric recorders")
+	mrs, ok := r.metricsBySelector[selector]
+	if !ok {
+		return nil
+	}
+	var errs []error
+	for _, mr := range mrs {
+		errs = append(errs, mr.Close())
+	}
+	return errors.Join(errs...)
+}
+
+func (r *Runner) stopWorkloads(selector string) error {
+	log.WithField("selector", selector).Info("Stopping workloads")
+	ws, ok := r.workloadsBySelector[selector]
+	if !ok {
+		return nil
+	}
+	var errs []error
+	for _, w := range ws {
+		errs = append(errs, w.Close())
+	}
+	return errors.Join(errs...)
+}
+
+func (r *Runner) sendActionTimestamp(ctx context.Context, action *experimentpb.ActionSpec, prefix string) bool {
+	actionName := strings.ToLower(experimentpb.ActionType_name[int32(action.Type)])
+	name := fmt.Sprintf("%s_%s:%s", prefix, actionName, action.Name)
+	row := &metrics.ResultRow{
+		Timestamp: time.Now(),
+		Name:      name,
+		Value:     0.0,
+	}
+	select {
+	case <-ctx.Done():
+		return true
+	case r.metricsResultCh <- row:
+		return false
+	}
 }
 
 func sleep(ctx context.Context, dur time.Duration) bool {
@@ -269,8 +333,8 @@ func (r *Runner) prepareWorkloads(ctx context.Context, spec *experimentpb.Experi
 	if err := r.vizier.Prepare(); err != nil {
 		return err
 	}
-	workloads := make([]deploy.Workload, len(spec.WorkloadSpecs))
-	for i, s := range spec.WorkloadSpecs {
+	r.workloadsBySelector = make(map[string][]deploy.Workload)
+	for _, s := range spec.WorkloadSpecs {
 		w, err := deploy.NewWorkload(r.pxCtx, r.containerRegistryRepo, s)
 		if err != nil {
 			return err
@@ -279,13 +343,12 @@ func (r *Runner) prepareWorkloads(ctx context.Context, spec *experimentpb.Experi
 		if err := w.Prepare(); err != nil {
 			return err
 		}
-		workloads[i] = w
+		r.workloadsBySelector[s.ActionSelector] = append(r.workloadsBySelector[s.ActionSelector], w)
 	}
-	r.workloads = workloads
 	return nil
 }
 
-func (r *Runner) runBQInserter(expID uuid.UUID, resultCh <-chan *metrics.ResultRow) {
+func (r *Runner) runBQInserter(expID uuid.UUID) {
 	defer r.wg.Done()
 
 	bqCh := make(chan interface{})
@@ -298,7 +361,7 @@ func (r *Runner) runBQInserter(expID uuid.UUID, resultCh <-chan *metrics.ResultR
 	}
 	go inserter.Run(bqCh)
 
-	for row := range resultCh {
+	for row := range r.metricsResultCh {
 		bqRow, err := MetricsRowToResultRow(expID, row)
 		if err != nil {
 			log.WithError(err).Error("Failed to convert result row")
