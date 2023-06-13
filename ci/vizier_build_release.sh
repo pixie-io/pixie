505a492d6c5d3e5b50175fea934bb66d93ccbda3
James Bartlett <jamesbartlett@pixielabs.ai>
2023-06-13 21:07:15 +0000

    [artifacts] Push artifacts to mirrors defined in yaml config.
    Signed-off-by: James Bartlett <jamesbartlett@pixielabs.ai>

--- ci/vizier_build_release.sh
+++ ci/vizier_build_release.sh
@@ -20,13 +20,14 @@ set -ex
 
 printenv
 
-artifacts_dir="${ARTIFACTS_DIR:?}"
 versions_file="$(realpath "${VERSIONS_FILE:?}")"
 repo_path=$(pwd)
 release_tag=${TAG_NAME##*/v}
 
 # shellcheck source=ci/image_utils.sh
 . "${repo_path}/ci/image_utils.sh"
+# shellcheck source=ci/artifact_utils.sh
+. "${repo_path}/ci/artifact_utils.sh"
 
 echo "The release tag is: ${release_tag}"
 
@@ -34,44 +35,30 @@ bazel run -c opt //src/utils/artifacts/versions_gen:versions_gen -- \
       --repo_path "${repo_path}" --artifact_name vizier --versions_file "${versions_file}"
 
 build_type="--//k8s:build_type=public"
-bucket="pixie-dev-public"
 if [[ $release_tag == *"-"* ]]; then
   build_type="--//k8s:build_type=dev"
-  # Use the same bucket as above for RCs
 fi
-
-output_path="gs://${bucket}/vizier/${release_tag}"
-latest_output_path="gs://${bucket}/vizier/latest"
 
 push_all_multiarch_images "//k8s/vizier:vizier_images_push" "//k8s/vizier:list_image_bundle" "${release_tag}" "${build_type}"
 
 bazel build --config=stamp -c opt --//k8s:image_version="${release_tag}" \
     --config=stamp "${build_type}" //k8s/vizier:vizier_yamls
 
-output_path="gs://${bucket}/vizier/${release_tag}"
 yamls_tar="${repo_path}/bazel-bin/k8s/vizier/vizier_yamls.tar"
 
-sha256sum "${yamls_tar}" | awk '{print $1}' > sha
-gsutil cp "${yamls_tar}" "${output_path}/vizier_yamls.tar"
-gsutil cp sha "${output_path}/vizier_yamls.tar.sha256"
-
-cp "${yamls_tar}" "${artifacts_dir}/vizier_yamls.tar"
-cp sha "${artifacts_dir}/vizier_yamls.tar.sha256"
+upload_artifact_to_mirrors "vizier" "${release_tag}" "${yamls_tar}" "vizier_yamls.tar"
 
 # Upload templated YAMLs.
 tmp_dir="$(mktemp -d)"
 bazel run -c opt //src/utils/template_generator:template_generator -- \
       --base "${yamls_tar}" --version "${release_tag}" --out "${tmp_dir}"
 tmpl_path="${tmp_dir}/yamls.tar"
-sha256sum "${tmpl_path}" | awk '{print $1}' > tmplSha
-gsutil cp "${tmpl_path}" "${output_path}/vizier_template_yamls.tar"
-gsutil cp tmplSha "${output_path}/vizier_template_yamls.tar.sha256"
+upload_artifact_to_mirrors "vizier" "${release_tag}" "${tmpl_path}" "vizier_template_yamls.tar"
 
 # Update helm chart if it is a release.
 if [[ $public == "True" ]]; then
   # Update Vizier YAMLS in latest.
-  gsutil cp "${yamls_tar}" "${latest_output_path}/vizier_yamls.tar"
-  gsutil cp sha "${latest_output_path}/vizier_yamls.tar.sha256"
+  upload_artifact_to_mirrors "vizier" "latest" "${yamls_tar}" "vizier_yamls.tar"
 
   ./ci/helm_build_release.sh "${release_tag}" "${tmpl_path}"
 fi
