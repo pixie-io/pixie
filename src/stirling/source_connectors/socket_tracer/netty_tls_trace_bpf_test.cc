16d885b79efe70d3c6daf98a12677dcf1ebed64b
Dom Del Nano <ddelnano@pixielabs.ai>
2023-06-16 18:08:13 +0000

    Record TLS library source in connection level prometheus metrics
    Signed-off-by: Dom Del Nano <ddelnano@pixielabs.ai>
    (cherry picked from commit 2e4c95bd5fd97ae1e2d71f187309fa2cc21ea483)

--- src/stirling/source_connectors/socket_tracer/netty_tls_trace_bpf_test.cc
+++ src/stirling/source_connectors/socket_tracer/netty_tls_trace_bpf_test.cc
@@ -18,6 +18,7 @@
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <prometheus/text_serializer.h>
 
 #include <string>
 
@@ -167,6 +168,11 @@ TYPED_TEST(NettyTLSTraceTest, mtls_thriftmux_client) {
   EXPECT_THAT(server_records, Contains(EqMuxRecord(tinit)));
   EXPECT_THAT(server_records, Contains(EqMuxRecord(pingRecord)));
   EXPECT_THAT(server_records, Contains(EqMuxRecord(dispatchRecord)));
+
+  auto& registry = GetMetricsRegistry();
+  auto metrics = registry.Collect();
+  auto metrics_text = prometheus::TextSerializer().Serialize(metrics);
+  LOG(WARNING) << absl::Substitute("with metric text: $0", metrics_text);
 }
 
 }  // namespace stirling
