390f5f293507037f96b67bd40931c05474fabcd7
Vihang Mehta <vihang@pixielabs.ai>
2023-06-24 06:05:26 +0000

    Cleanup unused configs in create cluster script
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>

--- scripts/create_gke_cluster.sh
+++ scripts/create_gke_cluster.sh
@@ -42,36 +42,12 @@ set_pixies_cluster_config() {
   SUBNETWORK=projects/pl-pixies/regions/us-west1/subnetworks/us-west1-0
 }
 
-set_stirling_dynamic_perf_eval_cluster_config() {
-  PROJECT=pl-pixies
-  ZONE=us-west1-a
-  NETWORK=projects/pl-pixies/global/networks/dev
-  SUBNETWORK=STIRLING_PERF_DYNAMIC
-  LABELS="k8s-dev-cluster=,stirling-perf-eval-dynamic="
-}
-
-# Configuration for devinfra clusters -- jenkins, bazel remote builds,
-# bazel caching etc.
-set_devinfra_cluster_config() {
-  PROJECT=pl-dev-infra
-  ZONE=us-west1-a
-  NETWORK=projects/pl-dev-infra/global/networks/dev
-  SUBNETWORK=projects/pl-dev-infra/regions/us-west1/subnetworks/us-west1-0
-}
-
 # Configuration for Pixie cloud (prod) cluster.
 set_prod_cluster_config() {
   PROJECT=pixie-prod
   ZONE=us-west1-a
   NETWORK=projects/pixie-prod/global/networks/prod
   SUBNETWORK=projects/pixie-prod/regions/us-west1/subnetworks/us-west-1-0
-}
-
-set_skylab_cluster_config() {
-  PROJECT=pixie-skylab
-  ZONE=us-west1-a
-  NETWORK=projects/pixie-skylab/global/networks/default
-  SUBNETWORK=projects/pixie-skylab/regions/us-west1/subnetworks/default
 }
 
 print_config() {
@@ -95,9 +71,6 @@ usage() {
 
   echo "Usage: $0 [-c <cluster_name>] [-p|-b|-s] [-m <machine_type>] [-n <num_nodes>] [-i <image>]"
   echo " -p          : Prod cluster config."
-  echo " -b          : DevInfra cluster config."
-  echo " -s          : Skylab cluster config."
-  echo " -S          : Stirling dynamic perf eval cluster config (these clusters may be auto reaped)."
   echo " -f          : Disable autoscaling of the node pool."
   echo " -c <string> : name of your cluster. [default: ${CLUSTER_NAME}]"
   echo " -n <int>    : number of nodes in the cluster [default: ${NUM_NODES}]"
@@ -118,7 +91,7 @@ parse_args() {
 
   local OPTIND
   # Process the command line arguments.
-  while getopts "pbsSfc:n:m:i:d:z:" opt; do
+  while getopts "pfc:n:m:i:d:z:" opt; do
     case ${opt} in
       f)
         AUTOSCALING=false
@@ -146,15 +119,6 @@ parse_args() {
       p)
         set_prod_cluster_config
         ;;
-      b)
-        set_devinfra_cluster_config
-        ;;
-      s)
-        set_skylab_cluster_config
-        ;;
-      S)
-        set_stirling_dynamic_perf_eval_cluster_config
-        ;;
       :)
         echo "Invalid option: $OPTARG requires an argument" 1>&2
         ;;
@@ -177,13 +141,6 @@ if [[ $AUTOSCALING == true ]]; then
   AUTOSCALING_ARGS=(--enable-autoscaling --min-nodes "${MIN_NODES}" --max-nodes "${MAX_NODES}")
 fi
 
-SUBNETWORK_ARGS=()
-if [[ $SUBNETWORK == "STIRLING_PERF_DYNAMIC" ]]; then
-  SUBNETWORK_ARGS=(--create-subnetwork "name=subnet-${CLUSTER_NAME}")
-else
-  SUBNETWORK_ARGS=(--subnetwork "${SUBNETWORK}")
-fi
-
 ##################
 # Start the cluster
 ##################
@@ -202,7 +159,7 @@ gcloud beta container --project "${PROJECT}" clusters create "${CLUSTER_NAME}" \
  "${AUTOSCALING_ARGS[@]}" \
  --enable-ip-alias \
  --network "${NETWORK}" \
- "${SUBNETWORK_ARGS[@]}" \
+ --subnetwork "${SUBNETWORK}" \
  --addons HorizontalPodAutoscaling,HttpLoadBalancing \
  --no-enable-autoupgrade \
  --no-enable-autorepair \
