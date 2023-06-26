ef896ba3db7524e7560dcd1e49dab68f52102761
Vihang Mehta <vihang@pixielabs.ai>
2023-06-26 16:05:48 -0700

    Cleanup unused configs in create cluster script (#1574)
    Summary: These config modes are no longer used.
    
    Type of change: /kind cleanup
    
    Test Plan: N/A This is unused code.
    
    ---------
    
    Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>

--- scripts/create_gke_cluster.sh
+++ scripts/create_gke_cluster.sh
@@ -25,11 +25,12 @@
 set_default_values() {
   CLUSTER_NAME="dev-cluster-${USER}"
   AUTOSCALING=true
+  DYNAMIC_SUBNET=false
   NUM_NODES=2
   MIN_NODES=1
   MAX_NODES=5
   MACHINE_TYPE=e2-standard-4
-  IMAGE_NAME=ubuntu_containerd
+  IMAGE_NAME=COS_CONTAINERD
   DISK_SIZE=100
   LABELS="k8s-dev-cluster="
 }
@@ -39,24 +40,7 @@ set_pixies_cluster_config() {
   PROJECT=pl-pixies
   ZONE=us-west1-a
   NETWORK=projects/pl-pixies/global/networks/dev
-  SUBNETWORK=projects/pl-pixies/regions/us-west1/subnetworks/us-west1-0
-}
-
-set_stirling_dynamic_perf_eval_cluster_config() {
-  PROJECT=pl-pixies
-  ZONE=us-west1-a
-  NETWORK=projects/pl-pixies/global/networks/dev
-  SUBNETWORK=STIRLING_PERF_DYNAMIC
-  LABELS="k8s-dev-cluster=,stirling-perf-eval-dynamic="
-}
-
-# Configuration for devinfra clusters -- jenkins, bazel remote builds,
-# bazel caching etc.
-set_devinfra_cluster_config() {
-  PROJECT=pl-dev-infra
-  ZONE=us-west1-a
-  NETWORK=projects/pl-dev-infra/global/networks/dev
-  SUBNETWORK=projects/pl-dev-infra/regions/us-west1/subnetworks/us-west1-0
+  SUBNETWORK_ARGS=(--subnetwork projects/pl-pixies/regions/us-west1/subnetworks/us-west1-0)
 }
 
 # Configuration for Pixie cloud (prod) cluster.
@@ -64,14 +48,7 @@ set_prod_cluster_config() {
   PROJECT=pixie-prod
   ZONE=us-west1-a
   NETWORK=projects/pixie-prod/global/networks/prod
-  SUBNETWORK=projects/pixie-prod/regions/us-west1/subnetworks/us-west-1-0
-}
-
-set_skylab_cluster_config() {
-  PROJECT=pixie-skylab
-  ZONE=us-west1-a
-  NETWORK=projects/pixie-skylab/global/networks/default
-  SUBNETWORK=projects/pixie-skylab/regions/us-west1/subnetworks/default
+  SUBNETWORK_ARGS=(--subnetwork projects/pixie-prod/regions/us-west1/subnetworks/us-west-1-0)
 }
 
 print_config() {
@@ -84,7 +61,7 @@ print_config() {
   echo "  DISK_SIZE        : ${DISK_SIZE}"
   echo "  ZONE             : ${ZONE}"
   echo "  NETWORK          : ${NETWORK}"
-  echo "  SUBNETWORK       : ${SUBNETWORK}"
+  echo "  SUBNETWORK       : ${SUBNETWORK_ARGS[1]}"
   echo "  LABELS           : ${LABELS}"
   echo ""
 }
@@ -93,16 +70,14 @@ usage() {
   # Reset to default values, so we can print them.
   set_default_values
 
-  echo "Usage: $0 [-c <cluster_name>] [-p|-b|-s] [-m <machine_type>] [-n <num_nodes>] [-i <image>]"
+  echo "Usage: $0 [-c <cluster_name>] [-p|-f|-S] [-m <machine_type>] [-n <num_nodes>] [-i <image>] [-d <disk_size>] [-z <zone>]"
   echo " -p          : Prod cluster config."
-  echo " -b          : DevInfra cluster config."
-  echo " -s          : Skylab cluster config."
-  echo " -S          : Stirling dynamic perf eval cluster config (these clusters may be auto reaped)."
   echo " -f          : Disable autoscaling of the node pool."
+  echo " -S          : Use a dynamically created subnetwork."
   echo " -c <string> : name of your cluster. [default: ${CLUSTER_NAME}]"
   echo " -n <int>    : number of nodes in the cluster [default: ${NUM_NODES}]"
   echo " -m <string> : machine type [default: ${MACHINE_TYPE}]"
-  echo " -i <string> : base image [default: ${IMAGE_NAME}] (can also use COS)"
+  echo " -i <string> : base image [default: ${IMAGE_NAME}]"
   echo " -d <int>    : disk size per node (GB) [default: ${DISK_SIZE}]"
   echo " -z <string> : zone [default: ${ZONE}]"
   echo "Example: $0 -c dev-cluster-000 -n 4 -i UBUNTU"
@@ -113,17 +88,23 @@ parse_args() {
   set_default_values
 
   # Default is to create a cluster for pixie developers.
-  # This can be overridden by the -p/-b/-s flags.
+  # This can be overridden by the -p flags.
   set_pixies_cluster_config
 
   local OPTIND
   # Process the command line arguments.
-  while getopts "pbsSfc:n:m:i:d:z:" opt; do
+  while getopts "pfSc:n:m:i:d:z:" opt; do
     case ${opt} in
+      p)
+        set_prod_cluster_config
+        ;;
       f)
         AUTOSCALING=false
         unset MIN_NODES
         unset MAX_NODES
+        ;;
+      S)
+        DYNAMIC_SUBNET=true
         ;;
       c)
         CLUSTER_NAME=$OPTARG
@@ -143,18 +124,6 @@ parse_args() {
       z)
         ZONE=$OPTARG
         ;;
-      p)
-        set_prod_cluster_config
-        ;;
-      b)
-        set_devinfra_cluster_config
-        ;;
-      s)
-        set_skylab_cluster_config
-        ;;
-      S)
-        set_stirling_dynamic_perf_eval_cluster_config
-        ;;
       :)
         echo "Invalid option: $OPTARG requires an argument" 1>&2
         ;;
@@ -170,19 +139,16 @@ parse_args() {
 }
 
 parse_args "$@"
-print_config
 
+if [[ $DYNAMIC_SUBNET == true ]]; then
+  SUBNETWORK_ARGS=(--create-subnetwork "name=subnet-${CLUSTER_NAME}")
+fi
 AUTOSCALING_ARGS=()
 if [[ $AUTOSCALING == true ]]; then
   AUTOSCALING_ARGS=(--enable-autoscaling --min-nodes "${MIN_NODES}" --max-nodes "${MAX_NODES}")
 fi
 
-SUBNETWORK_ARGS=()
-if [[ $SUBNETWORK == "STIRLING_PERF_DYNAMIC" ]]; then
-  SUBNETWORK_ARGS=(--create-subnetwork "name=subnet-${CLUSTER_NAME}")
-else
-  SUBNETWORK_ARGS=(--subnetwork "${SUBNETWORK}")
-fi
+print_config
 
 ##################
 # Start the cluster
