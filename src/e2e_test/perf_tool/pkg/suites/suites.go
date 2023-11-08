28733d30b5fe74734ef286b65d69e2fcd397ca7f
Benjamin Kilimnik <bkilimnik@pixielabs.ai>
2023-11-08 00:36:27 +0000

    lint
    Signed-off-by: Benjamin Kilimnik <bkilimnik@pixielabs.ai>

--- src/e2e_test/perf_tool/pkg/suites/suites.go
+++ src/e2e_test/perf_tool/pkg/suites/suites.go
@@ -32,7 +32,7 @@ type ExperimentSuite func() map[string]*pb.ExperimentSpec
 var ExperimentSuiteRegistry = map[string]ExperimentSuite{
 	"nightly":   nightlyExperimentSuite,
 	"http-grid": httpGridSuite,
-	"k8ssandra":    k8ssandraExperimentSuite,
+	"k8ssandra": k8ssandraExperimentSuite,
 }
 
 func nightlyExperimentSuite() map[string]*pb.ExperimentSpec {
