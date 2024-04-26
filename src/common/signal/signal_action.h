e13e2d8b5e923f67baa307afd2274fdb705cb0a5
Kartik Pattaswamy <kpattaswamy@pixielabs.ai>
2024-04-26 19:17:15 +0000

    use guard size and edit comments
    Signed-off-by: Kartik Pattaswamy <kpattaswamy@pixielabs.ai>

--- src/common/signal/signal_action.h
+++ src/common/signal/signal_action.h
@@ -73,15 +73,13 @@ class SignalAction : public NotCopyable {
   const size_t altstack_size_;
   /**
    * Determine the number of bytes to allocate to altstack_size_.
-   * Makes sure that it is a multiple of the system page size.
    */
   size_t DetermineAltStackSize() const {
-    // The size of altstack_ should be at least 4 * page size or size MINSIGSTKSZ if it is
+    // The size of altstack_size_ should be at least 4 * guard size or size MINSIGSTKSZ if it is
     // greater. This size needs to be a multiple of the system page size.
-    const size_t page_size = px::system::Config::GetInstance().PageSizeBytes();
-    const size_t min_altstack_size = 4 * page_size;
-    const size_t sig_page_count = IntRoundUpDivide(static_cast<size_t>(MINSIGSTKSZ), page_size);
-    const size_t sig_stack_size = sig_page_count * page_size;
+    const size_t min_altstack_size = 4 * guard_size_;
+    const size_t sig_page_count = IntRoundUpDivide(static_cast<size_t>(MINSIGSTKSZ), guard_size_);
+    const size_t sig_stack_size = sig_page_count * guard_size_;
 
     return std::max(min_altstack_size, sig_stack_size);
   }
