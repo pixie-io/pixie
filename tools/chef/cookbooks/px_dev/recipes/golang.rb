d2ba8fa3787cd3ca0f40ba7ba1abc4e1633e6d24
Dom Del Nano <ddelnano@gmail.com>
2024-09-10 05:39:23 +0000

    Remove genny to get a successful build on current go version and update docker.properties
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- tools/chef/cookbooks/px_dev/recipes/golang.rb
+++ tools/chef/cookbooks/px_dev/recipes/golang.rb
@@ -42,7 +42,6 @@ ENV['PATH'] = "/opt/px_dev/tools/golang/bin:#{ENV['PATH']}"
 execute 'install go binaries' do
   ENV['GOPATH'] = "/opt/px_dev/gopath"
   command %(go install github.com/golang/mock/mockgen@v1.5.0 && \
-            go install github.com/cheekybits/genny@v1.0.0 && \
             go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1 && \
             go install k8s.io/code-generator/cmd/client-gen@v0.20.6 && \
             go install github.com/go-bindata/go-bindata/go-bindata@v3.1.2+incompatible && \
