661fb2d553597196ced00534f8e6c83c3ccd7c1e
Dom Del Nano <ddelnano@gmail.com>
2024-10-11 19:38:59 +0000

    Ensure px.split is used in the doc string
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- src/carnot/funcs/builtins/json_ops.h
+++ src/carnot/funcs/builtins/json_ops.h
@@ -262,7 +262,8 @@ class SplitUDF : public udf::ScalarUDF {
           | df = px.DataFrame('http_events', start_time='-5m')
           | # Returns By=http://frontend.px.dev;URI=http://testclient.px.dev
           | df.xfcc_hdr = px.pluck(df.req_headers, 'X-Forwarded-Client-Cert')
-          | df.by= px.pluck_array(df.xfcc_hdr, 0) # Returns "By=http://frontend.px.dev"
+          | df.xfcc_parts = px.split(df.xfcc_hdr, ';')
+          | df.by = px.pluck_array(df.xfcc_hdr, 0) # Returns "By=http://frontend.px.dev"
           | df.uri = px.pluck_array(df.xfcc_hdr, 1) # Returns "URI=http://testclient.px.dev"
       )doc")
         .Arg("input_str", "The string to split.")
