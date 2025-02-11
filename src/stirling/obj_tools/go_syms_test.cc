b334f6d3086bfb56866459cded3f6b354f802e4c
Dom Del Nano <ddelnano@gmail.com>
2025-02-11 00:32:12 +0000

    Revert change to copyright comment. Remove go 1.17 container targets from tls trace test
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- src/stirling/obj_tools/go_syms_test.cc
+++ src/stirling/obj_tools/go_syms_test.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019- The Pixie Authors.
+ * Copyright 2018- The Pixie Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
