925f5f29cac181b848ccd68c4318ae00946c3175
Dom Del Nano <ddelnano@gmail.com>
2025-05-13 21:45:33 +0000

    Consolidate Makefile logic to arch agonstic target. Remove unnecessary build args in favor of pure env var usage
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- tools/docker/linux_headers_image/build_linux_headers.sh
+++ tools/docker/linux_headers_image/build_linux_headers.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+# Copyright 2018- The Pixie Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 usage() {
-    echo "Usage: $0 [workspace] [kernel_version] [arch] [cross_compile] [output_dir]"
-    echo "Example: $0 /px 5.4.0-42-generic x86_64 /usr/bin/x86_64-linux-gnu- /output"
+    echo "Usage: $0 [workspace] [kernel_version] [arch] [output_dir]"
+    echo "Example: $0 /px 5.4.0-42-generic x86_64 /output"
+    echo "If cross-compiling, set the CROSS_COMPILE environment variable to the cross-compiler prefix."
 }
 
-if [ "$#" -ne 5 ]; then
+if [ "$#" -ne 4 ]; then
+    echo "Invalid number of arguments. Expected 4, but received $# ($*)"
     usage
     exit 1
 fi
@@ -13,14 +31,19 @@ fi
 WORKSPACE=$1
 KERN_VERSION=$2
 ARCH=$3
-# shellcheck disable=SC2034
-CROSS_COMPILE=$4
-OUTPUT_DIR=$5
+OUTPUT_DIR=$4
 
-if [ "${CROSS_COMPILE}" == "false" ]; then
-    CROSS_COMPILE=""
+if [ -z "${WORKSPACE}" ] || [ -z "${KERN_VERSION}" ] || [ -z "${ARCH}" ] || [ -z "${OUTPUT_DIR}" ]; then
+    usage
+    exit 1
 fi
-echo "Cross compile: ${CROSS_COMPILE}"
+
+if [ "${ARCH}" != "$(uname -m)" ]; then
+    if [ -z "${CROSS_COMPILE}" ]; then
+        echo "CROSS_COMPILE is not set. Please set it to the cross-compiler prefix."
+        exit 1
+    fi
+fi
 
 mkdir -p "${WORKSPACE}"/src
 pushd "${WORKSPACE}"/src || exit
