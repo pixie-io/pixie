3cc2cc19f5a15619d41d59a88b35037f20bcea88
Dom Delnano <ddelnano@gmail.com>
2025-07-02 16:44:11 -0700

    Move test demo apps out of Bazel to ease Go version deprecation and support testing offsetgen instrumentation (#2217)

---
+++ src/stirling/testing/demo_apps/go_grpc_tls_pl/server/update_ghcr.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+# Copyright 2018- The Pixie Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+declare -A GO_IMAGE_DIGEST_MAP=(
+  ["1.18-alpine@sha256:77f25981bd57e60a510165f3be89c901aec90453fd0f1c5a45691f6cb1528807"]="v1.57.2"
+  ["1.19-alpine@sha256:0ec0646e208ea58e5d29e558e39f2e59fccf39b7bda306cb53bbaff91919eca5"]="v1.58.3"
+  ["1.20-alpine@sha256:e47f121850f4e276b2b210c56df3fda9191278dd84a3a442bfe0b09934462a8f"]="v1.58.3"
+  ["1.21-alpine@sha256:2414035b086e3c42b99654c8b26e6f5b1b1598080d65fd03c7f499552ff4dc94"]="v1.58.3"
+  ["1.22-alpine@sha256:1699c10032ca2582ec89a24a1312d986a3f094aed3d5c1147b19880afe40e052"]="v1.58.3"
+)
+version=1.0
+
+IMAGES=()
+
+for go_image_digest in "${!GO_IMAGE_DIGEST_MAP[@]}"; do
+  go_version=${go_image_digest%%-*}
+  tag="ghcr.io/pixie-io/golang_${go_version//./_}_grpc_server_with_buildinfo:$version"
+  google_golang_grpc=${GO_IMAGE_DIGEST_MAP[$go_image_digest]}
+  echo "Building and pushing image: $tag"
+  docker build . --build-arg GO_IMAGE_DIGEST="${go_image_digest}" --build-arg GOOGLE_GOLANG_GRPC="${google_golang_grpc}" -t "${tag}"
+  docker push "${tag}"
+  sha=$(docker inspect --format='{{index .RepoDigests 0}}' "${tag}" | cut -f2 -d'@')
+  IMAGES+=("${tag}@${sha}")
+done
+
+echo ""
+echo "Images pushed!"
+echo "IMPORTANT: Now update //bazel/container_images.bzl with the following digest: $sha"
+echo "Images:"
+for image in "${IMAGES[@]}"; do
+  echo "  - $image"
+done
