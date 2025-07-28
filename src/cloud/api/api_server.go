654d58eb10cfead1552f2663a7b497bb891b073f
Dom Del Nano <ddelnano@gmail.com>
2025-07-28 20:39:29 +0000

    Revert "Remove api-server's hard dependency on elasticsearch"
    This reverts commit ffebc53bcdf66f6a8e24cb7e6d549a5de34e1b00.

--- src/cloud/api/api_server.go
+++ src/cloud/api/api_server.go
@@ -144,7 +144,7 @@ func main() {
 	}
 	es, err := esutils.NewEsClient(esConfig)
 	if err != nil {
-		log.WithError(err).Error("Could not connect to elastic. Autocompletion will not work")
+		log.WithError(err).Fatal("Could not connect to elastic")
 	}
 
 	mux := http.NewServeMux()
